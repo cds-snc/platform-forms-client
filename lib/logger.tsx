82ca96367d7754edb47d5a16740bdab94e7fcb26
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-02-03 14:35:06 -0500

    Log API and pretty output on dev

--- lib/logger.tsx
+++ lib/logger.tsx
@@ -1,14 +1,41 @@
 import pino from "pino";
 // create pino loggger
 export const logMessage = pino({
-  level: "info",
-  browser: { asObject: true },
+  level: process.env.DEBUG ? "debug" : "info",
+  browser: {
+    asObject: true,
+    transmit: {
+      level: "info",
+      send: (level, logEvent) => {
+        let msg = logEvent.messages[0];
+        if (Array.isArray(msg)) {
+          msg.push("Client Side Log:");
+        } else {
+          msg = `Client Side Log: ${msg}`;
+        }
+
+        const headers = {
+          type: "application/json",
+        };
+        const blob = new Blob([JSON.stringify({ msg, level })], headers);
+        navigator.sendBeacon("/api/log", blob);
+      },
+    },
+  },
+  formatters: {
+    level: (label) => ({ level: label }),
+  },
+  base: null,
+  prettyPrint:
+    process.env.DEBUG || process.env.NODE_ENV === "development" ? true : false,
 });
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const logger = <A extends any[], R>(f: (...a: A) => R) => (
   ...args: A
 ): R => {
+  // Add nicer formatting for arguments being passed
+  logMessage.debug(`${f.name} function called`);
   let value;
   try {
     value = f(...args);
@@ -16,6 +43,8 @@ export const logger = <A extends any[], R>(f: (...a: A) => R) => (
     logMessage.error(error);
     throw error;
   }
+  // Add formatting for value being returned
+  logMessage.debug(`${f.name} function returned`);
   return value;
 };
 
