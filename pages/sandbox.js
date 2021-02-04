3a8461280bbf4252869ba7d1be08e1acd83960ba
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-02-04 11:57:04 -0500

    Refactor links list into subcomponent

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -5,7 +5,19 @@ import { getFormByStatus, getFormByID } from "../lib/dataLayer.tsx";
 import { getProperty } from "../lib/formBuilder";
 
 const Sandbox = ({ t, i18n }) => {
-  const formIDs = getFormByStatus(false);
+  const LinksList = () => {
+    const formIDs = getFormByStatus(false);
+    return formIDs.map((formID) => {
+      const form = getFormByID(formID);
+      return (
+        <p key={`link-${form.id}`}>
+          <Link href={`/${form.id}`}>
+            {form[getProperty("title", i18n.language)].toString()}
+          </Link>
+        </p>
+      );
+    });
+  };
 
   return (
     <>
@@ -16,16 +28,7 @@ const Sandbox = ({ t, i18n }) => {
           <h2 className="text-3xl mb-5">{t("product.title")}</h2>
           <p>{t("product.text")}</p>
           <h2 className="text-3xl mb-5">{t("formList.title")}</h2>
-          {formIDs.map((formID) => {
-            const form = getFormByID(formID);
-            return (
-              <p key={`link-${form.id}`}>
-                <Link href={`/${form.id}`}>
-                  {form[getProperty("title", i18n.language)].toString()}
-                </Link>
-              </p>
-            );
-          })}
+          <LinksList />
         </div>
 
         <div className="mb-20">
