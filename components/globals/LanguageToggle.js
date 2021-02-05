44155532ef9f61b3fcbfe25e51ba59a36bd96ba1
nmakuch <makuch.nick@gmail.com>
2021-02-04 19:52:32 -0500

    visually hidden

--- components/globals/LanguageToggle.js
+++ components/globals/LanguageToggle.js
@@ -13,7 +13,7 @@ const LanguageToggle = ({ t, i18n }) => {
 
   return (
     <>
-      <h2 className="sr-only">{t("lang-toggle")}</h2>
+      <h2 className="sr-only visually-hidden">{t("lang-toggle")}</h2>
       <div className="gc-language-toggle">
         {locale == "en" ? (
           <button
