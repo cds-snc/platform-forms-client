88c83689ef64151e6a1c0d5667ad457a65e525e7
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-02-08 14:57:30 -0500

    Fix types on dynamic form

--- components/containers/DynamicForm/DynamicForm.tsx
+++ components/containers/DynamicForm/DynamicForm.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classnames from "classnames";
+import { I18n } from "next-i18next";
 import Head from "next/head";
 import { Form } from "../../forms/Form/Form";
 import { useRouter } from "next/router";
@@ -8,7 +9,7 @@ import { FormMetadataProperties } from "../../../lib/types";
 import { withTranslation } from "../../../i18n";
 
 interface DynamicFormProps {
-  i18n?: object;
+  i18n: I18n;
   formMetadata: FormMetadataProperties;
   className?: string;
   children?: React.ReactNodeArray;
@@ -16,10 +17,10 @@ interface DynamicFormProps {
 
 export const DynamicForm = (props: DynamicFormProps): React.ReactElement => {
   const { formMetadata, i18n } = props;
-  const language = i18n.language;
+  const language = i18n.language as string;
   const classes = classnames("gc-form-wrapper", props.className);
   const currentForm = getRenderedForm(formMetadata, language);
-  const formTitle: string = formMetadata[getProperty("title", language)];
+  const formTitle = formMetadata[getProperty("title", language)] as string;
   const router = useRouter();
 
   return (
