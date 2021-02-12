2f46f0dd98bd5817cb15df110d7118001aaffde8
nmakuch <makuch.nick@gmail.com>
2021-02-11 23:18:00 -0500

    custom checkboxes finished

--- components/forms/Checkbox/Checkbox.tsx
+++ components/forms/Checkbox/Checkbox.tsx
@@ -32,7 +32,7 @@ export const Checkbox = (
         {...inputProps}
       />
       <label className="gc-checkbox-label" htmlFor={id}>
-        {label}
+        <span>{label}</span>
       </label>
     </div>
   );
