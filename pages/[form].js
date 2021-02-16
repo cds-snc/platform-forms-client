29c1467fbabbe7ab1babbfb85a4b76c9b3620694
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-02-16 14:35:22 -0500

    Add preview page and a cypress test

--- pages/[form].js
+++ pages/[form].js
@@ -2,7 +2,20 @@ import { getFormByID } from "../lib/dataLayer";
 import DynamicForm from "../components/containers/DynamicForm/DynamicForm";
 
 export async function getServerSideProps(context) {
-  const form = await getFormByID(context.params.form);
+  let form = null;
+  const formId = context.params.form;
+
+  if (formId === "preview-form" && context.query) {
+    // If we're previewing a form, get the object from the query string
+    const queryObj = context.query;
+    const parsedForm =
+      queryObj && queryObj.formObject ? JSON.parse(queryObj.formObject) : null;
+    form = parsedForm && parsedForm.form ? parsedForm.form : null;
+  } else {
+    //Otherwise, get the form object via the dataLayer library
+    form = await getFormByID(context.params.form);
+  }
+
   if (!form) {
     return {
       redirect: {
