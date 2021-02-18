fa74e5f2c6855196081ba4aba9970b0d7206a75e
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-02-18 10:46:36 -0500

    Update IDs for the cypress form

--- cypress/integration/platform_intake_spec.js
+++ cypress/integration/platform_intake_spec.js
@@ -7,18 +7,18 @@ describe(
       cy.get("h1").contains("CDS Platform Intake Form");
     });
     it("Fill out the form", () => {
-      cy.get("input[id='2']").type("Santa").should("have.value", "Santa");
-      cy.get("input[id='3']")
+      cy.get("input[id='3']").type("Santa").should("have.value", "Santa");
+      cy.get("input[id='4']")
         .type("santa@northpole.global")
         .should("have.value", "santa@northpole.global");
-      cy.get("input[id='4']")
+      cy.get("input[id='5']")
         .type("CDS Gifts")
         .should("have.value", "CDS Gifts");
-      cy.get("input[id=5]")
+      cy.get("input[id=6]")
         .type("1 877-636-0656")
         .should("have.value", "1 877-636-0656");
       cy.get(".gc-checkbox-label").click({ multiple: true });
-      cy.get("textarea[id='7']")
+      cy.get("textarea[id='8']")
         .type("More emails: santa1@northpole.global, santa2@northpole.global")
         .should(
           "have.value",
