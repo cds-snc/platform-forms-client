3a70e7c26256f0b9d887a9e7459dd8913bf4ccaa
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-03-03 09:36:06 -0500

    Final cleanup of i18n

--- components/globals/Fip.js
+++ components/globals/Fip.js
@@ -1,25 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withTranslation } from "next-i18next";
+import { useTranslation } from "next-i18next";
 import LanguageToggle from "./LanguageToggle";
 
-const Fip = ({ t, i18n }) => (
-  <div data-testid="fip" className="gc-fip">
-    <div className="canada-flag">
-      <a href={t("fip.link")} aria-label={t("fip.text")}>
-        <img
-          src={"/img/sig-blk-" + i18n.language + ".svg"}
-          alt={t("fip.text")}
-        />
-      </a>
+const Fip = () => {
+  const { t, i18n } = useTranslation("common");
+  return (
+    <div data-testid="fip" className="gc-fip">
+      <div className="canada-flag">
+        <a href={t("fip.link")} aria-label={t("fip.text")}>
+          <img
+            src={"/img/sig-blk-" + i18n.language + ".svg"}
+            alt={t("fip.text")}
+          />
+        </a>
+      </div>
+      <LanguageToggle />
     </div>
-    <LanguageToggle />
-  </div>
-);
-
-Fip.propTypes = {
-  t: PropTypes.func.isRequired,
-  i18n: PropTypes.object.isRequired,
+  );
 };
 
-export default withTranslation("common")(Fip);
+export default Fip;
