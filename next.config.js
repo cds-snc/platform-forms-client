2cbfb7af4e26d7310a11bd6609ce46a5d88dc569
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-03-03 09:44:31 -0500

    remove hmr for i18n

--- next.config.js
+++ next.config.js
@@ -6,17 +6,5 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  webpack(config, options) {
-    if (!options.isServer && config.mode === "development") {
-      const { I18NextHMRPlugin } = require("i18next-hmr/plugin");
-      config.plugins.push(
-        new I18NextHMRPlugin({
-          localesDir: path.resolve(__dirname, "public/static/locales"),
-        })
-      );
-    }
-
-    return config;
-  },
   reactStrictMode: true,
 };
