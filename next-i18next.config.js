069348bb91090deacd8d5be7c1857e1f667d331b
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-03-03 09:44:31 -0500

    remove hmr for i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,20 +1,8 @@
 const path = require("path");
-const i18nSettings = {
+module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
   },
   localePath: path.resolve("./public/static/locales"),
 };
-
-if (process.env.NODE_ENV !== "production") {
-  if (process.browser) {
-    const { applyClientHMR } = require("i18next-hmr/client");
-    applyClientHMR(i18nSettings.i18n);
-  } else {
-    const { applyServerHMR } = require("i18next-hmr/server");
-    applyServerHMR(i18nSettings.i18n);
-  }
-}
-
-module.exports = i18nSettings;
