69b66901964c879748f62f101153dc104bbc1036
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-03-09 15:14:48 -0500

    Initial regex validation

--- lib/validation.tsx
+++ lib/validation.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FormValues, DynamicFormProps, InnerFormProps, FormElement, Responses } from "./types";
 import { FormikProps } from "formik";
+import { TFunction } from "next-i18next";
 
 /**
  * validateOnSubmit is called during Formik's submission event to validate the fields
@@ -20,10 +21,18 @@ export const validateOnSubmit = (values: FormValues, props: DynamicFormProps): R
     }
 
     const currentValidation = currentItem.properties?.validation;
+    const currentValue: string = values[item].toString() || "";
+    const currentRegex = getRegexByType(currentValidation.type, props.t, currentValue);
 
     // Check for required fields
-    if (currentValidation && currentValidation?.required && !values[item]) {
+    if (currentValidation && currentValidation?.required && !currentValue) {
       errors[item] = props.t("input-validation.required");
+    } else if (currentValidation.type && currentRegex && currentRegex.regex) {
+      // Check for different types of fields, email, date, number, custom etc
+      const regex = new RegExp(currentRegex.regex);
+      if (currentValue && !regex.test(currentValue)) {
+        errors[item] = currentRegex.error;
+      }
     }
   }
 
@@ -51,4 +60,35 @@ export const getErrorList = (
     });
   }
   return errorList && errorList.length ? <ol className="gc-ordered-list">{errorList}</ol> : null;
+};
+
+const getRegexByType = (type: string | undefined, t: TFunction, value?: string) => {
+  const REGEX_CONFIG = {
+    email: {
+      regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      error: t("input-validation.email"),
+    },
+    text: {
+      regex: /^[0-9a-zA-Z]+$/,
+      error: t("input-validation.text"),
+    },
+    number: {
+      regex: /^[0-9]+$/,
+      error: t("input-validation.number"),
+    },
+    date: {
+      regex: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/,
+      error: t("input-validation.date"),
+    },
+    phone: {
+      regex: /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/,
+      error: t("input-validation.phone"),
+    },
+    custom: {
+      regex: value,
+      error: t("input-validation.regex"),
+    },
+  };
+
+  return type ? REGEX_CONFIG[type] : null;
 };
