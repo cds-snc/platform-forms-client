218b324b07138686db2a291860e5b12b7aaa9acb
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-03-09 12:06:14 -0500

    Update storybook

--- components/forms/RichText/RichText.stories.tsx
+++ components/forms/RichText/RichText.stories.tsx
@@ -9,4 +9,10 @@ export default {
   },
 };
 
-export const defaultRichText = (): React.ReactElement => <RichText>This is a RichText</RichText>;
+const richText = ` <h1>Rich Text</h1>
+
+The Canadian Digital Service teams up with federal departments and agencies to put people’s needs at the centre of government services.
+
+<h2>Working together</h2>
+Our aim is to make services easier for government to deliver. We collaborate with people who work in government to address service delivery problems. We test with people who need government services to find design solutions that are easy to use.`;
+export const defaultRichText = (): React.ReactElement => <RichText>{richText}</RichText>;
