f3a7c35bd5847656341db584616fd881b9559335
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-03-26 09:38:31 -0400

    Add ability to link the logo to the department's website

--- components/globals/Fip.js
+++ components/globals/Fip.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useTranslation } from "next-i18next";
 import LanguageToggle from "./LanguageToggle";
 import { getProperty } from "../../lib/formBuilder";
 
 const Fip = (props) => {
   const { t, i18n } = useTranslation("common");
-  const formTheme = props.formMetadata ? props.formMetadata.theme : null;
+  const formTheme = props.formMetadata ? props.formMetadata.brand : null;
 
   const customLogo =
     formTheme && formTheme[getProperty("logo", i18n.language)]
@@ -13,17 +14,25 @@ const Fip = (props) => {
       : null;
 
   const logo = customLogo ? customLogo : "/img/sig-blk-" + i18n.language + ".svg";
+  const linkUrl =
+    formTheme && formTheme[getProperty("url", i18n.language)]
+      ? formTheme[getProperty("url", i18n.language)]
+      : t("fip.link");
 
   return (
     <div data-testid="fip" className="gc-fip">
       <div className="canada-flag">
-        <a href={t("fip.link")} aria-label={t("fip.text")}>
+        <a href={linkUrl} aria-label={t("fip.text")}>
           <img src={logo} alt={t("fip.text")} />
         </a>
       </div>
       <LanguageToggle />
     </div>
   );
+};
+
+Fip.propTypes = {
+  formMetadata: PropTypes.object,
 };
 
 export default Fip;
