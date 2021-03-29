e7eb361d064460f8a4169576494907af27206586
nmakuch <makuch.nick@gmail.com>
2021-03-29 14:39:33 -0400

    updating media queries for confirmation banner

--- components/globals/ConfirmationBanner.tsx
+++ components/globals/ConfirmationBanner.tsx
@@ -22,7 +22,7 @@ export const ConfirmationBanner = ({
   return (
     <div className={classes} {...props} >
       <h2 className="text-h2 mb-6 md:text-small_h2">{title}</h2>
-      <p className="mb-5 gc-p">{lightText}</p>
+      <p className="mb-5 md:text-small_p">{lightText}</p>
       <p className="font-bold md:text-small_p">{boldText}</p>
     </div>
   );
