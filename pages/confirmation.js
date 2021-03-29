0694f0414f74254abec43a0c26adc30430794152
nmakuch <makuch.nick@gmail.com>
2021-03-29 14:37:57 -0400

    generic confirmation page

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import parse from "html-react-parser";
+import ConfirmationBanner from "../components/globals/ConfirmationBanner.tsx";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
@@ -8,25 +9,27 @@ const Confirmation = () => {
   const { t } = useTranslation("confirmation");
   const router = useRouter();
   const { urlQuery, htmlEmail } = router.query;
-
-  const backToLink = urlQuery ? <a href={urlQuery}>{t("backLink")}</a> : null;
+  const backToLink = urlQuery ? <p>If you're finished, you can navigate <a href={urlQuery}>{t("backLink")}</a>.</p> : null;
 
   return (
     <>
       <h1 className="gc-h1">{t("title")}</h1>
 
       <div>
-        <p>{t("body")}</p>
+        <ConfirmationBanner
+          title={t("bannerTitle")}
+          lightText={t("bannerLight")}
+          boldText={t("bannerDark")}
+        />
+        <p className="gc-p">{t("body")} <a href="mailto:info@cds-snc.ca">info@cds-snc.ca</a>.</p>
+        <div className="gc-form-confirmation">{backToLink}</div>
+        {htmlEmail ? (
+          <div className="p-5 mt-5 border-double border-gray-400 border-4">
+            <h2>Email to Form Owner Below:</h2>
+            <div className="pt-5 email-preview">{parse(htmlEmail)}</div>
+          </div>
+        ) : null}
       </div>
-
-      <div className="gc-form-confirmation">{backToLink}</div>
-
-      {htmlEmail ? (
-        <div className="p-5 mt-5 border-double border-gray-400 border-4">
-          <h2>Email to Form Owner Below:</h2>
-          <div className="pt-5 email-preview">{parse(htmlEmail)}</div>
-        </div>
-      ) : null}
     </>
   );
 };
