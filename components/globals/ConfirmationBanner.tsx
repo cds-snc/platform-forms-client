2735c81fc80e1206c2fdcd9f78efba1d30e0a730
nmakuch <makuch.nick@gmail.com>
2021-03-29 14:37:57 -0400

    generic confirmation page

---
+++ components/globals/ConfirmationBanner.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+
+interface ConfirmationBannerProps {
+  title: String;
+  lightText: String;
+  boldText: String;
+}
+
+export const ConfirmationBanner = ({
+  title,
+  lightText,
+  boldText,
+  className,
+  ...props
+}: ConfirmationBannerProps & React.HTMLAttributes<HTMLDivElement>): React.ReactElement => {
+  const classes = classnames(
+    "gc-confirmation-banner bg-gray-200 py-16 px-20 mb-10 w-5/6 xl:w-full md:py-10 md:px-10",
+    className
+  );
+
+  return (
+    <div className={classes} {...props} >
+      <h2 className="text-h2 mb-6 md:text-small_h2">{title}</h2>
+      <p className="mb-5 gc-p">{lightText}</p>
+      <p className="font-bold md:text-small_p">{boldText}</p>
+    </div>
+  );
+};
+
+export default ConfirmationBanner;
--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ConfirmationBanner from "../components/globals/ConfirmationBanner.tsx";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
@@ -8,17 +9,21 @@ const Confirmation = () => {
   const router = useRouter();
   const urlQuery = router.query;
   const backToLink =
-    urlQuery && urlQuery.referrerUrl ? <a href={urlQuery.referrerUrl}>{t("backLink")}</a> : null;
+    urlQuery && urlQuery.referrerUrl ? <p className="gc-p">If you're finished, you can navigate <a href={urlQuery.referrerUrl}>{t("backLink")}</a>.</p> : null;
 
   return (
     <>
       <h1 className="gc-h1">{t("title")}</h1>
 
       <div>
-        <p>{t("body")}</p>
+        <ConfirmationBanner
+          title={t("bannerTitle")}
+          lightText={t("bannerLight")}
+          boldText={t("bannerDark")}
+        />
+        <p className="gc-p">{t("body")} <a href="mailto:info@cds-snc.ca">info@cds-snc.ca</a>.</p>
+        <div className="gc-form-confirmation">{backToLink}</div>
       </div>
-
-      <div className="gc-form-confirmation">{backToLink}</div>
     </>
   );
 };
