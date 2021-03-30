fbafc2a9db3412338619ee8a6adb151ff76ddcf1
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-03-30 10:22:44 -0400

    text input, area, and rich text tests

---
+++ components/forms/TextArea/TextArea.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import Form from "../Form/Form";
+import { GenerateElement } from "../../../lib/formBuilder";
+
+const textAreaData = {
+  id: 2,
+  type: "textArea",
+  properties: {
+    titleEn: "What is the problem you are facing",
+    titleFr: "Quel est le problème auquel vous êtes confronté?",
+    placeholderEn: "Something difficult",
+    placeholderFr: "Quelque chose difficile",
+    descriptionEn: "Here be a description",
+    descriptionFr: "Pour décrire, ou pas décire..",
+    charLimit: 100,
+    validation: {
+      required: true,
+    },
+  },
+};
+
+describe("Generate a text area", () => {
+  afterEach(cleanup);
+  test("...in English", () => {
+    render(
+      <Form t={(key) => key}>
+        <GenerateElement element={textAreaData} language="en" />
+      </Form>
+    );
+    // Label properly renders
+    expect(screen.getByLabelText(textAreaData.properties.titleEn)).toBeInTheDocument();
+    // Description properly render
+    expect(screen.getByText(textAreaData.properties.descriptionEn)).toBeInTheDocument();
+    // Placeholder properly renders
+    expect(screen.getByPlaceholderText(textAreaData.properties.placeholderEn)).toBeInTheDocument();
+    // Field marked as required
+    expect(screen.getByRole("textbox")).toBeRequired();
+  });
+  test("...in French", () => {
+    render(
+      <Form t={(key) => key}>
+        <GenerateElement element={textAreaData} language="fr" />
+      </Form>
+    );
+    // Label properly renders
+    expect(screen.getByLabelText(textAreaData.properties.titleFr)).toBeInTheDocument();
+    // Description properly render
+    expect(screen.getByText(textAreaData.properties.descriptionFr)).toBeInTheDocument();
+    // Placeholder properly renders
+    expect(screen.getByPlaceholderText(textAreaData.properties.placeholderFr)).toBeInTheDocument();
+    // Field marked as required
+    expect(screen.getByRole("textbox")).toBeRequired();
+  });
+});
