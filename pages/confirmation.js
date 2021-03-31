efb06d614b29935a2be7ca7c09a0791d55761136
nmakuch <makuch.nick@gmail.com>
2021-03-31 16:09:36 -0400

    cleanup

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -1,11 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import parse from "html-react-parser";
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 8177aedbf7fce67d6d73ab931feba335476f570e
->>>>>>> e4f8226aff0be12af3ee5863dc7f5d159329bab0
 import ConfirmationBanner from "../components/globals/ConfirmationBanner.tsx";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
@@ -14,27 +8,13 @@ import { useRouter } from "next/router";
 const Confirmation = () => {
   const { t } = useTranslation("confirmation");
   const router = useRouter();
-<<<<<<< HEAD
   const { urlQuery, htmlEmail } = router.query;
-<<<<<<< HEAD
   const backToLink = urlQuery ? (
     <p>
       {t("linkSentence")}
       <a href={urlQuery}>{t("backLink")}</a>.
     </p>
   ) : null;
-=======
-  const backToLink = urlQuery ? <p>{t("linkSentence")}<a href={urlQuery}>{t("backLink")}</a>.</p> : null;
-=======
-  const urlQuery = router.query;
-  const backToLink =
-    urlQuery && urlQuery.referrerUrl ? (
-      <p>
-        {t("linkSentence")} <a href={urlQuery.referrerUrl}>{t("backLink")}</a>.
-      </p>
-    ) : null;
->>>>>>> 8177aedbf7fce67d6d73ab931feba335476f570e
->>>>>>> e4f8226aff0be12af3ee5863dc7f5d159329bab0
 
   return (
     <>
@@ -49,21 +29,12 @@ const Confirmation = () => {
         <div className="confirmation-content">
           <div className="gc-form-confirmation">{backToLink}</div>
         </div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e4f8226aff0be12af3ee5863dc7f5d159329bab0
         {htmlEmail ? (
           <div className="p-5 mt-5 border-double border-gray-400 border-4">
             <h2>Email to Form Owner Below:</h2>
             <div className="pt-5 email-preview">{parse(htmlEmail)}</div>
           </div>
         ) : null}
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 8177aedbf7fce67d6d73ab931feba335476f570e
->>>>>>> e4f8226aff0be12af3ee5863dc7f5d159329bab0
       </div>
     </>
   );
