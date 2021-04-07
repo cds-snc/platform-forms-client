e02d3846542fdb7fb663116ffe509766762e0f10
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-04-07 15:47:15 -0400

    Label test

---
+++ components/forms/Label/Label.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import { Label } from "./Label";
+
+describe("Label component", () => {
+  afterEach(cleanup);
+  const text = "This is a label";
+  it("renders without errors", () => {
+    render(<Label htmlFor="testInput">{text}</Label>);
+    expect(screen.queryByTestId("label")).toBeInTheDocument();
+    expect(screen.queryByText(text)).toBeInTheDocument();
+    expect(screen.queryByTestId("label")).toHaveAttribute("for", "testInput");
+  });
+});
