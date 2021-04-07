31c57334c086857bfabda3f024026776653dcfe6
Fitore Jaha Price <fitore@Fitores-MacBook-Pro.local>
2021-04-07 14:29:04 -0400

    Add endPage text for the TSB form

--- pages/confirmation.js
+++ pages/confirmation.js
@@ -5,28 +5,33 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { RichText } from "../components/forms";
 
+const getPageContent = (t, pageText, urlQuery) => {
+  // Check if there's a custom text for the end page specified in the form's JSON config
+  if (pageText && pageText !== undefined) {
+    return <RichText className="confirmation">{JSON.parse(pageText)}</RichText>;
+  }
+
+  // Otherwise, display the default confirmation text
+  const backToLink = urlQuery ? <a href={urlQuery}>{t("backLink")}</a> : null;
+  return (
+    <>
+      <h1 className="gc-h1">{t("title")}</h1>
+      <div>
+        <p>{t("body")}</p>
+      </div>
+      <div className="gc-form-confirmation">{backToLink}</div>
+    </>
+  );
+};
+
 const Confirmation = () => {
   const { t } = useTranslation("confirmation");
   const router = useRouter();
   const { urlQuery, htmlEmail, pageText } = router.query;
 
-  const backToLink = urlQuery ? <a href={urlQuery}>{t("backLink")}</a> : null;
-
-  const customPageText = pageText ? (
-    <RichText className="confirmation">{JSON.parse(pageText)}</RichText>
-  ) : null;
-
   return (
     <>
-      <h1 className="gc-h1">{t("title")}</h1>
-
-      <div>
-        <p>{t("body")}</p>
-      </div>
-
-      <div className="gc-form-confirmation">{backToLink}</div>
-
-      {customPageText ? customPageText : null}
+      {getPageContent(t, pageText, urlQuery)}
 
       {htmlEmail ? (
         <div className="p-5 mt-5 border-double border-gray-400 border-4">
