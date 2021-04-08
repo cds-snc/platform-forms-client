517942022cbc485a69d8c919d8e70e0c0520bf8c
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-04-08 11:49:17 -0400

    Form tests

---
+++ components/forms/Form/Form.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import Form from "./Form";
+
+const formMetadata = {
+  id: 1,
+  version: 1,
+  titleEn: "Test Form",
+  titleFr: "Formulaire de test",
+  layout: [1],
+  elements: [
+    {
+      id: 1,
+      type: "richText",
+      properties: {
+        titleEn: "",
+        titleFr: "",
+        descriptionEn:
+          "Thank you so much for your interest in the Canadian Digital Service’s Forms product. <br/><br/> Please provide your information below so CDS can contact you about improving, updating, or digitizing a form.",
+        descriptionFr:
+          "Merci beaucoup de l’intérêt que vous portez au produit de Formulaire du Service Numérique Canadien. <br/><br/> Veuillez fournir vos renseignements ci-dessous afin que le SNC puisse vous contacter pour discuter davantage l'amélioration, la mise à jour ou la numérisation d'un formulaire.",
+        validation: {
+          required: false,
+        },
+      },
+    },
+  ],
+};
+
+// Need more tests here like submit...
+
+describe("Generate a form component", () => {
+  afterEach(cleanup);
+  test("...with fake children", () => {
+    render(
+      <Form formMetadata={formMetadata} language="en" t={(key) => key}>
+        <div data-testid="test-child"></div>
+      </Form>
+    );
+    screen.debug();
+    expect(screen.getByTestId("form"))
+      .toBeInTheDocument()
+      .toContainElement(screen.getByTestId("test-child"));
+    // Label properly renders
+  });
+});
