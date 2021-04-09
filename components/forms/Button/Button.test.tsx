2fcd90f33405fbefd783719eb31234921d82c6a9
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-04-09 15:14:08 -0400

    Button Test

--- components/forms/Button/Button.test.tsx
+++ components/forms/Button/Button.test.tsx
@@ -1,28 +1,39 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup, screen, fireEvent } from "@testing-library/react";
 import { Button } from "./Button";
 
 describe("Button component", () => {
-  it("renders without errors", () => {
-    const { queryByTestId } = render(<Button type="button">Click Me</Button>);
-    expect(queryByTestId("button")).toBeInTheDocument();
+  afterEach(cleanup);
+  test("renders without errors", () => {
+    render(<Button type="button">Click Me</Button>);
+    expect(screen.queryByTestId("button")).toBeInTheDocument();
   });
 
   describe("renders correct className", () => {
-    it("gc-button", () => {
-      const { queryByTestId } = render(<Button type="button">Click Me</Button>);
-      expect(queryByTestId("button")).toHaveClass("gc-button");
+    test("gc-button", () => {
+      render(<Button type="button">Click Me</Button>);
+      expect(screen.queryByTestId("button")).toHaveClass("gc-button");
     });
   });
 
   describe("renders secondary button", () => {
-    it("gc-button", () => {
-      const { queryByTestId } = render(
+    test("gc-button", () => {
+      render(
         <Button type="button" secondary={true}>
           Click Me
         </Button>
       );
-      expect(queryByTestId("button")).toHaveClass("gc-button--secondary");
+      expect(screen.queryByTestId("button")).toHaveClass("gc-button--secondary");
     });
+  });
+  test("button click", () => {
+    const mockOnClick = jest.fn();
+    render(
+      <Button type="button" onClick={mockOnClick}>
+        Click Me
+      </Button>
+    );
+    fireEvent.click(screen.getByRole("button", { name: "Click Me" }));
+    expect(mockOnClick).toHaveBeenCalled().toHaveBeenCalledTimes(1);
   });
 });
