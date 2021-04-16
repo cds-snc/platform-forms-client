cf3a4431993e78ff8fe498d90fb0e68e094ca80d
brdunfield <bethany.dunfield@cds-snc.ca>
2021-04-16 11:09:55 -0600

    Preliminary pass at improving validation

--- lib/validation.tsx
+++ lib/validation.tsx
@@ -80,19 +80,27 @@ const getRegexByType = (type: string | undefined, t: TFunction, value?: string) 
 
   const REGEX_CONFIG: RegexProps = {
     email: {
-      regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      regex: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.([a-zA-Z0-9-]{2,}))+$/,
       error: t("input-validation.email"),
     },
+    alphanumeric: {
+      regex: /^( )*[A-Za-z0-9\s]+( )*$/,
+      error: t("input-validation.alphanumeric"), /* message needs a translation */
+    },
     text: {
-      regex: /^(\s)*[A-Za-z]+((\s)?(('|-|\.)?([A-Za-z])+))*(\s)*$/,
-      error: t("input-validation.text"),
+      regex: /^.*[^\n]+.*$/,
+      error: t("input-validation.regex") /* TODO update */,
+    },
+    name: {
+      regex: null,
+      error: t("input-validation.regex"), // No error message needed for regex
     },
     number: {
-      regex: /^[\d|.|,]+/,
+      regex: /^[\d|.|,| ]+/,
       error: t("input-validation.number"),
     },
     date: {
-      regex: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/,
+      regex: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/, //mm/dd/yyyy
       error: t("input-validation.date"),
     },
     phone: {
