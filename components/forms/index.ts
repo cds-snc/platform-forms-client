14d1079a199498ffbf325ce6b30b5b98a4587900
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-04-16 10:21:54 -0400

    Phone number validation improvement

--- components/forms/index.ts
+++ components/forms/index.ts
@@ -4,7 +4,6 @@ export { ErrorMessage } from "./ErrorMessage/ErrorMessage";
 export { Button } from "./Button/Button";
 export { Checkbox } from "./Checkbox/Checkbox";
 export { Dropdown } from "./Dropdown/Dropdown";
-export { Fieldset } from "./Fieldset/Fieldset";
 export { FormGroup } from "./FormGroup/FormGroup";
 export { Label } from "./Label/Label";
 export { Radio } from "./Radio/Radio";
