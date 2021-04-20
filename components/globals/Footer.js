5311378432e5a2a37be6a6619d0513ada4a6a71e
Fitore Jaha-Price <73135530+fitore-cds@users.noreply.github.com>
2021-04-20 15:40:31 -0400

    Small tweaks for the splash page, translations (#133)
    * Update favicon, alt text, translations
    
    * Add routeUtils to find page name, classes etc
    
    * Fix types
    
    * Add a check for the splash page as it's locale agnostic
    
    * Oy, the linter
    
    * Add basic test for routeUtil on splash page

--- components/globals/Footer.js
+++ components/globals/Footer.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
+import { isSplashPage } from "../../lib/routeUtils";
 
 const Footer = () => {
-  const { t } = useTranslation("common");
+  const { t, i18n } = useTranslation("common");
+
   return (
     <footer className="gc-footer" data-testid="footer">
       <div className="gc-footer-container items-start">
         <div>
-          <ul>
-            <li>
-              <a href={t("footer.privacy.link")}>{t("footer.privacy.desc")}</a>
-            </li>
-            <li>
-              <a href={t("footer.terms.link")}>{t("footer.terms.desc")}</a>
-            </li>
-          </ul>
+          {isSplashPage() ? null : (
+            <ul>
+              <li>
+                <Link href={t("footer.privacy.link")} locale={i18n.language}>
+                  {t("footer.privacy.desc")}
+                </Link>
+              </li>
+              <li>
+                <a href={t("footer.terms.link")}>{t("footer.terms.desc")}</a>
+              </li>
+            </ul>
+          )}
         </div>
         <div>
           <img alt={t("fip.text")} src="/img/wmms-blk.svg" />
