e0ce9bcd237daed4568e3cd28c16a43d1729159d
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2021-05-11 13:26:19 -0400

    Authentication using CDS Google accounts (#168)

--- next.config.js
+++ next.config.js
@@ -24,11 +24,20 @@ module.exports = {
       return [];
     }
   },
-  webpack: (config) => {
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
     });
+    if (!options.isServer && config.mode === "development") {
+      const { I18NextHMRPlugin } = require("i18next-hmr/plugin");
+      config.plugins.push(
+        new I18NextHMRPlugin({
+          localesDir: path.resolve(__dirname, "public/static/locales"),
+        })
+      );
+    }
+
     return config;
   },
 };
