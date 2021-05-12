e4c941eab63aa4d8c19c34055585e43b8cebde21
nmakuch <makuch.nick@gmail.com>
2021-05-12 12:21:30 -0400

    tweaking link styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,7 @@ module.exports = {
       purple: {
         ...colors.purple,
         default: "#6300C7",
+        active: "#452862",
       },
       white: {
         ...colors.white,
@@ -83,8 +84,7 @@ module.exports = {
       },
       blue: {
         ...colors.blue,
-        dark: "#2B4380",
-        active: "#3654A1",
+        dark: "#284162",
         light: "#335075",
         hover: "#0535D2",
         active: "#03279B",
