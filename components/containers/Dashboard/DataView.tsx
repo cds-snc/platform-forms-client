707ef3b6b15324b6fc4128c4222c7b28e35a5f16
brdunfield <bethany.dunfield@cds-snc.ca>
2021-05-17 09:43:54 -0600

    typescript

--- components/containers/Dashboard/DataView.tsx
+++ components/containers/Dashboard/DataView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 //import classnames from "classnames";
 //import { useTranslation } from "next-i18next";
 import Head from "next/head";
@@ -8,13 +8,11 @@ interface DataViewProps {
 }
 
 export const DataView = (props: DataViewProps): React.ReactElement => {
-  const TemplatesList = (): React.ReactElement => {
-    return props.templatesJSON.map(
-      (template): React.ReactElement => {
-        return <li key={props.templatesJSON.indexOf(template)}>{JSON.stringify(template)}</li>;
-      }
-    );
-  };
+  const jsonElements = props.templatesJSON.map(
+    (template): React.ReactElement => {
+      return <li key={props.templatesJSON.indexOf(template)}>{JSON.stringify(template)}</li>;
+    }
+  );
 
   return (
     <>
@@ -23,7 +21,7 @@ export const DataView = (props: DataViewProps): React.ReactElement => {
       </Head>
 
       <h1 className="gc-h1">Data View</h1>
-      <TemplatesList />
+      <Fragment>{jsonElements}</Fragment>
     </>
   );
 };
