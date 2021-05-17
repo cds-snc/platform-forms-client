fddec549650a94a607807d29f494aff3fe09441a
brdunfield <bethany.dunfield@cds-snc.ca>
2021-05-17 13:14:34 -0600

    Wrap template admin pages in auth, and clean up DataView styling a bit so it's more functional / readable

--- pages/admin/view-templates.js
+++ pages/admin/view-templates.js
@@ -2,26 +2,29 @@
 import DataView from "../../components/containers/Dashboard/DataView";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { invokeLambda } from "../api/templates";
+import { requireAuthentication } from "../../lib/auth";
 
-export async function getServerSideProps(context) {
-  // getStaticProps is serverside, and therefore instead of doing a request,
-  // we import the invoke Lambda function directly
-  const payload = {
-    method: "GET",
-  };
-  const lambdaResult = JSON.parse(await invokeLambda(payload));
-  console.log(lambdaResult);
-  const templatesJSON =
-    lambdaResult.data && lambdaResult.data.records && lambdaResult.data.records.length > 0
-      ? lambdaResult.data.records
-      : [];
+export const getServerSideProps = requireAuthentication(async (context) => {
+  {
+    // getStaticProps is serverside, and therefore instead of doing a request,
+    // we import the invoke Lambda function directly
+    const payload = {
+      method: "GET",
+    };
+    const lambdaResult = JSON.parse(await invokeLambda(payload));
+    console.log(lambdaResult);
+    const templatesJSON =
+      lambdaResult.data && lambdaResult.data.records && lambdaResult.data.records.length > 0
+        ? lambdaResult.data.records
+        : [];
 
-  return {
-    props: {
-      templatesJSON: templatesJSON,
-      ...(await serverSideTranslations(context.locale, ["common", "welcome", "confirmation"])),
-    }, // will be passed to the page component as props
-  };
-}
+    return {
+      props: {
+        templatesJSON: templatesJSON,
+        ...(await serverSideTranslations(context.locale, ["common", "welcome", "confirmation"])),
+      }, // will be passed to the page component as props
+    };
+  }
+});
 
 export default DataView;
