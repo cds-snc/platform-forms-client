896ba4cfc15f4001eb9bf516e580bf7483c747be
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-05-28 16:04:52 -0400

    Add method security to Templates API

--- pages/api/templates.js
+++ pages/api/templates.js
@@ -1,21 +1,40 @@
 import { crudTemplates } from "../../lib/dataLayer";
 import { logMessage } from "../../lib/logger";
+import { getSession } from "next-auth/client";
 
-// handler
+const isAllowed = (session, method) => {
+  switch (method) {
+    case "GET":
+      return true;
+    case "INSERT":
+    case "UPDATE":
+    case "DELETE":
+      return session ? true : false;
+    default:
+      return false;
+  }
+};
 
 const templates = async (req, res) => {
-  return crudTemplates(JSON.parse(req.body))
-    .then((response) => {
-      if (response) {
-        res.status(200).json(response);
-      } else {
+  const session = await getSession({ req });
+  const requestBody = JSON.parse(req.body);
+
+  if (isAllowed(session, requestBody.method)) {
+    return crudTemplates({ ...requestBody, session })
+      .then((response) => {
+        if (response) {
+          res.status(200).json(response);
+        } else {
+          res.status(500).json({ error: "Error on Server Side" });
+        }
+      })
+      .catch((err) => {
+        logMessage.error(err);
         res.status(500).json({ error: "Error on Server Side" });
-      }
-    })
-    .catch((err) => {
-      logMessage.error(err);
-      res.status(500).json({ error: "Error on Server Side" });
-    });
+      });
+  } else {
+    return res.status(403).json({ error: "Forbidden" });
+  }
 };
 
 export default templates;
