b4807b63ec6c45699414bc4cd11adc4b519f95ee
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-06-15 16:28:31 -0400

    add auth on api endpoints

--- pages/api/flags/[key]/enable.tsx
+++ pages/api/flags/[key]/enable.tsx
@@ -1,9 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
 import { enableFlag, checkAll } from "@lib/flags";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const key = req.query.key as string;
-  await enableFlag(key);
-  const flags = await checkAll();
-  res.status(200).json(flags);
+  const session = await getSession({ req });
+  if (session) {
+    const key = req.query.key as string;
+    await enableFlag(key);
+    const flags = await checkAll();
+    res.status(200).json(flags);
+  } else {
+    res.status(403).send("Forbidden");
+  }
 };
--- pages/api/flags/[key]/remove.tsx
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { removeFlag, checkAll } from "@lib/flags";
-
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const key = req.query.key as string;
-  await removeFlag(key);
-  const flags = await checkAll();
-  res.status(200).json(flags);
-};
