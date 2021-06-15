fa1a217fd89e80d9839863ccddc89f861a11eddd
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-06-15 16:54:10 -0400

    Added comment

--- lib/flags.tsx
+++ lib/flags.tsx
@@ -7,6 +7,7 @@ const getRedisInstance = () => {
   if (process.env.REDIS_URL) {
     return new Redis(process.env.REDIS_URL);
   } else {
+    // not ideal but works when you don't have local redis running.
     return new mockRedis();
   }
 };
