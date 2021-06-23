311a880fa8c823479920543d2dd9520089a102f2
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-06-23 10:38:20 -0400

    Add google tag manager

--- components/globals/Base.js
+++ components/globals/Base.js
@@ -26,15 +26,16 @@ const Base = ({ children }) => {
       <Head>
         {googleTag && (
           <React.Fragment>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-8PNSS76E3B"></script>
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                
-                  gtag('config', 'G-8PNSS76E3B');
+                <!-- Google Tag Manager -->
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-W3ZVVX5');
+                <!-- End Google Tag Manager -->
               `,
               }}
             />
@@ -51,6 +52,17 @@ const Base = ({ children }) => {
         />
         <title>{t("title")}</title>
       </Head>
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W3ZVVX5"
+          title="Google Tag Manager Iframe Window"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
       <SkipLink />
       <div className={classes}>
         {!isEmbeddable && (
