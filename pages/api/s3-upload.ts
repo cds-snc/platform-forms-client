9dc67706a3b921efb63f391e5cb60aae340127f0
rkeita <raphael.keita@cds-snc.ca>
2021-08-12 19:15:52 -0400

    Create a presigned URL for a object uploaded

--- pages/api/s3-upload.ts
+++ pages/api/s3-upload.ts
@@ -2,10 +2,11 @@ import { ReadStream } from "fs";
 import fs from "fs";
 import { v4 as uuid } from "uuid";
 import { UploadResult, UploadFailure, UploadSuccess } from "../../lib/types";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import formidable from "formidable";
 
-const s3Client = new S3Client({ region: process.env.AWS_BUCKET_REGION });
+const s3Client = new S3Client({ region: process.env.AWS_BUCKET_REGION }); // could be hard coded
 
 /**
  * This function tries to upload a given file to aws S3 bucket and returns a data object
@@ -21,19 +22,21 @@ const uploadFileToS3 = async (
   filePath: string
 ): Promise<UploadResult> => {
   try {
+    const data = await readStream2buffer(fs.createReadStream(file.path));
     // setting the parameters
     const uploadParams = {
       Bucket: bucketName,
-      Body: await readStream2buffer(fs.createReadStream(file.path)).then((data) => {
-        return data;
-      }),
+      Body: data,
       Key: `${bucketName}/user_file/${new Date().toISOString().slice(0, 10)}/${uuid()}.${filePath}`,
     };
 
-    const responseData = await s3Client.send(new PutObjectCommand(uploadParams));
+    await s3Client.send(new PutObjectCommand(uploadParams));
+    const signedUrl = await getSignedUrl(s3Client, new GetObjectCommand(uploadParams), {
+      expiresIn: 3600 * 24 * 4,
+    });
     const result: UploadSuccess = {
       isValid: true,
-      successValue: { location: uploadParams.Key, ...responseData },
+      successValue: { location: signedUrl },
     };
     return result;
   } catch (error) {
