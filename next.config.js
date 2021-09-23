508747e42c67ceabb480173c468b335fffc83ec4
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-09-23 16:12:09 -0400

    Add security headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,41 @@ const path = require("path");
 const { i18n } = require("./next-i18next.config");
 const localesDir = path.resolve("public/static/locales");
 
+let prod = process.env.NODE_ENV == "production";
+
+let csp = ``;
+csp += `base-uri 'self';`;
+csp += `form-action 'self';`;
+csp += `default-src 'self';`;
+csp += `script-src 'self' ${prod ? "" : "'unsafe-eval'"};`; // NextJS requires 'unsafe-eval' in dev (faster source maps)
+csp += `style-src 'self' https://fonts.googleapis.com 'unsafe-inline' data:;`; // NextJS requires 'unsafe-inline'
+csp += `img-src 'self' https: data:;`;
+csp += `font-src 'self' https://fonts.gstatic.com;`;
+csp += `frame-src https://www.googletagmanager.com;`;
+
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: csp,
+  },
+];
+
 module.exports = {
   i18n,
   sassOptions: {
@@ -43,5 +78,14 @@ module.exports = {
       use: [{ loader: "babel-loader" }],
     });
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
