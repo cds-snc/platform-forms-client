95e43633c77cea2903daf5e5911891f1caae8e19
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2021-10-04 13:35:12 -0400

    Add security headers and Content Security Policy (#452)

--- components/globals/Base.js
+++ components/globals/Base.js
@@ -1,19 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Head from "next/head";
 import Footer from "./Footer";
+import Head from "next/head";
 import PhaseBanner from "./PhaseBanner";
 import SkipLink from "./SkipLink";
 import Fip from "./Fip";
 import AdminNav from "./AdminNav";
 import { useTranslation } from "next-i18next";
 import { getPageClassNames } from "../../lib/routeUtils";
-import { useFlag } from "../../lib/hooks/flags";
 
 const Base = ({ children }) => {
-  const { t } = useTranslation("common");
-
-  const googleTag = useFlag("googleAnalytics");
   const formConfig =
     children && children.props && children.props.formConfig ? children.props.formConfig : null;
   const classes = getPageClassNames(formConfig);
@@ -22,39 +18,16 @@ const Base = ({ children }) => {
   const isEmbeddable = formConfig && children && children.props && children.props.isEmbeddable;
 
   const shouldDisplayAlphaBanner = formConfig ? formConfig.displayAlphaBanner : true;
+  const { t } = useTranslation("common");
 
   return (
     <>
       <Head>
-        {googleTag && (
-          <React.Fragment>
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                <!-- Google Tag Manager -->
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-W3ZVVX5');
-                <!-- End Google Tag Manager -->
-              `,
-              }}
-            />
-          </React.Fragment>
-        )}
-        <script type="text/javascript" src="/static/scripts/form-polyfills.js"></script>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="32x32" />
-        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Lato:400,700%7CNoto+Sans:400,700&amp;display=fallback"
-          rel="stylesheet"
-        />
         <title>{t("title")}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="32x32" />
       </Head>
-
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-W3ZVVX5"
