1ea57d06607b342a11fe33b09c7d4bf8e2cd9845
James Eberhardt <james.eberhardt@cds-snc.ca>
2021-10-12 13:19:31 -0400

    Updated tests.

--- tests/templates.test.js
+++ tests/templates.test.js
@@ -2,6 +2,7 @@ import { createMocks } from "node-mocks-http";
 import templates from "../pages/api/templates";
 import client from "next-auth/client";
 import validFormTemplate from "./validFormTemplate.json";
+import brokenFormTemplate from "./brokenFormTemplate.json";
 
 import fetchMock from "jest-fetch-mock";
 
@@ -46,28 +47,7 @@ jest.mock("@aws-sdk/client-lambda", () => {
             return Promise.resolve({
               Payload: encoder.encode(
                 JSON.stringify({
-                  data: {
-                    records: [
-                      {
-                        formConfig: {
-                          form: { titleEn: "test" },
-                          publishingStatus: true,
-                        },
-                      },
-                      {
-                        formConfig: {
-                          form: { titleEn: "test" },
-                          publishingStatus: true,
-                        },
-                      },
-                      {
-                        formConfig: {
-                          form: { titleEn: "test" },
-                          publishingStatus: false,
-                        },
-                      },
-                    ],
-                  },
+                  data: { records: [{ formID: 8 }] },
                 })
               ),
             });
@@ -81,14 +61,14 @@ jest.mock("@aws-sdk/client-lambda", () => {
   };
 });
 
-describe("Test HTTP access scenarios", () => {
+describe("Test JSON validation scenarios", () => {
   beforeEach(() => {
     fetchMock.enableMocks();
   });
-  it("Allows Access if INSERT", async () => {
+  it("Should pass with valid JSON", async () => {
     const mockSession = {
       expires: "1",
-      user: { email: "a", name: "Delta", image: "c" },
+      user: { email: "a@b.com", name: "Testing Forms", image: "null" },
     };
 
     client.getSession.mockReturnValueOnce([mockSession, false]);
@@ -109,5 +89,30 @@ describe("Test HTTP access scenarios", () => {
     await templates(req, res);
 
     expect(res.statusCode).toBe(200);
+  });
+
+  it("Should fail with invalid JSON", async () => {
+    const mockSession = {
+      expires: "1",
+      user: { email: "a", name: "Delta", image: "c" },
+    };
+
+    client.getSession.mockReturnValueOnce([mockSession, false]);
+
+    fetchMock.mockResponseOnce(JSON.stringify({ testing: 300 }));
+    const { req, res } = createMocks({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Origin: "http://localhost:3000",
+      },
+      body: JSON.stringify({
+        formConfig: brokenFormTemplate,
+        method: "INSERT",
+      }),
+    });
+
+    await templates(req, res);
+    expect(res.statusCode).toBe(400);
   });
 });
