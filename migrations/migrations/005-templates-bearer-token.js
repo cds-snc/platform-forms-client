5519c4c8fa46539e4d3e1d9db126841487fad410
Moro-Code <omar@omarnasr.ca>
2021-11-09 19:11:42 -0500

    fix: remove not null constraint as bearer token will need to be created after the record is inserted since formId is generated

--- migrations/migrations/005-templates-bearer-token.js
+++ migrations/migrations/005-templates-bearer-token.js
@@ -1,7 +1,5 @@
 const alterTemplatesTableToAddBearerToken = `
   ALTER TABLE templates ADD COLUMN bearer_token TEXT;
-  UPDATE templates SET bearer_token = 'tempstring';
-  ALTER TABLE templates ALTER COLUMN bearer_token SET NOT NULL;
 `;
 
 module.exports.generateSql = () => `${alterTemplatesTableToAddBearerToken}`;
