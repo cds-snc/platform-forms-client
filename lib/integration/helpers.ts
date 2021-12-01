39d83cd7d8abc5304bafd7aab9487b81994c0350
Moro-Code <omar@omarnasr.ca>
2021-12-01 10:41:44 -0500

    fix: tests relating to replacing console with logMessage

--- lib/integration/helpers.ts
+++ lib/integration/helpers.ts
@@ -13,10 +13,8 @@ import {
   PublicFormSchemaProperties,
 } from "../types";
 
-
 // Get the form json object by using the form ID
 // Returns => json object of form
-
 async function _getFormByID(formID: string): Promise<PublicFormSchemaProperties | undefined> {
   try {
     const response = await axios({
