647e64958d16e37bd1b8f05414804f63158e1196
Raphael Keita <raphael.keita@cds-snc.ca>
2021-12-13 11:53:02 -0500

    Create secure API to associate emails to a specific form. (#551)
    * Add GC's email validator and AddEmailToFrom method
    
    * Add docstring
    
    * Add comments
    
    * Fix await keyword before dbconnector
    
    * Update changelog file
    
    * Update docstring
    
    * Refactoring comments and errors msgs
    
    * Fix docstrings
    
    * Optimize count query
    
    * Remove blank line
    
    * Update docstrings
    
    * Moved email.domain.json into app root.
    
    * incorporate review feedback: remove tsUtils  and update addEmailToForm
    
    * fixed stalled requests
    
    * fix: passing false into the request payload causes undesirable effect
    
    * fix:linting
    
    * Fix formID not found statuscode value and incorporate reviewer's feedbacks.
    
    * fix unit tests
    
    Co-authored-by: Omar Nasr <omar@omarnasr.ca>

--- pages/api/id/[form]/owners.ts
+++ pages/api/id/[form]/owners.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import dbConnector from "../../../../lib/integration/dbConnector";
 import isUserSessionExist from "../../../../lib/middleware/HttpSessionExist";
 import executeQuery from "../../../../lib/integration/queryManager";
+import { isValidGovEmail } from "@lib/validation";
+import emailDomainList from "../../../../email.domains.json";
 
 const owners = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
@@ -12,8 +14,9 @@ const owners = async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
       case "PUT":
         return await activateOrDeactivateFormOwners(req, res);
       case "POST":
-        // TODO handle card #491 to associate a specific email to a form
-        break;
+        return await addEmailToForm(req, res);
+      default:
+        return res.status(400).json({ error: "Bad Request" });
     }
   } catch (err) {
     res.status(500).json({ error: "Error on Server Side" });
@@ -55,12 +58,12 @@ export async function activateOrDeactivateFormOwners(
 ): Promise<void> {
   //Extracting req body
   const requestBody = req.body ? JSON.parse(req.body) : undefined;
-  //Payload validation
-  if (!requestBody?.email || !requestBody?.active || typeof requestBody.active !== "boolean") {
+  //Payload validation fix: true case scenario
+  if (!requestBody?.email || typeof requestBody.active !== "boolean") {
     //Invalid payload
     return res.status(400).json({ error: "Invalid payload fields are not define" });
   }
-  const { email, active } = requestBody; // TODO email should be checked i.e (valid Gov email). See #491
+  const { email, active } = requestBody;
   const formID = req.query.form as string;
   if (!formID) return res.status(400).json({ error: "Malformed API Request Invalid formID" });
   //Update form_users's records
@@ -73,5 +76,42 @@ export async function activateOrDeactivateFormOwners(
   if (resultObject.rowCount > 0) return res.status(200).json(resultObject.rows);
   //A 404 status code for a form Not Found in form_users
   return res.status(404).json({ error: "Form or email Not Found" });
+}
+/**
+ * This method aims to create a unique binding between a form template and a user
+ * who needs to access the form's data later on. Here are some requirements that must be satisfied
+ * in order to associate users to collect a form's data
+ * Rule 1: a valid government email address
+ * Rule 2: the FromID/template must exist
+ * Rule 3: the email must not be already associated to the template
+ * @param req The request object
+ * @param res The response object
+ */
+export async function addEmailToForm(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  //Get request body
+  const requestBody = req.body ? JSON.parse(req.body) : undefined;
+  //Checkimg the payload's content
+  if (!requestBody?.email || !isValidGovEmail(requestBody.email, emailDomainList.domains)) {
+    return res.status(400).json({ error: "The email is not a valid GC email" });
+  }
+  const formID = req.query.form as string;
+  if (!formID) return res.status(400).json({ error: "Malformed API Request Invalid formID" });
+  try {
+    const result = await executeQuery(
+      await dbConnector(),
+      "INSERT INTO form_users (template_id, email) VALUES ($1, $2) RETURNING id",
+      [formID, requestBody.email as string]
+    );
+    return res.status(200).json({ success: result.rows[0] });
+  } catch (error) {
+    const message = error.message;
+    //formID foreign key violation
+    if (message.includes("violates foreign key constraint")) {
+      return res.status(404).json({ error: "The formID does not exist" });
+      //violating email and template_id uniqueness
+    } else if (message.includes("violates unique constraint")) {
+      return res.status(400).json({ error: "This email is already binded to this form" });
+    }
+  }
 }
 export default isRequestAllowed(["GET", "POST", "PUT"], isUserSessionExist(owners));
