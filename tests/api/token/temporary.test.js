6d56ca3253602be11174594e3f342a0f562aae35
Clément JANIN <clement.janin@cds-snc.ca>
2022-01-18 08:34:37 -0500

    feat: send email when temporary token is generated (#584)

--- tests/api/token/temporary.test.js
+++ tests/api/token/temporary.test.js
@@ -15,6 +15,22 @@ jest.mock("@lib/integration/dbConnector", () => {
   return jest.fn(() => mockClient);
 });
 
+let IsGCNotifyServiceAvailable = true;
+
+const mockSendEmail = {
+  sendEmail: jest.fn(() => {
+    if (IsGCNotifyServiceAvailable) {
+      return Promise.resolve();
+    } else {
+      return Promise.reject(new Error("something went wrong"));
+    }
+  }),
+};
+
+jest.mock("notifications-node-client", () => ({
+  NotifyClient: jest.fn(() => mockSendEmail),
+}));
+
 describe("TemporaryBearerToken tests", () => {
   beforeAll(() => {
     process.env.TOKEN_SECRET = "some_secret_some_secret_some_secret_some_secret";
@@ -91,5 +107,39 @@ describe("TemporaryBearerToken tests", () => {
 
     await temporary(req, res);
     expect(res.statusCode).toEqual(403);
+  });
+
+  it("throws error with GC Notify service unavailable", async () => {
+    IsGCNotifyServiceAvailable = false;
+
+    const token = jwt.sign({ formID: "1" }, process.env.TOKEN_SECRET, { expiresIn: "1y" });
+    const { req, res } = createMocks({
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Origin: "http://localhost:3000",
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        email: "test@cds-snc.ca",
+      }),
+    });
+
+    executeQuery.mockImplementation((client, sql) => {
+      if (sql.includes("SELECT bearer_token as bearerToken FROM templates")) {
+        return {
+          rows: [{ bearerToken: token }],
+          rowCount: 1,
+        };
+      } else {
+        return {
+          rows: [{ id: "1", email: "test@cds-snc.ca", active: true }],
+          rowCount: 1,
+        };
+      }
+    });
+
+    await temporary(req, res);
+    expect(res.statusCode).toEqual(500);
   });
 });
