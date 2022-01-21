3c5c5f65d8ab460363059ce94f3d6338f4035f0b
James Eberhardt <james.eberhardt@cds-snc.ca>
2022-01-20 19:03:04 -0500

    Refactored function.

--- components/admin/BearerRefresh/BearerRefresh.tsx
+++ components/admin/BearerRefresh/BearerRefresh.tsx
@@ -2,6 +2,7 @@ import { Button } from "@components/forms";
 import { logMessage } from "@lib/logger";
 import { BearerResponse, FormDBConfigProperties } from "@lib/types";
 import axios from "axios";
+import server from "i18next-hmr/server";
 import { useTranslation } from "next-i18next";
 import React, { useEffect, useState } from "react";
 
@@ -23,21 +24,20 @@ const BearerRefresh = (props: BearerRefreshProps): React.ReactElement => {
    * Connects to the database and refreshes the bearer token for the specified form ID.
    *
    * @param formID
-   * @returns the response data from the server
    */
-  const handleRefreshBearerToken = async (formID: number) => {
+  const handleRefreshBearerToken = async (formID: number | undefined) => {
     try {
+      setErrorState({ message: "" });
       const serverResponse = await axios({
         url: `/api/id/${formID}/bearer`,
         method: "POST",
         timeout: 0,
       });
-      setErrorState({ message: "" });
-      return serverResponse.data;
+      const { bearerToken } = serverResponse.data as unknown as BearerResponse;
+      setBearerTokenState(bearerToken);
     } catch (err) {
       logMessage.error(err as Error);
       setErrorState({ message: t("settings.bearerTokenRefreshError") });
-      return err;
     }
   };
 
@@ -59,15 +59,7 @@ const BearerRefresh = (props: BearerRefreshProps): React.ReactElement => {
         readOnly
       ></textarea>
       <br />
-      <Button
-        type="button"
-        onClick={async () => {
-          const { bearerToken } = (await handleRefreshBearerToken(
-            form?.formID ? form.formID : 0
-          )) as unknown as BearerResponse;
-          setBearerTokenState(bearerToken);
-        }}
-      >
+      <Button type="button" onClick={() => handleRefreshBearerToken(form?.formID)}>
         {t("settings.refreshButton")}
       </Button>
     </>
