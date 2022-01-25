da2e5fe367751956815cf5083ab6f1a591a7374f
rkeita <raphael.keita@cds-snc.ca>
2022-01-25 11:38:18 -0500

    fix:typo in attribute's values

--- components/globals/Footer.js
+++ components/globals/Footer.js
@@ -13,21 +13,13 @@ const Footer = () => {
             <ul className="gc-horizontal-list">
               <li className="gc-horizontal-item">
                 {/* Docs when and why ot use _blank/noopener https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/target 
-                https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#attr-noopener */}
-                <a
-                  href={t("footer.privacy.link")}
-                  target="_blank"
-                  rel="noopener noreferer noreferrer"
-                >
+                https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#attr-noreferrer */}
+                <a href={t("footer.privacy.link")} target="_blank" rel="noreferrer">
                   {t("footer.privacy.desc")}
                 </a>
               </li>
               <li className="gc-horizontal-item">
-                <a
-                  href={t("footer.terms.link")}
-                  target="_blank"
-                  rel="noopener noreferer noreferrer"
-                >
+                <a href={t("footer.terms.link")} target="_blank" rel="noreferrer">
                   {t("footer.terms.desc")}
                 </a>
               </li>
