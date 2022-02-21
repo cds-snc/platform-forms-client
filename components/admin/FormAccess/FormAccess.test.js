8ba1768f7d2d89967d56cb5e8e4a7f1782c2b2a8
James Eberhardt <james.eberhardt@cds-snc.ca>
2022-02-21 18:31:49 -0500

    Fixed up tests.

--- components/admin/FormAccess/FormAccess.test.js
+++ components/admin/FormAccess/FormAccess.test.js
@@ -7,92 +7,111 @@ import FormAccess from "./FormAccess";
 jest.mock("axios");
 
 describe("Form Access Component", () => {
-  afterEach(cleanup);
   const formConfig = { formID: 1 };
+
+  afterEach(cleanup);
+
   it("renders without errors", async () => {
     mockedAxios.mockResolvedValue({
       status: 200,
-      data: [],
+      data: [
+        {
+          id: 4,
+          email: "test1@cds-snc.ca",
+          active: true,
+        },
+      ],
     });
-    await act(async () => {
-      render(<FormAccess formID={formConfig.formID}></FormAccess>);
-    });
-    expect(screen.getByTestId("add-email")).toBeInTheDocument();
+
+    render(<FormAccess formID={formConfig.formID}></FormAccess>);
+    expect(await screen.findByTestId("add-email")).toBeInTheDocument();
   });
 
   it("submits a new email address and display it in the list", async () => {
     const testEmailAddress = "test@cds-snc.ca";
-
     mockedAxios.mockResolvedValue({
       status: 200,
-      data: [],
+      data: [
+        {
+          id: 4,
+          email: "test1@cds-snc.ca",
+          active: true,
+        },
+      ],
     });
+
+    render(<FormAccess formID={formConfig.formID}></FormAccess>);
+
+    const input = await screen.findByLabelText("settings.formAccess.addEmailAriaLabel");
+    fireEvent.change(input, { target: { value: testEmailAddress } });
+
+    mockedAxios.mockResolvedValueOnce({
+      status: 200,
+      data: {
+        success: {
+          id: 1,
+        },
+      },
+    });
+    fireEvent.click(screen.getByTestId("add-email"));
+    expect(await screen.findByText(testEmailAddress)).toBeInTheDocument;
+  });
+
+  it("submits a new email address for a form that does not exist, and receives an error from the API", async () => {
+    const testEmailAddress = "test@cds-snc.ca";
+    mockedAxios
+      .mockResolvedValueOnce({
+        status: 200,
+        data: [
+          {
+            id: 4,
+            email: "test1@cds-snc.ca",
+            active: true,
+          },
+        ],
+      })
+      .mockResolvedValueOnce({
+        status: 404,
+        data: { error: "The formID does not exist" },
+      });
+
     await act(async () => {
       render(<FormAccess formID={formConfig.formID}></FormAccess>);
     });
-    const input = screen.getByLabelText("settings.formAccess.addEmailAriaLabel");
-    await act(async () => {
-      fireEvent.change(input, { target: { value: testEmailAddress } });
-    });
-    await act(async () => {
-      mockedAxios.mockResolvedValue({
-        status: 200,
-        data: {
-          success: {
-            id: 1,
-          },
-        },
-      });
-      fireEvent.click(screen.getByTestId("add-email"));
-    });
-    expect(screen.findByText(testEmailAddress)).toBeInTheDocument;
+
+    const input = await screen.findByLabelText("settings.formAccess.addEmailAriaLabel");
+
+    await fireEvent.change(input, { target: { value: testEmailAddress } });
+
+    await fireEvent.click(screen.getByTestId("add-email"));
+    expect(await screen.findByTestId("alert")).toBeInTheDocument;
   });
 
   it("submits a new email address that is not a Government of Canada email, and receives an error from the API", async () => {
     const testEmailAddress = "test@test.ca";
 
-    mockedAxios.mockResolvedValue({
-      status: 200,
-      data: [],
-    });
-    await act(async () => {
-      render(<FormAccess formID={formConfig.formID}></FormAccess>);
-    });
-    const input = screen.getByLabelText("settings.formAccess.addEmailAriaLabel");
-    await act(async () => {
-      fireEvent.change(input, { target: { value: testEmailAddress } });
-    });
-    await act(async () => {
-      mockedAxios.mockResolvedValue({
+    mockedAxios
+      .mockResolvedValueOnce({
+        status: 200,
+        data: [
+          {
+            id: 4,
+            email: "test1@cds-snc.ca",
+            active: true,
+          },
+        ],
+      })
+      .mockResolvedValueOnce({
         status: 400,
         data: { error: "The email is not a valid GC email" },
       });
-      fireEvent.click(screen.getByTestId("add-email"));
-    });
-    expect(screen.findByRole("alert")).toBeInTheDocument;
-  });
 
-  it("submits a new email address for a form that does not exist", async () => {
-    const testEmailAddress = "test@cds-snc.ca";
+    render(<FormAccess formID={formConfig.formID}></FormAccess>);
 
-    mockedAxios.mockResolvedValue({
-      status: 200,
-      data: [],
-    });
-    await act(async () => {
-      render(<FormAccess formID={formConfig.formID}></FormAccess>);
-    });
-    const input = screen.getByLabelText("settings.formAccess.addEmailAriaLabel");
-    await act(async () => {
-      fireEvent.change(input, { target: { value: testEmailAddress } });
-    });
-    await act(async () => {
-      mockedAxios.mockResolvedValue({
-        status: 404,
-        data: { error: "The formID does not exist" },
-      });
-      fireEvent.click(screen.getByTestId("add-email"));
-    });
-    expect(screen.findByRole("alert")).toBeInTheDocument;
+    const input = await screen.findByLabelText("settings.formAccess.addEmailAriaLabel");
+    await fireEvent.change(input, { target: { value: testEmailAddress } });
+
+    await fireEvent.click(screen.getByTestId("add-email"));
+    expect(await screen.findByRole("alert")).toBeInTheDocument;
   });
 });
