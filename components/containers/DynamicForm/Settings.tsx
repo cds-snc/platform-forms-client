d322b4ad42af263c0cbe658c5e7c9688b9561265
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-02-24 11:03:14 -0500

    update timeout value to match project std

--- components/containers/DynamicForm/Settings.tsx
+++ components/containers/DynamicForm/Settings.tsx
@@ -27,7 +27,7 @@ const handleDelete = async (formID: number) => {
       method: "DELETE",
       formID: formID,
     },
-    timeout: 0,
+    timeout: process.env.NODE_ENV === "production" ? 60000 : 0,
   })
     .then((serverResponse) => {
       //success - redirect to view-templates page
