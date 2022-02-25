d1128b29bb8d3905da03c8a233535ccee52c22c7
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-02-25 10:57:39 -0500

    jest util refactor

--- lib/jestUtils.ts
+++ lib/jestUtils.ts
@@ -5,13 +5,13 @@
  * @returns boolean of whether message was found in logs
  */
 export const checkLogs = (logs: Array<Array<string>>, message: string): boolean => {
-  const foundLogs = logs.filter((log) => {
+  const foundLogs = logs.flat().filter((log) => {
     if (log.includes(message)) {
       return true;
     }
   });
 
-  if (foundLogs.flat().length > 0) {
+  if (foundLogs.length > 0) {
     return true;
   }
   return false;
