fa781c153ed47be405bea6d996b53c24dd5c0fa6
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-03-11 10:20:16 -0500

    do not fail on uncaught app errors

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,3 +21,9 @@ import "./commands";
 
 // Import Axe-Core library
 import "cypress-axe";
+
+Cypress.on("uncaught:exception", () => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
