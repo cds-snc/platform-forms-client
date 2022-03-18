9a86c36875178d59cede3c92af7d4c0148c17d17
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-03-18 13:34:27 -0400

    Welcome page was scope sharing between tests

--- cypress/integration/welcome_page.spec.js
+++ cypress/integration/welcome_page.spec.js
@@ -8,8 +8,6 @@ describe("Welcome Page", () => {
   it("Change page language", () => {
     cy.get("button[lang='fr']").click();
     cy.url().should("contain", "/fr");
-  });
-  it("Fr page renders", () => {
     cy.get("h1").should("contain", "Bienvenue aux formulaires GC");
   });
 });
