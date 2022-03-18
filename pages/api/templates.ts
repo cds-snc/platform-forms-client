6856036ce4ba29644e959a410e465f9ed76b9415
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-03-18 14:36:09 -0400

    Ensure non-auth Template responses are sanitized

--- pages/api/templates.ts
+++ pages/api/templates.ts
@@ -1,18 +1,43 @@
 import { crudTemplates } from "@lib/integration/crud";
 
-import { middleware, jsonValidator, cors } from "@lib/middleware";
+import { middleware, jsonValidator, cors, sessionExists } from "@lib/middleware";
 import templatesSchema from "@lib/middleware/schemas/templates.schema.json";
 import { NextApiRequest, NextApiResponse } from "next";
 import { isAdmin } from "@lib/auth";
+import { CrudTemplateResponse, PublicFormSchemaProperties } from "@lib/types";
 
 const allowedMethods = ["GET", "POST", "PUT", "DELETE"];
+const authenticatedMethods = ["POST", "PUT", "DELETE"];
+
+const onlyIncludePublicProperties = async ({
+  data: { records },
+}: CrudTemplateResponse): Promise<{ data: Array<PublicFormSchemaProperties> }> => {
+  if (records) {
+    const sanitizedResponse = records.map((template) => {
+      return {
+        formID: template.formID,
+        publishingStatus: template.formConfig.publishingStatus,
+        displayAlphaBanner: template.formConfig.displayAlphaBanner ?? true,
+        ...template.formConfig.form,
+      };
+    });
+    return { data: sanitizedResponse };
+  } else {
+    throw new Error("No records found");
+  }
+};
 
 const templates = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const session = await isAdmin({ req });
     const response = await crudTemplates({ ...req.body, method: req.method, session });
     if (response) {
-      res.status(200).json(response);
+      if (req.method === "GET") {
+        const publicTemplates = await onlyIncludePublicProperties(response);
+        res.status(200).json(publicTemplates);
+      } else {
+        res.status(200).json(response);
+      }
     } else {
       res.status(500).json({ error: "Error on Server Side" });
     }
@@ -22,6 +47,10 @@ const templates = async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 export default middleware(
-  [cors({ allowedMethods }), jsonValidator(templatesSchema, { jsonKey: "formConfig" })],
+  [
+    cors({ allowedMethods }),
+    sessionExists(authenticatedMethods),
+    jsonValidator(templatesSchema, { jsonKey: "formConfig" }),
+  ],
   templates
 );
