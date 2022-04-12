9df404b41712bb97bd5c081ca42ec478381fa03c
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-04-12 14:03:28 -0400

    Best practice: useTranslation like a hook

--- components/forms/Form/Form.tsx
+++ components/forms/Form/Form.tsx
@@ -5,6 +5,7 @@ import { validateOnSubmit, getErrorList, setFocusOnErrorMessage } from "@lib/val
 import { submitToAPI } from "@lib/integration/helpers";
 import { Button, Alert } from "../index";
 import { logMessage } from "@lib/logger";
+import { useTranslation } from "next-i18next";
 import { FormValues, InnerFormProps, DynamicFormProps, Responses } from "@lib/types";
 import axios from "axios";
 import { useFlag, useExternalScript } from "@lib/hooks";
@@ -26,10 +27,14 @@ declare global {
  * This is the "inner" form component that isn't connected to Formik and just renders a simple form
  * @param props
  */
-const InnerForm = (props: InnerFormProps & FormikProps<FormValues> & DynamicFormProps) => {
-  const { children, handleSubmit, t, isSubmitting, formConfig } = props;
+const InnerForm: React.FC<InnerFormProps & FormikProps<FormValues> & DynamicFormProps> = (
+  props: InnerFormProps & FormikProps<FormValues> & DynamicFormProps
+) => {
+  const { children, handleSubmit, isSubmitting, formConfig } = props;
   const [canFocusOnError, setCanFocusOnError] = useState(false);
   const [lastSubmitCount, setLastSubmitCount] = useState(-1);
+
+  const { t } = useTranslation();
 
   const errorList = props.errors ? getErrorList(props) : null;
   const errorId = "gc-form-errors";
@@ -129,91 +134,87 @@ const InnerForm = (props: InnerFormProps & FormikProps<FormValues> & DynamicForm
     };
   }, [submitTimer]);
 
-  return (
+  return isSubmitting || (props.submitCount > 0 && props.isValid && !formStatusError) ? (
+    <Loader loading={isSubmitting} message={t("loading")} />
+  ) : (
     <>
-      {isSubmitting || (props.submitCount > 0 && props.isValid && !formStatusError) ? (
-        <Loader loading={isSubmitting} message={t("loading")} />
-      ) : (
-        <>
-          {formStatusError && (
-            <Alert type="error" heading={formStatusError} tabIndex={0} id={serverErrorId} />
-          )}
-          {errorList && (
-            <Alert
-              type="error"
-              heading={t("input-validation.heading")}
-              validation={true}
-              id={errorId}
-              tabIndex={0}
-            >
-              {errorList}
-            </Alert>
-          )}
-          {/**
-           * method attribute needs to stay here in case javascript does not load
-           * otherwise GET request will be sent which will result in leaking all the user data
-           * to the URL
-           */}
-          <form
-            id="form"
-            data-testid="form"
-            method="POST"
-            onSubmit={(e) => {
-              if (submitTimer) {
-                setSubmitTooEarly(true);
-                window.dataLayer = window.dataLayer || [];
-                window.dataLayer.push({
-                  event: "form_submission_spam_trigger",
-                  formID: formConfig.formID,
-                  formTitle: formConfig.titleEn,
-                  submitTime: submitDelay - submitTimer,
-                });
-                if (submitTimer < 5) setSubmitTimer(5);
-                e.preventDefault();
-                return;
-              }
-              setSubmitTooEarly(false);
-
-              if (isReCaptchaEnableOnSite) {
-                handleSubmitReCaptcha(e);
-              } else {
-                handleSubmit(e);
-              }
-            }}
-            noValidate
-          >
-            {children}
-            <div
-              className={classNames({
-                "border-l-2": submitTimer >= 0 && submitTooEarly,
-                "border-red-default": submitTimer >= 0 && submitTooEarly,
-                "border-green-default": submitTimer == 0 && submitTooEarly,
-                "pl-3": submitTimer >= 0 && submitTooEarly,
-              })}
-            >
-              {submitTimer > 0 && submitTooEarly && (
-                <div role="alert">
-                  <p className="gc-label text-red-default">
-                    {t("spam-error.error-part-1")} {submitDelay} {t("spam-error.error-part-2")}
-                  </p>
-                  <p className="gc-description">
-                    {t("spam-error.prompt-part-1")} {submitTimer} {t("spam-error.prompt-part-2")}
-                  </p>
-                </div>
-              )}
-              {submitTimer == 0 && submitTooEarly && (
-                <div role="alert">
-                  <p className="gc-label text-green-default">{t("spam-error.success-message")}</p>
-                  <p className="gc-description">{t("spam-error.success-prompt")}</p>
-                </div>
-              )}
-              <div className="buttons">
-                <Button type="submit">{t("submitButton")}</Button>
-              </div>
-            </div>
-          </form>
-        </>
+      {formStatusError && (
+        <Alert type="error" heading={formStatusError} tabIndex={0} id={serverErrorId} />
       )}
+      {errorList && (
+        <Alert
+          type="error"
+          heading={t("input-validation.heading")}
+          validation={true}
+          id={errorId}
+          tabIndex={0}
+        >
+          {errorList}
+        </Alert>
+      )}
+      {/**
+       * method attribute needs to stay here in case javascript does not load
+       * otherwise GET request will be sent which will result in leaking all the user data
+       * to the URL
+       */}
+      <form
+        id="form"
+        data-testid="form"
+        method="POST"
+        onSubmit={(e) => {
+          if (submitTimer) {
+            setSubmitTooEarly(true);
+            window.dataLayer = window.dataLayer || [];
+            window.dataLayer.push({
+              event: "form_submission_spam_trigger",
+              formID: formConfig.formID,
+              formTitle: formConfig.titleEn,
+              submitTime: submitDelay - submitTimer,
+            });
+            if (submitTimer < 5) setSubmitTimer(5);
+            e.preventDefault();
+            return;
+          }
+          setSubmitTooEarly(false);
+
+          if (isReCaptchaEnableOnSite) {
+            handleSubmitReCaptcha(e);
+          } else {
+            handleSubmit(e);
+          }
+        }}
+        noValidate
+      >
+        {children}
+        <div
+          className={classNames({
+            "border-l-2": submitTimer >= 0 && submitTooEarly,
+            "border-red-default": submitTimer >= 0 && submitTooEarly,
+            "border-green-default": submitTimer == 0 && submitTooEarly,
+            "pl-3": submitTimer >= 0 && submitTooEarly,
+          })}
+        >
+          {submitTimer > 0 && submitTooEarly && (
+            <div role="alert">
+              <p className="gc-label text-red-default">
+                {t("spam-error.error-part-1")} {submitDelay} {t("spam-error.error-part-2")}
+              </p>
+              <p className="gc-description">
+                {t("spam-error.prompt-part-1")} {submitTimer} {t("spam-error.prompt-part-2")}
+              </p>
+            </div>
+          )}
+          {submitTimer == 0 && submitTooEarly && (
+            <div role="alert">
+              <p className="gc-label text-green-default">{t("spam-error.success-message")}</p>
+              <p className="gc-description">{t("spam-error.success-prompt")}</p>
+            </div>
+          )}
+          <div className="buttons">
+            <Button type="submit">{t("submitButton")}</Button>
+          </div>
+        </div>
+      </form>
     </>
   );
 };
