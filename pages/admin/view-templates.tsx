b47946c21bf4a7819734d29199c5c82cf7e96756
James Eberhardt <james.eberhardt@cds-snc.ca>
2022-05-17 17:55:00 -0400

    Move query from lambda to nextjs.

--- pages/admin/view-templates.tsx
+++ pages/admin/view-templates.tsx
@@ -1,22 +1,17 @@
-//import getConfig from "next/config";
 import DataView from "../../components/containers/Admin/Dashboard/DataView";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { crudTemplates } from "../../lib/integration/crud";
-import { requireAuthentication } from "../../lib/auth";
+import { requireAuthentication } from "@lib/auth";
+import executeQuery from "@lib/integration/queryManager";
+import dbConnector from "@lib/integration/dbConnector";
+import { logMessage } from "@lib/logger";
+import { CrudTemplateResponse } from "@lib/types";
 
 export const getServerSideProps = requireAuthentication(async (context) => {
   {
     // getStaticProps is serverside, and therefore instead of doing a request,
     // we import the invoke Lambda function directly
 
-    const lambdaResult = await crudTemplates({ method: "GET" });
-    const templatesJSON =
-      lambdaResult &&
-      lambdaResult.data &&
-      lambdaResult.data.records &&
-      lambdaResult.data.records.length > 0
-        ? lambdaResult.data.records
-        : [];
+    const templatesJSON = await getTemplates();
 
     if (context.locale) {
       return {
@@ -29,5 +24,22 @@ export const getServerSideProps = requireAuthentication(async (context) => {
     return { props: {} };
   }
 });
+
+/*@description
+ * Get all templates
+ * @returns An array of all Templates
+ */
+const getTemplates = async (): Promise<CrudTemplateResponse[]> => {
+  try {
+    const result = await executeQuery(
+      await dbConnector(),
+      'SELECT id as "formID", json_config as "formConfig", organization FROM Templates'
+    );
+    return result.rows as CrudTemplateResponse[];
+  } catch (e) {
+    logMessage.error(e as Error);
+    return [];
+  }
+};
 
 export default DataView;
