3f140ac93498b046e89fae1abc6b83f010a52eb4
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-06-17 11:02:55 -0400

    Add a doc string

--- lib/logger.ts
+++ lib/logger.ts
@@ -29,6 +29,12 @@ export const logMessage = pino({
   }),
 });
 
+/**
+ * Sanitizes error objects to ensure potential personal data is not transmitted
+ * from the browser to the server
+ * @param log Error object
+ * @returns Error object
+ */
 const sanitizeErrorObject = (log: unknown) => {
   if (Axios.isAxiosError(log)) {
     // If it's an axios error delete the data key that can
