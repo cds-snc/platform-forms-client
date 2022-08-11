ae2bbe61b55ec1b0dd8dff80d3294fb13341f199
Raphael Keita <raphael.keita@cds-snc.ca>
2022-08-11 13:32:17 -0400

    Logout (#891)
    * finish up components test and add cypress logout spec
    
    * fix cypress
    
    * update changelog
    
    * remove extra include
    
    * fix binfing element implicitly has an any type
    
    * replace lastLoginTime by name to address cypress test rendering issue
    
    * style to tailwind
    
    * fix i18 in logout url
    
    * a11 for button
    
    * fix typo
    
    * removed unnecessary mock
    
    * replace lastlogintime placeholder and fix cypress tests

---
+++ pages/auth/logout.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/react";
+
+const Logout = () => {
+  const { i18n, t } = useTranslation("logout");
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <div>
+        <h2 className="gc-h2">{t("messageContent")}</h2>
+        <div className="gc-last-login-time">
+          <>
+            {t("lastLoginTime")} : {session?.user?.lastLoginTime}
+          </>
+        </div>
+        <div className="gc-go-to-login-btn">
+          <Link href={`/${i18n.language}/auth/login`}>{t("goToLoginLabel")}</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      ...(context.locale && (await serverSideTranslations(context.locale, ["common", "logout"]))),
+    },
+  };
+};
+
+export default Logout;
