b89a572b5d77b093dfac4587b2911ac41c1892e5
rkeita <raphael.keita@cds-snc.ca>
2022-08-18 15:46:20 -0400

    remove log message

--- pages/api/acceptableuse.ts
+++ pages/api/acceptableuse.ts
@@ -1,14 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { cors, middleware, csrfProtected } from "@lib/middleware";
 import { setAcceptableUse } from "@lib/acceptableUseCache";
-import { logMessage } from "@lib/logger";
 
 const acceptableUse = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { userID } = req.body;
     if (!userID) return res.status(404).json({ error: "Bad request" });
     setAcceptableUse(userID);
-    logMessage.info("user id " + userID);
     res.status(200).json({});
   } catch (err) {
     res.status(500).json({ error: "Malformed API Request" });
