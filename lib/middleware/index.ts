76bffb9ec7170731eeeb0331ade3bc0aab8db34f
Clément JANIN <clement.janin@cds-snc.ca>
2022-08-19 09:50:58 -0400

    Feature/lock access after failed login attempts (#915)
    * feat: add login lockout API
    
    * feat: plug login lockout API to existing temporary token API
    
    * feat: create locked access log or log error message when user is locked out
    
    * Streamline Error 'Form access token is invalid'
    
    Co-authored-by: Bryan Robitaille <bryan.robitaille.work@gmail.com>

--- lib/middleware/index.ts
+++ lib/middleware/index.ts
@@ -1,7 +1,6 @@
 export { middleware } from "./middleware";
 export { cors } from "./cors";
 export { sessionExists } from "./sessionExists";
-export { validBearerToken } from "./validBearerToken";
 export { validTemporaryToken } from "./validTemporaryToken";
 export { jsonValidator } from "./jsonValidator";
 export { csrfProtected } from "./csrfProtected";
