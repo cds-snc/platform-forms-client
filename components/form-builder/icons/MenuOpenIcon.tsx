f7d134832dfc3439247e4c2d9ff4c6c29a9adaca
Paul Craig <paul@pcraig3.ca>
2022-09-06 12:54:44 -0400

    New icon

---
+++ components/form-builder/icons/MenuOpenIcon.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+export const MenuOpenIcon = () => (
+  <svg height={24} width={24} xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 36v-3h26v3Zm33.9-2.6-9.45-9.45 9.4-9.4L42 16.7l-7.25 7.25 7.3 7.3ZM6 25.4v-3h20v3ZM6 15v-3h26v3Z" />
+  </svg>
+);
