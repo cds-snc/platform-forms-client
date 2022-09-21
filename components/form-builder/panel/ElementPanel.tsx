698d8096a0f306563626cd3266245a75576e808c
Paul Craig <paul@pcraig3.ca>
2022-09-21 16:11:31 -0400

    Add an extra comment explaining why we look for validation types

--- components/form-builder/panel/ElementPanel.tsx
+++ components/form-builder/panel/ElementPanel.tsx
@@ -98,6 +98,12 @@ const getSelectedOption = (item: ElementTypeWithIndex): ElementOption => {
   if (!type) {
     return elementOptions[2];
   } else if (type === "textField") {
+    /**
+     * Email, phone, and date fields are specialized text field types.
+     * That is to say, their "type" is "textField" but they have specalized validation "type"s.
+     * So if we have a "textField", we want to first check properties.validation.type to see if
+     * it is a true Short Answer, or one of the other types.
+     */
     type = elements[item.index].properties.validation.type || type;
   }
 
