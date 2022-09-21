a761dce094a95761666e2c388f75e574b22a5c07
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-09-21 17:04:23 -0400

    Start setting up plateUI and some plugins

--- components/form-builder/plate-editor/RichTextEditor.tsx
+++ components/form-builder/plate-editor/RichTextEditor.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { Plate } from "@udecode/plate-core";
+import { createPlugins, Plate } from "@udecode/plate-core";
 import { editableProps } from "./editableProps";
 import { MyValue } from "./types";
 import PropTypes from "prop-types";
+import { createBoldPlugin, createItalicPlugin } from "@udecode/plate-basic-marks";
+import { createHeadingPlugin } from "@udecode/plate-heading";
+import { createParagraphPlugin } from "@udecode/plate-paragraph";
+import { createBlockquotePlugin } from "@udecode/plate-block-quote";
+import { plateUI } from "./plateUI";
+
+const plugins = createPlugins<MyValue>(
+  [
+    createBoldPlugin(),
+    createItalicPlugin(),
+    createHeadingPlugin(),
+    createParagraphPlugin(),
+    createBlockquotePlugin(),
+  ],
+  {
+    components: plateUI,
+  }
+);
 
 export const RichTextEditor = ({ value, onChange }) => {
   return (
     <Plate<MyValue>
       editableProps={editableProps}
       initialValue={value}
+      plugins={plugins}
       onChange={(value) => {
         onChange(JSON.stringify(value));
       }}
