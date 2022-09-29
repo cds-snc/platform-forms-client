f7a4b1f7bbc07b5acb789a6264d912a57f7e2628
Tim Arney <tim@line37.com>
2022-09-29 14:51:20 -0400

    fix TS error

--- components/form-builder/plate-editor/helpers/markdown.ts
+++ components/form-builder/plate-editor/helpers/markdown.ts
@@ -1,57 +1,38 @@
-import {
-  ELEMENT_PARAGRAPH,
-  ELEMENT_BLOCKQUOTE,
-  ELEMENT_LINK,
-  MARK_CODE,
-  MARK_ITALIC,
-  MARK_BOLD,
-  MARK_STRIKETHROUGH,
-  ELEMENT_IMAGE,
-  ELEMENT_CODE_BLOCK,
-  ELEMENT_HR,
-  ELEMENT_UL,
-  ELEMENT_OL,
-  ELEMENT_LI,
-  ELEMENT_H1,
-  ELEMENT_H2,
-  ELEMENT_H3,
-  ELEMENT_H4,
-  ELEMENT_H5,
-  ELEMENT_H6,
-} from "@udecode/plate";
 import serialize from "./serialize";
+import { BlockType, LeafType } from "remark-slate";
 
-export const serializeMd = (value) => {
-  return serialize(
-    { children: value },
-    {
-      nodeTypes: {
-        paragraph: ELEMENT_PARAGRAPH,
-        block_quote: ELEMENT_BLOCKQUOTE,
-        link: ELEMENT_LINK,
-        inline_code_mark: MARK_CODE,
-        emphasis_mark: MARK_ITALIC,
-        strong_mark: MARK_BOLD,
-        delete_mark: MARK_STRIKETHROUGH,
-        // NOTE: underline, subscript superscript not yet supported by remark-slate
-        // underline: MARK_UNDERLINE,
-        // subscript: MARK_SUBSCRIPT,
-        // superscript: MARK_SUPERSCRIPT,
-        image: ELEMENT_IMAGE,
-        code_block: ELEMENT_CODE_BLOCK,
-        thematic_break: ELEMENT_HR,
-        ul_list: ELEMENT_UL,
-        ol_list: ELEMENT_OL,
-        listItem: ELEMENT_LI,
-        heading: {
-          1: ELEMENT_H1,
-          2: ELEMENT_H2,
-          4: ELEMENT_H4,
-          3: ELEMENT_H3,
-          5: ELEMENT_H5,
-          6: ELEMENT_H6,
-        },
+export const serializeMd = (value: (BlockType | LeafType)[]) => {
+  if (value && value.length && value[0] && typeof value[0] !== "undefined") {
+    return;
+  }
+
+  return serialize(value[0], {
+    nodeTypes: {
+      paragraph: "paragraph",
+      block_quote: "block_quote",
+      link: "link",
+      inline_code_mark: "code",
+      emphasis_mark: "italic",
+      strong_mark: "bold",
+      delete_mark: "strikeThrough",
+      // NOTE: underline, subscript superscript not yet supported by remark-slate
+      // underline: MARK_UNDERLINE,
+      // subscript: MARK_SUBSCRIPT,
+      // superscript: MARK_SUPERSCRIPT,
+      image: "image",
+      code_block: "code_block",
+      thematic_break: "thematic_break",
+      ul_list: "ul_list",
+      ol_list: "ol_list",
+      listItem: "list_item",
+      heading: {
+        1: "heading_one",
+        2: "heading_two",
+        3: "heading_three",
+        4: "heading_four",
+        5: "heading_five",
+        6: "heading_six",
       },
-    }
-  );
+    },
+  });
 };
