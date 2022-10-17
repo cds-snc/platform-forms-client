ef4003b10b33f0fdc344813ffbedeadbd9fca9ee
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-10-17 14:51:59 -0400

    stub out some of it

--- components/form-builder/translate/DownloadCSV.tsx
+++ components/form-builder/translate/DownloadCSV.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import useTemplateStore from "../store/useTemplateStore";
 
 const CopyFormContentButton = styled.button`
   border: 2px solid #26374a;
@@ -10,5 +11,13 @@ const CopyFormContentButton = styled.button`
 `;
 
 export const DownloadCSV = () => {
-  return <CopyFormContentButton>Download .csv</CopyFormContentButton>;
+  const { form } = useTemplateStore();
+
+  const generateCSV = () => {
+    const data = [["description", "english", "french"]];
+
+    data.push(["Form introduction - Title", form.titleEn, form.titleFr]);
+  };
+
+  return <CopyFormContentButton onClick={generateCSV}>Download .csv</CopyFormContentButton>;
 };
