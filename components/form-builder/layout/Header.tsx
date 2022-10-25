d3c3a45fba01a0b9a63e9a4ce5936dcd92f9d94d
Tim Arney <tim@line37.com>
2022-10-25 15:18:09 -0400

    add save button to header

--- components/form-builder/layout/Header.tsx
+++ components/form-builder/layout/Header.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import LanguageToggle from "../../globals/LanguageToggle";
+import { DownloadFileButton } from "./DownloadFileButton";
 import LoginMenu from "../../auth/LoginMenu";
 import { useSession } from "next-auth/react";
+import styled from "styled-components";
+import useNavigationStore from "../store/useNavigationStore";
+
+const StyledH2 = styled.h2`
+  display: inline-block;
+  margin-right: 40px;
+`;
+
 export const Header = () => {
   const { status } = useSession();
+  const { currentTab } = useNavigationStore();
   return (
     <div className="border-b-3 border-blue-dark mt-10 mb-10">
       <div className="md:mx-8 lg:mx-16 xl:mx-32 xxl:mx-48 mx-64">
         <div className="flex" style={{ justifyContent: "space-between" }}>
           <div className="">
-            <h2>GC Forms</h2>
+            <StyledH2>GC Forms</StyledH2>
+            {currentTab !== "start" && <DownloadFileButton />}
           </div>
           <div className="inline-flex">
             {<LoginMenu isAuthenticated={status === "authenticated"} />}
