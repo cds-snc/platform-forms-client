1d7c0624bb85946b00089641f709106d2fba5c3a
Tim Arney <timarney@users.noreply.github.com>
2022-10-25 17:25:03 -0400

    feat: Add per-page layout + use 2-column design for form builder (#1177)
    * add per page layout setup
    
    * update header
    
    * update wrapper
    
    * add footer
    
    * remove title
    
    * Add sub-navigation bar
    
    * Remove translate from sidebar
    
    * move form title field
    
    * Adjust translate title
    
    * adds rich text locked desc
    
    * space between
    
    * add save button to header
    
    * feat: Add left nav menu
    
    Well, the menu was already there, but I added the icons and the CSS
    to make it work.
    I also removed styled-components from the nav, so it's all tailwind, all the time now.
    
    * Make sure the form title fits in the form panel
    
    * fix: use Canada.ca max page width of 1170px
    
    * allow null
    
    * fix: Add wet container max-widths to our new layout
    
    * fix: Keep left nav items selected when subnav items are clicked
    
    Co-authored-by: Dave Samojlenko <dave.samojlenko@cds-snc.ca>
    Co-authored-by: Paul Craig <paul@pcraig3.ca>

---
+++ components/form-builder/layout/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import LanguageToggle from "../../globals/LanguageToggle";
+import { DownloadFileButton } from "./DownloadFileButton";
+import LoginMenu from "../../auth/LoginMenu";
+import { useSession } from "next-auth/react";
+import styled from "styled-components";
+import useNavigationStore from "../store/useNavigationStore";
+
+const StyledH2 = styled.h2`
+  display: inline-block;
+  margin-right: 40px;
+`;
+
+export const Header = () => {
+  const { status } = useSession();
+  const { currentTab } = useNavigationStore();
+  return (
+    <div className="border-b-3 border-blue-dark mt-10 mb-10">
+      <div className="container--wet">
+        <div className="flex" style={{ justifyContent: "space-between" }}>
+          <div className="">
+            <StyledH2>GC Forms</StyledH2>
+            {currentTab !== "start" && <DownloadFileButton />}
+          </div>
+          <div className="inline-flex">
+            {<LoginMenu isAuthenticated={status === "authenticated"} />}
+            {<LanguageToggle />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
