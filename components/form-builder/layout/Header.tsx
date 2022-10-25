b3dfe7a143bfc5756c81dd7fa21153ff6675eb5f
Tim Arney <tim@line37.com>
2022-10-25 11:37:50 -0400

    update header

--- components/form-builder/layout/Header.tsx
+++ components/form-builder/layout/Header.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import LanguageToggle from "../../globals/LanguageToggle";
+import LoginMenu from "../../auth/LoginMenu";
+import { useSession } from "next-auth/react";
 export const Header = () => {
+  const { status } = useSession();
   return (
-    <div className="grid grid-cols-12 gap-4">
-      <div className="col-start-2 col-span-full">
+    <div className="grid grid-cols-12 gap-4 mt-10 mb-10 border-b-3 border-blue-dark">
+      <div className="col-start-2 col-end-4">
         <h2>GC Forms</h2>
+      </div>
+      <div className="inline-flex col-start-10 col-end-12">
+        {<LoginMenu isAuthenticated={status === "authenticated"} />}
+        {<LanguageToggle />}
       </div>
     </div>
   );
