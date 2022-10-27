5bdef993e89fac288786c1a285ced60fe5905c87
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-10-27 08:36:35 -0400

    wip

--- components/form-builder/layout/Layout.tsx
+++ components/form-builder/layout/Layout.tsx
@@ -6,14 +6,15 @@ import useTemplateStore from "../store/useTemplateStore";
 import useNavigationStore from "../store/useNavigationStore";
 import { LeftNavigation } from "./LeftNavigation";
 
-import { Language, LocalizedFormProperties } from "../types";
+import { Language, LocalizedFormProperties, TemplateSchema } from "../types";
 import { Save } from "./Save";
 import { Start } from "./Start";
 import { Preview } from "./Preview";
 import { Translate } from "../translate/Translate";
 import { EditNavigation } from "./EditNavigation";
 import { PreviewNavigation } from "./PreviewNavigation";
-
+import { FormRecord } from "@lib/types";
+import { sortByLayout } from "../util";
 const StyledHeader = styled.h1`
   border-bottom: none;
   margin-bottom: 2rem;
@@ -24,8 +25,13 @@ const StyledPreviewWrapper = styled.div`
   padding: 20px;
 `;
 
-export const Layout = () => {
-  const { localizeField, form, setLang } = useTemplateStore();
+type LayoutProps = {
+  tab: string;
+  initialForm: FormRecord | null;
+};
+
+export const Layout = ({ tab, initialForm }: LayoutProps) => {
+  const { localizeField, form, setLang, importTemplate } = useTemplateStore();
   const { currentTab, setTab } = useNavigationStore();
   const { t, i18n } = useTranslation("form-builder");
   const locale = i18n.language as Language;
@@ -41,12 +47,30 @@ export const Layout = () => {
     setLang(locale);
   }, [locale]);
 
+  // Refactor later to pass tab prop as intial state to useNavigationStore();
+  useEffect(() => {
+    setTab(tab);
+  }, [tab]);
+
+  useEffect(() => {
+    if (initialForm) {
+      const typeRefactoredForm = { ...initialForm } as unknown as TemplateSchema;
+      typeRefactoredForm.form.endPage = {
+        descriptionEn: "",
+        descriptionFr: "",
+      };
+      // typeRefactoredForm.form.elements = sortByLayout(typeRefactoredForm.form);
+      importTemplate(typeRefactoredForm);
+    }
+  }, [initialForm]);
+
   /* eslint-disable */
   return (
     <main className="container--wet">
       <div className="grid grid-cols-12 gap-4">
-
-        {currentTab !== "start" && <LeftNavigation currentTab={currentTab} handleClick={handleClick} />}
+        {currentTab !== "start" && (
+          <LeftNavigation currentTab={currentTab} handleClick={handleClick} />
+        )}
 
         {currentTab === "start" && (
           <div className="col-span-12">
@@ -56,14 +80,14 @@ export const Layout = () => {
 
         {currentTab === "create" && (
           <div className="col-start-4 col-span-9">
-          <EditNavigation currentTab={currentTab} handleClick={handleClick} />
+            <EditNavigation currentTab={currentTab} handleClick={handleClick} />
             <ElementPanel />
           </div>
         )}
 
         {currentTab === "preview" && (
           <div className="col-start-4 col-span-9">
-          <PreviewNavigation currentTab={currentTab} handleClick={handleClick} />
+            <PreviewNavigation currentTab={currentTab} handleClick={handleClick} />
             <StyledPreviewWrapper>
               <h1>{form[localizeField(LocalizedFormProperties.TITLE)]}</h1>
               <Preview />
@@ -73,18 +97,17 @@ export const Layout = () => {
 
         {currentTab === "translate" && (
           <div className="col-start-4 col-span-9">
-          <EditNavigation currentTab={currentTab} handleClick={handleClick} />
+            <EditNavigation currentTab={currentTab} handleClick={handleClick} />
             <Translate />
           </div>
         )}
 
         {currentTab === "save" && (
           <div className="col-start-4 col-span-9">
-          <StyledHeader>{t("saveH1")}</StyledHeader>
+            <StyledHeader>{t("saveH1")}</StyledHeader>
             <Save />
           </div>
         )}
-
       </div>
     </main>
   );
---
+++ pages/admin/form-builder/[[...params]].tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { requireAuthentication } from "@lib/auth";
+import { Layout } from "../../../components/form-builder/layout/Layout";
+import { Header } from "../../../components/form-builder/layout/Header";
+import { UserRole } from "@prisma/client";
+import { NextPageWithLayout } from "../../_app";
+import Footer from "../../../components/globals/Footer";
+import { getTemplateByID } from "@lib/templates";
+import { FormRecord } from "@lib/types";
+
+type PageProps = {
+  tab: string;
+  initialForm: FormRecord | null;
+};
+
+const Page: NextPageWithLayout<PageProps> = ({ tab, initialForm }: PageProps) => {
+  return <Layout tab={tab} initialForm={initialForm} />;
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <div id="form-builder">
+      <Header />
+      {page}
+      <Footer />
+    </div>
+  );
+};
+
+export const getServerSideProps = requireAuthentication(async (context) => {
+  const [tab = "start", formID = null] = context?.query?.params || [];
+  const FormbuilderParams: { tab: string; initialForm: null | FormRecord } = {
+    tab,
+    initialForm: null,
+  };
+
+  if (formID) {
+    FormbuilderParams.initialForm = await getTemplateByID(formID);
+  }
+
+  return {
+    props: {
+      ...FormbuilderParams,
+      ...(context.locale &&
+        (await serverSideTranslations(context.locale, ["common", "form-builder"]))),
+    },
+  };
+}, UserRole.ADMINISTRATOR);
+
+export default Page;
