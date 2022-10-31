0d0a80abdf5b75df13078c06eff45b6f0bdae952
Tim Arney <tim@line37.com>
2022-10-31 09:57:16 -0400

    add instructions

--- components/form-builder/layout/Preview.tsx
+++ components/form-builder/layout/Preview.tsx
@@ -5,11 +5,14 @@ import { Form } from "../preview/Form";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { getRenderedForm } from "@lib/formBuilder";
+import { usePublish } from "../hooks/usePublish";
+import { useCallback } from "react";
 
 export const Preview = ({ isPreview }: { isPreview: boolean }) => {
   const { getSchema } = useTemplateStore();
   const stringified = getSchema();
-  const { formId } = useNavigationStore();
+  const { formId, setFormId } = useNavigationStore();
+  const { uploadJson } = usePublish(false);
 
   const formRecord = {
     id: formId || "test0form00000id000asdf11",
@@ -22,9 +25,32 @@ export const Preview = ({ isPreview }: { isPreview: boolean }) => {
 
   const currentForm = getRenderedForm(formRecord, language, t);
 
+  const handlePublish = useCallback(async () => {
+    const result = await uploadJson(getSchema(), formId);
+    if (result && result?.error) {
+      return;
+    }
+
+    setFormId(result?.id);
+  }, [setFormId]);
+
+  let instructions = null;
+
+  if (!isPreview) {
+    instructions = (
+      <div>
+        <button onClick={handlePublish}>Save Draft</button>
+      </div>
+    );
+  }
+
   return (
-    <Form formRecord={formRecord} language={language} router={router} t={t} isPreview={isPreview}>
-      {currentForm}
-    </Form>
+    <>
+      <p>{formId}</p>
+      {instructions}
+      <Form formRecord={formRecord} language={language} router={router} t={t} isPreview={isPreview}>
+        {currentForm}
+      </Form>
+    </>
   );
 };
