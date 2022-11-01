6173a4e4f108bb1a1187ae05046935904392c970
Sam <sam.sadasivan@cds-snc.ca>
2022-11-01 11:07:30 -0400

    Updated privacy description

--- components/form-builder/panel/PrivacyDescription.tsx
+++ components/form-builder/panel/PrivacyDescription.tsx
@@ -10,8 +10,10 @@ export const PrivacyDescription = () => {
   return (
     <StyledDiv>
       <p>
-        This privacy statement will appear on your form above the submit button.{" "}
-        <a href="http://canada.ca">Find out more about what should be included.</a>
+        You are responsible for drafting privacy notices that follow the requirements of the{" "}
+        <a href="https://www.tbs-sct.canada.ca/pol/doc-eng.aspx?id=18309">
+          Directive on Privacy Practices
+        </a>
       </p>
     </StyledDiv>
   );
