a9a6790f6e4bfac6f6e2e51407b53e6d33a5726b
Tim Arney <tim@line37.com>
2022-11-07 11:53:03 -0500

    start adding translations

--- components/form-builder/layout/Published.tsx
+++ components/form-builder/layout/Published.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { RocketIcon } from "../icons/RocketIcon";
 import { Button } from "../shared/Button";
+import { useTranslation } from "next-i18next";
 
 export const Published = ({ id }: { id: string }) => {
   const linkEn = `http://localhost:3000/en/id/${id}`;
   const linkFr = `http://localhost:3000/fr/id/${id}`;
+  const { t } = useTranslation("form-builder");
   return (
     <div>
       <div className="p-7 mb-10 flex bg-green-50">
@@ -14,8 +16,8 @@ export const Published = ({ id }: { id: string }) => {
           </div>
         </div>
         <div>
-          <h2 className="mb-1 pb-0">Your form is published!</h2>
-          <p className="mb-5 mt-0">View and share your published form with these links: </p>
+          <h2 className="mb-1 pb-0"> {t("publishedTitle")}</h2>
+          <p className="mb-5 mt-0">{t("publishedViewLinks")}</p>
           <p>
             <strong>English:</strong> <a href={linkEn}>{linkEn}</a>
           </p>
@@ -25,7 +27,7 @@ export const Published = ({ id }: { id: string }) => {
         </div>
       </div>
       <div className="mb-5">
-        <h3 className="mb-1">Published form errors</h3>
+        <h3 className="mb-1">{t("publishedErrors")}</h3>
         <p>
           If you are experiencing any problems with your published form, please{" "}
           <a href="http://example.com">contact support.</a>
@@ -38,7 +40,7 @@ export const Published = ({ id }: { id: string }) => {
         </p>
       </div>
       <div>
-        <Button>Back to My forms</Button>
+        <Button>{t("publishedBack")}</Button>
       </div>
     </div>
   );
