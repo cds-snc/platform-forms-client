9ffaeb808f3ad9715529727de121bd8cce7d9643
Pete <107579368+thiessenp-cds@users.noreply.github.com>
2022-11-08 10:28:25 -0500

    Adds initial work for feature myforms (#1231)
    * Adds initial work for feature myforms
    
    * Fixes from PR comments
    
    * Sets Draft view as default for My Forms
    
    Co-authored-by: Bryan Robitaille <bryan.robitaille@cds-snc.ca>

---
+++ components/myforms/Tabs/Tab.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export interface TabProps {
+  children: React.ReactNode;
+  id: string;
+  tabpanelId: string;
+  url: string;
+  isActive: boolean;
+  callback?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Tab = (props: TabProps): React.ReactElement => {
+  const {
+    children,
+    id,
+    tabpanelId,
+    url,
+    isActive,
+    callback = () => {
+      // Note: shallow=true avoids a getServerSideProps call on component update
+      router.push(url, undefined, { shallow: true });
+    },
+  } = props;
+  const router = useRouter();
+
+  return (
+    <button
+      id={id}
+      className={`gc-button-link ${isActive ? " no-underline hover:underline" : ""}`}
+      role="tab"
+      aria-selected={isActive ? "true" : "false"}
+      aria-controls={tabpanelId}
+      onClick={callback}
+    >
+      {children}
+    </button>
+  );
+};
