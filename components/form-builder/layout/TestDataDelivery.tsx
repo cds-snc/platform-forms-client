e294b00b5661fa1680b9650671d901d74cd21922
Tim Arney <tim@line37.com>
2022-11-09 08:25:06 -0500

    update success

--- components/form-builder/layout/TestDataDelivery.tsx
+++ components/form-builder/layout/TestDataDelivery.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/router";
 import { getRenderedForm } from "@lib/formBuilder";
 import { useNavigationStore } from "../store/useNavigationStore";
 import Link from "next/link";
+import { RocketIcon } from "../icons/RocketIcon";
 
 export const TestDataDelivery = () => {
   const { localizeField, getSchema, id, setId, email } = useTemplateStore((s) => ({
@@ -35,7 +36,7 @@ export const TestDataDelivery = () => {
   const language = i18n.language as string;
   const currentForm = getRenderedForm(formRecord, language, t);
   const [error, setError] = useState(false);
-  const [sent, setSent] = useState<string | null>(null);
+  const [sent, setSent] = useState<string | null>();
 
   const { uploadJson } = usePublish();
 
@@ -92,7 +93,17 @@ export const TestDataDelivery = () => {
       <div className="border-3 border-dashed border-blue-focus p-4 mb-8">
         <h1>{formRecord.form[localizeField(LocalizedFormProperties.TITLE)]}</h1>
         {sent ? (
-          <div>Form ID: {sent} sent --- check your email</div>
+          <div className="p-7 mb-10 flex bg-green-50">
+            <div className="flex">
+              <div className="flex p-7">
+                <RocketIcon className="block self-center" />
+              </div>
+            </div>
+            <div>
+              <h2 className="mb-1 pb-0"> {t("dataDeliveredTitle")}</h2>
+              <p className="mb-5 mt-0">{t("dataDeliveredMessage")}</p>
+            </div>
+          </div>
         ) : (
           <Form
             formRecord={formRecord}
