625a47b6d6804e0d3685b7b764ddb495e8f55b85
Tim Arney <tim@line37.com>
2022-11-10 11:59:24 -0500

    add strings with link

--- components/form-builder/layout/PublishNoAuth.tsx
+++ components/form-builder/layout/PublishNoAuth.tsx
@@ -5,14 +5,22 @@ import Link from "next/link";
 export const PublishNoAuth = () => {
   const { t, i18n } = useTranslation("form-builder");
   const signIn = <Link href={`/${i18n.language}/auth/login`}>{t("signIn")}</Link>;
-  const createIn = <Link href={`/${i18n.language}/auth/login`}>{t("createOne")}</Link>;
+  const createOne = <Link href={`/${i18n.language}/auth/login`}>{t("createOne")}</Link>;
   return (
     <>
       <h1 className="border-0 mb-2">{t("publishNoAuthTitle")}</h1>
       <h3 className="border-0 mb-5">{t("publishNoAuthSubTitle")}</h3>
-      <p className="mb-0">
-        {t("publishNoAuthInstructions", { sign_in: signIn, create_one: createIn })}
-      </p>
+      {i18n.language === "en" ? (
+        <p className="mb-0">
+          To publish your form, {signIn} to your GC Forms account. If you do not have a GC Forms
+          account, {createOne}
+        </p>
+      ) : (
+        <p className="mb-0">
+          FR To publish your form, {signIn} to your GC Forms account. If you do not have a GC Forms
+          account, {createOne}
+        </p>
+      )}
     </>
   );
 };
