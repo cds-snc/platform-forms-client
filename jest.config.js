3f962e8b1add74f6d214d8af223ea8355043611a
Tim Arney <tim@line37.com>
2022-11-14 10:49:06 -0500

    mock local

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   clearMocks: true,
   preset: "ts-jest/presets/js-with-ts",
-  setupFiles: ["<rootDir>/__utils__/jestShim.ts"],
+  setupFiles: ["<rootDir>/__utils__/jestShim.ts", "jest-localstorage-mock"],
   setupFilesAfterEnv: [
     "<rootDir>/__utils__/setupTests.ts",
     "<rootDir>/__utils__/prismaConnector.ts",
