5f21d241450f18bf879244bb83ddd78f9d750b06
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-11-15 15:48:50 -0500

    Fix PublishNoAuth links

--- components/form-builder/layout/PublishNoAuth.tsx
+++ components/form-builder/layout/PublishNoAuth.tsx
@@ -4,17 +4,17 @@ import React from "react";
 
 export const PublishNoAuth = () => {
   const { t, i18n } = useTranslation("form-builder");
-  const signIn = <a href={`/${i18n.language}/auth/login`}>{t("signIn")}</a>;
-  const createOne = <a href={`/${i18n.language}/auth/register`}>{t("createOne")}</a>;
+  const signIn = `/${i18n.language}/auth/login`;
+  const createOne = `/${i18n.language}/auth/register`;
   return (
     <>
       <h1 className="border-0 mb-2">{t("publishNoAuthTitle")}</h1>
       <h3 className="border-0 mb-5">{t("publishNoAuthSubTitle")}</h3>
-      <p className="mb-0">
+      <div className="mb-0">
         <Markdown options={{ forceBlock: true }}>
           {t("publishNoAuthText", { signIn, createOne })}
         </Markdown>
-      </p>
+      </div>
     </>
   );
 };
