37c52c2e6fdc269c69a682fbb980909c2708ff8f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-11-21 16:23:06 -0500

    Set tab index if only one item

--- components/form-builder/panel/PanelActions.tsx
+++ components/form-builder/panel/PanelActions.tsx
@@ -82,11 +82,11 @@ export const PanelActions = ({
   const getTabIndex = (item: string) => {
     if (item === "moveUp" && isFirstItem) return -1;
     if (item === "moveDown" && isLastItem) return -1;
+    if (elements.length === 1 && item === "duplicate") return 0;
 
     if (currentFocusIndex === items.findIndex((i) => i.txt === item)) return 0;
 
     if (currentFocusIndex === 0) {
-      if (isFirstItem && isLastItem && item === "duplicate") return 0;
       if (isFirstItem && item === "moveDown") return 0;
     }
 
