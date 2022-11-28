97a5b4b10e5d3c2ad661ffe74153a9aa23d6c125
Tim Arney <tim@line37.com>
2022-11-28 11:07:58 -0500

    update my forms link

--- components/form-builder/layout/Header.tsx
+++ components/form-builder/layout/Header.tsx
@@ -76,7 +76,7 @@ export const Header = () => {
         <div className="inline-flex gap-4">
           <div className="md:text-small_base text-base font-normal not-italic">
             {ability?.can("view", "FormRecord") && (
-              <Link href="/myforms/start">{t("adminNav.myForms", { ns: "common" })}</Link>
+              <Link href="/myforms/drafts">{t("adminNav.myForms", { ns: "common" })}</Link>
             )}
           </div>
           {<LoginMenu isAuthenticated={status === "authenticated"} />}
