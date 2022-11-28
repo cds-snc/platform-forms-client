abe26ee766d570e28246a1a1c75ed4f221844065
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-11-28 11:53:42 -0500

    Refactor to separate component file

--- components/form-builder/layout/navigation/LeftNavigation.tsx
+++ components/form-builder/layout/navigation/LeftNavigation.tsx
@@ -1,38 +1,8 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React from "react";
 import { useTranslation } from "next-i18next";
 import { DesignIcon, PreviewIcon, ShareIcon, PublishIcon, SaveIcon } from "../../icons";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import Link from "next/link";
-
-const SideNavLink = ({ children, href }: { children: ReactElement; href: string }) => {
-  const { asPath, isReady } = useRouter();
-  const [active, setActive] = useState(false);
-
-  useEffect(() => {
-    // Check if the router fields are updated client-side
-    if (isReady) {
-      const linkPathname = new URL(href as string, location.href).pathname;
-      const activePathname = new URL(asPath, location.href).pathname;
-
-      if (linkPathname === activePathname) {
-        setActive(true);
-      }
-    }
-  }, [asPath, isReady, href, setActive]);
-  return (
-    <Link href={href}>
-      <a
-        href={href}
-        className={`${
-          active ? "font-bold" : ""
-        } group no-underline rounded block xl:w-36 xl:pb-0 xl:pt-2 xl:mb-2 mb-4 -ml-1 pl-1 pr-2 md:pr-0 text-black-default hover:text-blue-hover visited:text-black-default focus:text-white-default focus:bg-blue-hover active:no-underline active:bg-blue-hover active:text-white-default`}
-      >
-        {children}
-      </a>
-    </Link>
-  );
-};
+import { LeftNavLink } from "./LeftNavLink";
 
 export const LeftNavigation = () => {
   const { t } = useTranslation("form-builder");
@@ -43,42 +13,42 @@ export const LeftNavigation = () => {
 
   return (
     <nav className="col-span-3" aria-label={t("navLabelFormBuilder")}>
-      <SideNavLink href="/form-builder/edit">
+      <LeftNavLink href="/form-builder/edit">
         <>
           <DesignIcon className={iconClassname} />
           {t("edit")}
         </>
-      </SideNavLink>
+      </LeftNavLink>
 
-      <SideNavLink href="/form-builder/preview">
+      <LeftNavLink href="/form-builder/preview">
         <>
           <PreviewIcon className={iconClassname} />
           {t("preview")}
         </>
-      </SideNavLink>
+      </LeftNavLink>
 
-      <SideNavLink href="/form-builder/share">
+      <LeftNavLink href="/form-builder/share">
         <>
           <ShareIcon className={iconClassname} />
           {t("share")}
         </>
-      </SideNavLink>
+      </LeftNavLink>
 
       {status !== "authenticated" && (
-        <SideNavLink href="/form-builder/save">
+        <LeftNavLink href="/form-builder/save">
           <>
             <SaveIcon className={iconClassname} />
             {t("save")}
           </>
-        </SideNavLink>
+        </LeftNavLink>
       )}
 
-      <SideNavLink href="/form-builder/publish">
+      <LeftNavLink href="/form-builder/publish">
         <>
           <PublishIcon className={iconClassname} />
           {t("publish")}
         </>
-      </SideNavLink>
+      </LeftNavLink>
     </nav>
   );
 };
