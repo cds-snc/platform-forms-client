472e599f6e6af16c85b95a7d8558347a807f9fed
Tim Arney <tim@line37.com>
2022-11-28 14:05:30 -0500

    update header

--- pages/form-builder/index.tsx
+++ pages/form-builder/index.tsx
@@ -14,6 +14,7 @@ import { AccessControlError, createAbility } from "@lib/privileges";
 
 const Page: NextPageWithLayout<PageProps> = () => {
   const { t } = useTranslation("form-builder");
+
   const title = `${t("gcFormsStart")} — ${t("gcForms")}`;
   return (
     <PageTemplate title={title} leftNav={false}>
