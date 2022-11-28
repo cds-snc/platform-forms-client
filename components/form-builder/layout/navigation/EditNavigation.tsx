6964670448a8398d4b2eb24c73885779135bebf4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-11-28 11:49:30 -0500

    Convert sub navs

--- components/form-builder/layout/navigation/EditNavigation.tsx
+++ components/form-builder/layout/navigation/EditNavigation.tsx
@@ -1,41 +1,14 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
-import { useNavigationStore } from "../../store/useNavigationStore";
-import { useRouter } from "next/router";
+import { SubNavLink } from "./SubNavLink";
 
-export const EditNavigation = ({ currentTab }: { currentTab?: string }) => {
+export const EditNavigation = () => {
   const { t } = useTranslation("form-builder");
-  const router = useRouter();
-
-  const { setTab } = useNavigationStore((s) => ({
-    currentTab: s.currentTab,
-    setTab: s.setTab,
-  }));
 
   return (
     <nav className="mb-8 flex divide-x-2 divide-gray-600" aria-label={t("navLabelEditor")}>
-      <button
-        className={`pr-4 ${
-          currentTab === "create" ? "font-bold" : ""
-        } outline-blue-focus outline-offset-2`}
-        onClick={() => {
-          setTab("create");
-          router.push({ pathname: `/form-builder/edit` });
-        }}
-      >
-        {t("questions")}
-      </button>
-      <button
-        className={`pl-4 ${
-          currentTab === "translate" ? "font-bold" : ""
-        } outline-blue-focus outline-offset-2`}
-        onClick={() => {
-          setTab("translate");
-          router.push({ pathname: `/form-builder/translate` });
-        }}
-      >
-        {t("translate")}
-      </button>
+      <SubNavLink href="/form-builder/edit">{t("questions")}</SubNavLink>
+      <SubNavLink href="/form-builder/translate">{t("translate")}</SubNavLink>
     </nav>
   );
 };
--- components/form-builder/layout/navigation/PreviewNavigation.tsx
+++ components/form-builder/layout/navigation/PreviewNavigation.tsx
@@ -1,58 +1,21 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useNavigationStore } from "@components/form-builder/store";
+import { SubNavLink } from "./SubNavLink";
 
-export const PreviewNavigation = ({ currentTab }: { currentTab?: string }) => {
+export const PreviewNavigation = () => {
   const { t } = useTranslation("form-builder");
-
-  const { setTab } = useNavigationStore((s) => ({
-    currentTab: s.currentTab,
-    setTab: s.setTab,
-  }));
-
-  const router = useRouter();
   const { status } = useSession();
+
   return (
     <nav className="mb-8 flex divide-x-2 divide-gray-600" aria-label={t("navLabelPreview")}>
-      <button
-        className={`pr-4 ${
-          currentTab === "preview" ? "font-bold" : ""
-        } outline-blue-focus outline-offset-2`}
-        onClick={() => {
-          setTab("preview");
-          router.push({ pathname: `/form-builder/preview` });
-        }}
-      >
-        {t("preview")}
-      </button>
+      <SubNavLink href="/form-builder/preview">{t("preview")}</SubNavLink>
       {status === "authenticated" && (
         <>
-          <button
-            className={`pl-4 pr-4 ${
-              currentTab === "test-data-delivery" ? "font-bold" : ""
-            } outline-blue-focus outline-offset-2`}
-            onClick={() => {
-              setTab("test-data-delivery");
-              router.push({ pathname: `/form-builder/test-data-delivery` });
-            }}
-          >
-            {t("testDataDelivery")}
-          </button>
+          <SubNavLink href="/form-builder/test-data-delivery">{t("testDataDelivery")}</SubNavLink>
         </>
       )}
-      <button
-        className={`pl-4 ${
-          currentTab === "settings" ? "font-bold" : ""
-        } outline-blue-focus outline-offset-2`}
-        onClick={() => {
-          setTab("settings");
-          router.push({ pathname: `/form-builder/settings` });
-        }}
-      >
-        {t("settings")}
-      </button>
+      <SubNavLink href="/form-builder/settings">{t("settings")}</SubNavLink>
     </nav>
   );
 };
