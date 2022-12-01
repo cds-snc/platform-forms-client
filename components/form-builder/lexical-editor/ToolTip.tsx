d09df17eec3bed74513209e55a5d7e2e219c01d6
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-12-01 11:40:15 -0500

    add a11y label to tooltips

--- components/form-builder/lexical-editor/ToolTip.tsx
+++ components/form-builder/lexical-editor/ToolTip.tsx
@@ -1,13 +1,20 @@
+import _ from "lodash";
 import React from "react";
 
 export const ToolTip = ({ children, text }: { children: React.ReactElement; text: string }) => {
+  const id = _.uniqueId("tooltip-");
+
   const Children = () =>
-    React.cloneElement(children, { className: children.props.className + " peer" });
+    React.cloneElement(children, {
+      className: children.props.className + " peer",
+      "aria-labelledby": id,
+    });
 
   return (
     <span className="relative">
       <Children />
       <span
+        id={id}
         className={`invisible whitespace-nowrap peer-hover:visible peer-focus:visible bg-gray-800 text-white rounded absolute p-1 text-sm -top-10 w-36 text-center -left-14 after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700`}
       >
         {text}
