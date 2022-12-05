09a190d33af817314a2f087e4027f201d8e66757
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-12-05 13:16:25 -0500

    Only show Preview menu when logged in (#1410)

--- pages/form-builder/preview/test-data-delivery.tsx
+++ pages/form-builder/preview/test-data-delivery.tsx
@@ -9,12 +9,18 @@ import {
   PageTemplate,
   Template,
 } from "@components/form-builder/app";
+import { useSession } from "next-auth/react";
 
 const Page: NextPageWithLayout<PageProps> = () => {
   const { t } = useTranslation("form-builder");
   const title = `${t("gcFormsTest")} — ${t("gcForms")}`;
+  const { status } = useSession();
+
   return (
-    <PageTemplate title={title} navigation={<PreviewNavigation />}>
+    <PageTemplate
+      title={title}
+      navigation={status === "authenticated" ? <PreviewNavigation /> : undefined}
+    >
       <TestDataDelivery />
     </PageTemplate>
   );
