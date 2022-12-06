0abdeb4e07c4be694084489efbd815c273a1eaa1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-12-06 11:43:20 -0500

    conditionally show menu part

--- components/form-builder/app/navigation/PreviewNavigation.tsx
+++ components/form-builder/app/navigation/PreviewNavigation.tsx
@@ -4,7 +4,7 @@ import { SubNavLink } from "./SubNavLink";
 import { useTemplateStore } from "@components/form-builder/store";
 import { Language } from "@components/form-builder/types";
 import { useActivePathname } from "@components/form-builder/hooks";
-import e from "cors";
+import { useSession } from "next-auth/react";
 
 export const PreviewNavigation = () => {
   const { t } = useTranslation("form-builder");
@@ -13,6 +13,7 @@ export const PreviewNavigation = () => {
     translationLanguagePriority: s.translationLanguagePriority,
   }));
   const { activePathname } = useActivePathname();
+  const { status } = useSession();
 
   const switchLang = (lang: Language) => {
     setTranslationLanguagePriority(lang);
@@ -20,10 +21,12 @@ export const PreviewNavigation = () => {
 
   return (
     <div className="relative">
-      <nav className="mb-8 flex divide-x-2 divide-gray-600" aria-label={t("navLabelPreview")}>
-        <SubNavLink href="/form-builder/preview">{t("preview")}</SubNavLink>
-        <SubNavLink href="/form-builder/preview/test-data-delivery">{t("test")}</SubNavLink>
-      </nav>
+      {status === "authenticated" && (
+        <nav className="mb-8 flex divide-x-2 divide-gray-600" aria-label={t("navLabelPreview")}>
+          <SubNavLink href="/form-builder/preview">{t("preview")}</SubNavLink>
+          <SubNavLink href="/form-builder/preview/test-data-delivery">{t("test")}</SubNavLink>
+        </nav>
+      )}
       {activePathname.endsWith("/preview") && (
         <div className="absolute right-0 top-0 mt-1">
           <label htmlFor="lang" className="font-bold">
