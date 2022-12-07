c7de9ee9756945210819ab375f929d3abda3eb84
Tim Arney <tim@line37.com>
2022-12-07 15:11:58 -0500

    use tailwind vs sc

--- components/form-builder/app/edit/PrivacyDescription.tsx
+++ components/form-builder/app/edit/PrivacyDescription.tsx
@@ -1,18 +1,12 @@
 import Markdown from "markdown-to-jsx";
 import { useTranslation } from "next-i18next";
 import React from "react";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-  font-size: 1rem;
-  margin-bottom: 20px;
-`;
 
 export const PrivacyDescription = () => {
   const { t } = useTranslation("form-builder");
   return (
-    <StyledDiv>
+    <div className="mb-8 text-[1rem]">
       <Markdown options={{ forceBlock: true }}>{t("privacyNoticeDescription")}</Markdown>
-    </StyledDiv>
+    </div>
   );
 };
