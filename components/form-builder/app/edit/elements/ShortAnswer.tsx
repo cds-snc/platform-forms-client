42080566481a1e38f377f1078c77f050a52d2b5b
Tim Arney <tim@line37.com>
2022-12-09 07:51:36 -0500

    wrap text hint

--- components/form-builder/app/edit/elements/ShortAnswer.tsx
+++ components/form-builder/app/edit/elements/ShortAnswer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ShortAnswer = ({ children, ...props }: { children: string }) => {
   return (
-    <div {...props} className="text-hint">
+    <div {...props} className="mt-5 border-bottom-1 border-dotted border-gray-400">
       {children}
     </div>
   );
