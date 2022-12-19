cf6819c779f1bc53196a371c75f6c30e18227d06
Tim Arney <tim@line37.com>
2022-12-19 15:44:06 -0500

    test updates

--- components/form-builder/app/edit/elements/tests/Options.test.js
+++ components/form-builder/app/edit/elements/tests/Options.test.js
@@ -9,36 +9,27 @@ describe("Options", () => {
   it("renders with props and test content", async () => {
     const user = userEvent.setup();
 
-    const item = {
-      id: 1,
-      index: 1,
-      type: "radio",
-      properties: {
-        titleEn: "question 1",
-        titleFr: "question 1 fr",
-        choices: [],
-        validation: { required: false },
-        descriptionEn: "description 1 en",
-        descriptionFr: "description 1 fr",
-      },
-    };
+    const item = { id: 1, index: 0, ...store.form.elements[0] };
 
     const rendered = await withProviders(store, <Options item={item} />);
-    const button = rendered.container.querySelector("#add-option-1");
+
+    const button = rendered.container.querySelector("#add-option-0");
     expect(button).toHaveTextContent("addOption");
 
     await user.click(button);
 
     const option1 = rendered.getByPlaceholderText("option 1");
-    expect(option1).toHaveAttribute("id", "option--1--1");
+    expect(option1).toHaveAttribute("id", "option--0--1");
+    expect(option1).toHaveAttribute("value", "q1 choice 1");
 
     const option2 = rendered.getByPlaceholderText("option 2");
-    expect(option2).toHaveAttribute("id", "option--1--2");
+    expect(option2).toHaveAttribute("id", "option--0--2");
+    expect(option2).toHaveAttribute("value", "q1 choice 2");
 
-    expect(rendered.container.querySelectorAll("input")).toHaveLength(2);
+    expect(rendered.container.querySelectorAll("input")).toHaveLength(3);
 
-    const remove1 = rendered.container.querySelector("#remove--1--1");
+    const remove1 = rendered.container.querySelector("#remove--0--1");
     await user.click(remove1);
-    expect(rendered.container.querySelectorAll("input")).toHaveLength(1);
+    expect(rendered.container.querySelectorAll("input")).toHaveLength(2);
   });
 });
