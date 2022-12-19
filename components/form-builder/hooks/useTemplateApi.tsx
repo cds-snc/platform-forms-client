db5e3feb4618938749e82a38f33f8e63aa661409
Tim Arney <timarney@users.noreply.github.com>
2022-12-19 12:25:07 -0500

    Initial Template api provider (#1445)
    * initial setup
    
    * update save button
    
    * add throw
    
    Co-authored-by: Dave Samojlenko <dave.samojlenko@cds-snc.ca>

---
+++ components/form-builder/hooks/useTemplateApi.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useState, useContext } from "react";
+import { useTemplateStore } from "../store";
+import { usePublish } from "../hooks";
+import { useTranslation } from "next-i18next";
+import { logMessage } from "@lib/logger";
+
+interface TemplateApiType {
+  error: string | null;
+  saveForm: () => Promise<string | false>;
+}
+
+const defaultTemplateApi: TemplateApiType = {
+  error: null,
+  saveForm: async () => new Promise((resolve) => resolve(false)),
+};
+
+const TemplateApiContext = createContext<TemplateApiType>(defaultTemplateApi);
+
+export function TemplateApiProvider({ children }: { children: React.ReactNode }) {
+  const { t } = useTranslation(["form-builder"]);
+  const [error, setError] = useState<string | null>(null);
+  const { getSchema, id } = useTemplateStore((s) => ({
+    id: s.id,
+    getSchema: s.getSchema,
+  }));
+
+  const { uploadJson } = usePublish();
+
+  const saveForm = async () => {
+    try {
+      const schema = JSON.parse(getSchema());
+      delete schema.id;
+      delete schema.isPublished;
+
+      const result = await uploadJson(JSON.stringify(schema), id);
+
+      if (result && result?.error) {
+        throw result?.error as Error;
+      }
+
+      setError(null);
+      return result?.id;
+    } catch (err) {
+      logMessage.error(err as Error);
+      setError(t("errorSaving"));
+      return false;
+    }
+  };
+
+  return (
+    <TemplateApiContext.Provider value={{ error, saveForm }}>
+      {children}
+    </TemplateApiContext.Provider>
+  );
+}
+
+export const useTemplateApi = () => useContext(TemplateApiContext);
