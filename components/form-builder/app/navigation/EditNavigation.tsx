fd772b342466a6ee07e9c582df7a41ece8eab23a
Peter Thiessen <peter.thiessen@cds-snc.ca>
2022-12-20 13:37:57 -0500

    Updates main nav and form-builder language switcher

--- components/form-builder/app/navigation/EditNavigation.tsx
+++ components/form-builder/app/navigation/EditNavigation.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { SubNavLink } from "./SubNavLink";
-import { useTemplateStore } from "@components/form-builder/store";
 import { useActivePathname } from "@components/form-builder/hooks";
-import { ToggleLeft, ToggleRight } from "@components/form-builder/icons";
+import { LangSwitcher } from "../shared/LangSwitcher";
 
 export const EditNavigation = () => {
   const { t } = useTranslation("form-builder");
-  const { toggleTranslationLanguagePriority, translationLanguagePriority } = useTemplateStore(
-    (s) => ({
-      translationLanguagePriority: s.translationLanguagePriority,
-      toggleTranslationLanguagePriority: s.toggleTranslationLanguagePriority,
-    })
-  );
   const { activePathname } = useActivePathname();
-
-  const switchLang = () => {
-    toggleTranslationLanguagePriority();
-  };
 
   return (
     <div className="relative flex">
@@ -26,45 +15,8 @@ export const EditNavigation = () => {
         <SubNavLink href="/form-builder/edit/translate">{t("translate")}</SubNavLink>
       </nav>
       {activePathname.endsWith("/edit") && (
-        <div className="absolute right-0 mr-24 top-0 flex items-baseline">
-          <span id="switch-heading" className="font-bold text-sm mr-1" aria-hidden="true">
-            {t("editingIn")}{" "}
-          </span>
-          <div
-            role="button spinbutton"
-            tabIndex={0}
-            aria-activedescendant={
-              translationLanguagePriority === "en" ? "switch-english" : "switch-french"
-            }
-            onClick={() => switchLang()}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" || e.key === " ") {
-                switchLang();
-              }
-            }}
-          >
-            <span
-              id="switch-english"
-              className="text-sm"
-              aria-label={`${t("editingIn")} ${t("English")}`}
-            >
-              {t("English")}{" "}
-            </span>
-            {translationLanguagePriority === "en" && (
-              <ToggleLeft className="inline-block w-12 fill-violet-300" />
-            )}
-            {translationLanguagePriority === "fr" && (
-              <ToggleRight className="inline-block w-12 fill-fuchsia-300" />
-            )}
-            <span
-              id="switch-french"
-              className="text-sm"
-              aria-label={`${t("editingIn")} ${t("French")}`}
-            >
-              {" "}
-              {t("French")}
-            </span>
-          </div>
+        <div className="absolute right-0 mr-24 top-0">
+          <LangSwitcher descriptionLangKey="editingIn" />
         </div>
       )}
     </div>
--- components/form-builder/app/navigation/PreviewNavigation.tsx
+++ components/form-builder/app/navigation/PreviewNavigation.tsx
@@ -1,62 +1,11 @@
 import React from "react";
-import { useTranslation } from "next-i18next";
-import { useTemplateStore } from "@components/form-builder/store";
-import { ToggleLeft, ToggleRight } from "@components/form-builder/icons";
+import { LangSwitcher } from "../shared/LangSwitcher";
 
 export const PreviewNavigation = () => {
-  const { t } = useTranslation("form-builder");
-  const { toggleTranslationLanguagePriority, translationLanguagePriority } = useTemplateStore(
-    (s) => ({
-      translationLanguagePriority: s.translationLanguagePriority,
-      toggleTranslationLanguagePriority: s.toggleTranslationLanguagePriority,
-    })
-  );
-
-  const switchLang = () => {
-    toggleTranslationLanguagePriority();
-  };
-
   return (
     <div className="relative">
-      <div className="absolute right-0 top-0 flex items-baseline">
-        <span id="switch-heading" className="font-bold text-sm mr-1" aria-hidden="true">
-          {t("previewingIn")}
-        </span>
-        <div
-          role="button spinbutton"
-          tabIndex={0}
-          aria-activedescendant={
-            translationLanguagePriority === "en" ? "switch-english" : "switch-french"
-          }
-          onClick={() => switchLang()}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === " ") {
-              switchLang();
-            }
-          }}
-        >
-          <span
-            id="switch-english"
-            className="text-sm"
-            aria-label={`${t("previewingIn")} ${t("English")}`}
-          >
-            {t("English")}{" "}
-          </span>
-          {translationLanguagePriority === "en" && (
-            <ToggleLeft className="inline-block w-12 fill-violet-300" />
-          )}
-          {translationLanguagePriority === "fr" && (
-            <ToggleRight className="inline-block w-12 fill-fuchsia-300" />
-          )}
-          <span
-            id="switch-french"
-            className="text-sm"
-            aria-label={`${t("previewingIn")} ${t("French")}`}
-          >
-            {" "}
-            {t("French")}
-          </span>
-        </div>
+      <div className="absolute right-0 top-0">
+        <LangSwitcher descriptionLangKey="previewingIn" />
       </div>
     </div>
   );
