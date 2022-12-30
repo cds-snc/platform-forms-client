b66b7136478ebce01ab5d7cc4ed6cea0eb6801c6
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-12-30 09:08:37 -0500

    Get CSV headers dynamically

--- utils/cognitoBackup/retrieveUsers.ts
+++ utils/cognitoBackup/retrieveUsers.ts
@@ -2,6 +2,7 @@ import {
   CognitoIdentityProviderClient,
   ListUsersCommand,
   ListUsersCommandOutput,
+  GetCSVHeaderCommand,
   UserType,
 } from "@aws-sdk/client-cognito-identity-provider";
 import { createObjectCsvWriter } from "csv-writer";
@@ -17,35 +18,24 @@ interface UserAttributes {
   [key: string]: string | undefined;
 }
 
-const CSV_HEADERS = [
-  "name",
-  "given_name",
-  "family_name",
-  "middle_name",
-  "nickname",
-  "preferred_username",
-  "profile",
-  "picture",
-  "website",
-  "email",
-  "email_verified",
-  "gender",
-  "birthdate",
-  "zoneinfo",
-  "locale",
-  "phone_number",
-  "phone_number_verified",
-  "address",
-  "updated_at",
-  "cognito:mfa_enabled",
-  "cognito:username",
-];
+/**
+ * Gets the CSV header for the user import job
+ */
+const getCSVHeader = async (
+  cognitoClient: CognitoIdentityProviderClient
+): Promise<string[] | undefined> => {
+  const params = { UserPoolId: USER_POOL_ID };
+  console.log(`Getting CSV Header: ${JSON.stringify(params)}`);
+  const response = await cognitoClient.send(new GetCSVHeaderCommand(params));
 
-async function saveUsers(users: Array<UserType>) {
-  const userAttributes = Object.keys(users[0]);
+  console.log(`Got CSV Headers: ${JSON.stringify(response)}`);
+  return response.CSVHeader;
+};
+
+async function saveUsers(users: Array<UserType>, csvHeaders: string[]) {
   const csvWriter = createObjectCsvWriter({
     path: `user_pool_${USER_POOL_ID}_users.csv`,
-    header: CSV_HEADERS.map((header) => ({ id: header, title: header })),
+    header: csvHeaders.map((header) => ({ id: header, title: header })),
   });
 
   await csvWriter.writeRecords(
@@ -81,6 +71,9 @@ const main = async () => {
     region: COGNITO_REGION,
   });
 
+  const csvHeaders = await getCSVHeader(cognitoClient);
+  if (typeof csvHeaders === "undefined") throw new Error("Could not retriveve CSV Headers");
+
   let paginationToken = undefined;
   let paginationComplete = false;
   let paginationRound = 0;
@@ -103,7 +96,7 @@ const main = async () => {
 
     if (data.Users) {
       console.log(`Saving ${data.Users.length} users in downloaded batch ${paginationRound}`);
-      saveUsers(data.Users);
+      saveUsers(data.Users, csvHeaders);
     }
   }
 };
