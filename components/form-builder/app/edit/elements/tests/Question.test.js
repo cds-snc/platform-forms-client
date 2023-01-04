a3d714fbc05d8e38b9b907bb05ee0a7c94527bc5
Tim Arney <tim@line37.com>
2023-01-04 13:59:27 -0500

    add test

--- components/form-builder/app/edit/elements/tests/Question.test.js
+++ components/form-builder/app/edit/elements/tests/Question.test.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { cleanup, render, act } from "@testing-library/react";
+import { cleanup, render, act, waitFor } from "@testing-library/react";
 import { Question } from "../question/Question";
 import { defaultStore as store, Providers } from "@formbuilder/test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("Question", () => {
   afterEach(() => {
@@ -66,6 +67,40 @@ describe("Question", () => {
     // > especially if there's no visual indication of the async task completing.
     await act(async () => {
       await promise;
+    });
+  });
+
+  it("Calls updater function", async () => {
+    const mockCallback = jest.fn((e) => e);
+    const user = userEvent.setup();
+
+    const item = {
+      id: 1,
+      index: 0,
+      type: "date",
+      properties: {
+        titleEn: "date input",
+        titleFr: "",
+        choices: [],
+        validation: { required: false },
+        descriptionEn: "",
+        descriptionFr: "",
+      },
+    };
+
+    const rendered = render(
+      <Providers form={store.form}>
+        <Question item={item} onQuestionChange={mockCallback} />
+      </Providers>
+    );
+
+    const question = rendered.container.querySelector("#item0");
+    await user.type(question, "!!!");
+
+    expect(question).toHaveValue("date input!!!");
+
+    await waitFor(() => {
+      expect(mockCallback).toHaveBeenCalled();
     });
   });
 });
