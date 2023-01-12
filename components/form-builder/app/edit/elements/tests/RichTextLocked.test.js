f6cf695d115b284edea2c38261181768db7553da
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-01-12 11:13:45 -0500

    Remove unused bits

--- components/form-builder/app/edit/elements/tests/RichTextLocked.test.js
+++ components/form-builder/app/edit/elements/tests/RichTextLocked.test.js
@@ -7,8 +7,6 @@ describe("RichTextLocked", () => {
   afterEach(cleanup);
   it("renders rich text editor in a locked panel", async () => {
     let rendered = await withProviders(store, <RichTextLocked />);
-    // const editor = rendered.getByRole("textbox");
-    // expect(editor.innerHTML).toContain("Rich text locked");
 
     const locked = rendered.getByTestId("locked-item");
     expect(locked).toBeInTheDocument();
