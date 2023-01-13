3535dabb8632a78f6450f35ca28c6c73147f14e8
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-01-13 09:23:47 -0500

    Set reactStrict back to true

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  reactStrictMode: false,
+  reactStrictMode: true,
   poweredByHeader: false,
   swcMinify: true,
   compiler: {
