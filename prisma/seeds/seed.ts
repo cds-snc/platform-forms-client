774e1dc247a991949c2ec7f4398a5d878a9d2879
Clément Janin <clement.janin@cds-snc.ca>
2023-01-18 14:42:58 -0500

    feat: add code to migrate old Templates

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -3,6 +3,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { parse } from "ts-command-line-args";
 import seedTemplates from "./fixtures/templates";
 import seedPrivileges from "./fixtures/privileges";
+import { AnyObject } from "@lib/types";
 
 const prisma = new PrismaClient();
 
@@ -74,6 +75,74 @@ async function publishingStatusMigration() {
   console.log(`${templatesToMigrate.length} were migrated for Publishing Status`);
 }
 
+async function templateSchemaMigration() {
+  const templates = await prisma.template.findMany({
+    select: {
+      id: true,
+      jsonConfig: true,
+    },
+  });
+
+  const templatesToMigrate = templates
+    .filter((template) => (template.jsonConfig as Record<string, unknown>).submission !== undefined)
+    .map((template) => {
+      const formProperties = template.jsonConfig as AnyObject;
+
+      const newJsonConfiguration = {
+        titleEn: formProperties.form.titleEn ?? "",
+        titleFr: formProperties.form.titleFr ?? "",
+        introduction: {
+          descriptionEn: formProperties.form.introduction.descriptionEn ?? "",
+          descriptionFr: formProperties.form.introduction.descriptionFr ?? "",
+        },
+        privacyPolicy: {
+          descriptionEn: formProperties.form.privacyPolicy.descriptionEn ?? "",
+          descriptionFr: formProperties.form.privacyPolicy.descriptionFr ?? "",
+        },
+        confirmation: {
+          descriptionEn: formProperties.form.endPage.descriptionEn ?? "",
+          descriptionFr: formProperties.form.endPage.descriptionFr ?? "",
+          referrerUrlEn: formProperties.form.endPage.referrerUrlEn ?? "",
+          referrerUrlFr: formProperties.form.endPage.referrerUrlFr ?? "",
+        },
+        layout: formProperties.form.layout,
+        elements: formProperties.form.elements,
+      };
+
+      const name = formProperties.internalTitleEn ?? "";
+
+      const deliveryOption = {
+        emailAddress: formProperties.submission.email ?? "",
+        emailSubjectEn: formProperties.form.emailSubjectEn ?? "",
+        emailSubjectFr: formProperties.form.emailSubjectFr ?? "",
+      };
+
+      const securityAttribute = formProperties.form.securityAttribute ?? "Unclassified";
+
+      return prisma.template.update({
+        where: {
+          id: template.id,
+        },
+        data: {
+          name: name,
+          jsonConfig: newJsonConfiguration as Prisma.JsonObject,
+          deliveryOption: {
+            create: {
+              emailAddress: deliveryOption.emailAddress as string,
+              emailSubjectEn: deliveryOption.emailSubjectEn as string,
+              emailSubjectFr: deliveryOption.emailSubjectFr as string,
+            },
+          },
+          securityAttribute: securityAttribute as string,
+        },
+      });
+    });
+
+  await Promise.all(templatesToMigrate);
+
+  console.log(`${templatesToMigrate.length} were migrated for new Template schema`);
+}
+
 async function main() {
   const { environment = "production" } = parse<{ environment?: string }>({
     environment: { type: String, optional: true },
@@ -85,6 +154,9 @@ async function main() {
 
   console.log("Running 'publishingStatus' migration");
   await publishingStatusMigration();
+
+  console.log("Running 'templateSchema' migration");
+  await templateSchemaMigration();
 }
 
 main()
