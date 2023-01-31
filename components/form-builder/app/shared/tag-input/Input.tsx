d5b981eb66a25f7db637e0fd30c9cccbe83cf1e2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-01-31 14:47:39 -0500

    Make onBlur and onKeyUp optional

--- components/form-builder/app/shared/tag-input/Input.tsx
+++ components/form-builder/app/shared/tag-input/Input.tsx
@@ -3,8 +3,8 @@ export const Input = ({
   onKeyUp,
   onBlur,
 }: {
-  onKeyUp: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  onBlur: (e: { target: HTMLInputElement }) => void;
+  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onBlur?: (e: { target: HTMLInputElement }) => void;
 }) => {
   return (
     <div className="grow my-[4px] mx-[8px]">
@@ -13,8 +13,8 @@ export const Input = ({
         className="w-full border-none p-1 outline-none"
         type="text"
         name="tag-input"
-        onKeyUp={onKeyUp}
-        onBlur={onBlur}
+        onKeyUp={onKeyUp && onKeyUp}
+        onBlur={onBlur && onBlur}
       />
     </div>
   );
