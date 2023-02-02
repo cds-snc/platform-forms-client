cad04d534d6db39ecce7eadcda4c3d0dc4f0fb64
Tim Arney <tim@line37.com>
2023-02-02 06:10:21 -0500

    add select none to dialog desc

--- components/form-builder/app/edit/elements/element-dialog/ElementDialog.tsx
+++ components/form-builder/app/edit/elements/element-dialog/ElementDialog.tsx
@@ -39,7 +39,7 @@ export const ElementDialog = ({
 
   return (
     <Dialog dialogRef={dialog} handleClose={handleClose}>
-      <div className="grid grid-cols-[30%_70%] w-full">
+      <div className="grid grid-cols-[30%_70%] w-full select-none">
         <div>
           <h4 className="mb-5">{t("addElementDialog.questionElement")}</h4>
           <ListBox
