071f664fa72dda04ddd764d81b9ddb8b5ca0aab7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-02-03 16:39:38 -0500

    improved styling

--- components/form-builder/app/shared/tag-input/Tags.tsx
+++ components/form-builder/app/shared/tag-input/Tags.tsx
@@ -14,12 +14,12 @@ export const Tag = ({ tag, onRemove }: TagProps) => {
   return (
     <div
       data-testid="tag"
-      className="m-1 pl-3 pr-3 border-grey-default border-2 rounded-l-xl rounded-r-xl inline-block relative"
+      className="m-1 px-2 border-grey-default border-2 rounded-l-xl rounded-r-xl inline-block"
     >
-      <span className="break-all inline-bock mr-6">
+      <span className="break-all inline-bock">
         {tag}
         <button
-          className="[&_svg]:fill-gray-500 [&_svg]:hover:fill-red-500 [&_svg]:focus:fill-red-500 ml-2 absolute p-[1px] top-1"
+          className="[&_svg]:fill-gray-500 [&_svg]:hover:fill-red-500 [&_svg]:focus:fill-red-500 ml-2 p-[1px] align-middle"
           onClick={() => onRemove(tag)}
           aria-label={label}
         >
