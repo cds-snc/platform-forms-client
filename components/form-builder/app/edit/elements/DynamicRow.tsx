54f6e153455165a75bc93f2f9e6d03a88a830961
Tim Arney <tim@line37.com>
2023-02-08 13:37:29 -0500

    button cleanup

--- components/form-builder/app/edit/elements/DynamicRow.tsx
+++ components/form-builder/app/edit/elements/DynamicRow.tsx
@@ -170,22 +170,15 @@ export const DynamicRow = ({ elIndex, ...props }: { elIndex: number }) => {
                 onRequiredChange(elIndex, subIndex, checked);
               }}
             />
-            <div className="mt-5 flex justify-end">
+            <div className="mt-5 flex justify-end relative">
+              <div className="absolute w-[100%] border-b-2 border-dotted border-gray-400 pt-6"></div>
               <AddElementButton
                 text={t("addToSet")}
                 position={subIndex}
                 handleAdd={handleAddElement}
                 filterElements={elementFilter}
               />
-              <Button
-                theme="secondary"
-                className="btn btn-danger inline-block ml-5 !border-1.5 !py-2 !px-4 leading-6 text-sm"
-                onClick={() => removeSubItem(elIndex, item.id)}
-              >
-                {t("removeFromSet")}
-              </Button>
               {/* 
-                
                 Note: we modify the item index for the modal / state 
                 The "actual" item index remains untouched
   
@@ -196,6 +189,14 @@ export const DynamicRow = ({ elIndex, ...props }: { elIndex: number }) => {
                 subIndex={subIndex}
                 item={{ ...item, index: item.id }}
               />
+
+              <Button
+                theme="secondary"
+                className="btn btn-danger inline-block ml-5 !border-1.5 !py-2 !px-4 leading-6 text-sm"
+                onClick={() => removeSubItem(elIndex, item.id)}
+              >
+                {t("removeFromSet")}
+              </Button>
             </div>
           </div>
         );
