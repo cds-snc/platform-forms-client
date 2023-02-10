6e86fccb84c6cb210977674baa35a42cb4983e6a
Tim Arney <tim@line37.com>
2023-02-10 10:49:31 -0500

    fix content

--- components/form-builder/app/edit/elements/element-dialog/descriptions/RepeatableQuestionSet.tsx
+++ components/form-builder/app/edit/elements/element-dialog/descriptions/RepeatableQuestionSet.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+
 export const RepeatableQuestionSet = ({
   title,
   description,
@@ -9,7 +11,15 @@ export const RepeatableQuestionSet = ({
   return (
     <div>
       <div className="font-bold text-[1.5rem]">{title}</div>
-      <p>{description}</p>
+      <p className="mb-10">{description}</p>
+      <Image
+        layout="responsive"
+        width={"558"}
+        height={"313"}
+        alt=""
+        className="block w-full mb-10"
+        src="/img/form-builder-dynamic-row.svg"
+      />
     </div>
   );
 };
