a77d1992f18c963b3d9452163a39ef135d367174
Peter Thiessen <peter.thiessen@cds-snc.ca>
2023-02-13 16:36:30 -0500

    Adds download responses sign in screen

--- pages/form-builder/responses/[[...params]].tsx
+++ pages/form-builder/responses/[[...params]].tsx
@@ -16,6 +16,7 @@ import { logMessage } from "@lib/logger";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { checkOne } from "@lib/cache/flags";
+import Link from "next/link";
 
 interface ResponsesProps {
   vaultSubmissions: VaultSubmissionList[];
@@ -24,15 +25,10 @@ interface ResponsesProps {
 const Responses: NextPageWithLayout<ResponsesProps> = ({ vaultSubmissions }: ResponsesProps) => {
   const { t } = useTranslation("form-builder");
   const { status } = useSession();
-
-  //TODO
-  // const [codes, setCodes] = useState<string[]>([]);
-  // const validateCodes = (code:string) => {
-  //   return true;
-  // }
+  const isAuthenticated = status === "authenticated";
 
   const secondaryButtonClass =
-    "whitespace-nowrap relative py-0 px-2 rounded-lg border-2 border-solid inline-flex items-center active:top-0.5 focus:outline-[3px] focus:outline-blue-focus focus:outline focus:outline-offset-2 focus:bg-blue-focus focus:text-white-default disabled:cursor-not-allowed disabled:text-gray-500  bg-white-default text-black-default border-black-default hover:text-white-default hover:bg-gray-600 active:text-white-default active:bg-gray-500";
+    "whitespace-nowrap text-sm rounded-full bg-white-default text-black-default border-black-default hover:text-white-default hover:bg-gray-600 active:text-white-default active:bg-gray-500 py-2 px-5 rounded-lg border-2 border-solid inline-flex items-center active:top-0.5 focus:outline-[3px] focus:outline-blue-focus focus:outline focus:outline-offset-2 focus:bg-blue-focus focus:text-white-default disabled:cursor-not-allowed disabled:text-gray-500";
 
   const dialogConfirmReceipt = useDialogRef();
   const [isShowConfirmReceiptDialog, setIsShowConfirmReceiptDialog] = useState(false);
@@ -51,10 +47,6 @@ const Responses: NextPageWithLayout<ResponsesProps> = ({ vaultSubmissions }: Res
     <Button onClick={dialogReportProblemsHandleClose}>{t("responses.reportProblems")}</Button>
   );
 
-  // const handleConfirmReceiptSubmit = () => {
-  //TODO
-  // }
-
   return (
     <>
       <Head>
@@ -62,24 +54,32 @@ const Responses: NextPageWithLayout<ResponsesProps> = ({ vaultSubmissions }: Res
       </Head>
       <PageTemplate title={t("responses.title")}>
         <div className="flex justify-between items-baseline">
-          <h1 className="text-2xl border-none font-normal">{t("responses.title")}</h1>
+          <h1 className="text-2xl border-none font-normal">
+            {isAuthenticated ? t("responses.title") : t("responses.unauthenticated.title")}
+          </h1>
           <nav className="flex gap-3">
-            <Button
-              onClick={() => setIsShowConfirmReceiptDialog(true)}
-              className="text-sm rounded-full"
-              theme="secondary"
-              disabled={status !== "authenticated"}
-            >
-              {t("responses.confirmReceipt")}
-            </Button>
-            <Button
-              onClick={() => setIsShowReportProblemsDialog(true)}
-              theme="secondary"
-              className="text-sm rounded-full"
-              disabled={status !== "authenticated"}
-            >
-              {t("responses.reportProblems")}
-            </Button>
+            {isAuthenticated && (
+              <Button
+                onClick={() => setIsShowConfirmReceiptDialog(true)}
+                className="text-sm rounded-full"
+                theme="secondary"
+                disabled={status !== "authenticated"}
+              >
+                {t("responses.confirmReceipt")}
+              </Button>
+            )}
+
+            {isAuthenticated && (
+              <Button
+                onClick={() => setIsShowReportProblemsDialog(true)}
+                theme="secondary"
+                className="text-sm rounded-full"
+                disabled={status !== "authenticated"}
+              >
+                {t("responses.reportProblems")}
+              </Button>
+            )}
+
             <StyledLink
               href="/form-builder/settings"
               className={`text-sm no-underline ${secondaryButtonClass} rounded-full`}
@@ -88,56 +88,75 @@ const Responses: NextPageWithLayout<ResponsesProps> = ({ vaultSubmissions }: Res
             </StyledLink>
           </nav>
         </div>
-        <div className="border-2 border-solid border-black">
-          <table>
-            <thead>
-              <tr>
-                <th>Response ID</th>
-                <th>Status</th>
-                <th>Download Response</th>
-                <th>Last Downloaded By</th>
-                <th>Confirm Receipt</th>
-                <th>Removal</th>
-              </tr>
-            </thead>
-            <tbody>
-              <>
-                {vaultSubmissions?.length > 0 ? (
-                  vaultSubmissions.map((submission, index) => (
-                    <tr key={index}>
-                      <td>{submission.name}</td>
-                      <td>{submission.status}</td>
-                      <td></td>
-                      <td></td>
-                      <td>{submission.retrieved ? "Confirmed" : "Confirm By XXXX"}</td>
-                      <td>Not Set</td>
-                    </tr>
-                  ))
-                ) : (
+
+        {isAuthenticated && (
+          <>
+            <div className="border-2 border-solid border-black">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Response ID</th>
+                    <th>Status</th>
+                    <th>Download Response</th>
+                    <th>Last Downloaded By</th>
+                    <th>Confirm Receipt</th>
+                    <th>Removal</th>
+                  </tr>
+                </thead>
+                <tbody>
                   <>
-                    <tr key="1">
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td>Not Set</td>
-                    </tr>
-                    <tr key="2">
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td>Not Set</td>
-                    </tr>
+                    {vaultSubmissions?.length > 0 ? (
+                      vaultSubmissions.map((submission, index) => (
+                        <tr key={index}>
+                          <td>{submission.name}</td>
+                          <td>{submission.status}</td>
+                          <td></td>
+                          <td></td>
+                          <td>{submission.retrieved ? "Confirmed" : "Confirm By XXXX"}</td>
+                          <td>Not Set</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <>
+                        <tr key="1">
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>Not Set</td>
+                        </tr>
+                        <tr key="2">
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td>Not Set</td>
+                        </tr>
+                      </>
+                    )}
                   </>
-                )}
-              </>
-            </tbody>
-          </table>
-        </div>
-        <div>TODO download button here</div>
+                </tbody>
+              </table>
+            </div>
+            <div>TODO download button here</div>
+          </>
+        )}
+
+        {!isAuthenticated && (
+          <>
+            <p>
+              {t("responses.unauthenticated.toAccessPagePart1")}{" "}
+              <Link href={"/auth/login"}>{t("responses.unauthenticated.signIn")}</Link>{" "}
+              {t("responses.unauthenticated.toAccessPagePart2")}
+            </p>
+            <p className="mt-8">
+              {t("responses.unauthenticated.noAccount")}{" "}
+              <Link href={"/signup/register"}>{t("responses.unauthenticated.createOne")}</Link>.
+            </p>
+          </>
+        )}
       </PageTemplate>
 
       {isShowConfirmReceiptDialog && (
