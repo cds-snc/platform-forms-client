67372661d143006848f9192088734367475720d6
Tim Arney <timarney@users.noreply.github.com>
2023-02-15 09:59:23 -0500

    Dynamic panel action buttons - pt 1 (#1618)
    initial re-factor
    
    Co-authored-by: Dave Samojlenko <dave.samojlenko@cds-snc.ca>

--- components/form-builder/app/edit/tests/PanelActions.test.js
+++ components/form-builder/app/edit/tests/PanelActions.test.js
@@ -29,8 +29,8 @@ describe("PanelActions roving index", () => {
     });
 
     const toolbar = screen.getByTestId("panel-actions");
-    const moveUpButton = screen.getByTestId("move-up");
-    const moveDownButton = screen.getByTestId("move-down");
+    const moveUpButton = screen.getByTestId("moveUp");
+    const moveDownButton = screen.getByTestId("moveDown");
     const duplicateButton = screen.getByTestId("duplicate");
     const removeButton = screen.getByTestId("remove");
     const moreButton = screen.getByTestId("more");
@@ -106,8 +106,8 @@ describe("PanelActions roving index", () => {
     });
 
     const toolbar = screen.getByTestId("panel-actions");
-    const moveUpButton = screen.getByTestId("move-up");
-    const moveDownButton = screen.getByTestId("move-down");
+    const moveUpButton = screen.getByTestId("moveUp");
+    const moveDownButton = screen.getByTestId("moveDown");
     const duplicateButton = screen.getByTestId("duplicate");
     const removeButton = screen.getByTestId("remove");
     const moreButton = screen.getByTestId("more");
@@ -170,8 +170,8 @@ describe("PanelActions roving index", () => {
     });
 
     const toolbar = screen.getByTestId("panel-actions");
-    const moveUpButton = screen.getByTestId("move-up");
-    const moveDownButton = screen.getByTestId("move-down");
+    const moveUpButton = screen.getByTestId("moveUp");
+    const moveDownButton = screen.getByTestId("moveDown");
     const duplicateButton = screen.getByTestId("duplicate");
     const removeButton = screen.getByTestId("remove");
     const moreButton = screen.getByTestId("more");
