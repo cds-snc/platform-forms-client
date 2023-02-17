cc332a2074bfc691dc9aa5ee37a1344798259c7c
Tim Arney <tim@line37.com>
2023-02-17 13:29:42 -0500

    cleanup

--- components/form-builder/getPath.test.ts
+++ components/form-builder/getPath.test.ts
@@ -1,32 +1,44 @@
-import { getPath, parseId } from "./getPath";
+import { getElementIndexes, parseRootId } from "./getPath";
 
-describe("Parse element ID", () => {
+describe("Parse root ID", () => {
   it("parses root id", () => {
-    expect(parseId(1)).toEqual(1);
-    expect(parseId(12)).toEqual(12);
-  });
-
-  it("parses sub id", () => {
-    expect(parseId(1201)).toEqual(12);
-    expect(parseId(1202)).toEqual(12);
-    expect(parseId(1210)).toEqual(12);
+    expect(parseRootId(1)).toEqual(1);
+    expect(parseRootId(9)).toEqual(9);
+    expect(parseRootId(12)).toEqual(12);
+    expect(parseRootId(101)).toEqual(1);
+    expect(parseRootId(201)).toEqual(2);
+    expect(parseRootId(1201)).toEqual(12);
+    expect(parseRootId(1202)).toEqual(12);
+    expect(parseRootId(1210)).toEqual(12);
   });
 });
 
-describe("Get path by ID", () => {
+describe("Get array indexes for path by element ID", () => {
   it("parses elIndex", () => {
     const elements = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 12 }];
-    expect(getPath(elements, 1210)).toEqual([3, null]);
+    expect(getElementIndexes(elements, 1210)).toEqual([3, null]);
   });
 
-  it("parses elIndex and subIndex", () => {
+  it("parses 3 digit elIndex and subIndex", () => {
+    const elements = [
+      { id: 1 },
+      { id: 2, properties: { subElements: [{ id: 201 }, { id: 202 }, { id: 203 }] } },
+      { id: 3 },
+      { id: 12, properties: { subElements: [{ id: 1201 }, { id: 1202 }, { id: 1210 }] } },
+    ];
+    const [elIndex, subIndex] = getElementIndexes(elements, 201);
+    expect(elIndex).toEqual(1);
+    expect(subIndex).toEqual(0);
+  });
+
+  it("parses 4 digit elIndex and subIndex", () => {
     const elements = [
       { id: 1 },
       { id: 2 },
       { id: 3 },
       { id: 12, properties: { subElements: [{ id: 1201 }, { id: 1202 }, { id: 1210 }] } },
     ];
-    const [elIndex, subIndex] = getPath(elements, 1210);
+    const [elIndex, subIndex] = getElementIndexes(elements, 1210);
     expect(elIndex).toEqual(3);
     expect(subIndex).toEqual(2);
     expect;
--- components/form-builder/getPath.ts
+++ components/form-builder/getPath.ts
@@ -1,23 +1,31 @@
-export const parseId = (id: number) => {
-  // split 4 digit id into 2 digits
+export const parseRootId = (id: number) => {
+  // split 3 or 4 digit id parent id
   const idStr = id.toString();
+  const sliceAt = idStr.length == 3 ? 1 : 2;
   const idArr = idStr.split("");
-  const first = idArr.slice(0, 2).join("");
+  const first = idArr.slice(0, sliceAt).join("");
   return Number(first);
 };
 
-export const getPath = (elements: any, id: number) => {
-  const elId = parseId(id);
-  const parentIndex = elements.findIndex((el: any) => el.id === elId);
+export interface Element {
+  id: number;
+  properties?: {
+    subElements?: Element[];
+  };
+}
 
-  if (parentIndex === -1) return [null, null];
+export const getElementIndexes = <T extends Element>(elements: T[], id: number) => {
+  const elId = parseRootId(id);
+  const elIndex = elements.findIndex((el: T) => el.id === elId);
 
-  if (elements[parentIndex] && elements[parentIndex].properties?.subElements) {
-    const subIndex = elements[parentIndex].properties.subElements.findIndex(
-      (el: any) => el.id === id
-    );
-    return subIndex > -1 ? [parentIndex, subIndex] : [parentIndex, null];
+  if (elIndex === -1) return [null, null];
+
+  const props = elements[elIndex].properties;
+
+  if (props?.subElements) {
+    const subIndex = props?.subElements.findIndex((el) => el.id === id) ?? -1;
+    return subIndex > -1 ? [elIndex, subIndex] : [elIndex, null];
   }
 
-  return [parentIndex, null];
+  return [elIndex, null];
 };
