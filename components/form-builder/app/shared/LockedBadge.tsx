391d3f7727ce86e5469c170bcb044c97e95d9848
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-02-21 12:03:58 -0500

    re-do add button

--- components/form-builder/app/shared/LockedBadge.tsx
+++ components/form-builder/app/shared/LockedBadge.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { LockIcon } from "../../icons";
 
-export const LockedBadge = () => {
+export const LockedBadge = ({ className }: { className: string }) => {
   const { t } = useTranslation("form-builder");
+  const badgeClass = className ? className : "absolute right-0 top-[-10px]";
   return (
     <div className="relative">
-      <div className="absolute right-0 top-[-10px] px-2 py-2 inline-block bg-gray-200 border-1 border-gray-500">
+      <div className={`${badgeClass} px-2 py-2 inline-block bg-gray-200 border-1 border-gray-500`}>
         <label className="flex text-sm line-height-[38px]" data-testid="locked-item">
           <LockIcon className="inline-block mr-1 !w-7" />
           <span className="inline-block pt-[3px] ">{t("lockedBlock")}</span>
