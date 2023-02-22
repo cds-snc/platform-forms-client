bc232accff24ff8a1b1dbfd88c6b4054ca49045f
Philippe Caron <amazingphilippe@gmail.com>
2023-02-22 14:05:35 -0500

    Fix attestation test to click on the label instead of the container. Removed gcds tokens import temporarily.

--- cypress/e2e/attestation.cy.js
+++ cypress/e2e/attestation.cy.js
@@ -15,15 +15,15 @@ describe("Attestation functionality", () => {
   });
 
   it("Displays error when submitting form with a single checkbox selected", () => {
-    cy.get("div[data-testid='1.0']").click();
+    cy.get("div[data-testid='1.0'] label").click();
     cy.get("[type='submit']").click();
     cy.get("li").contains("Read and check all boxes to confirm the items in this section.");
     cy.get("p").contains("Read and check all boxes to confirm the items in this section.");
   });
 
   it("Submits properly", () => {
-    cy.get("div[data-testid='1.0']").click();
-    cy.get("[data-testid='1.1']").click();
+    cy.get("div[data-testid='1.0'] label").click();
+    cy.get("[data-testid='1.1'] label").click();
     cy.get("[type='submit']").click();
     cy.url().should("include", `/confirmation`);
     cy.get("h1").contains("Your submission has been received");
