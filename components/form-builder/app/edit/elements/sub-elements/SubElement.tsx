cccadf2a23e39090145d74cdf92cb5eb2f41f9a6
Tim Arney <tim@line37.com>
2023-02-22 09:30:52 -0500

    add dynamicRow check for add button

--- components/form-builder/app/edit/elements/sub-elements/SubElement.tsx
+++ components/form-builder/app/edit/elements/sub-elements/SubElement.tsx
@@ -234,28 +234,31 @@ export const SubElement = ({ item, elIndex, ...props }: { item: FormElement; elI
           </div>
         );
       })}
-      <div className="max-w-[800px] border-1 border-gray-300 h-auto -mt-px first-of-type:rounded-t-md last-of-type:rounded-b-md">
-        <LockedBadge className="absolute right-7 top-[15px]" />
-        <div className="mx-7 mt-5 mb-7">
-          <h2 className="text-h3 pb-3">{t("questionSet.addAnother.title")}</h2>
-          <p className="mb-8 text-[1rem] pt-5">{t("questionSet.addAnother.description")}</p>
-          <Input
-            id={`repeatable-button-${elIndex}`}
-            {...getLocalizationAttribute()}
-            key={`repeatable-button-${elIndex}-${translationLanguagePriority}`}
-            value={
-              item.properties[
-                localizeField(LocalizedElementProperties.PLACEHOLDER, translationLanguagePriority)
-              ] || ""
-            }
-            className="w-full"
-            placeholder={t("questionSet.addAnother.placeholder")}
-            onChange={(e) => {
-              handlePlaceHolderText(elIndex, e);
-            }}
-          />
+
+      {item.type === "dynamicRow" && (
+        <div className="max-w-[800px] border-1 border-gray-300 h-auto -mt-px first-of-type:rounded-t-md last-of-type:rounded-b-md">
+          <LockedBadge className="absolute right-7 top-[15px]" />
+          <div className="mx-7 mt-5 mb-7">
+            <h2 className="text-h3 pb-3">{t("questionSet.addAnother.title")}</h2>
+            <p className="mb-8 text-[1rem] pt-5">{t("questionSet.addAnother.description")}</p>
+            <Input
+              id={`repeatable-button-${elIndex}`}
+              {...getLocalizationAttribute()}
+              key={`repeatable-button-${elIndex}-${translationLanguagePriority}`}
+              value={
+                item.properties[
+                  localizeField(LocalizedElementProperties.PLACEHOLDER, translationLanguagePriority)
+                ] || ""
+              }
+              className="w-full"
+              placeholder={t("questionSet.addAnother.placeholder")}
+              onChange={(e) => {
+                handlePlaceHolderText(elIndex, e);
+              }}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
