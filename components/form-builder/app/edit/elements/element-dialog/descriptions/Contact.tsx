b8d7c9f37119468ebf1f8e63fa18149a86268944
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-06 15:07:05 -0500

    Update examples and strings

--- components/form-builder/app/edit/elements/element-dialog/descriptions/Contact.tsx
+++ components/form-builder/app/edit/elements/element-dialog/descriptions/Contact.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { useTranslation } from "next-i18next";
 
-export const Contact = ({ title, description }: { title: string; description: string }) => {
+const Text = ({ label, description }: { label: string; description?: string }) => {
+  return (
+    <div className="ml-1">
+      <p className="mb-1 font-bold mt-1 text-sm">{label}</p>
+      {description && <p className="mb-2 text-sm">{description}</p>}
+      <div className="w-[350px] h-[40px] rounded border-black-default border-2 p-1 mb-2" />
+    </div>
+  );
+};
+
+export const Contact = ({ title }: { title: string }) => {
+  const { t } = useTranslation("form-builder");
+
   return (
     <div>
-      <div className="font-bold text-[1.5rem]">{title}</div>
-      <p className="mb-2">{description}</p>
+      <div className="font-bold text-[1.5rem] mb-2">{title}</div>
+      <Text
+        label={t("addElementDialog.contact.phone.label")}
+        description={t("addElementDialog.contact.phone.description")}
+      />
+      <Text
+        label={t("addElementDialog.contact.email.label")}
+        description={t("addElementDialog.contact.email.description")}
+      />
     </div>
   );
 };
