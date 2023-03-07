4a781064d677f46585f33bf1fabca8c256ffb6e4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-07 15:56:01 -0500

    Get symbol to sit a bit higher

--- components/form-builder/app/shared/InfoDetails.tsx
+++ components/form-builder/app/shared/InfoDetails.tsx
@@ -13,10 +13,10 @@ export const InfoDetails = ({
       <summary className="inline-block p-2 list-none [&::-webkit-details-marker]:hidden cursor-pointer hover:text-white-default hover:bg-gray-600 focus:bg-blue-focus focus:text-white-default [&_svg]:hover:fill-white [&_svg]:focus:fill-white focus:outline-[3px] focus:outline-blue-focus focus:outline focus:outline-offset-2 border-white-default hover:border-black-default border-2 rounded-lg">
         {summary}
         <span className="inline group-open:hidden">
-          <AddIcon className="inline" />
+          <AddIcon className="inline pb-[2px]" />
         </span>
         <span className="hidden group-open:inline">
-          <RemoveIcon className="inline" />
+          <RemoveIcon className="inline pb-[2px]" />
         </span>
       </summary>
       {children}
