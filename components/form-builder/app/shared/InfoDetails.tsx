ce62b3d4c17e70d0582c160d545843cd18bf08df
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-07 16:28:17 -0500

    InfoDetails component (#1702)
    * initial setup
    
    * testing
    
    * styles
    
    * Remove popover package
    
    * Convert popover to details/summary and use that component for share modal
    
    * Get symbol to sit a bit higher
    
    * lint
    
    * lint
    
    ---------
    
    Co-authored-by: Tim Arney <tim@line37.com>

---
+++ components/form-builder/app/shared/InfoDetails.tsx
@@ -0,0 +1,25 @@
+import { AddIcon, RemoveIcon } from "@components/form-builder/icons";
+import React from "react";
+
+export const InfoDetails = ({
+  summary,
+  children,
+}: {
+  summary: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <details className="group mt-5">
+      <summary className="inline-block p-2 list-none [&::-webkit-details-marker]:hidden cursor-pointer hover:text-white-default hover:bg-gray-600 focus:bg-blue-focus focus:text-white-default [&_svg]:hover:fill-white [&_svg]:focus:fill-white focus:outline-[3px] focus:outline-blue-focus focus:outline focus:outline-offset-2 border-white-default hover:border-black-default border-2 rounded-lg">
+        {summary}
+        <span className="inline group-open:hidden">
+          <AddIcon className="inline pb-[2px]" />
+        </span>
+        <span className="hidden group-open:inline">
+          <RemoveIcon className="inline pb-[2px]" />
+        </span>
+      </summary>
+      {children}
+    </details>
+  );
+};
