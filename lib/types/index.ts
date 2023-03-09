15315d5a2050f99194d94b3f02196879af4e4c0a
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-03-09 15:47:04 -0500

    fix weird missing type conflict

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -47,7 +47,7 @@ export type {
 
 export type { FormOwner } from "./user-types";
 
-export type { Abilities, Privilege, UserAbility, Permission } from "./privileges-types";
+export type { Abilities, Privilege, UserAbility, Permission, AnyObject } from "./privileges-types";
 
 // Utility type creator
 export type TypeOmit<T, K extends PropertyKey> = { [P in keyof T as Exclude<P, K>]: T[P] };
