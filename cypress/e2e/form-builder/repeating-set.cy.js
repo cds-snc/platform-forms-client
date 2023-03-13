4b2bd1b9c7d8e02280a7fcbc66473bbb6b90bbb7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-13 12:38:46 -0400

    test more details

--- cypress/e2e/form-builder/repeating-set.cy.js
+++ cypress/e2e/form-builder/repeating-set.cy.js
@@ -23,6 +23,51 @@ describe("Test FormBuilder Repeating set", () => {
     });
   });
 
+  it("Adds a Repeating set with a few questions", () => {
+    cy.visit("/form-builder/edit");
+    cy.get("button").contains("Add block").click();
+
+    cy.get('[data-testid="dynamicRow"]').click();
+    cy.get("button").contains("Select block").click();
+    cy.get("#item-1").type("This is a repeating set");
+
+    cy.get('[data-testid="add-element"]').contains("Add to set").click();
+    cy.get('[data-testid="textField"]').click();
+    cy.get("button").contains("Select block").click();
+
+    cy.get("#item-101").type("This is a short answer question");
+
+    cy.get('[data-testid="addToSet"]').contains("Add to set").click();
+    cy.get('[data-testid="radio"]').click();
+    cy.get("button").contains("Select block").click();
+
+    cy.get("#item-102").type("This is a single choice question");
+    cy.get("#option--1--1").type("This is an option");
+
+    cy.visit("/form-builder/preview");
+    cy.get('[id="1.0.0"]').type("An answer");
+    cy.get('label[for="1.0.1.0"]').click();
+    cy.get('[id="1.0.1.0"]').should("be.checked");
+  });
+
+  it("Adds a Repeating set with custom add label", () => {
+    cy.visit("/form-builder/edit");
+    cy.get("button").contains("Add block").click();
+
+    cy.get('[data-testid="dynamicRow"]').click();
+    cy.get("button").contains("Select block").click();
+    cy.get("#item-1").type("This is a repeating set");
+
+    cy.get('[data-testid="add-element"]').contains("Add to set").click();
+    cy.get('[data-testid="textField"]').click();
+    cy.get("button").contains("Select block").click();
+
+    cy.get("#repeatable-button-0").type("something");
+    cy.visit("/form-builder/preview");
+
+    cy.get('[data-testid="add-row-button-1"]').contains("Add something");
+  });
+
   it("Adds a Repeating set with max rows", () => {
     cy.visit("/form-builder/edit");
     cy.get("button").contains("Add block").click();
