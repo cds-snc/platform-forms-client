bc7635660431f79ce2ec68d1d2012b6752c93a1b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-13 12:08:02 -0400

    Test repeated q max rows

--- cypress/e2e/form-builder/repeating-set.cy.js
+++ cypress/e2e/form-builder/repeating-set.cy.js
@@ -23,18 +23,33 @@ describe("Test FormBuilder Repeating set", () => {
     });
   });
 
-  it("Adds a Repeating set", () => {
+  it("Adds a Repeating set with max rows", () => {
     cy.visit("/form-builder/edit");
     cy.get("button").contains("Add block").click();
 
     cy.get('[data-testid="dynamicRow"]').click();
     cy.get("button").contains("Select block").click();
     cy.get("#item-1").type("This is a repeating set");
+
+    cy.get('[data-testid="more"]').click();
+    cy.get("#maxNumberOfRows--modal--0").type("2");
+    cy.get("button").contains("Save").click();
+
     cy.get('[data-testid="add-element"]').contains("Add to set").click();
     cy.get('[data-testid="textField"]').click();
     cy.get("button").contains("Select block").click();
 
-    // cy.visit("/form-builder/preview");
-    // cy.get('[data-testid="textInput"]').should("have.attr", "autocomplete", "name");
+    cy.get("#item-101").type("This is a short answer question");
+
+    cy.visit("/form-builder/preview");
+
+    cy.get('[id="1.0.0"]').type("An answer");
+    cy.get('[data-testid="add-row-button-1"]').click();
+
+    cy.get('[id="1.1.0"]').type("Another answer");
+    cy.get("button").contains("Add").should("not.exist");
+
+    cy.get("button").contains("Delete 2").click();
+    cy.get("button").contains("Add").should("exist");
   });
 });
