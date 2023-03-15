ceb9dd32d2c45d83f22e2935cac42565dbae9378
rkeita <raphael.keita@cds-snc.ca>
2023-03-15 13:25:27 -0400

    WAF regex rule

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
-import { NextRequest, NextResponse, userAgent } from "next/server";
-
+import { NextRequest, NextResponse } from "next/server";
+// only to test
 export function middleware(req: NextRequest) {
-  const browserNameList = ["internet explorer 11", "ie11", "ie"];
-  if (req.nextUrl.pathname.match(/\/(id.+)/)) {
-    const { browser } = userAgent(req);
-    if (browser.name && browserNameList.includes(browser.name.toLowerCase())) {
-      return NextResponse.rewrite(`${req.nextUrl.origin}/not-supported`);
-    }
+  if (
+    req.nextUrl.pathname.match(
+      /^\/(?:en|fr)?\/?(?:(admin|id|api|auth|signup|myforms|not-supported|terms-avis|index|404|js-disabled|form-builder|sla|unlock-publishing|changelog|static|_next|img)(?:\/[\w-]+)*)?(?:\/.*)?$/
+    )
+  ) {
+    return NextResponse.next();
+  } else {
+    // should display 404 page
+    return NextResponse.rewrite(`${req.nextUrl.origin}/invalid`);
   }
-  return NextResponse.next();
 }
