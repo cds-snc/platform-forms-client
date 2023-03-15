ee7dd0eef2453c2db26ea2474430888ae44a9ab0
Tim Arney <tim@line37.com>
2023-03-15 15:16:25 -0400

    table setup

--- migrations/migrations/012-settings.js
+++
@@ -1,8 +0,0 @@
-const createSettingsTable = `
-  CREATE TABLE settings (
-    id SERIAL PRIMARY KEY,
-    brandingRequestFormId VARCHAR(255),
-  );
-`;
-
-module.exports.generateSql = () => `${createSettingsTable}`;
