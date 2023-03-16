4b01c0f394d9be90a39b5f370c82c811ed7db052
Tim Arney <tim@line37.com>
2023-03-16 09:41:29 -0400

    update api

--- pages/admin/settings.tsx
+++ pages/admin/settings.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement, useCallback, useEffect, useState } from "react";
 import useSWR from "swr";
+import axios from "axios";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { requireAuthentication } from "@lib/auth";
 import { useTranslation } from "next-i18next";
@@ -7,12 +8,17 @@ import Head from "next/head";
 import { checkPrivileges } from "@lib/privileges";
 import AdminNavLayout from "@components/globals/layouts/AdminNavLayout";
 
-const fetcher = (url: string) => fetch(url, { method: "POST" }).then((response) => response.json());
+const _get = (url: string) => fetch(url).then((response) => response.json());
+const _post = (url: string, formId: string) =>
+  axios(url, {
+    method: "POST",
+    data: { brandingRequestForm: formId },
+  });
 
 const Settings = () => {
   const { t } = useTranslation("admin-settings");
 
-  const { data, error } = useSWR("/api/settings", fetcher);
+  const { data, error } = useSWR("/api/settings", _get);
   const [brandingFormId, setBrandingFormId] = useState("");
 
   useEffect(() => {
@@ -21,10 +27,13 @@ const Settings = () => {
     }
   }, [data]);
 
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // @todo save the branding form id
-  }, []);
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      _post("/api/settings", brandingFormId);
+    },
+    [brandingFormId]
+  );
 
   if (error) return <p>Sorry... Something went wrong</p>;
 
@@ -36,26 +45,25 @@ const Settings = () => {
       <h1>{t("title")}</h1>
       <div className="gc-form">
         <pre>{JSON.stringify(data)}</pre>
-        {data && data.brandingRequestForm && (
-          <form onSubmit={handleSubmit}>
-            <div className="focus-group">
-              <label htmlFor="branding-form-id" className="gc-label">
-                {t("brandingRequestId")}
-              </label>
-              <input
-                className="gc-input-text"
-                onChange={(e) => setBrandingFormId(e.target.value)}
-                value={brandingFormId}
-                type="text"
-                name="branding-form-id"
-                id="branding-form-id"
-              />
-            </div>
-            <button className="gc-button gc-button--primary gc-button--lg" type="submit">
-              {t("save")}
-            </button>
-          </form>
-        )}
+
+        <form onSubmit={handleSubmit}>
+          <div className="focus-group mb-2">
+            <label htmlFor="branding-form-id" className="gc-label">
+              {t("brandingRequestId")}
+            </label>
+            <input
+              className="gc-input-text mb-1"
+              onChange={(e) => setBrandingFormId(e.target.value)}
+              value={brandingFormId}
+              type="text"
+              name="branding-form-id"
+              id="branding-form-id"
+            />
+          </div>
+          <button className="gc-button gc-button--primary gc-button--lg" type="submit">
+            {t("save")}
+          </button>
+        </form>
       </div>
     </>
   );
--- pages/api/settings/index.ts
+++ pages/api/settings/index.ts
@@ -3,6 +3,7 @@ import { middleware, cors, sessionExists } from "@lib/middleware";
 import { MiddlewareProps } from "@lib/types";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@lib/integration/prismaConnector";
+import { logMessage } from "@lib/logger";
 
 const settings = async (
   req: NextApiRequest,
@@ -11,29 +12,40 @@ const settings = async (
 ) => {
   try {
     if (!session) return res.status(401).json({ error: "Unauthorized" });
-    // get branding request form settings
-    const data = await prisma.settings.findFirst({
-      select: {
-        brandingRequestForm: true,
-      },
+
+    if (req.method === "GET") {
+      const data = await prisma.settings.findFirst({
+        select: { brandingRequestForm: true },
+      });
+
+      return res.status(200).json(data);
+    }
+
+    const { brandingRequestForm } = req.body;
+
+    const recordID = await prisma.settings.findFirst({
+      select: { id: true },
     });
 
-    /*
+    if (recordID) {
+      const result = await prisma.settings.update({
+        where: { id: recordID.id },
+        data: { brandingRequestForm: brandingRequestForm },
+      });
+
+      return res.status(200).json(result);
+    }
+
     const result = await prisma.settings.create({
-      data: {
-        brandingRequestForm: "123",
-      },
-      select: {
-        id: true,
-      },
+      data: { brandingRequestForm: req.body.brandingRequestForm },
     });
-    */
 
-    return res.status(200).json(data);
+    res.status(201).json(result);
   } catch (err) {
+    logMessage.error(err);
     if (err instanceof AccessControlError) return res.status(403).json({ error: "Forbidden" });
     else return res.status(500).json({ error: "Malformed API Request" });
   }
 };
 
-export default middleware([cors({ allowedMethods: ["POST"] }), sessionExists()], settings);
+export default middleware([cors({ allowedMethods: ["POST", "GET"] }), sessionExists()], settings);
