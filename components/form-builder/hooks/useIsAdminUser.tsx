1443aa68fe296c9460807e561ae18770313f9ae2
Tim Arney <tim@line37.com>
2023-03-16 13:56:35 -0400

    update for jest test

--- components/form-builder/hooks/useIsAdminUser.tsx
+++ components/form-builder/hooks/useIsAdminUser.tsx
@@ -4,7 +4,7 @@ import { useAccessControl } from "@lib/hooks";
 export const useIsAdminUser = () => {
   const { ability, refreshAbility } = useAccessControl();
   useEffect(() => {
-    refreshAbility();
+    refreshAbility && refreshAbility();
     // we only want to run this once on mount
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
