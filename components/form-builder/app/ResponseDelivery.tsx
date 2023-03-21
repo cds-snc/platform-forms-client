1d7902a439b2445c5be22ea0968702272ad3da4e
Tim Arney <tim@line37.com>
2023-03-21 13:03:17 -0400

    add card to branding page

--- components/form-builder/app/ResponseDelivery.tsx
+++ components/form-builder/app/ResponseDelivery.tsx
@@ -1,56 +1,14 @@
 import React from "react";
-import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 import { SetResponseDelivery } from "./SetResponseDelivery";
-import { Card } from "@components/globals/card/Card";
-import { themes } from "./shared/Button";
-import Link from "next/link";
+import { SettingsLoggedOut } from "./SettingsLoggedOut";
 
 export const ResponseDelivery = () => {
-  const { t, i18n } = useTranslation("form-builder");
-  const { status } = useSession();
-  const signInLink = `/${i18n.language}/auth/login`;
-  const createAccountLink = `/${i18n.language}/signup/register`;
-
+  const { t } = useTranslation("form-builder");
   return (
     <>
       <h1 className="visually-hidden">{t("formSettings")}</h1>
-
-      {status !== "authenticated" && (
-        <Card
-          icon={
-            <picture>
-              <img src="/img/cloud-lock.png" width="200" height="147" alt="" />
-            </picture>
-          }
-        >
-          <p className="gc-h2 text-[#748094]">{t("settingsResponseDelivery.card.title")}</p>
-          <p className="mb-6">
-            {t("settingsResponseDelivery.card.text1")}{" "}
-            <a href={signInLink}>{t("settingsResponseDelivery.card.text2")}</a>.{" "}
-            {t("settingsResponseDelivery.card.text3")}{" "}
-            <a href={createAccountLink}>{t("settingsResponseDelivery.card.text4")}</a>.
-          </p>
-          <p>
-            <Link href={signInLink} legacyBehavior>
-              <a
-                href={signInLink}
-                className={`${themes.primary} ${themes.base} ${themes.htmlLink} mr-4`}
-              >
-                {t("settingsResponseDelivery.card.signinButton")}
-              </a>
-            </Link>
-            <Link href={createAccountLink} legacyBehavior>
-              <a
-                href={createAccountLink}
-                className={`text-black-default active:text-black-default visited:text-black-default ${themes.secondary} ${themes.base} no-underline active:shadow-none focus:shadow-none`}
-              >
-                {t("settingsResponseDelivery.card.createButton")}
-              </a>
-            </Link>
-          </p>
-        </Card>
-      )}
+      <SettingsLoggedOut />
       <SetResponseDelivery />
     </>
   );
