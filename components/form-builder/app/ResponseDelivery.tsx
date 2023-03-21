1c99b3a71a13eb5196a2316221d94f62cba30f56
Tim Arney <tim@line37.com>
2023-03-21 12:44:30 -0400

    add logged out view

--- components/form-builder/app/ResponseDelivery.tsx
+++ components/form-builder/app/ResponseDelivery.tsx
@@ -1,56 +1,57 @@
 import React from "react";
+import { useSession } from "next-auth/react";
 import { useTranslation } from "next-i18next";
-import { useTemplateStore } from "../store";
 import { SetResponseDelivery } from "./SetResponseDelivery";
-
-const Label = ({ htmlFor, children }: { htmlFor: string; children?: JSX.Element | string }) => {
-  return (
-    <label className="block font-bold mb-1" htmlFor={htmlFor}>
-      {children}
-    </label>
-  );
-};
-
-const HintText = ({ id, children }: { id: string; children?: JSX.Element | string }) => {
-  return (
-    <span className="block text-sm mb-1" id={id}>
-      {children}
-    </span>
-  );
-};
+import { Card } from "@components/globals/card/Card";
+import { themes } from "./shared/Button";
+import Link from "next/link";
 
 export const ResponseDelivery = () => {
   const { t, i18n } = useTranslation("form-builder");
-
-  const { isPublished } = useTemplateStore((s) => ({
-    isPublished: s.isPublished,
-  }));
+  const { status } = useSession();
+  const signInLink = `/${i18n.language}/auth/login`;
+  const createAccountLink = `/${i18n.language}/signup/register`;
 
   return (
     <>
       <h1 className="visually-hidden">{t("formSettings")}</h1>
 
-      <SetResponseDelivery />
-
-      {isPublished && (
-        <div className="mb-10">
-          <Label htmlFor="response-delivery">{t("settingsResponseTitle")}</Label>
-          <HintText id="response-delivery-hint-1">{t("settingsResponseHint1")}</HintText>
-          <HintText id="response-delivery-hint-2">{t("settingsResponseHint2")}</HintText>
-          <div className="mt-4 mb-4 p-4 bg-purple-200 text-sm inline-block">
-            {t("settingsResponseNotePublished")}
-            <a
-              href={`/${i18n.language}/form-builder/support`}
-              className="ml-2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {t("contactSupport")}
-            </a>
-            .
-          </div>
-        </div>
+      {status !== "authenticated" && (
+        <Card
+          icon={
+            <picture>
+              <img src="/img/cloud-lock.png" width="200" height="147" alt="" />
+            </picture>
+          }
+        >
+          <p className="gc-h2 text-[#748094]">{t("settingsResponseDelivery.card.title")}</p>
+          <p className="mb-6">
+            {t("settingsResponseDelivery.card.text1")}{" "}
+            <a href={signInLink}>{t("settingsResponseDelivery.card.text2")}</a>.{" "}
+            {t("settingsResponseDelivery.card.text3")}{" "}
+            <a href={createAccountLink}>{t("settingsResponseDelivery.card.text4")}</a>.
+          </p>
+          <p>
+            <Link href={signInLink} legacyBehavior>
+              <a
+                href={signInLink}
+                className={`${themes.primary} ${themes.base} ${themes.htmlLink} mr-4`}
+              >
+                {t("settingsResponseDelivery.card.signinButton")}
+              </a>
+            </Link>
+            <Link href={createAccountLink} legacyBehavior>
+              <a
+                href={createAccountLink}
+                className={`text-black-default active:text-black-default visited:text-black-default ${themes.secondary} ${themes.base} no-underline active:shadow-none focus:shadow-none`}
+              >
+                {t("settingsResponseDelivery.card.createButton")}
+              </a>
+            </Link>
+          </p>
+        </Card>
       )}
+      <SetResponseDelivery />
     </>
   );
 };
