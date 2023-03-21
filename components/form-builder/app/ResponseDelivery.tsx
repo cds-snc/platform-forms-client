b813ed02337e7ef5c93ba7b5cb076e8e582c018e
Tim Arney <timarney@users.noreply.github.com>
2023-03-21 13:39:09 -0400

    Settings - logged out (#1763)
    * add logged out view
    
    * add card to branding page

--- components/form-builder/app/ResponseDelivery.tsx
+++ components/form-builder/app/ResponseDelivery.tsx
@@ -1,56 +1,15 @@
 import React from "react";
 import { useTranslation } from "next-i18next";
-import { useTemplateStore } from "../store";
 import { SetResponseDelivery } from "./SetResponseDelivery";
-
-const Label = ({ htmlFor, children }: { htmlFor: string; children?: JSX.Element | string }) => {
-  return (
-    <label className="block font-bold mb-1" htmlFor={htmlFor}>
-      {children}
-    </label>
-  );
-};
-
-const HintText = ({ id, children }: { id: string; children?: JSX.Element | string }) => {
-  return (
-    <span className="block text-sm mb-1" id={id}>
-      {children}
-    </span>
-  );
-};
+import { SettingsLoggedOut } from "./SettingsLoggedOut";
 
 export const ResponseDelivery = () => {
-  const { t, i18n } = useTranslation("form-builder");
-
-  const { isPublished } = useTemplateStore((s) => ({
-    isPublished: s.isPublished,
-  }));
-
+  const { t } = useTranslation("form-builder");
   return (
     <>
       <h1 className="visually-hidden">{t("formSettings")}</h1>
-
+      <SettingsLoggedOut />
       <SetResponseDelivery />
-
-      {isPublished && (
-        <div className="mb-10">
-          <Label htmlFor="response-delivery">{t("settingsResponseTitle")}</Label>
-          <HintText id="response-delivery-hint-1">{t("settingsResponseHint1")}</HintText>
-          <HintText id="response-delivery-hint-2">{t("settingsResponseHint2")}</HintText>
-          <div className="mt-4 mb-4 p-4 bg-purple-200 text-sm inline-block">
-            {t("settingsResponseNotePublished")}
-            <a
-              href={`/${i18n.language}/form-builder/support`}
-              className="ml-2"
-              target="_blank"
-              rel="noreferrer"
-            >
-              {t("contactSupport")}
-            </a>
-            .
-          </div>
-        </div>
-      )}
     </>
   );
 };
