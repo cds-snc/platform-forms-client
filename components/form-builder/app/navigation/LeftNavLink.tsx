a07ba87e6ced4aaf02724e6902f91f4ff4617e2d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-22 17:17:07 -0400

    left align nav

--- components/form-builder/app/navigation/LeftNavLink.tsx
+++ components/form-builder/app/navigation/LeftNavLink.tsx
@@ -5,7 +5,6 @@ import { useActivePathname, cleanPath } from "../../hooks/useActivePathname";
 export const LeftNavLink = ({ children, href }: { children: ReactElement; href: string }) => {
   const [active, setActive] = useState(false);
   const { isReady, activePathname } = useActivePathname();
-
   useEffect(() => {
     if (isReady) {
       const linkPathname = cleanPath(
@@ -23,7 +22,7 @@ export const LeftNavLink = ({ children, href }: { children: ReactElement; href: 
         href={href}
         className={`${
           active ? "font-bold" : ""
-        } group xl:text-center no-underline rounded block xl:w-36 xl:pb-0 xl:pt-2 xl:mb-2 mb-4 -ml-1 pl-1 pr-2 md:pr-0 text-black-default hover:text-blue-hover visited:text-black-default focus:text-white-default focus:bg-blue-hover active:no-underline active:bg-blue-hover active:text-white-default !shadow-none`}
+        } group no-underline rounded block w-36 pb-0 pt-2 mb-2 pr-0 laptop:pr-2 text-black-default hover:text-blue-hover visited:text-black-default focus:text-white-default focus:bg-blue-hover active:no-underline active:bg-blue-hover active:text-white-default !shadow-none`}
       >
         {children}
       </a>
