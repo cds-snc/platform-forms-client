ae373909da541f3274c4b3e216298da1e01ef9f7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-23 15:56:58 -0400

    Update MyForms LeftNavigation to match FormBuilder

--- components/myforms/LeftNav/LeftNavLink.tsx
+++ components/myforms/LeftNav/LeftNavLink.tsx
@@ -18,7 +18,7 @@ export const LeftNavLink = ({
         href={href}
         className={`${
           isActive ? "font-bold" : ""
-        } group xl:text-center no-underline rounded block xl:w-36 xl:pb-0 xl:pt-2 xl:mb-2 mb-4 -ml-1 pl-1 pr-2 md:pr-0 text-black-default hover:text-blue-hover visited:text-black-default focus:text-white-default focus:bg-blue-hover active:no-underline active:bg-blue-hover active:text-white-default`}
+        } group no-underline rounded block w-38 py-1 mb-2 pl-2 pr-0 laptop:pr-2 text-black-default hover:text-blue-hover visited:text-black-default focus:text-white-default focus:bg-blue-hover active:no-underline active:bg-blue-hover active:text-white-default !shadow-none`}
       >
         {children}
       </a>
