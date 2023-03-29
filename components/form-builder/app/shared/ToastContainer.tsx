ac04a317866ec5b7685c63f419983f8bc0f6baf9
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-03-29 11:23:24 -0400

    Adjust colours and revert type changes

--- components/form-builder/app/shared/ToastContainer.tsx
+++ components/form-builder/app/shared/ToastContainer.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { ToastContainer as OriginalContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+// import "react-toastify/dist/ReactToastify.minimal.css";
 
 const contextClass = {
   success: "bg-green-50",
   error: "bg-red-100",
-  info: "bg-blue-100",
-  warning: "bg-yellow-400",
+  info: "bg-blue-50",
+  warning: "bg-yellow-50",
   default: "bg-white",
 };
 
 type ToastType = keyof typeof contextClass;
 
-export const ToastContainer = ({ type = "default" }: { type?: ToastType }) => {
+export const ToastContainer = () => {
   return (
     <OriginalContainer
-      toastClassName={`${contextClass[type]} relative flex drop-shadow-md p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer`}
-      bodyClassName="flex p-4 text-sm text-black"
+      toastClassName={({ type }) =>
+        contextClass[type || "default"] +
+        " relative flex drop-shadow-md p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
+      }
+      bodyClassName={() => "flex p-4 text-base text-black"}
       position={toast.POSITION.TOP_CENTER}
       autoClose={3000}
       hideProgressBar={true}
