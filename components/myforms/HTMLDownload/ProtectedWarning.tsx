e135a0d533d2ac75957af2713e6ff3f1ded81fd0
Clément Janin <clement.janin@cds-snc.ca>
2023-04-14 13:53:58 -0400

    fix: data classification in HTML response does not match form settings

--- components/myforms/HTMLDownload/ProtectedWarning.tsx
+++ components/myforms/HTMLDownload/ProtectedWarning.tsx
@@ -3,20 +3,35 @@ import { useTranslation } from "next-i18next";
 
 // Note: use lang prop if you want to force a specific language e.g. HTMLDownload component
 interface ProtectedWarningProps {
+  securityAttribute: string;
   lang?: string;
 }
 
 export const ProtectedWarning = (props: ProtectedWarningProps) => {
   const { t, i18n } = useTranslation(["my-forms"]);
   const { lang = i18n.language } = props;
+
+  const renderSecurityAttribute = () => {
+    switch (props.securityAttribute) {
+      case "Unclassified":
+        return t("dataClassification.unclassified", { lng: lang });
+      case "Protected A":
+        return t("dataClassification.protectedA", { lng: lang });
+      case "Protected B":
+        return t("dataClassification.protectedB", { lng: lang });
+      default:
+        throw new Error(`Unsupported security attribute ${props.securityAttribute}`);
+    }
+  };
+
   return (
     <div className="flex p-4 justify-between items-center bg-gray-200 border border-gray-300 text-sm">
       <div className="flex flex-col">
-        <p className="font-bold">{t("protectedBWarning.officialRecord", { lng: lang })}</p>
-        <p>{t("protectedBWarning.retainCopy", { lng: lang })}</p>
+        <p className="font-bold">{t("dataClassification.officialRecord", { lng: lang })}</p>
+        <p>{t("dataClassification.retainCopy", { lng: lang })}</p>
       </div>
       <div className="px-4 py-2 bg-white-default font-bold border border-gray-300">
-        {t("protectedBWarning.protectedB", { lng: lang })}
+        {renderSecurityAttribute()}
       </div>
     </div>
   );
