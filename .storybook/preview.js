909d148b1f045264be0a4e4eac54420d3455a630
Tim Arney <timarney@users.noreply.github.com>
2023-04-18 14:26:02 -0400

    Remove storybook (#1937)
    *  storybook removal
    
    * add md docs
    
    * ignore polyfills

--- .storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { addDecorator } from "@storybook/react";
-import { Layout } from "./Layout";
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-};
-
-addDecorator((storyFn) => <Layout>{storyFn()}</Layout>);
---
+++ documentation/assets/architecture.md
@@ -0,0 +1,9 @@
+# Architecture
+
+## System Architecture
+
+<img src="./Forms_System_Architecture.png" alt="System Architecture" />
+
+## Cloud Architecture
+
+<img src="./Forms_Cloud_Architecture.png" alt="Cloud Architecture" />
