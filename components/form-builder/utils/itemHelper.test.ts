c9cab9b3886a4ee7c183b3de5d1c3b61f9e259ab
Tim Arney <tim@line37.com>
2023-04-25 11:37:14 -0400

    default obj

--- components/form-builder/utils/itemHelper.test.ts
+++ components/form-builder/utils/itemHelper.test.ts
@@ -1,70 +1,84 @@
 import { FormElement } from "@lib/types";
-import { defaultField, setDescription, setTitle, updateElement } from "./itemHelper";
+import { setDescription, setTitle, updateElement } from "./itemHelper";
 
-let defaultElement = { ...defaultField };
+function getItem() {
+  return {
+    id: 0,
+    type: "textField",
+    properties: {
+      subElements: [],
+      choices: [{ en: "", fr: "" }],
+      titleEn: "",
+      titleFr: "",
+      validation: {
+        required: false,
+      },
+      descriptionEn: "",
+      descriptionFr: "",
+      placeholderEn: "",
+      placeholderFr: "",
+    },
+  } as FormElement;
+}
 
 describe("Set localized Item properties", () => {
   it("sets description en", () => {
-    const item = setDescription(defaultField, "en", "desc en");
+    const item = setDescription(getItem(), "en", "desc en");
     expect(item.properties.descriptionEn).toEqual("desc en");
   });
 
   it("sets description fr", () => {
-    const item = setDescription(defaultField, "fr", "desc fr");
+    const item = setDescription(getItem(), "fr", "desc fr");
     expect(item.properties.descriptionFr).toEqual("desc fr");
   });
 
   it("sets title en", () => {
-    const item = setTitle(defaultField, "en", "title en");
+    const item = setTitle(getItem(), "en", "title en");
     expect(item.properties.titleEn).toEqual("title en");
   });
 
   it("sets title fr", () => {
-    const item = setTitle(defaultField, "fr", "title fr");
+    const item = setTitle(getItem(), "fr", "title fr");
     expect(item.properties.titleFr).toEqual("title fr");
   });
 });
 
 describe("Update elements", () => {
-  beforeEach(() => {
-    defaultElement = { ...defaultField } as FormElement;
-  });
-
   it("sets properties for phone", () => {
-    const item = updateElement(defaultElement, "phone");
+    const item = updateElement(getItem(), "phone");
     expect(item.type).toEqual("textField");
     expect(item.properties.validation?.type).toEqual("phone");
     expect(item.properties.autoComplete).toEqual("phone");
   });
 
   it("sets properties for email", () => {
-    const item = updateElement(defaultElement, "email");
+    const item = updateElement(getItem(), "email");
     expect(item.type).toEqual("textField");
     expect(item.properties.validation?.type).toEqual("email");
     expect(item.properties.autoComplete).toEqual("email");
   });
 
   it("sets properties for date field", () => {
-    const item = updateElement(defaultElement, "date");
+    const item = updateElement(getItem(), "date");
     expect(item.type).toEqual("textField");
     expect(item.properties.validation?.type).toEqual("date");
   });
 
   it("sets properties for number", () => {
-    const item = updateElement(defaultElement, "number");
+    const item = updateElement(getItem(), "number");
     expect(item.type).toEqual("textField");
     expect(item.properties.validation?.type).toEqual("number");
   });
 
   it("sets properties for attestation", () => {
-    const item = updateElement(defaultElement, "attestation");
+    const item = updateElement(getItem(), "attestation");
     expect(item.type).toEqual("checkbox");
     expect(item.properties.validation?.all).toEqual(true);
     expect(item.properties.validation?.required).toEqual(true);
   });
 
   it("sets properties for richText", () => {
-    const item = updateElement(defaultElement, "richText");
+    const item = updateElement(getItem(), "richText");
     expect(item.type).toEqual("richText");
   });
 });
