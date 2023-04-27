7396b9791d52c3a76e8294c83a0e2303774ef41c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-04-27 13:49:28 -0400

    bugfix: move Close button later in the tab order

--- components/form-builder/app/shared/Dialog.tsx
+++ components/form-builder/app/shared/Dialog.tsx
@@ -64,29 +64,28 @@ export const Dialog = ({
       ref={dialogRef}
     >
       <div
-        className={`mx-8 tablet:max-w-[700px] overflow-y-scroll max-h-[80%] tablet:mx-auto mt-12 laptop:mt-24 bg-white border-2 border-black rounded-xl ${className}`}
+        className={`relative mx-8 tablet:max-w-[700px] overflow-y-scroll max-h-[80%] tablet:mx-auto mt-12 laptop:mt-24 bg-white border-2 border-black rounded-xl ${className}`}
       >
-        <div className="relative">
-          {title && (
-            <h2 className={headerStyle ? headerStyle : "pb-4 inline-block mt-4 ml-4"} tabIndex={-1}>
-              {title}
-            </h2>
-          )}
-          <Button
-            theme="link"
-            className="group absolute right-0 top-0 mr-4 mt-4 z-[1000]"
-            aria-label={t("close")}
-            onClick={close}
-            dataTestId="close-dialog"
-          >
-            <span className="block w-30 mr-2">
-              <Close className="group-focus:fill-white-default inline-block mr-2" />
-              {t("close")}
-            </span>
-          </Button>
-        </div>
+        {title && (
+          <h2 className={headerStyle ? headerStyle : "pb-4 inline-block mt-4 ml-4"} tabIndex={-1}>
+            {title}
+          </h2>
+        )}
+
         <div className={`px-4 ${title ? "" : "mt-10"}`}>{children}</div>
         {actions && <div className="py-4 px-4 border-t-1 border-gray-400 flex">{actions}</div>}
+        <Button
+          theme="link"
+          className="group absolute right-0 top-0 mr-4 mt-4 z-[1000]"
+          aria-label={t("close")}
+          onClick={close}
+          dataTestId="close-dialog"
+        >
+          <span className="block w-30 mr-2">
+            <Close className="group-focus:fill-white-default inline-block mr-2" />
+            {t("close")}
+          </span>
+        </Button>
       </div>
     </dialog>
   );
