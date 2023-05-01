bd85d7ef29753a6a56ad67723d8cf6d7ae2dd6a2
Tim Arney <tim@line37.com>
2023-05-01 17:40:29 -0400

    update selector

--- components/form-builder/app/edit/elements/element-dialog/tests/ElementDialog.cy.tsx
+++ components/form-builder/app/edit/elements/element-dialog/tests/ElementDialog.cy.tsx
@@ -236,7 +236,9 @@ describe("<ElementDialog />", () => {
     const handleAddTypeSpy = cy.spy().as("handleAddTypeSpy");
 
     cy.mount(<ElementDialog handleClose={handleCloseSpy} handleAddType={handleAddTypeSpy} />);
-    cy.get("body").tab();
+
+    // cy.get("body").tab(); // flakes out
+    cy.get('[data-testid="listbox"]');
     cy.focused().should("have.attr", "data-testid", "listbox");
     cy.get('[data-testid="richText"]').should("have.attr", "aria-selected", "true");
 
