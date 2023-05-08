c5c097ad94aa09b224c325448d3b18a79da3d80c
Clément Janin <clement.janin@cds-snc.ca>
2023-05-08 15:40:06 -0400

    fix: not supported page is now working as intended

--- pages/not-supported.tsx
+++ pages/not-supported.tsx
@@ -1,153 +1,72 @@
 /* eslint-disable @next/next/no-img-element  */
-import React, { ReactElement } from "react";
+import React from "react";
+import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { GetServerSideProps } from "next";
-import { getPublicTemplateByID } from "@lib/templates";
-import { PublicFormRecord } from "@lib/types";
-import LanguageToggle from "@components/globals/LanguageToggle";
-import { useRouter } from "next/router";
 
-const NotSupported = ({
-  formInfo,
-  lang,
-  host,
-}: {
-  formInfo: PublicFormRecord;
-  lang: string;
-  host: string;
-}) => {
+const NotSupported = ({ referer }: { referer: string | null }) => {
   const { t } = useTranslation("not-supported");
-  const router = useRouter();
 
   return (
     <>
-      <title>{t("notSupported.title")}</title>
-      <main role="main">
-        <div className="block xs:mx-4 sm:mx-4 lg:mx-16 xl:mx-32 xxl:mx-48">
-          <div className="flex flex-col md:h-auto" style={{ height: "80vh" }}>
-            <div id="en">
-              <div className="flex justify-between items-center my-10">
-                <div className="xxs:w-flag-fold xs:w-flag-5s md:w-44 w-flag-desktop">
-                  {formInfo?.form.brand ? (
-                    <BrandPage formData={formInfo} lang={lang} />
-                  ) : (
-                    <DefaultPage lang={lang} />
-                  )}
-                </div>
-                <LanguageToggle />
-              </div>
-              <h1 className="md:text-small_h1 text-h1 mb-10">{t("notSupported.title")}</h1>
+      <Head>
+        <title>{t("title")}</title>
+      </Head>
 
-              <p className="my-8">{t("notSupported.body1")}</p>
-              <div className="flex sm:justify-between">
-                <div className="mr-2 w-1/3 h-full py-1 px-2 border-1 border-gray-600 lg:w-auto xl:w-auto sm:w-auto sm:mr-0">{`${host}/${router.locale}${router.asPath}`}</div>
-                <button
-                  className="bg-gray-300 hover:bg-gray-400 active:bg-gray-400 py-1 px-4 sm:p-0.5"
-                  onClick={() => {
-                    navigator.clipboard.writeText(`${host}/${router.locale}${router.asPath}`);
-                  }}
-                >
-                  {t("notSupported.copy-link")}
-                </button>
-              </div>
+      <div className="flex flex-col h-full">
+        <h1 className="md:text-small_h1 text-h1 mb-10">{t("title")}</h1>
 
-              <div>
-                <p className="my-8">{t("notSupported.body2")}</p>
+        {referer && (
+          <>
+            <p className="my-8">{t("body1")}</p>
+            <div className="flex sm:justify-between">
+              <div className="mr-2 w-1/3 h-full py-1 px-2 border-1 border-gray-600 lg:w-auto xl:w-auto sm:w-auto sm:mr-0 break-all">
+                {referer}
               </div>
-              <ul className="list-none p-0">
-                <li>
-                  <a href={t("notSupported.chrome-link")}>Chrome</a>
-                </li>
-                <li>
-                  <a href={t("notSupported.edge-link")}>Edge</a>
-                </li>
-                <li>
-                  <a href={t("notSupported.mozilla-link")}>Firefox</a>
-                </li>
-                <li>
-                  <a href={t("notSupported.safari-link")}>
-                    Safari {t("notSupported.safari-description")}
-                  </a>
-                </li>
-              </ul>
+              <button
+                className="bg-gray-300 hover:bg-gray-400 active:bg-gray-400 py-1 px-4 sm:p-0.5"
+                onClick={() => {
+                  navigator.clipboard.writeText(`${referer}`);
+                }}
+              >
+                {t("copy-link")}
+              </button>
             </div>
-          </div>
+          </>
+        )}
+
+        <div>
+          <p className="my-8">{t("body2")}</p>
         </div>
-      </main>
+        <ul className="list-none p-0">
+          <li>
+            <a href={t("chrome-link")}>Chrome</a>
+          </li>
+          <li>
+            <a href={t("edge-link")}>Edge</a>
+          </li>
+          <li>
+            <a href={t("mozilla-link")}>Firefox</a>
+          </li>
+          <li>
+            <a href={t("safari-link")}>Safari {t("safari-description")}</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
-};
-
-const DefaultPage = ({ lang }: { lang: string }) => {
-  return (
-    <>
-      {lang == "en" ? (
-        <img src="/img/sig-blk-en.svg" alt="GoC" />
-      ) : (
-        <img src="/img/sig-blk-fr.svg" alt="GoC" />
-      )}
-    </>
-  );
-};
-
-const BrandPage = ({ formData, lang }: { formData: PublicFormRecord; lang: string }) => {
-  const brand = formData.form?.brand;
-
-  return (
-    <>
-      {" "}
-      {brand ? (
-        lang == "en" ? (
-          <img src={brand?.logoEn} alt={brand?.logoTitleEn} />
-        ) : (
-          <img src={brand?.logoFr} alt={brand?.logoTitleFr} />
-        )
-      ) : (
-        <></>
-      )}
-    </>
-  );
-};
-
-NotSupported.getLayout = function (page: ReactElement) {
-  return <>{page}</>;
-};
-
-const getFormIDFromURL = (url: string) => {
-  const regex = /\/(id.+)/;
-  const regexResult = url.match(regex);
-  let splitURLPath;
-  if (regexResult) {
-    splitURLPath = regexResult[0].split("/");
-    return splitURLPath[splitURLPath.length - 1];
-  }
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const con = context.req.headers;
-  let localeProps = {};
-  let formConfig;
-  if (context.locale) {
-    localeProps = await serverSideTranslations(context.locale, ["common", "not-supported"]);
-  }
-  if (con.referer) {
-    const formId = getFormIDFromURL(con.referer);
-    if (formId) {
-      //to avoid formInfo cannot be serialized as json if `getServerSideProps` return undefined
-      formConfig = (await getPublicTemplateByID(formId)) ?? null;
-    }
-    return {
-      props: {
-        ...(context.locale && localeProps),
-        lang: context.locale,
-        formInfo: formConfig,
-        host: con.host,
-      },
-    };
-  }
+  const referer: string | null = (context.query.referer as string) ?? null;
+
   return {
-    props: { ...(context.locale && localeProps), lang: context.locale, host: con.host },
+    props: {
+      ...(context.locale &&
+        (await serverSideTranslations(context.locale, ["common", "not-supported"]))),
+      referer: referer,
+    },
   };
 };
 
