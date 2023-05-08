07255dec2c102a0a220e7171e8d1f6e02128a59d
Tim Arney <tim@line37.com>
2023-05-08 13:33:43 -0400

    use isready

--- components/globals/LanguageToggle.tsx
+++ components/globals/LanguageToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { StyledLink } from "./StyledLink/StyledLink";
@@ -6,11 +6,18 @@ import { StyledLink } from "./StyledLink/StyledLink";
 const LanguageToggle = () => {
   const { t, i18n } = useTranslation("common");
   const locale = i18n.language;
-  const { pathname } = useRouter();
+  const { pathname, asPath, isReady } = useRouter();
+  const href = useRef(pathname);
+
+  useEffect(() => {
+    if (isReady && href.current !== asPath) {
+      href.current = asPath;
+    }
+  }, [pathname, asPath, isReady]);
 
   return (
     <StyledLink
-      href={pathname}
+      href={href.current}
       className="text-base text-right"
       locale={locale === "en" ? "fr" : "en"}
       ariaLabel={`${t("lang-toggle")}: ${locale == "en" ? "Français" : "English"}`}
