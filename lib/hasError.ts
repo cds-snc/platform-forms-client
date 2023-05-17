a281ec74f4e5a486def36070dbbafad65c5adeec
Tim Arney <tim@line37.com>
2023-05-17 09:02:22 -0400

    update has error

--- lib/hasError.ts
+++ lib/hasError.ts
@@ -1,4 +1,23 @@
-export const hasError = (errorNames: string | string[], message: string) => {
+import axios, { AxiosError } from "axios";
+
+export const hasError = (
+  errorNames: string | string[],
+  error: string | Error | AxiosError | unknown | undefined = ""
+) => {
+  if (!error) {
+    return false;
+  }
+
+  let message = "";
+
+  if (axios.isAxiosError(error)) {
+    message = error.response?.data?.message as string;
+  } else if (error instanceof Error) {
+    message = error.message;
+  } else {
+    message = error.toString();
+  }
+
   if (typeof errorNames === "string") {
     errorNames = [errorNames];
   }
--- lib/tests/hasError.test.ts
+++ lib/tests/hasError.test.ts
@@ -1,6 +1,7 @@
 import { hasError } from "@lib/hasError";
+import mockedAxios from "axios";
 
-describe("Extract Form data", () => {
+describe("Handles string values", () => {
   test("error single value array", () => {
     const message = "UsernameExistsException: An account with the given email already exists.";
     expect(hasError(["UsernameExistsException"], message)).toBe(true);
@@ -29,5 +30,57 @@ describe("Extract Form data", () => {
   test("no error single string", () => {
     const message = "CodeMismatchException: Invalid code provided, please try again.";
     expect(hasError("UsernameExistsException", message)).toBe(false);
+  });
+});
+
+describe("Handles Error messages", () => {
+  test("contains error message", () => {
+    const message = new Error(
+      "UsernameExistsException: An account with the given email already exists."
+    );
+    expect(hasError(["UsernameExistsException"], message)).toBe(true);
+  });
+
+  test("doesn't contain error message", () => {
+    const message = new Error("CodeMismatchException: Invalid code provided, please try again.");
+    expect(hasError(["UsernameExistsException"], message)).toBe(false);
+  });
+});
+
+describe("Handles Axios Error messages", () => {
+  test("has axios error", () => {
+    const error = {
+      config: {},
+      level: 50,
+      response: {
+        status: 400,
+        statusText: "Bad Request",
+        data: {
+          message: "UsernameExistsException: An account with the given email already exists.",
+        },
+      },
+      isAxiosError: true,
+      toJSON: () => ({}),
+    };
+
+    expect(hasError(["UsernameExistsException"], error)).toBe(true);
+  });
+
+  test("doesn't have axios error", () => {
+    const error = {
+      config: {},
+      level: 50,
+      response: {
+        status: 400,
+        statusText: "Bad Request",
+        data: {
+          message: "CodeMismatchException: Invalid code provided, please try again.",
+        },
+      },
+      isAxiosError: true,
+      toJSON: () => ({}),
+    };
+
+    expect(hasError(["UsernameExistsException"], error)).toBe(false);
   });
 });
