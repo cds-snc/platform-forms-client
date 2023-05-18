540456d3650633fba03fe78af064deb2d92f3aec
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-05-18 10:35:26 -0400

    create and send code through Notify

---
+++ lib/2fa.ts
@@ -0,0 +1,36 @@
+import { NotifyClient } from "notifications-node-client";
+import { logMessage } from "@lib/logger";
+import crypto from "crypto";
+const TEMPLATE_ID = process.env.MFA_TEMPLATE_ID;
+const NOTIFY_API_KEY = process.env.NOTIFY_API_KEY;
+
+export const generateVerificationCode = async () => {
+  // Temporary - for testing purposes
+  // Will create more robust code generation in the future
+  return crypto.randomBytes(5).toString("hex");
+};
+
+export const sendVerificationCode = async (email: string, verificationCode: string) => {
+  try {
+    // attempt to send the code to the user through Notify
+    // setup the notify client
+    const notify = new NotifyClient("https://api.notification.canada.ca", NOTIFY_API_KEY);
+
+    await notify.sendEmail(TEMPLATE_ID, email, {
+      personalisation: {
+        passwordReset: false,
+        accountVerification: false,
+        resendCode: false,
+        mfa: true,
+        code: verificationCode,
+      },
+    });
+  } catch (err) {
+    logMessage.error(
+      `{"status": "failed", "message": "Notify Failed To Send the Code", "error":${
+        (err as Error).message
+      }}`
+    );
+    throw new Error("Notify failed to send the code");
+  }
+};
