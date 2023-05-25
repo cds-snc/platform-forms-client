df3e240a7eee913189ed43cc33d52b9e9f0fdd17
Tim Arney <timarney@users.noreply.github.com>
2023-05-25 15:11:06 -0400

    2fa sign-in screen (#2152)

---
+++ lib/hooks/auth/useVerify.ts
@@ -0,0 +1,31 @@
+import { logMessage } from "@lib/logger";
+import { signIn } from "next-auth/react";
+
+export const useVerify = () => {
+  const verify = async ({
+    username,
+    verificationCode,
+    authenticationFlowToken,
+  }: {
+    username: string;
+    verificationCode: string;
+    authenticationFlowToken: string;
+  }) => {
+    try {
+      return signIn("cognito", {
+        username: username,
+        verificationCode: verificationCode,
+        authenticationFlowToken,
+        redirect: false,
+        json: true,
+      });
+    } catch (err) {
+      logMessage.error(err);
+      return false;
+    }
+  };
+
+  return {
+    verify,
+  };
+};
