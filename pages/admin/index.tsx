28d4888f7ee273ccb50fa4f375d5c76beb287009
Tim Arney <timarney@users.noreply.github.com>
2023-06-01 09:01:36 -0400

    Admin fr strings (#2194)
    cleanup

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,16 +1,14 @@
 import React, { ReactElement } from "react";
 import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { requireAuthentication } from "@lib/auth";
-import Link from "next/link";
 import Head from "next/head";
 import { NextPageWithLayout } from "@pages/_app";
 import AdminNavLayout from "@components/globals/layouts/AdminNavLayout";
-import { useAccessControl } from "@lib/hooks";
+import { checkPrivilegesAsBoolean } from "@lib/privileges";
 
+// keeping this here if we want to add a welcome page
 const AdminWelcome: NextPageWithLayout = () => {
   const { t } = useTranslation(["admin-login", "common"]);
-  const { ability } = useAccessControl();
 
   return (
     <>
@@ -18,46 +16,29 @@ const AdminWelcome: NextPageWithLayout = () => {
         <title>{t("title")}</title>
       </Head>
       <h1 className="border-0 mb-10">{t("title")}</h1>
-      <div className="flex flex-wrap">
-        <div className="mb-10 w-96">
-          <h3>Manage users</h3>
-          {ability?.can("view", "User") && (
-            <p>
-              <Link href="/admin/users">{t("adminNav.users", { ns: "common" })}</Link>
-            </p>
-          )}
-        </div>
-        <div className="mb-10  w-96">
-          <h3>Application Settings</h3>
-          {ability?.can("view", "Flag") && (
-            <p>
-              <Link href="/admin/flags">{t("adminNav.features", { ns: "common" })}</Link>
-            </p>
-          )}
-          {ability?.can("view", "Flag") && (
-            <p>
-              <Link href="/admin/settings">{t("adminNav.settings", { ns: "common" })}</Link>
-            </p>
-          )}
-          {ability?.can("view", "Privilege") && (
-            <p>
-              <Link href="/admin/privileges">{t("adminNav.privileges", { ns: "common" })}</Link>
-            </p>
-          )}
-        </div>
-      </div>
     </>
   );
 };
 AdminWelcome.getLayout = (page: ReactElement) => {
   return <AdminNavLayout user={page.props.user}>{page}</AdminNavLayout>;
 };
-export const getServerSideProps = requireAuthentication(async ({ locale }) => {
-  return {
-    props: {
-      ...(locale && (await serverSideTranslations(locale, ["common", "admin-login"]))),
-    },
-  };
+export const getServerSideProps = requireAuthentication(async ({ user: { ability } }) => {
+  const canViewUsers = checkPrivilegesAsBoolean(ability, [{ action: "view", subject: "User" }]);
+  if (canViewUsers) {
+    return {
+      redirect: {
+        destination: `/admin/users`,
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: `/myforms`,
+        permanent: false,
+      },
+    };
+  }
 });
 
 export default AdminWelcome;
