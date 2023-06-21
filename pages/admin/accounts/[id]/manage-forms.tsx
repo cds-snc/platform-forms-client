d9e594059b3004b07c7171f5a16477626a85e85a
Tim Arney <tim@line37.com>
2023-06-21 12:32:55 -0400

    update page

--- pages/admin/accounts/[id]/manage-forms.tsx
+++ pages/admin/accounts/[id]/manage-forms.tsx
@@ -3,18 +3,72 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { requireAuthentication } from "@lib/auth";
 import { useTranslation } from "next-i18next";
 import Head from "next/head";
+import { getUser } from "@lib/users";
 import { checkPrivileges } from "@lib/privileges";
 import AdminNavLayout from "@components/globals/layouts/AdminNavLayout";
+import { getAllTemplates } from "@lib/templates";
+import { LinkButton } from "@components/globals";
 
-const ManageForms = ({ userId }: { userId: string }) => {
-  const { t } = useTranslation("admin-forms");
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  active: boolean;
+};
+
+type Templates = Array<{
+  id: string;
+  titleEn: string;
+  titleFr: string;
+  isPublished: boolean;
+  [key: string]: string | boolean;
+}>;
+
+const ManageForms = ({ user, templates }: { user: User; templates: Templates }) => {
+  const { t, i18n } = useTranslation("admin-forms");
   return (
     <>
       <Head>
-        <title>{t("manageForms")}</title>
+        <title>{t("title")}</title>
       </Head>
-      <h1 className="border-0 mb-10">{t("title")}</h1>
-      {userId}
+      <div>
+        <h1 className="mb-10 border-0">
+          <span className="block">{user.name}</span>
+          {t("title")}
+        </h1>
+      </div>
+      <ul className="m-0 list-none p-0">
+        {templates.map(({ id, titleEn, titleFr, isPublished }) => {
+          const bgColor = isPublished ? "#95CCA2" : "#FFD875";
+          return (
+            <li
+              className="mb-4 flex max-w-xl flex-row rounded-md border-2 border-black p-2"
+              key={id}
+            >
+              <div className="m-auto grow basis-1/3 p-4">
+                <div className="flex justify-between">
+                  <div>
+                    <h2 className="text-base">
+                      {titleEn} / {titleFr}
+                    </h2>
+                    <LinkButton.Secondary
+                      href={`/${i18n.language}/form-builder/responses/${id}`}
+                      className="mb-2 mr-3"
+                    >
+                      {t("gotoResponses")}
+                    </LinkButton.Secondary>
+                  </div>
+                  <div>
+                    <span className="p-2" style={{ backgroundColor: bgColor }}>
+                      {isPublished ? t("published") : t("draft")}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
@@ -27,11 +81,31 @@ export const getServerSideProps = requireAuthentication(
   async ({ locale, params, user: { ability } }) => {
     const id = params?.id || null;
     checkPrivileges(ability, [{ action: "view", subject: "Setting" }]);
+    const user = await getUser(id as string, ability);
+
+    let templates: Templates = [];
+    if (id) {
+      templates = (await getAllTemplates(ability, id as string)).map((template) => {
+        const {
+          id,
+          form: { titleEn, titleFr },
+          isPublished,
+        } = template;
+        return {
+          id,
+          titleEn,
+          titleFr,
+          isPublished,
+        };
+      });
+    }
+
     return {
       props: {
         ...(locale &&
           (await serverSideTranslations(locale, ["common", "admin-forms", "admin-login"]))),
-        userId: id,
+        user,
+        templates,
       },
     };
   }
