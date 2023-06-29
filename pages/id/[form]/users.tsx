23c54a0c5155772f79979b035576cf7c2ab1ed4b
Tim Arney <tim@line37.com>
2023-06-29 10:08:12 -0400

    add success fail for manage users

--- pages/id/[form]/users.tsx
+++ pages/id/[form]/users.tsx
@@ -9,6 +9,7 @@ import Head from "next/head";
 
 import { getTemplateWithAssociatedUsers } from "@lib/templates";
 import AdminNavLayout from "@components/globals/layouts/AdminNavLayout";
+import { Alert, ErrorStatus } from "@components/forms/Alert/Alert";
 import { getUsers } from "@lib/users";
 import { Button } from "@components/globals";
 import { checkPrivileges } from "@lib/privileges";
@@ -59,11 +60,13 @@ const Users = ({
 }: AssignUsersToTemplateProps): React.ReactElement => {
   const { t, i18n } = useTranslation("admin-users");
   const language = i18n.language as string;
+  const [message, setMessage] = useState<ReactElement | null>(null);
   const assignedToFormRecord = usersToOptions(usersAssignedToFormRecord);
   const [selectedUsers, setSelectedUsers] =
     useState<{ value: string; label: string | null }[]>(assignedToFormRecord);
 
   const saveAssignations = async () => {
+    setMessage(null);
     const usersToAdd: { id: string; action: "add" | "remove" }[] = selectedUsers
       .filter((user) => {
         return !usersAssignedToFormRecord.map((u) => u.id).includes(user.value);
@@ -80,8 +83,37 @@ const Users = ({
         return { id: user.id, action: "remove" };
       });
 
-    await updateUsersToTemplateAssignations(formRecord.id, usersToAdd.concat(usersToRemove));
-    // @todo add success / fail  message
+    const response = await updateUsersToTemplateAssignations(
+      formRecord.id,
+      usersToAdd.concat(usersToRemove)
+    );
+
+    if (response && response.status === 200) {
+      setMessage(
+        <Alert
+          type={ErrorStatus.SUCCESS}
+          focussable={true}
+          heading={t("responseSuccess.title")}
+          tabIndex={0}
+          className="mb-2"
+        >
+          {t("responseSuccess.message")}
+        </Alert>
+      );
+      return response.data;
+    }
+
+    setMessage(
+      <Alert
+        type={ErrorStatus.ERROR}
+        focussable={true}
+        heading={t("responseFail.title")}
+        tabIndex={0}
+        className="mb-2"
+      >
+        {t("responseFail.message")}
+      </Alert>
+    );
   };
 
   return (
@@ -93,6 +125,8 @@ const Users = ({
       <h1 className="mb-0 border-b-0 text-h1 font-bold md:mb-10 md:text-small_h1">
         {`${t("title")} - ${formRecord.form[getProperty("title", language)] as string}`}
       </h1>
+
+      {message && message}
 
       <p className="mb-4">{t("assignUsersToTemplate")}</p>
       <p className="mb-4 font-bold">{t("enterOwnersEmail")} </p>
