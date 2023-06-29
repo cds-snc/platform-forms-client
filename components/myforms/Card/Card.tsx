72e031ff9e9f79529fd14e31b1c8fecc68faa591
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-06-29 11:38:09 -0400

    display block

--- components/myforms/Card/Card.tsx
+++ components/myforms/Card/Card.tsx
@@ -150,7 +150,7 @@ export const Card = (props: CardProps): React.ReactElement => {
         {deliveryOption && !deliveryOption.emailAddress && (
           <>
             {overdue > 0 ? (
-              <span className="mt-4 text-sm text-red">
+              <span className="mt-4 block text-sm text-red">
                 <MessageIcon className="mr-2 inline-block" />
                 <Markdown options={{ forceBlock: false }}>
                   {t("card.actionRequired", { responses: overdue, link: responsesLink })}
