1a9da080083dc5ae93dbde1c43b7b6ff0d83fc03
Tim Arney <timarney@users.noreply.github.com>
2023-07-14 07:31:29 -0400

    Account screen - scroll to user (#2397)
    * add session storage + add auto scroll

--- pages/admin/accounts/[id]/manage-forms.tsx
+++ pages/admin/accounts/[id]/manage-forms.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState, useRef } from "react";
+import React, { ReactElement, useState, useRef, useEffect } from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { requireAuthentication } from "@lib/auth";
 import { useTranslation } from "next-i18next";
@@ -19,6 +19,7 @@ import { TemplateStoreProvider } from "@components/form-builder/store";
 import { useAccessControl } from "@lib/hooks/useAccessControl";
 import { useRefresh } from "@lib/hooks";
 import { ExclamationIcon } from "@components/form-builder/icons";
+import { setStorageValue, STORAGE_KEY } from "@lib/sessionStorage";
 
 type User = {
   id: string;
@@ -82,6 +83,7 @@ const ManageForms = ({
   const { ability } = useAccessControl();
   const { refreshData } = useRefresh();
   const canManageForms = ability?.can("update", "FormRecord");
+
   return (
     <>
       <Head>
@@ -106,7 +108,11 @@ const ManageForms = ({
           const backgroundColor = isPublished ? "#95CCA2" : "#FEE39F";
           const borderColor = isPublished ? "#95CCA2" : "#FFD875";
           return (
-            <li className="mb-4 max-w-2xl rounded-md border-2 border-black p-4" key={id}>
+            <li
+              className="mb-4 max-w-2xl rounded-md border-2 border-black p-4"
+              key={id}
+              id={`form-${id}`}
+            >
               <div className="flex flex-row items-start justify-between">
                 <h2 className="mb-0 mr-2 overflow-hidden pb-0 text-base">
                   {i18n.language === "en" ? (
@@ -183,14 +189,14 @@ const ManageForms = ({
   );
 };
 
-const BackToAccounts = () => {
+const BackToAccounts = ({ id }: { id: string }) => {
   const { t } = useTranslation("admin-users");
-  return <BackLink href="/admin/accounts">{t("backToAccounts")}</BackLink>;
+  return <BackLink href={`/admin/accounts?id=${id}`}>{t("backToAccounts")}</BackLink>;
 };
 
 ManageForms.getLayout = (page: ReactElement) => {
   return (
-    <AdminNavLayout user={page.props.user} backLink={<BackToAccounts />}>
+    <AdminNavLayout user={page.props.user} backLink={<BackToAccounts id={page.props.formUser} />}>
       <TemplateStoreProvider {...{ locale: page.props.locale }}>{page}</TemplateStoreProvider>
     </AdminNavLayout>
   );
