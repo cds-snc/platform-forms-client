08d91bf13ea942d9a3a4d826e225fab2729169e2
Daine Trinidad <daine.trinidad@gmail.com>
2023-07-14 07:50:46 -0700

    feat: Delete test responses on publish (#2367)
    * wip: first pass at deleting test responses upon publish
    
    * fix: remove ui limit for getting list of submissions and responses
    
    * feat: new helper utils for checking delivery options
    
    * chore: moved helper utils to util file
    
    * chore: change typescript return type
    
    * chore: update width
    
    * feat: db query to check if the form is published before deleting responses
    
    * chore: some cleanup
    
    * chore: move db check to function that actually deletes
    
    * chore: cleanup
    
    * feat: add test for deleting when a form is published
    
    * chore: better error exception & cleanup
    
    * fix: remove ? on parameter
    
    * chore: update delivery option helper signature
    
    * chore: lint fixes
    
    * Fixes minor issues with infocard semantics
    
    * chore: remove export for some functions
    
    ---------
    
    Co-authored-by: Peter Thiessen <peter.thiessen@cds-snc.ca>

--- lib/tests/vault.test.ts
+++ lib/tests/vault.test.ts
@@ -6,11 +6,14 @@ import Redis from "ioredis-mock";
 import { mockClient } from "aws-sdk-client-mock";
 import { prismaMock } from "@jestUtils";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
-import { createAbility } from "@lib/privileges";
-import { Base, mockUserPrivileges } from "__utils__/permissions";
+import { AccessControlError, createAbility } from "@lib/privileges";
+import { Base, ManageForms, mockUserPrivileges } from "__utils__/permissions";
 import { Session } from "next-auth";
-import { numberOfUnprocessedSubmissions } from "@lib/vault";
+import { deleteResponses, numberOfUnprocessedSubmissions } from "@lib/vault";
 import formConfiguration from "@jestFixtures/cdsIntakeTestForm.json";
+import { DeliveryOption } from "@lib/types";
+import { BatchWriteItemCommand } from "@aws-sdk/client-dynamodb";
+import { TemplateAlreadyPublishedError } from "@lib/templates";
 
 const ddbMock = mockClient(DynamoDBDocumentClient);
 
@@ -169,5 +172,106 @@ describe("Vault `numberOfUnprocessedSubmissions` function", () => {
     await numberOfUnprocessedSubmissions(ability, "formtestID", true);
 
     expect(ddbMock.commandCalls(QueryCommand).length).toBe(4);
+  });
+});
+const buildPrismaResponse = (
+  id: string,
+  jsonConfig: object,
+  isPublished = false,
+  deliveryOption?: DeliveryOption,
+  securityAttribute = "Unclassified"
+) => {
+  return {
+    id,
+    jsonConfig,
+    deliveryOption,
+    isPublished,
+    securityAttribute,
+  };
+};
+describe("Deleting test responses (submissions)", () => {
+  beforeEach(() => {
+    ddbMock.reset();
+    redis.flushall();
+  });
+  it("Should be able to delete if the user has the ManageForms privileges", async () => {
+    const fakeSession = {
+      user: {
+        id: "1",
+        privileges: mockUserPrivileges(Base.concat(ManageForms), { user: { id: "1" } }),
+      },
+    };
+    const ability = createAbility(fakeSession as Session);
+    // Mock an unpublished form
+    (prismaMock.template.findUnique as jest.MockedFunction<any>).mockResolvedValue({
+      ...buildPrismaResponse("formtestID", formConfiguration, false),
+      users: [{ id: "1" }],
+    });
+
+    const formResponses = [
+      {
+        formID: "formtestID",
+        name: "Form Test ID",
+        confirmationCode: "Form-Test-ID",
+      },
+      {
+        formID: "formtestID",
+        name: "Form Test ID 2",
+        confirmationCode: "Form-Test-ID-2",
+      },
+    ];
+    const response = await deleteResponses(ability, ddbMock, formResponses, "formtestID");
+
+    expect(ddbMock.commandCalls(BatchWriteItemCommand).length).toBe(1);
+    expect(response.responsesDeleted).toBe(2);
+  });
+
+  it("Should not be able to delete if the user doesn't have the ManageForms privileges", async () => {
+    const fakeSession = {
+      user: { id: "1", privileges: mockUserPrivileges([], { user: { id: "1" } }) },
+    };
+    const ability = createAbility(fakeSession as Session);
+    (prismaMock.template.findUnique as jest.MockedFunction<any>).mockResolvedValue({
+      ...buildPrismaResponse("formtestID", formConfiguration),
+      users: [{ id: "1" }],
+    });
+
+    await expect(async () => {
+      await deleteResponses(ability, ddbMock, [], "formtestID");
+    }).rejects.toThrowError(new AccessControlError(`Access Control Forbidden Action`));
+  });
+
+  it("Should not be able to delete responses if the form is published", async () => {
+    const fakeSession = {
+      user: {
+        id: "1",
+        privileges: mockUserPrivileges(Base.concat(ManageForms), { user: { id: "1" } }),
+      },
+    };
+    const ability = createAbility(fakeSession as Session);
+
+    (prismaMock.template.findUnique as jest.MockedFunction<any>).mockResolvedValue({
+      ...buildPrismaResponse("formtestID", formConfiguration, true),
+      users: [{ id: "1" }],
+    });
+
+    const formResponses = [
+      {
+        formID: "formtestID",
+        name: "Form Test ID",
+        confirmationCode: "Form-Test-ID",
+      },
+      {
+        formID: "formtestID",
+        name: "Form Test ID 2",
+        confirmationCode: "Form-Test-ID-2",
+      },
+    ];
+
+    await expect(async () => {
+      await deleteResponses(ability, ddbMock, formResponses, "formtestID");
+    }).rejects.toThrowError(
+      new TemplateAlreadyPublishedError("Form is published. Cannot delete draft form responses.")
+    );
   });
 });
