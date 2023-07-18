ef4d99dce8b570289c315cb90899556338bb98de
Peter Thiessen <peter.thiessen@cds-snc.ca>
2023-07-18 15:38:06 -0400

    Updates accounts page to use id selectors

--- cypress/e2e/accounts_page.cy.ts
+++ cypress/e2e/accounts_page.cy.ts
@@ -1,10 +1,8 @@
-// TODO update tests to use the below for clicking buttons and panels, avoids future problem with adding new users
-// const adminUserEmail = "testadmin.user@cds-snc.ca";
-// const testUserEmail = "test.user@cds-snc.ca";
-
-//TODO: use data-testid's for more stable selectors in the future e.g. instead of searching for text "All"
-
 describe("Accounts Page", () => {
+  const adminUserEmail = "testadmin.user@cds-snc.ca";
+  const testUserEmail = "test.user@cds-snc.ca";
+  const deactivatedUserEmail = "test.deactivated@cds-snc.ca";
+
   beforeEach(() => {
     cy.login(true, true);
     cy.visitPage("/en/admin/accounts");
@@ -19,63 +17,88 @@ describe("Accounts Page", () => {
     cy.get("h1").should("contain", "Accounts");
   });
 
-  // NOTE: these tests assume TWO accounts in the system. If you add another user, update the length checks
   describe("Accounts tabs/filters and cards", () => {
-    it("Clicking tabs/filters update with expected content", () => {
+    it("Clicking tabs/filters updates with expected content", () => {
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 2);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
 
       cy.get("button").contains("Active").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 2);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li")
+        .contains(deactivatedUserEmail)
+        .should("not.exist");
 
       cy.get("button").contains("Deactivated").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 0);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail).should("not.exist");
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail).should("not.exist");
     });
 
-    it("Clicking lock/unlock of an account updates the button text state", () => {
+    it("Clicking lock/unlock publishing of an account updates the button text state", () => {
       cy.get("button").contains("All").click();
       // Lock an account
-      cy.get("ul[data-testid='accountsList'] li").last().contains("Lock publishing").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Lock publishing").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Unlock publishing");
 
-      //Unlock an account
-      cy.get("ul[data-testid='accountsList'] li").last().contains("Unlock publishing");
+      // Unlock an account
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Unlock publishing").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Lock publishing");
     });
 
     it("Clicking manage forms navigates to the related page", () => {
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").last().contains("Manage forms").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Manage forms").click();
       cy.get("h1").contains("Manage forms");
     });
 
     it("Account deactivation updates the card and related tabs/filters lists correctly", () => {
-      // Deactivate account
+      // Deactivate the test.user and check the tab states updated correctly
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").last().contains("More").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("More").click();
       cy.get("div[role='menuitem']").contains("Deactivate account").click();
       cy.get("dialog").contains("Deactivate account").click();
-      cy.get("ul[data-testid='accountsList'] li").last().contains("Reactivate account");
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Reactivate account");
 
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 2);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
 
       cy.get("button").contains("Active").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 1);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail).should("not.exist");
+      cy.get("ul[data-testid='accountsList'] li")
+        .contains(deactivatedUserEmail)
+        .should("not.exist");
 
       cy.get("button").contains("Deactivated").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 1);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail).should("not.exist");
 
-      // Reactivate account
+      // Reactivate the test.user account and check the tab states updated correctly
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").last().contains("Reactivate account").click();
+      cy.get(`li[data-testid="${testUserEmail}"]`).contains("Reactivate account").click();
 
       cy.get("button").contains("All").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 2);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
 
       cy.get("button").contains("Active").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 2);
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail);
+      cy.get("ul[data-testid='accountsList'] li")
+        .contains(deactivatedUserEmail)
+        .should("not.exist");
 
       cy.get("button").contains("Deactivated").click();
-      cy.get("ul[data-testid='accountsList'] li").should("have.length", 0);
+      cy.get("ul[data-testid='accountsList'] li").contains(deactivatedUserEmail);
+      cy.get("ul[data-testid='accountsList'] li").contains(testUserEmail).should("not.exist");
+      cy.get("ul[data-testid='accountsList'] li").contains(adminUserEmail).should("not.exist");
     });
   });
 });
