ee3bda09e89b1fc1518c2fcb1d9a2c1eee014e52
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-07-19 16:04:14 -0400

    Download table cypress test (#2419)
    * Add some component tests around DownloadTable
    
    * test
    
    * Add cypress tsconfig.json
    
    * Add data attribute for testing
    
    * Flesh out tests a bit

--- .eslint-tailwindcss.js
+++ .eslint-tailwindcss.js
@@ -6,7 +6,7 @@ module.exports = {
   
         parser: "@typescript-eslint/parser",
         parserOptions: {
-          project: ["./tsconfig.json"],
+          project: ["./tsconfig.json", "./cypress/tsconfig.json"],
         },
         plugins: ["eslint-plugin-tailwindcss"],
       },
