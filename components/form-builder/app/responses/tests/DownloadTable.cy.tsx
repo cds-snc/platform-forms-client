f9023f868f50212eb6ddb48e3d0ff046c368324f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-07-19 15:24:47 -0400

    Flesh out tests a bit

--- components/form-builder/app/responses/tests/DownloadTable.cy.tsx
+++ components/form-builder/app/responses/tests/DownloadTable.cy.tsx
@@ -1,48 +1,16 @@
 import React from "react";
-import { DownloadTable } from "@components/form-builder/app/responses/DownloadTable";
-import { Nagware } from "@components/form-builder/app/Nagware";
+import { DownloadTable } from "../../responses/DownloadTable";
+import { Nagware } from "../../Nagware";
 import Router from "next/router";
-import { NagLevel, VaultStatus, VaultSubmissionList } from "@lib/types";
+import { NagLevel, VaultStatus, VaultSubmissionList } from "../../../../../lib/types";
 
-const vaultSubmissions: VaultSubmissionList[] = [
-  {
-    formID: "clg17xha50008efkgfgxa8l4f",
-    status: VaultStatus.NEW,
-    securityAttribute: "Unclassified",
-    name: "03-04-0022",
-    createdAt: 1686584665000,
-    lastDownloadedBy: "",
-    confirmedAt: undefined,
-    downloadedAt: undefined,
-    removedAt: undefined,
-  },
-  {
-    formID: "clg17xha50008efkgfgxa8l4f",
-    status: VaultStatus.NEW,
-    securityAttribute: "Unclassified",
-    name: "03-04-0c36",
-    createdAt: 1680549843373,
-    lastDownloadedBy: "peter.thiessen@cds-snc.ca",
-    confirmedAt: 1680551168405,
-    downloadedAt: 1680550035137,
-    removedAt: 1683143168405,
-  },
-  {
-    formID: "clg17xha50008efkgfgxa8l4f",
-    status: VaultStatus.NEW,
-    securityAttribute: "Unclassified",
-    name: "03-04-8a87",
-    createdAt: 1689695065000,
-    lastDownloadedBy: "",
-    confirmedAt: undefined,
-    downloadedAt: undefined,
-    removedAt: undefined,
-  },
-];
+const today = new Date("July 16, 2023").valueOf();
 
-const nagwareResult = {
-  level: NagLevel.UnsavedSubmissionsOver35DaysOld,
-  numberOfSubmissions: 1,
+const subDays = (date: number, days: number) => {
+  const dt = new Date(date);
+  const dt2 = new Date();
+  dt2.setDate(dt.getDate() - days);
+  return dt2;
 };
 
 describe("<Nagware />", () => {
@@ -51,12 +19,13 @@ describe("<Nagware />", () => {
       <Nagware
         nagwareResult={{
           level: NagLevel.UnsavedSubmissionsOver35DaysOld,
-          numberOfSubmissions: 1,
+          numberOfSubmissions: 5,
         }}
       />
     );
 
     cy.get("[role=alert]").should("have.attr", "class").and("contain", "bg-[#f3e9e8]");
+    cy.get("[data-testid=numberOfSubmissions]").should("contain", "5");
   });
 
   it("renders UnconfirmedOver35Days", () => {
@@ -64,12 +33,13 @@ describe("<Nagware />", () => {
       <Nagware
         nagwareResult={{
           level: NagLevel.UnconfirmedSubmissionsOver35DaysOld,
-          numberOfSubmissions: 1,
+          numberOfSubmissions: 3,
         }}
       />
     );
 
     cy.get("[role=alert]").should("have.attr", "class").and("contain", "bg-[#f3e9e8]");
+    cy.get("[data-testid=numberOfSubmissions]").should("contain", "3");
   });
 
   it("renders UnsavedOver21Days", () => {
@@ -83,6 +53,7 @@ describe("<Nagware />", () => {
     );
 
     cy.get("[role=alert]").should("have.attr", "class").and("contain", "bg-amber-100");
+    cy.get("[data-testid=numberOfSubmissions]").should("contain", "1");
   });
 
   it("renders UnconfirmedOver21Days", () => {
@@ -90,12 +61,13 @@ describe("<Nagware />", () => {
       <Nagware
         nagwareResult={{
           level: NagLevel.UnconfirmedSubmissionsOver21DaysOld,
-          numberOfSubmissions: 1,
+          numberOfSubmissions: 2,
         }}
       />
     );
 
     cy.get("[role=alert]").should("have.attr", "class").and("contain", "bg-amber-100");
+    cy.get("[data-testid=numberOfSubmissions]").should("contain", "2");
   });
 });
 
@@ -111,15 +83,153 @@ describe("<DownloadTable />", () => {
 
     cy.stub(Router, "useRouter").returns(router);
     cy.intercept("GET", "/api/settings/nagwarePhaseWarned", { setting: "35" });
-    cy.intercept("GET", "/api/settings/nagwarePhaseEncouraged", { setting: "15" });
+    cy.intercept("GET", "/api/settings/nagwarePhaseEncouraged", { setting: "21" });
   });
 
-  it("opens the add element dialog", () => {
+  it("Blocks download of newer items when one is overdue by 35 days (account restricted)", () => {
+    const vaultSubmissions: VaultSubmissionList[] = [
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 20 days",
+        createdAt: subDays(today, 20).valueOf(), // (-20 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 23 days",
+        createdAt: subDays(today, 23).valueOf(), // (-23 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 36 days",
+        createdAt: subDays(today, 36).valueOf(), // (-36 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Today",
+        createdAt: today, // (today)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+    ];
+
+    const nagwareResult = {
+      level: NagLevel.UnsavedSubmissionsOver35DaysOld,
+      numberOfSubmissions: 1,
+    };
+
     cy.viewport(1050, 550);
     cy.mount(<DownloadTable vaultSubmissions={vaultSubmissions} nagwareResult={nagwareResult} />);
 
+    // The -35 overdue causes the rest to be blocked
     cy.get("tbody tr:nth-child(1)").should("have.attr", "class").and("contain", "opacity-50");
     cy.get("tbody tr:nth-child(2)").should("have.attr", "class").and("contain", "opacity-50");
+    cy.get("tbody tr:nth-child(3)").should("have.attr", "class").and("contain", "opacity-50");
+    cy.get("tbody tr:nth-child(4)").should("have.attr", "class").and("not.contain", "opacity-50");
+
+    cy.get("tbody tr:nth-child(4) td:nth-child(4)").should(
+      "contain",
+      "downloadResponsesTable.status.overdue"
+    );
+
+    // The -23 days should additionally have a warning
+    cy.get("tbody tr:nth-child(3) td:nth-child(4)").should(
+      "contain",
+      "downloadResponsesTable.status.overdue"
+    );
+  });
+
+  it("Warns for overdue submissions (not restricted)", () => {
+    const vaultSubmissions: VaultSubmissionList[] = [
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 20 days",
+        createdAt: subDays(today, 20).valueOf(), // (-20 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 23 days",
+        createdAt: subDays(today, 23).valueOf(), // (-23 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Minus 36 days",
+        createdAt: subDays(today, 30).valueOf(), // (-30 days)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+      {
+        formID: "clg17xha50008efkgfgxa8l4f",
+        status: VaultStatus.NEW,
+        securityAttribute: "Unclassified",
+        name: "Today",
+        createdAt: today, // (today)
+        lastDownloadedBy: "",
+        confirmedAt: undefined,
+        downloadedAt: undefined,
+        removedAt: undefined,
+      },
+    ];
+
+    const nagwareResult = {
+      level: NagLevel.UnsavedSubmissionsOver21DaysOld,
+      numberOfSubmissions: 1,
+    };
+
+    cy.viewport(1050, 550);
+    cy.mount(<DownloadTable vaultSubmissions={vaultSubmissions} nagwareResult={nagwareResult} />);
+
+    cy.get("tbody tr:nth-child(1)").should("have.attr", "class").and("not.contain", "opacity-50");
+    cy.get("tbody tr:nth-child(2)").should("have.attr", "class").and("not.contain", "opacity-50");
     cy.get("tbody tr:nth-child(3)").should("have.attr", "class").and("not.contain", "opacity-50");
+    cy.get("tbody tr:nth-child(4)").should("have.attr", "class").and("not.contain", "opacity-50");
+
+    cy.get("tbody tr:nth-child(4) td:nth-child(4)").should(
+      "contain",
+      "downloadResponsesTable.status.overdue"
+    );
+
+    // The -23 days should additionally have a warning
+    cy.get("tbody tr:nth-child(3) td:nth-child(4)").should(
+      "contain",
+      "downloadResponsesTable.status.overdue"
+    );
   });
 });
