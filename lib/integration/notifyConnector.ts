d8cd63991a3024c9d655649869e4b162a73e779f
Peter Thiessen <peter.thiessen@cds-snc.ca>
2023-07-20 14:17:18 -0400

    Adds a mock email response for mockNotify

--- lib/integration/notifyConnector.ts
+++ lib/integration/notifyConnector.ts
@@ -9,6 +9,23 @@ let notifyInstance: any = null;
 const mockNotify = {
   sendEmail: () => {
     logMessage.info("Mock Notify email sent.");
+
+    // Returns an example Notify response with sender etc. replaced with generic text
+    return {
+      "id": "740e5834-3a29-46b4-9a6f-16142fde533a",
+      "reference": "STRING",
+      "content": {
+        "subject": "SUBJECT TEXT",
+        "body": "MESSAGE TEXT",
+        "from_email": "SENDER EMAIL"
+      },
+      "uri": "https://api.notification.canada.ca/v2/notifications/740e5834-3a29-46b4-9a6f-16142fde533a",
+      "template": {
+        "id": "f33517ff-2a88-4f6e-b855-c550268ce08a",
+        "version": 1,
+        "uri": "https://api.notification.canada.ca/v2/template/f33517ff-2a88-4f6e-b855-c550268ce08a"
+      }
+    }
   },
 };
 
