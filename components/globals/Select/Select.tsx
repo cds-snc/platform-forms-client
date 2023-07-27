00da34485444932029f2ff4c05ccd4ab49a67184
Pete <107579368+thiessenp-cds@users.noreply.github.com>
2023-07-27 14:01:00 -0400

    Setup security questions (#2460)
    * Adds fields and localization strings
    
    * Moves work from signup to auth
    
    * Removing a test for now
    
    * Removes redirect to debug outside this PR
    
    * fix loop issue
    
    * add url
    
    * clean
    
    ---------
    
    Co-authored-by: Tim Arney <tim@line37.com>

---
+++ components/globals/Select/Select.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import classnames from "classnames";
+import { useField } from "formik";
+import { ErrorMessage } from "@components/forms";
+import { InputFieldProps } from "@lib/types";
+
+// NOTE: based on forms/Dropdown. This will allow us to customize the dropdown without worry of
+// accidentally modifying a forms-form select
+
+// TODO: probably remove Formik related stuff like useField
+
+interface SelectProps extends InputFieldProps {
+  id: string;
+  children: React.ReactNode;
+  className?: string;
+  ariaDescribedBy?: string;
+  required?: boolean;
+}
+
+export const Select = (props: SelectProps): React.ReactElement => {
+  const { children, id, className, required = false, ariaDescribedBy } = props;
+  const classes = classnames("gc-dropdown", className);
+  const [field, meta] = useField(props);
+
+  return (
+    <>
+      {meta.error && <ErrorMessage>{meta.error}</ErrorMessage>}
+
+      <select
+        data-testid="dropdown"
+        className={classes}
+        id={id}
+        required={required}
+        {...(ariaDescribedBy && { ariaDescribedBy: ariaDescribedBy })}
+        {...field}
+      >
+        {children}
+      </select>
+    </>
+  );
+};
