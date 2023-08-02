ab8626d69579776ab80b5b266ec796f37c52a2cc
Tim Arney <tim@line37.com>
2023-08-02 09:01:57 -0400

    update test

--- cypress/e2e/security_questions_redirect_page.cy.ts
+++ cypress/e2e/security_questions_redirect_page.cy.ts
@@ -5,7 +5,7 @@ describe("Security Questions Redirect", () => {
 
   it("Redirects to user profile page", () => {
     cy.login({ acceptableUse: true });
-    cy.visit("/en/form-builder"); // not sure why but need to navigate to another page first
+    // cy.visit("/en/form-builder"); // not sure why but need to navigate to another page first
     cy.visit("/auth/setup-security-questions");
     cy.get("h1").should("contain", "Profile");
     cy.get("h2").should("contain", "Security questions");
