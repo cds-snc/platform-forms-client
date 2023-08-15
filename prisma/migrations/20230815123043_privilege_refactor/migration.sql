23ef7d2b804bfe8ec87763ef5300fe52ec748f3e
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-08-15 08:52:34 -0400

    modify prisma migration comment

--- prisma/migrations/20230815123043_privilege_refactor/migration.sql
+++ prisma/migrations/20230815123043_privilege_refactor/migration.sql
@@ -1,11 +1,8 @@
 /*
   Warnings:
 
-  - You are about to drop the column `nameEn` on the `Privilege` table. All the data in the column will be lost.
   - You are about to drop the column `nameFr` on the `Privilege` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[name]` on the table `Privilege` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `name` to the `Privilege` table without a default value. This is not possible if the table is not empty.
-
+  - Rename column 'nameEn' to 'name' on the table `Privilege`.
 */
 -- DropIndex
 DROP INDEX "Privilege_nameEn_key";
