0627262c322269188ce11f4f941575df444e7dc0
Tim Arney <tim@line37.com>
2023-08-22 07:51:01 -0400

    update link

--- components/form-builder/app/Preview.tsx
+++ components/form-builder/app/Preview.tsx
@@ -62,19 +62,19 @@ export const Preview = () => {
     <>
       <div className="h-12"></div>
       <div
-        className={`border-3 border-dashed border-blue-focus p-4 mb-8 ${
+        className={`mb-8 border-3 border-dashed border-blue-focus p-4 ${
           status !== "authenticated" && ""
         }`}
         {...getLocalizationAttribute()}
       >
         {status !== "authenticated" ? (
-          <div className="bg-purple-200 p-2 inline-block mb-1">
+          <div className="mb-1 inline-block bg-purple-200 p-2">
             <Markdown options={{ forceBlock: true }}>
               {t("signInToTest", { ns: "form-builder", lng: language })}
             </Markdown>
           </div>
         ) : email ? (
-          <div className="bg-purple-200 p-2 inline-block mb-1">
+          <div className="mb-1 inline-block bg-purple-200 p-2">
             {t("submittedResponsesText", { ns: "form-builder", email })}{" "}
             <a className="visited:text-black-default" href={settingsLink}>
               {t("submittedResponsesChange", { ns: "form-builder" })}
@@ -82,13 +82,19 @@ export const Preview = () => {
             .
           </div>
         ) : (
-          <div className="bg-purple-200 p-2 inline-block mb-1">
+          <div className="mb-1 inline-block bg-purple-200 p-2">
             {t("submittedResponsesTextVault.text1", { ns: "form-builder" })}{" "}
-            <a className="visited:text-black-default" href={responsesLink}>
+            <a
+              className="visited:text-black-default focus:!text-white-default active:shadow-none"
+              href={responsesLink}
+            >
               {t("submittedResponsesTextVault.text2", { ns: "form-builder" })}
             </a>
             .{" "}
-            <a className="visited:text-black-default" href={settingsLink}>
+            <a
+              className="visited:text-black-default focus:!text-white-default active:shadow-none"
+              href={settingsLink}
+            >
               {t("submittedResponsesChange", { ns: "form-builder" })}
             </a>
             .
@@ -97,13 +103,13 @@ export const Preview = () => {
 
         {sent && (
           <>
-            <button className="mt-4 clear-both block" onClick={() => clearSent()}>
+            <button className="clear-both mt-4 block" onClick={() => clearSent()}>
               <BackArrowIcon className="inline-block" /> {t("backToForm", { ns: "form-builder" })}
             </button>
           </>
         )}
 
-        <Brand brand={brand} lang={language} className="mt-8 mb-12" />
+        <Brand brand={brand} lang={language} className="mb-12 mt-8" />
         <h1 className="mt-4">
           {formRecord.form[localizeField(LocalizedFormProperties.TITLE, language)] ||
             t("pagePreview", { ns: "form-builder" })}
@@ -145,7 +151,7 @@ export const Preview = () => {
                 </span>
                 {status !== "authenticated" && (
                   <div
-                    className="inline-block py-1 px-4 bg-purple-200"
+                    className="inline-block bg-purple-200 px-4 py-1"
                     {...getLocalizationAttribute()}
                   >
                     <Markdown options={{ forceBlock: true }}>
@@ -163,10 +169,10 @@ export const Preview = () => {
 
       {status !== "authenticated" && (
         <>
-          <span className="bg-slate-200 p-2 inline-block mb-1">
+          <span className="mb-1 inline-block bg-slate-200 p-2">
             {t("confirmationPage", { ns: "form-builder" })}
           </span>
-          <div className="border-3 border-dashed border-blue-focus p-4 mb-8">
+          <div className="mb-8 border-3 border-dashed border-blue-focus p-4">
             <RichText {...getLocalizationAttribute()}>
               {formRecord.form.confirmation
                 ? formRecord.form.confirmation[
