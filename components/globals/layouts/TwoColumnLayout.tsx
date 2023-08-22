c924846ea77a8b85ad20e371e6d1c3690f3a8093
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-08-22 16:03:21 -0400

    Remove auth check

--- components/globals/layouts/TwoColumnLayout.tsx
+++ components/globals/layouts/TwoColumnLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 
-import { useAccessControl } from "@lib/hooks/useAccessControl";
 import { ToastContainer } from "@components/form-builder/app/shared/Toast";
 
 export const TwoColumnLayout = ({
@@ -13,10 +12,6 @@ export const TwoColumnLayout = ({
   title: string;
   leftNav?: React.ReactElement;
 }) => {
-  // This will check to see if a user is deactivated and redirect them to the account deactivated page
-  useAccessControl(); // @TODO: this belongs somewhere else
-
-  // Wait until the Template Store has fully hydrated before rendering the page
   return (
     <div className="mx-4 shrink-0 grow basis-auto laptop:mx-32 desktop:mx-64">
       <ToastContainer />
