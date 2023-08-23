612bdd91bd8526ad73f6c808e18f46c1ffcba1b3
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-08-23 16:01:09 -0400

    Fix a element styling globally

--- components/globals/layouts/HeadMeta.tsx
+++ components/globals/layouts/HeadMeta.tsx
@@ -1,12 +1,19 @@
 import Head from "next/head";
 import React from "react";
 
+const css = `
+    a:active {
+        box-shadow: 4px 0 0 0 #303fc3, -4px 0 0 0 #303fc3 !important;
+    }
+`;
+
 export const HeadMeta = () => {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="32x32" />
+      <style>{css}</style>
     </Head>
   );
 };
