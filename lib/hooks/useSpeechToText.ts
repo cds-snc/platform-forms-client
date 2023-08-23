f42f2f63d344dbbfc34330d559aaa4f207dee03b
Peter Thiessen <peter.thiessen@cds-snc.ca>
2023-08-23 16:11:36 -0400

    Refactors as a class

--- lib/hooks/useSpeechToText.ts
+++ lib/hooks/useSpeechToText.ts
@@ -1,91 +1,134 @@
 import { logMessage } from "@lib/logger";
 import { useEffect, useRef } from "react";
 
-const getSRInstance = () => {
-  const SR =
-    window.SpeechRecognition ||
-    window.webkitSpeechRecognition ||
-    window.mozSpeechRecognition ||
-    window.msSpeechRecognition ||
-    window.oSpeechRecognition;
-  const sr = new SR();
-  sr.continuous = false; // Set to true to keep going, but then will need a stop button or something
-  sr.lang = "en-US";
-  sr.interimResults = false;
-  sr.maxAlternatives = 1;
-  return sr;
-};
+class SR {
+  started = false;
+
+  constructor({ continuous, lang, callback }) {
+    this.sr = this.createSR();
+
+    if (!this.sr) {
+      // TODO error handling
+      logMessage.error("SR failed to create");
+      return;
+    }
+
+    if (!callback || typeof callback !== "function") {
+      // TODO error handling
+      logMessage.error("SR requires a callback");
+      return;
+    }
+
+    this.callback = callback;
+
+    // Keeps recording ongoing until stop triggered
+    this.sr.continuous = continuous === true ? true : false;
+
+    this.sr.lang = lang || "en-US";
+
+    // Gives more immediate results vs until when stopped
+    this.sr.interimResults = false;
+
+    this.sr.maxAlternatives = 1;
+
+    this.behaviorSR();
+  }
+
+  createSR() {
+    const SR =
+      window.SpeechRecognition ||
+      window.webkitSpeechRecognition ||
+      window.mozSpeechRecognition ||
+      window.msSpeechRecognition ||
+      window.oSpeechRecognition;
+    const sr = new SR();
+    return sr;
+  }
+
+  behaviorSR() {
+    this.sr.onstart = function () {
+      logMessage.info("SR started.");
+    };
+
+    this.sr.onspeechend = function () {
+      this.stop();
+    }.bind(this);
+
+    this.sr.onresult = function (e) {
+      // NOTE: Could add a confidence check so e.g. below 70% ask to try again
+      // if (sr.results[0][0].confidence < .70) { do something }
+      const result = e.results[0][0].transcript;
+      this.callback(result);
+    }.bind(this);
+
+    this.sr.onnomatch = function () {
+      logMessage.info("SR failed to confidently convert speech to text.");
+    };
+  }
+
+  start() {
+    if (this.started) {
+      return;
+    }
+    this.started = true;
+    this.sr.start(); // NOTE: async api if needed
+  }
+
+  stop() {
+    if (!this.started) {
+      return;
+    }
+    this.sr.stop();
+    this.started = false;
+    logMessage.info("SR finished.");
+  }
+
+  abort() {
+    this.sr.abort();
+  }
+}
 
 export const useSpeechToText = ({
   elRef,
-  callback,
-}: {
+}: // callback,
+{
   elRef: React.MutableRefObject<null | HTMLInputElement | HTMLTextAreaElement>;
-  callback;
+  // callback;
 }) => {
-  const sr = getSRInstance();
-  const refAdded = useRef(false);
-  let started = false;
-
-  const start = () => {
-    if (!started) {
-      sr.start(); // NOTE: async api if needed
-    }
-  };
-
-  sr.onstart = function () {
-    started = true;
-    logMessage.info("started converting speech to text");
-  };
-
-  sr.onspeechend = function () {
-    sr.stop();
-    started = false;
-    logMessage.info("ended converting speech to text");
-  };
-
-  sr.onresult = function (e) {
-    // NOTE: Could add a confidence check so e.g. below 70% ask to try again
-    // if (sr.results[0][0].confidence < .70) { do something }
-    const result = e.results[0][0].transcript;
-    const el = elRef?.current;
-    if (!el) {
-      return;
-    }
-    if (callback) {
-      callback(result);
-    } else {
-      el.value = el.value === "" ? result : " " + result;
-    }
-  };
-
-  sr.onnomatch = function () {
-    logMessage.info("I didn't get that could you try again and speek slower.");
-  };
+  const sr = useRef(
+    new SR({
+      continuous: false,
+      lang: "en-US",
+      callback: (result) => {
+        logMessage.info(result);
+        const el = elRef?.current;
+        el.value = el.value === "" ? result : el.value + " " + result;
+      },
+    })
+  );
 
   useEffect(() => {
     const el = elRef?.current;
     if (!el) {
       return;
     }
-    if (refAdded.current) {
-      return;
-    }
-    refAdded.current = true;
 
     const focusListener = el.addEventListener("focus", (e) => {
-      logMessage.info("hi");
-      start();
+      sr.current.start();
     });
 
     const clickListener = el.addEventListener("click", (e) => {
-      logMessage.info("hi click");
-      start();
+      sr.current.start();
+    });
+
+    const blurListener = el.addEventListener("blur", (e) => {
+      sr.current.stop();
     });
 
     return () => {
       el.removeEventListener("focus", focusListener);
       el.removeEventListener("click", clickListener);
+      el.removeEventListener("blur", blurListener);
     };
   }, [elRef]);
 };
