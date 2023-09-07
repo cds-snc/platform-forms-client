b9b9a5c0310ed8db8d230c66f06c8970c0bfa7fe
Peter Thiessen <peter.thiessen@cds-snc.ca>
2023-09-07 11:19:03 -0400

    Fixes line items

--- components/form-builder/app/shared/line-item-entries/LineItemEntries.tsx
+++ components/form-builder/app/shared/line-item-entries/LineItemEntries.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { LineItems } from "./LineItems";
 import { scrollToBottom } from "@lib/clientHelpers";
 import { useTranslation } from "react-i18next";
-import { DialogStates } from "../../responses/DownloadTableDialog";
+import { DialogStates } from "../../responses/DownloadTableDialogConfirm";
 
 // TODO: handle duplicate entries?
 // TODO: should "backspace" on an empty input set the next entry into "edit mode"?
