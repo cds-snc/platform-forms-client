f2a7da7bb25301975c94c39b2b071d4899b49bcc
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-09-14 14:13:09 -0400

    Content was hidden below scroll horizon

--- cypress/e2e/form-builder/share.cy.ts
+++ cypress/e2e/form-builder/share.cy.ts
@@ -17,10 +17,10 @@ describe("Form builder share", () => {
     cy.get("[role='menuitem']").should("have.length", 1);
     cy.get("span").contains("Share by email").click();
     cy.get("dialog label").contains("Email address");
-    cy.get("summary").contains("See a preview of the email message").should("be.visible").click();
+    cy.get("summary").contains("See a preview of the email message").should("exist").click();
     cy.get("h4")
       .contains("Regular Test User has shared a form with you on GC Forms")
-      .should("be.visible");
+      .should("exist");
     cy.get("button").contains("Close").click();
   });
   it("Renders share flyout for unAuthenticated", () => {
@@ -30,13 +30,12 @@ describe("Form builder share", () => {
     cy.get("button").contains("Share").click();
     cy.get("[role='menuitem']").should("have.length", 1);
     cy.get("span").contains("Share by email").click();
-    cy.get("dialog").contains("Step 1").should("be.visible");
-    cy.get("button").contains("Download form file").should("be.visible");
-    cy.get("button").contains("Copy instructions").should("be.visible");
-    cy.get("summary").contains("See a preview of the email message").should("be.visible").click();
-    cy.get("h4")
-      .contains("A GC Forms user has shared a form with you on GC Forms")
-      .should("be.visible");
+    // Using "exist" instead of "be.visible" because the content is hidden by scolling
+    cy.get("dialog").contains("Step 1").should("exist");
+    cy.get("button").contains("Download form file").should("exist");
+    cy.get("button").contains("Copy instructions").should("exist");
+    cy.get("summary").contains("See a preview of the email message").should("exist").click();
+    cy.get("h4").contains("A GC Forms user has shared a form with you on GC Forms").should("exist");
     cy.get("button").contains("Close").click();
   });
 });
