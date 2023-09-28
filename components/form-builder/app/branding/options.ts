2d4c63bcb097d9347434efbdd9cc71e0e45308cd
Tim Arney <tim@line37.com>
2023-09-28 11:10:34 -0400

    add rcmp

--- components/form-builder/app/branding/options.ts
+++ components/form-builder/app/branding/options.ts
@@ -80,4 +80,13 @@ export const options = [
     logoTitleEn: "Financial Transactions and Reports Analysis Centre of Canada",
     logoTitleFr: "Centre d’analyse des opérations et déclarations financières du Canada",
   },
+  {
+    name: "rcmp",
+    urlEn: "https://www.rcmp-grc.gc.ca/en",
+    urlFr: "https://www.rcmp-grc.gc.ca/fr",
+    logoEn: "/img/branding/rcmp-en.svg",
+    logoFr: "/img/branding/rcmp-fr.svg",
+    logoTitleEn: "Royal Canadian Mounted Police",
+    logoTitleFr: "Gendarmerie royale du Canada",
+  },
 ];
