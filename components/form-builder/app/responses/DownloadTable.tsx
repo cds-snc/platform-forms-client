8dea0e548897c741cb7ec663fc17a41f94565a8f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-10-16 11:44:28 -0400

    Increase by 25ms

--- components/form-builder/app/responses/DownloadTable.tsx
+++ components/form-builder/app/responses/DownloadTable.tsx
@@ -239,7 +239,7 @@ export const DownloadTable = ({ vaultSubmissions, formId, nagwareResult }: Downl
       // await dhandle.requestPermission({ writable: true });
       // console.log(tableItems.checkedItems);
       // Array.from(tableItems.checkedItems, async ([submissionName]) => {
-      const interval = 125;
+      const interval = 150;
       let promise = Promise.resolve();
       tableItems.checkedItems.forEach((value, submissionName) => {
         if (!submissionName) {
