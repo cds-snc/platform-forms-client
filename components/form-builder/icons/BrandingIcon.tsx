3345834cbf98be62fd1d04cda7de962949cdb457
Tim Arney <timarney@users.noreply.github.com>
2023-10-17 15:42:36 -0400

    Quick select panel - add hover (#2789)
    * add hover
    
    * update icons
    
    * fix more button

--- components/form-builder/icons/BrandingIcon.tsx
+++ components/form-builder/icons/BrandingIcon.tsx
@@ -2,9 +2,8 @@ import React from "react";
 export const BrandingIcon = ({ className, title }: { className?: string; title?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="33"
-    height="27"
-    viewBox="0 0 33 27"
+    width={25}
+    height={20}
     className={className}
     focusable="false"
     aria-hidden={title ? false : true}
@@ -13,8 +12,8 @@ export const BrandingIcon = ({ className, title }: { className?: string; title?:
   >
     {title && <title>{title}</title>}
     <path
-      d="M13.5 21.5733H27.94V11.2533H13.5V21.5733ZM2.9 26.1333C2.26 26.1333 1.7 25.8933 1.22 25.4133C0.74 24.9333 0.5 24.3733 0.5 23.7333V2.93333C0.5 2.29333 0.74 1.73333 1.22 1.25333C1.7 0.773325 2.26 0.533325 2.9 0.533325H30.1C30.74 0.533325 31.3 0.773325 31.78 1.25333C32.26 1.73333 32.5 2.29333 32.5 2.93333V23.7333C32.5 24.3733 32.26 24.9333 31.78 25.4133C31.3 25.8933 30.74 26.1333 30.1 26.1333H2.9ZM2.9 23.7333H30.1V2.93333H2.9V23.7333Z"
       fill="#1E293B"
+      d="M10.25 16.18h10.83V8.44H10.25v7.74ZM2.3 19.6c-.48 0-.9-.18-1.26-.54C.68 18.7.5 18.28.5 17.8V2.2c0-.48.18-.9.54-1.26C1.4.58 1.82.4 2.3.4h20.4c.48 0 .9.18 1.26.54.36.36.54.78.54 1.26v15.6c0 .48-.18.9-.54 1.26-.36.36-.78.54-1.26.54H2.3Zm0-1.8h20.4V2.2H2.3v15.6Z"
     />
   </svg>
 );
