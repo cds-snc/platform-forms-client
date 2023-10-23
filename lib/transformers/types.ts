b6eca9b8283d116c389870938856bfc9c5c30891
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-10-23 16:00:14 -0400

    types

--- lib/transformers/types.ts
+++ lib/transformers/types.ts
@@ -4,3 +4,10 @@ export interface ResponseSubmission {
   confirmation_code: string;
   [key: string]: string | number;
 }
+
+export type JSONValue = string | number | boolean | { [x: string]: JSONValue } | Array<JSONValue>;
+
+export interface Translations {
+  en: JSONValue;
+  fr: JSONValue;
+}
