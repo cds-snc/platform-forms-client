03680c3bbb37d40f44c54fd993df20130da25a1e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-11-09 14:02:21 -0500

    Bring back htmlChecker

--- lib/middleware/jsonValidator.ts
+++ lib/middleware/jsonValidator.ts
@@ -2,11 +2,37 @@ import { Schema, Validator, ValidatorResult, PreValidatePropertyFunction } from 
 import { NextApiRequest, NextApiResponse } from "next";
 import { MiddlewareRequest, MiddlewareReturn } from "@lib/types";
 import { escapeAngleBrackets } from "@components/form-builder/util";
+import * as htmlparser2 from "htmlparser2";
 
 export type ValidateOptions = {
   jsonKey?: string;
   noHTML?: boolean;
   noValidateMethods?: string[];
+};
+
+/**
+ * This function when passed into the jsonschema validate function
+ * will receive all possible key value pairs defined in the schema
+ * and will check string values to ensure that there is not HTML
+ * @param object
+ * @param key
+ */
+const htmlChecker: PreValidatePropertyFunction = (object, key) => {
+  const value = object[key];
+  if (typeof value === "string") {
+    // we assume all strings can be valid html
+    // use htmlparse2 to construct document
+    // we want to ensure that the only thing in the document is a text element
+    const parsedString = htmlparser2.parseDocument(value);
+
+    // if there are zero children its an empty string, if there is more than one child there is html
+    if (parsedString.children.length === 0) return;
+    else if (parsedString.children.length !== 1) throw new Error(`HTML detected in JSON`);
+
+    const element = parsedString.children[0];
+    // if the element detected does not have a text type then it is html
+    if (element?.type !== "text") throw new Error(`HTML detected in JSON`);
+  }
 };
 
 /**
@@ -49,7 +75,7 @@ export const jsonValidator = (schema: Schema, options?: ValidateOptions): Middle
       const validatorResult: ValidatorResult = validator.validate(
         validateObject,
         schema,
-        options?.noHTML ? { preValidateProperty: cleanAngleBrackets } : undefined
+        options?.noHTML ? { preValidateProperty: htmlChecker } : undefined
       );
 
       if (validatorResult.valid) {
