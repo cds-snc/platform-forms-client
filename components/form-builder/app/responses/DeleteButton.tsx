cd3e7440512d44986c9c8817b6f16807b1270a12
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-11-17 15:42:18 -0500

    Confirm delete new responses dialog (#2872)
    Add Delete new responses confirmation dialog

--- components/form-builder/app/responses/DeleteButton.tsx
+++ components/form-builder/app/responses/DeleteButton.tsx
@@ -2,16 +2,22 @@ import { Button } from "@components/globals";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export const DeleteButton = () => {
+export const DeleteButton = ({
+  setShowConfirmNewDialog,
+}: {
+  setShowConfirmNewDialog: (showConfirmNewDialog: boolean) => void;
+}) => {
   const { t } = useTranslation("form-builder-responses");
 
   const handleDelete = () => {
-    alert("Not yet implemented");
+    setShowConfirmNewDialog(true);
   };
 
   return (
-    <Button theme="destructive" onClick={handleDelete}>
-      {t("downloadResponsesTable.deleteSelectedResponses")}
-    </Button>
+    <>
+      <Button theme="destructive" onClick={handleDelete}>
+        {t("downloadResponsesTable.deleteSelectedResponses")}
+      </Button>
+    </>
   );
 };
