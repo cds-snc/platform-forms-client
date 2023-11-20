fe9b775bd56e0159b7fcd1a2e84cd5419f7e2880
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-11-20 16:15:02 -0500

    just return

--- lib/responseDownloadFormats/json/index.ts
+++ lib/responseDownloadFormats/json/index.ts
@@ -1,13 +1,11 @@
 import { FormResponseSubmissions } from "../types";
 
 export const transform = (formResponseSubmissions: FormResponseSubmissions) => {
-  const responses = formResponseSubmissions.submissions.map((response) => {
+  return formResponseSubmissions.submissions.map((response) => {
     return {
       id: response.id,
       createdAt: new Date(response.createdAt).toISOString(),
       answers: response.answers,
     };
   });
-
-  return responses;
 };
