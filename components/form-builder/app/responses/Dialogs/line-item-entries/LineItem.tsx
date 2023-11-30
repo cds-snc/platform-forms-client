28ce8402d3abbf78347eae2fff54f29bf346e3c0
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-11-30 11:34:13 -0500

    (feat) Add problem response section (#2930)
    * Add problem status filter
    
    * Add/update strings
    
    * Move link to below table
    
    * update content / add button
    
    * add no problem responses card
    
    * Update strings
    
    * checkpoint commit
    
    * validate format inline and display error if there is an invalid item
    
    * replace all tabs
    
    * unused imports
    
    * set dialog error state instead of server erroor
    
    * Cleanup unused server error
    
    * string updates
    
    * string updates
    
    * Approuver > Autoriser
    
    * Missing translation: no problem responses
    
    * Approuver rather than autoriser
    
    * Missing translation
    
    ---------
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>

--- components/form-builder/app/responses/Dialogs/line-item-entries/LineItem.tsx
+++ components/form-builder/app/responses/Dialogs/line-item-entries/LineItem.tsx
@@ -13,13 +13,13 @@ export const LineItem = ({ value, isInvalid = false, onRemove }: LineItemProps) 
   const label = `${t("lineItemEntries.remove")} ${value}`;
   return (
     <li data-testid="value" className="pl-6 hover:bg-[#fffbf3]">
-      <div className="flex justify-between items-center pr-4 py-2">
+      <div className="flex items-center justify-between py-2 pr-4">
         <span className={`${isInvalid ? " text-red" : ""}`}>{value}</span>
         <button
           className={`
-            align-middle border border-black rounded-full [&_svg]:fill-white [&_rect]:fill-black mt-[5px] mb-[5px] mr-[5px]
-            [&_svg]:hover:fill-gray-600 [&_rect]:hover:fill-white 
-            [&_svg]:focus:fill-[#3242bc] [&_rect]:focus:fill-white focus:border-[#3242bc] focus:border-[4px] focus:p-[2px] focus:m-[0px]
+            my-[5px] mr-[5px] rounded-full border border-black align-middle focus:m-[0px] focus:border-[4px] focus:border-[#3242bc]
+            focus:p-[2px] [&_rect]:fill-black 
+            [&_rect]:hover:fill-white [&_rect]:focus:fill-white [&_svg]:fill-white [&_svg]:hover:fill-gray-600 [&_svg]:focus:fill-[#3242bc]
           `}
           onClick={() => onRemove(value)}
           aria-label={label}
