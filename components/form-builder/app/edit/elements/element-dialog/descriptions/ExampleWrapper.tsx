15c3619e1d4ea868c2387e2e456cf96e54ddec7b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-01-04 14:50:15 -0500

    Use violet

--- components/form-builder/app/edit/elements/element-dialog/descriptions/ExampleWrapper.tsx
+++ components/form-builder/app/edit/elements/element-dialog/descriptions/ExampleWrapper.tsx
@@ -18,7 +18,7 @@ export const ExampleWrapper = ({
 
   return (
     <div className={cn("relative rounded-lg border border-slate-400 bg-white p-4", className)}>
-      <div className="absolute -top-4 right-6 rounded border border-slate-400 bg-indigo-700 px-2 py-1 text-white">
+      <div className="absolute -top-4 right-6 rounded border border-slate-400 bg-violet-700 px-2 py-1 text-white">
         Try it out
       </div>
       <FormikProvider value={formik}>{children}</FormikProvider>
