df24215f28e5bf9956a60fec7ad296421a4dfabc
Tim Arney <tim@line37.com>
2024-01-04 10:22:51 -0500

    content

--- components/globals/RightPanel.tsx
+++ components/globals/RightPanel.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Dialog, Menu, Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Button } from "@components/globals";
 import { cn } from "@lib/utils";
 
@@ -10,17 +10,6 @@ export const RightPanel = () => {
     { name: "All", href: "#", current: true },
     { name: "Online", href: "#", current: false },
     { name: "Offline", href: "#", current: false },
-  ];
-  const team = [
-    {
-      name: "Leslie Alexander",
-      handle: "lesliealexander",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      status: "online",
-    },
-    // More people...
   ];
 
   return (
@@ -39,7 +28,7 @@ export const RightPanel = () => {
 
           <div className="fixed inset-0 overflow-hidden">
             <div className="absolute inset-0 overflow-hidden">
-              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <div className="pointer-events-none fixed inset-y-0 right-0 top-[72px] flex max-w-full pl-10 sm:pl-16">
                 <Transition.Child
                   as={Fragment}
                   enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -88,100 +77,7 @@ export const RightPanel = () => {
                           </nav>
                         </div>
                       </div>
-                      <ul role="list" className="flex-1 divide-y divide-gray-200 overflow-y-auto">
-                        {team.map((person) => (
-                          <li key={person.handle}>
-                            <div className="group relative flex items-center px-5 py-6">
-                              <a href={person.href} className="-m-1 block flex-1 p-1">
-                                <div
-                                  className="absolute inset-0 group-hover:bg-gray-50"
-                                  aria-hidden="true"
-                                />
-                                <div className="relative flex min-w-0 flex-1 items-center">
-                                  <span className="relative inline-block flex-shrink-0">
-                                    <img
-                                      className="h-10 w-10 rounded-full"
-                                      src={person.imageUrl}
-                                      alt=""
-                                    />
-                                    <span
-                                      className={cn(
-                                        person.status === "online" ? "bg-green-400" : "bg-gray-300",
-                                        "absolute top-0 right-0 block h-2.5 w-2.5 rounded-full ring-2 ring-white"
-                                      )}
-                                      aria-hidden="true"
-                                    />
-                                  </span>
-                                  <div className="ml-4 truncate">
-                                    <p className="truncate text-sm font-medium text-gray-900">
-                                      {person.name}
-                                    </p>
-                                    <p className="truncate text-sm text-gray-500">
-                                      {"@" + person.handle}
-                                    </p>
-                                  </div>
-                                </div>
-                              </a>
-                              <Menu
-                                as="div"
-                                className="relative ml-2 inline-block flex-shrink-0 text-left"
-                              >
-                                <Menu.Button className="group relative inline-flex h-8 w-8 items-center justify-center rounded-full bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                  <span className="absolute -inset-1.5" />
-                                  <span className="sr-only">Open options menu</span>
-                                  <span className="flex h-full w-full items-center justify-center rounded-full">
-                                    ...
-                                  </span>
-                                </Menu.Button>
-                                <Transition
-                                  as={Fragment}
-                                  enter="transition ease-out duration-100"
-                                  enterFrom="transform opacity-0 scale-95"
-                                  enterTo="transform opacity-100 scale-100"
-                                  leave="transition ease-in duration-75"
-                                  leaveFrom="transform opacity-100 scale-100"
-                                  leaveTo="transform opacity-0 scale-95"
-                                >
-                                  <Menu.Items className="absolute right-9 top-0 z-10 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                      <Menu.Item>
-                                        {({ active }) => (
-                                          <a
-                                            href="#"
-                                            className={cn(
-                                              active
-                                                ? "bg-gray-100 text-gray-900"
-                                                : "text-gray-700",
-                                              "block px-4 py-2 text-sm"
-                                            )}
-                                          >
-                                            View profile
-                                          </a>
-                                        )}
-                                      </Menu.Item>
-                                      <Menu.Item>
-                                        {({ active }) => (
-                                          <a
-                                            href="#"
-                                            className={cn(
-                                              active
-                                                ? "bg-gray-100 text-gray-900"
-                                                : "text-gray-700",
-                                              "block px-4 py-2 text-sm"
-                                            )}
-                                          >
-                                            Send message
-                                          </a>
-                                        )}
-                                      </Menu.Item>
-                                    </div>
-                                  </Menu.Items>
-                                </Transition>
-                              </Menu>
-                            </div>
-                          </li>
-                        ))}
-                      </ul>
+                      content here
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
