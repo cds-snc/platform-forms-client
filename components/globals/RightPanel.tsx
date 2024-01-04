2fd23f8e859d4e570f9959555824b9f16adc0f57
Tim Arney <tim@line37.com>
2024-01-04 10:28:28 -0500

    cleanup

--- components/globals/RightPanel.tsx
+++ components/globals/RightPanel.tsx
@@ -7,9 +7,9 @@ export const RightPanel = () => {
   const [open, setOpen] = useState(false);
 
   const tabs = [
-    { name: "All", href: "#", current: true },
-    { name: "Online", href: "#", current: false },
-    { name: "Offline", href: "#", current: false },
+    { name: "Questions", href: "#", current: true },
+    { name: "Translation", href: "#", current: false },
+    { name: "Logic", href: "#", current: false },
   ];
 
   return (
@@ -42,9 +42,6 @@ export const RightPanel = () => {
                     <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                       <div className="p-6">
                         <div className="flex items-start justify-between">
-                          <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
-                            Team
-                          </Dialog.Title>
                           <div className="ml-3 flex h-7 items-center">
                             <button
                               type="button"
