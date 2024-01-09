8d3241433a8bb49f8d0c49a1e90d8ad914ba7e4b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-01-09 15:51:59 -0500

    checkpoint

--- components/form-builder/app/edit/elements/element-dialog/ElementFilters.tsx
+++ components/form-builder/app/edit/elements/element-dialog/ElementFilters.tsx
@@ -1,6 +1,6 @@
 import { Groups } from "@components/form-builder/hooks/useElementOptions";
 import { cn } from "@lib/utils";
-import React from "react";
+import React, { useRef } from "react";
 
 export const ElementFilters = ({
   setSelectedGroups,
@@ -9,24 +9,24 @@ export const ElementFilters = ({
   setSelectedGroups: React.Dispatch<React.SetStateAction<Groups[]>>;
   selectedGroups: Groups[];
 }) => {
-  const updateSelectedGroups = (group: Groups | "all") => {
+  const updateSelectedGroups = (
+    group: Groups | "all",
+    pillRef: React.RefObject<HTMLButtonElement>
+  ) => {
     if (group === "all") {
       setSelectedGroups([]);
-      return;
-    }
-    if (!selectedGroups.includes(group)) {
+    } else if (!selectedGroups.includes(group)) {
       setSelectedGroups([...selectedGroups, group]);
-      return;
-    }
-
-    if (selectedGroups.includes(group)) {
+    } else if (selectedGroups.includes(group)) {
       setSelectedGroups(selectedGroups.filter((g) => g !== group));
-      return;
     }
+    pillRef.current?.focus();
   };
 
   const Pill = ({ group, children }: { group: Groups | "all"; children: React.ReactNode }) => {
     let selected = false;
+
+    const pillRef = useRef(null);
 
     if (group === "all") {
       selected = selectedGroups.length === 0;
@@ -36,11 +36,14 @@ export const ElementFilters = ({
 
     return (
       <button
+        ref={pillRef}
         className={cn(
           "rounded-full border border-slate-800 bg-white px-4 py-2",
           selected && "bg-blue-800 text-white"
         )}
-        onClick={() => updateSelectedGroups(group)}
+        onClick={() => updateSelectedGroups(group, pillRef)}
+        role="checkbox"
+        aria-checked={selected}
       >
         {children}
       </button>
@@ -48,12 +51,20 @@ export const ElementFilters = ({
   };
 
   return (
-    <div className="z-100 mt-4 flex gap-4">
+    <div className="z-100 mt-4 flex gap-4" aria-label="Filter form elements by type:">
       <Pill group="all">All</Pill>
-      <Pill group={Groups.BASIC}>Basic</Pill>
-      <Pill group={Groups.PRESET}>Preset</Pill>
-      <Pill group={Groups.ADVANCED}>Advanced</Pill>
-      <Pill group={Groups.OTHER}>Other</Pill>
+      <Pill group={Groups.BASIC}>
+        Basic <span className="visually-hidden">questions</span>
+      </Pill>
+      <Pill group={Groups.PRESET}>
+        Preset <span className="visually-hidden">questions</span>
+      </Pill>
+      <Pill group={Groups.ADVANCED}>
+        Advanced <span className="visually-hidden">questions</span>
+      </Pill>
+      <Pill group={Groups.OTHER}>
+        Other <span className="visually-hidden">elements</span>
+      </Pill>
     </div>
   );
 };
