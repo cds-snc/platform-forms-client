3bfeba5fc2e56664e95304c6075a7d38f6dc4646
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-01-11 16:53:40 -0500

    set origin control for static pages

--- next.config.js
+++ next.config.js
@@ -60,7 +60,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.NEXTAUTH_URL },
+          { key: "Access-Control-Allow-Origin", value: process.env.NEXTAUTH_URL ?? "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",
