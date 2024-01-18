be44e6f29febd6f6468564e7a851457a87d5155e
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-01-18 13:32:39 -0500

    fix forms not showing on /locale/forms

--- app/(gcforms)/[locale]/(form administration)/forms/[[...path]]/clientSide.tsx
+++ app/(gcforms)/[locale]/(form administration)/forms/[[...path]]/clientSide.tsx
@@ -10,8 +10,7 @@ import { TabPanel } from "@clientComponents/myforms/Tabs/TabPanel";
 import { StyledLink } from "@clientComponents/globals/StyledLink/StyledLink";
 import { clearTemplateStore } from "@clientComponents/form-builder/store/useTemplateStore";
 import { ResumeEditingForm } from "@clientComponents/form-builder/app/shared";
-
-import Head from "next/head";
+import { logMessage } from "@lib/logger";
 
 interface FormsDataItem {
   id: string;
@@ -29,7 +28,9 @@ interface MyFormsProps {
 }
 
 export default function RenderMyForms({ templates }: MyFormsProps) {
-  const path = usePathname();
+  // remove locale from path and get sub-route
+  const path = usePathname().split("/")[3];
+  logMessage.debug(`path: ${path}`);
 
   const { t } = useTranslation(["my-forms"]);
 
@@ -58,66 +59,60 @@ export default function RenderMyForms({ templates }: MyFormsProps) {
   }, []);
 
   return (
-    <>
-      <Head>
-        <title>{t("title")}</title>
-      </Head>
+    <div className="center mx-auto w-[980px]">
+      <h1 className="mb-8 border-b-0">{t("title")}</h1>
 
-      <div className="center mx-auto w-[980px]">
-        <h1 className="mb-8 border-b-0">{t("title")}</h1>
-
-        <div className="flex w-full justify-between">
-          <FilterNavigation />
-          <div ref={createNewFormRef} className="inline">
-            <LinkButton.Primary href="/form-builder">
-              <>
-                <span aria-hidden="true" className="mr-2 inline-block">
-                  +
-                </span>{" "}
-                {t("actions.createNewForm")}
-              </>
-            </LinkButton.Primary>
-          </div>
-        </div>
-
-        <ResumeEditingForm>
-          <StyledLink href="/form-builder/edit" className="mb-4 inline-block">
-            <span aria-hidden="true"> ← </span> {t("actions.resumeForm")}
-          </StyledLink>
-        </ResumeEditingForm>
-
-        <div aria-live="polite">
-          <TabPanel id="tabpanel-drafts" labeledbyId="tab-drafts" isActive={path === "drafts"}>
-            {templatesDrafts && templatesDrafts?.length > 0 ? (
-              <CardGrid cards={templatesDrafts} gridType="drafts"></CardGrid>
-            ) : (
-              <p>{t("cards.noDraftForms")}</p>
-            )}
-          </TabPanel>
-          <TabPanel
-            id="tabpanel-published"
-            labeledbyId="tab-published"
-            isActive={path === "published"}
-          >
-            {templatesPublished && templatesPublished?.length > 0 ? (
-              <CardGrid cards={templatesPublished} gridType="published"></CardGrid>
-            ) : (
-              <p>{t("cards.noPublishedForms")}</p>
-            )}
-          </TabPanel>
-          <TabPanel
-            id="tabpanel-all"
-            labeledbyId="tab-all"
-            isActive={path === "all" || path === "undefined"}
-          >
-            {templatesAll && templatesAll?.length > 0 ? (
-              <CardGrid cards={templatesAll} gridType="all"></CardGrid>
-            ) : (
-              <p>{t("cards.noForms")}</p>
-            )}
-          </TabPanel>
+      <div className="flex w-full justify-between">
+        <FilterNavigation />
+        <div ref={createNewFormRef} className="inline">
+          <LinkButton.Primary href="/form-builder">
+            <>
+              <span aria-hidden="true" className="mr-2 inline-block">
+                +
+              </span>{" "}
+              {t("actions.createNewForm")}
+            </>
+          </LinkButton.Primary>
         </div>
       </div>
-    </>
+
+      <ResumeEditingForm>
+        <StyledLink href="/form-builder/edit" className="mb-4 inline-block">
+          <span aria-hidden="true"> ← </span> {t("actions.resumeForm")}
+        </StyledLink>
+      </ResumeEditingForm>
+
+      <div aria-live="polite">
+        <TabPanel id="tabpanel-drafts" labeledbyId="tab-drafts" isActive={path === "drafts"}>
+          {templatesDrafts && templatesDrafts?.length > 0 ? (
+            <CardGrid cards={templatesDrafts} gridType="drafts"></CardGrid>
+          ) : (
+            <p>{t("cards.noDraftForms")}</p>
+          )}
+        </TabPanel>
+        <TabPanel
+          id="tabpanel-published"
+          labeledbyId="tab-published"
+          isActive={path === "published"}
+        >
+          {templatesPublished && templatesPublished?.length > 0 ? (
+            <CardGrid cards={templatesPublished} gridType="published"></CardGrid>
+          ) : (
+            <p>{t("cards.noPublishedForms")}</p>
+          )}
+        </TabPanel>
+        <TabPanel
+          id="tabpanel-all"
+          labeledbyId="tab-all"
+          isActive={path === "all" || typeof path === "undefined"}
+        >
+          {templatesAll && templatesAll?.length > 0 ? (
+            <CardGrid cards={templatesAll} gridType="all"></CardGrid>
+          ) : (
+            <p>{t("cards.noForms")}</p>
+          )}
+        </TabPanel>
+      </div>
+    </div>
   );
 }
