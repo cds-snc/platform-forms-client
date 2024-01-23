378391f70348dcb1c84f300ac454440b19dc5eff
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-01-23 09:55:16 -0500

    useAccessControl optimization

--- lib/hooks/useAccessControl.tsx
+++ lib/hooks/useAccessControl.tsx
@@ -6,6 +6,8 @@ import { PureAbility, createMongoAbility, MongoAbility } from "@casl/ability";
 import { Abilities } from "@lib/types";
 import { logMessage } from "@lib/logger";
 import { useTranslation } from "@i18n/client";
+import isEqual from "lodash.isequal";
+import { Session } from "next-auth";
 
 interface AccessControlInterface {
   ability: PureAbility<Abilities, unknown> | null;
@@ -16,13 +18,17 @@ const AbilityContext = createContext<AccessControlInterface>({} as AccessControl
 export const AccessControlProvider = ({ children }: { children: React.ReactNode }) => {
   const [ability, setAbility] = useState<PureAbility<Abilities, unknown> | null>(null);
   const { data, status, update } = useSession();
+  const [user, setUser] = useState<Session["user"] | undefined>(data?.user);
   const {
     i18n: { language },
   } = useTranslation();
 
-  //@todo: Do a deep comparison check on 'user' to ensure that the session has actually changed for values
-  // we're interested in.
-  const { user } = data ?? {};
+  //Do a deep comparison check on 'user' to ensure that the session has actually changed for values
+  useEffect(() => {
+    if (!isEqual(user, data?.user)) {
+      setUser(data?.user);
+    }
+  }, [data, user]);
 
   const refreshAbility = useCallback(async () => {
     // If the user is deactivated, sign them out automatically
