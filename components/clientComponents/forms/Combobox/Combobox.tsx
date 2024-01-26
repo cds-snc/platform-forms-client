81d7b3d478e857bb0de4bfc12dfb3d0029e2ffb8
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-01-26 12:10:31 -0500

    Use downshift hook for combobox

--- components/clientComponents/forms/Combobox/Combobox.tsx
+++ components/clientComponents/forms/Combobox/Combobox.tsx
@@ -2,101 +2,65 @@ import React from "react";
 import { InputFieldProps } from "@lib/types";
 import classnames from "classnames";
 import { useField } from "formik";
-import { ErrorMessage } from "@components/forms";
+import { ErrorMessage } from "@clientComponents/forms";
+import { useCombobox } from "downshift";
+
+import { cn } from "@lib/utils";
 
 interface ComboboxProps extends InputFieldProps {
   children?: React.ReactElement;
   choices?: string[];
 }
 
-interface ComboboxOptionProps {
-  id: string;
-  name: string;
-  value: string;
-}
-
-const ComboboxOption = (props: ComboboxOptionProps): React.ReactElement => {
-  return (
-    <li
-      role="option"
-      data-value={props.value}
-      aria-selected="false"
-      id={props.id}
-      className="cursor-pointer p-2 hover:bg-slate-300"
-      onClick={(e) => {
-        // setSelectedItem(e);
-      }}
-    >
-      {props.name}
-    </li>
-  );
-};
-
 export const Combobox = (props: ComboboxProps): React.ReactElement => {
   const { children, id, name, className, choices = [], required, ariaDescribedBy } = props;
   const classes = classnames("gc-combobox", className);
   const [field, meta] = useField(props);
-  const [query, setQuery] = React.useState("");
-  const [selectedItem, setSelectedItem] = React.useState("");
-  const [listOpen, setListOpen] = React.useState(false);
 
-  const handleInputOnBlur = () => {
-    //
-  };
-
-  const handleInputOnCick = (e) => {
-    setListOpen(true);
-  };
-
-  const handleInputOnChange = (e) => {
-    setListOpen(true);
-    setQuery(e.target.value);
-  };
-
-  const filteredChoices =
-    query === "" ? choices : choices.filter((choice) => choice.includes(query.toLowerCase()));
-
-  const options = filteredChoices.map((choice, i) => {
-    const innerId = `${id}.${i}`;
-    return <ComboboxOption id={innerId} key={`key-${innerId}`} value={choice} name={choice} />;
+  const [items, setItems] = React.useState(choices);
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
+    onInputValueChange({ inputValue }) {
+      setItems(
+        choices.filter((choice) => (inputValue ? choice.includes(inputValue.toLowerCase()) : true))
+      );
+    },
+    items,
+    itemToString(item) {
+      return item ? item : "";
+    },
   });
 
   return (
     <>
-      {/* {meta.error && <ErrorMessage>{meta.error}</ErrorMessage>} */}
-      <div className="gc-combobox relative">
-        <input
-          data-testid="combobox"
-          autoComplete="off"
-          className={classes}
-          type="text"
-          id={`${id}-combobox`}
-          role="combobox"
-          aria-controls={`${id}-listbox`}
-          aria-autocomplete="list"
-          aria-expanded="false"
-          data-active-option="item1"
-          aria-activedescendant=""
-          {...(name && { name })}
-          required={required}
-          aria-describedby={ariaDescribedBy}
-          {...field}
-          onClick={(event) => handleInputOnCick(event)}
-          onBlur={handleInputOnBlur}
-          onChange={(event) => handleInputOnChange(event)}
-          value={query}
-        />
-        {listOpen && (
-          <ul
-            id={`${id}-listbox`}
-            role="listbox"
-            aria-label=""
-            data-testid="listbox"
-            className="absolute top-16 z-10 ml-2 min-w-[250px] list-none rounded-md border border-slate-800 bg-white pl-0"
-          >
-            {children ? children : <>{options}</>}
-          </ul>
-        )}
+      <div className="gc-combobox">
+        {meta.error && <ErrorMessage>{meta.error}</ErrorMessage>}
+
+        <input placeholder="Best book ever" className="w-full p-1.5" {...getInputProps()} />
+
+        <ul className={`${!(isOpen && items.length) && "hidden"}`} {...getMenuProps()}>
+          {isOpen &&
+            items.map((item, index) => (
+              <li
+                className={cn(
+                  highlightedIndex === index && "bg-slate-300",
+                  selectedItem === item && "font-bold"
+                )}
+                key={item}
+                {...getItemProps({ item, index })}
+              >
+                <span>{item}</span>
+              </li>
+            ))}
+        </ul>
       </div>
     </>
   );
