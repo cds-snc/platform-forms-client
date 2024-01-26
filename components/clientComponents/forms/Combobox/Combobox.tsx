37ac752bccab0ac9fb5ffbc0c0e8043abe500892
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-01-26 15:30:25 -0500

    use client

--- components/clientComponents/forms/Combobox/Combobox.tsx
+++ components/clientComponents/forms/Combobox/Combobox.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
 import React from "react";
 import { InputFieldProps } from "@lib/types";
 import classnames from "classnames";
