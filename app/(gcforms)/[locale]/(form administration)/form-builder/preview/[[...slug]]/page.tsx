ef9d870723205765290b543017fec86f2d16da0f
Clément JANIN <clement.janin@cds-snc.ca>
2024-02-05 12:35:12 -0500

    fix: hydration bug in settings pages (#3232)
    fix: hydration bug on settings and preview pages

--- app/(gcforms)/[locale]/(form administration)/form-builder/preview/[[...slug]]/page.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/preview/[[...slug]]/page.tsx
@@ -1,8 +1,12 @@
 import { serverTranslation } from "@i18n";
 import { ClientSide } from "./clientSide";
 import { FormBuilderInitializer } from "@clientComponents/globals/layouts/FormBuilderLayout";
-
 import { Metadata } from "next";
+import { FormRecord } from "@lib/types";
+import { auth } from "@lib/auth";
+import { AccessControlError, createAbility } from "@lib/privileges";
+import { getFullTemplateByID } from "@lib/templates";
+import { redirect } from "next/navigation";
 
 export async function generateMetadata({
   params: { locale },
@@ -15,9 +19,39 @@ export async function generateMetadata({
   };
 }
 
-export default function Page({ params: { locale } }: { params: { locale: string } }) {
+export default async function Page({
+  params: { locale, slug = [] },
+}: {
+  params: { locale: string; slug: string[] };
+}) {
+  const FormbuilderParams: { locale: string; initialForm: null | FormRecord } = {
+    initialForm: null,
+    locale,
+  };
+
+  const session = await auth();
+
+  const formID = slug[0] || null;
+
+  if (session && formID) {
+    try {
+      const ability = createAbility(session);
+
+      const initialForm = await getFullTemplateByID(ability, formID);
+
+      if (initialForm === null) redirect(`/${locale}/404`);
+
+      FormbuilderParams.initialForm = initialForm;
+    } catch (e) {
+      if (e instanceof AccessControlError) redirect(`/${locale}/admin/unauthorized`);
+    }
+  }
+
   return (
-    <FormBuilderInitializer locale={locale}>
+    <FormBuilderInitializer
+      initialForm={FormbuilderParams.initialForm}
+      locale={FormbuilderParams.locale}
+    >
       <ClientSide />
     </FormBuilderInitializer>
   );
