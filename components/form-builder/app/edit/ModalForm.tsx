c2f8134f1365072247df62b450ccbf461f9afa2d
Tim Arney <timarney@users.noreply.github.com>
2024-02-09 10:31:50 -0500

    Fix: String ID showing up rather than text itself (#3247)
    fix typo

--- components/form-builder/app/edit/ModalForm.tsx
+++ components/form-builder/app/edit/ModalForm.tsx
@@ -64,7 +64,7 @@ export const ModalForm = ({
         />
       </div>
       <div className="mb-2">
-        <ModalLabel>{t("inputdescription")}</ModalLabel>
+        <ModalLabel>{t("inputDescription")}</ModalLabel>
         <Hint>{t("descriptionDescription")}</Hint>
         <TextArea
           id={`description--modal--${item.index}`}
