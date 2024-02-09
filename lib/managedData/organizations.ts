db35d96a282c958adf76b3244e6b43452522b8ce
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-02-09 10:37:46 -0500

    Use enum for string filters

--- lib/managedData/organizations.ts
+++ lib/managedData/organizations.ts
@@ -8,26 +8,32 @@ export type Organization = {
   [key: string]: string | null;
 };
 
+enum OrganizationType {
+  GCDepartment = "GC Department",
+  CrownCorp = "Crown Corp",
+  PTM = "PTM",
+}
+
 export const organizations: ManagedDataSet<Organization[]> = {
   values: organizationsData,
   filters: {
     departments: (values: Organization[]) =>
       values
-        .filter((item) => item.type === "GC Department")
+        .filter((item) => item.type === OrganizationType.GCDepartment)
         .map((item) => ({
           en: item.name_eng,
           fr: item.name_fra,
         })),
     crownCorporations: (values: Organization[]) =>
       values
-        .filter((item) => item.type === "Crown Corp")
+        .filter((item) => item.type === OrganizationType.CrownCorp)
         .map((item) => ({
           en: item.name_eng,
           fr: item.name_fra,
         })),
     provincialTerritorial: (values: Organization[]) =>
       values
-        .filter((item) => item.type === "PTM")
+        .filter((item) => item.type === OrganizationType.PTM)
         .map((item) => ({
           en: item.name_eng,
           fr: item.name_fra,
