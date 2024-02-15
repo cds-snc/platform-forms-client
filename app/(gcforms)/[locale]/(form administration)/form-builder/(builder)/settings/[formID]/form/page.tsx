4ee51ef8dc9e1c5d001751355cd30cc699ef23d8
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-02-15 09:41:45 -0500

    Type error - removed unused props

--- app/(gcforms)/[locale]/(form administration)/form-builder/(builder)/settings/[formID]/form/page.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/(builder)/settings/[formID]/form/page.tsx
@@ -4,8 +4,6 @@ import { requireAuthentication } from "@lib/auth";
 import { checkPrivilegesAsBoolean } from "@lib/privileges";
 import { getUsers } from "@lib/users";
 
-import { BackLink } from "@clientComponents/admin/LeftNav/BackLink";
-
 import { FormBuilderInitializer } from "@clientComponents/globals/layouts/FormBuilderLayout";
 
 import { ClientSide } from "./clientSide";
@@ -24,26 +22,10 @@ export async function generateMetadata({
   };
 }
 
-const BackToManageForms = async ({ backLink }: { backLink: string }) => {
-  const { t } = await serverTranslation("admin-users");
-
-  if (!backLink) return null;
-
-  return (
-    <div className="mb-10">
-      <BackLink href={`/admin/accounts/${backLink}/manage-forms`}>
-        {t("backToManageForms")}
-      </BackLink>
-    </div>
-  );
-};
-
 export default async function Page({
   params: { locale, formID },
-  searchParams: { backLink },
 }: {
   params: { locale: string; formID: string };
-  searchParams: { backLink: string };
 }) {
   const { user } = await requireAuthentication();
 
@@ -76,7 +58,7 @@ export default async function Page({
       return { id: user.id, name: user.name, email: user.email };
     });
     return (
-      <FormBuilderInitializer locale={locale} backLink={<BackToManageForms backLink={backLink} />}>
+      <FormBuilderInitializer locale={locale}>
         <ClientSide
           formRecord={templateWithAssociatedUsers.formRecord}
           usersAssignedToFormRecord={templateWithAssociatedUsers.users}
@@ -88,7 +70,7 @@ export default async function Page({
   }
 
   return (
-    <FormBuilderInitializer locale={locale} backLink={<BackToManageForms backLink={backLink} />}>
+    <FormBuilderInitializer locale={locale}>
       <ClientSide canManageOwnership={false} />
     </FormBuilderInitializer>
   );
