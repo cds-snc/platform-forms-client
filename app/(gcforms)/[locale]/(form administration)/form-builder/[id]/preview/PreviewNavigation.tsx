46482dbd8580163c6e59df0152f46bc603a303e9
Tim Arney <timarney@users.noreply.github.com>
2024-02-21 11:10:12 -0500

    Update preview next (#3265)
    * update
    
    ---------
    
    Co-authored-by: Dave Samojlenko <dave.samojlenko@cds-snc.ca>

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PreviewNavigation.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PreviewNavigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { LangSwitcher } from "../shared/LangSwitcher";
+import { LangSwitcher } from "@clientComponents/form-builder/app/shared/LangSwitcher";
 
 export const PreviewNavigation = () => {
   return (
