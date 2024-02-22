9888993e00ce5482176d888339861aaa2c15be38
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-02-22 12:14:16 -0500

    Move unlock-publishing to support route group

--- app/(gcforms)/[locale]/(support)/unlock-publishing/page.tsx
+++ app/(gcforms)/[locale]/(support)/unlock-publishing/page.tsx
@@ -2,26 +2,20 @@ import { serverTranslation } from "@i18n";
 import { requireAuthentication } from "@lib/auth";
 import { checkPrivilegesAsBoolean } from "@lib/privileges";
 import { Metadata } from "next";
-import { UnlockPublishing } from "./clientSide";
-import DefaultLayout from "@clientComponents/globals/layouts/DefaultLayout";
+import { UnlockPublishing } from "./UnlockPublishing";
 
 export async function generateMetadata({
-  params: { locale, slug },
+  params: { locale },
 }: {
-  params: { locale: string; slug: string[] };
+  params: { locale: string };
 }): Promise<Metadata> {
   const { t } = await serverTranslation("unlock-publishing", { lang: locale });
-  const [requested] = slug ?? [];
   return {
-    title: requested ? t("unlockPublishingSubmitted.title") : t("unlockPublishing.title"),
+    title: t("unlockPublishing.title"),
   };
 }
 
-export default async function Page({
-  params: { locale, slug },
-}: {
-  params: { locale: string; slug: string[] };
-}) {
+export default async function Page({ params: { locale } }: { params: { locale: string } }) {
   const { user } = await requireAuthentication();
 
   if (
@@ -37,11 +31,5 @@ export default async function Page({
     };
   }
 
-  const [requested] = slug ?? [];
-
-  return (
-    <DefaultLayout showLanguageToggle={true}>
-      <UnlockPublishing requested={requested === "requested"} />
-    </DefaultLayout>
-  );
+  return <UnlockPublishing />;
 }
