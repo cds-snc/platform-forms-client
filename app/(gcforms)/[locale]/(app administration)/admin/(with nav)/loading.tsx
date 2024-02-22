f9e32cfd4655ee68371dafee42613991ad5fb6c3
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-02-22 15:27:08 -0500

    Remove top loader

--- app/(gcforms)/[locale]/(app administration)/admin/(with nav)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "components/clientComponents/globals/Loader";
-
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Loader />
-    </div>
-  );
-}
--- components/clientComponents/globals/Loader.tsx
+++ components/clientComponents/globals/Loader.tsx
@@ -2,19 +2,22 @@ import React from "react";
 
 interface LoaderProps {
   message?: string;
+  className?: string;
 }
 
-export const Loader = ({ message }: LoaderProps): React.ReactElement => {
+export const Loader = ({ message, className }: LoaderProps): React.ReactElement => {
   return (
     <div
       id="react-hydration-loader"
       data-testid="loading-spinner"
       role="status"
-      className="text-center"
+      className={className}
     >
-      {message && <p className="pb-8">{message}</p>}
-      <div className="flex items-center justify-center">
-        <div className="h-40 w-40 animate-spin rounded-full border-y-4 border-indigo-700"></div>
+      <div className="text-center">
+        {message && <p className="pb-8">{message}</p>}
+        <div className="flex items-center justify-center">
+          <div className="h-40 w-40 animate-spin rounded-full border-y-4 border-indigo-700"></div>
+        </div>
       </div>
     </div>
   );
