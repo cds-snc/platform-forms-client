6114a987bf01690dfcd74a69de9961b800528ffa
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-02-23 09:01:28 -0500

    Updates global error but still a WIP

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,17 +1,46 @@
 "use client";
 
-// TODO: this should catch errors from the root layout but is not.
+import "../styles/app.scss";
+import "react-app-polyfill/stable";
+import { Noto_Sans, Lato } from "next/font/google";
+import { logMessage } from "@lib/logger";
+import { useEffect } from "react";
+
+const notoSans = Noto_Sans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
+
+const lato = Lato({
+  weight: ["400", "700"],
+  variable: "--font-lato",
+  subsets: ["latin"],
+});
+
+// In the very unlikely event, this should catch errors thrown from the root layout.tsx
+// Other cases: router error
 export default function GlobalError({
-  // error,
+  error,
   reset,
 }: {
-  // error: Error & { digest?: string }
+  error: Error & { digest?: string };
   reset: () => void;
 }) {
-  // TODO: semantics, design and content
+  useEffect(() => {
+    logMessage.error(`Global Error Handler Reached: ${(error as Error).message}`);
+  }, [error]);
+
+  // TODO: localization, design and testing
 
   return (
-    <html>
+    <html lang="en" dir="ltr" className={`${notoSans.variable} ${lato.variable}`}>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Something went wrong!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" sizes="32x32" />
+      </head>
       <body>
         <h2>Something went wrong!</h2>
         <button onClick={() => reset()}>Try again</button>
