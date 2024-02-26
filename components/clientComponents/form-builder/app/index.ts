fcec0844b2a0887eceb5395c0873f0ebaf0b318f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-02-26 14:36:49 -0500

    Cleanup

--- components/clientComponents/form-builder/app/index.ts
+++ components/clientComponents/form-builder/app/index.ts
@@ -1,7 +1,6 @@
 export { ResponseDelivery } from "./ResponseDelivery";
 export { Settings } from "./Settings";
 export { LeftNavigation } from "./navigation/LeftNavigation";
-export { Publish } from "../../../../app/(gcforms)/[locale]/(form administration)/form-builder/[id]/publish/Publish";
 export { ShareModal } from "./ShareModal";
 export { ShareModalUnauthenticated } from "./ShareModalUnauthenticated";
 export { FormOwnership } from "./FormOwnership";
