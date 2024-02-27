416b934807d2f4e265911b7d7696f8769b2b5263
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-02-27 11:57:00 -0500

    Fix some tests

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/tests/Question.test.js
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/tests/Question.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { cleanup, render, act, waitFor } from "@testing-library/react";
 import { Question } from "../question/Question";
-import { useTemplateStore } from "@formbuilder/store";
+import { useTemplateStore } from "@lib/store";
 import { defaultStore as store, Providers, localStorageMock } from "@formbuilder/test-utils";
 import userEvent from "@testing-library/user-event";
-import { LocalizedElementProperties } from "@clientComponents/form-builder/types";
+import { LocalizedElementProperties } from "@lib/types/form-builder-types";
 
 // Mock sessionStorage
 Object.defineProperty(window, "sessionStorage", {
