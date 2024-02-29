619bd41ceb48b8d44a0181c359942aece63e9a0d
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-02-29 08:31:40 -0500

    user properties on session for Header / forms page layout

--- app/(gcforms)/[locale]/(form administration)/forms/layout.tsx
+++ app/(gcforms)/[locale]/(form administration)/forms/layout.tsx
@@ -4,8 +4,6 @@ import { Header } from "@clientComponents/globals";
 import { Footer, SkipLink } from "@serverComponents/globals";
 import { auth } from "@lib/auth";
 import { redirect } from "next/navigation";
-import { createAbility } from "@lib/privileges";
-import { getUser } from "@lib/users";
 
 export default async function Layout({
   children,
@@ -16,14 +14,12 @@ export default async function Layout({
 }) {
   const session = await auth();
   if (!session) redirect(`/${locale}/auth/login`);
-  const ability = createAbility(session);
-  const user = await getUser(ability, session.user.id);
 
   return (
     <>
       <div className="flex h-full flex-col">
         <SkipLink />
-        <Header context={"default"} user={user && { email: user.email, name: user.name }} />
+        <Header context={"default"} user={{ email: session.user.email, name: session.user.name }} />
         <div className="mx-4 shrink-0 grow basis-auto laptop:mx-32 desktop:mx-64">
           <main id="content">{children}</main>
           <ToastContainer />
