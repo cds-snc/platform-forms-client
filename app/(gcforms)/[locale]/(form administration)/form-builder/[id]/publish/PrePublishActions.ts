9e192b72dabc3c5d63fa7bfe9c918ecbb4fdd7a6
Vivian Nobrega <vivan.nobrega@cds-snc.ca>
2024-03-14 15:13:08 -0400

    * Don't panic, that wasn't a real password.

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/publish/PrePublishActions.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/publish/PrePublishActions.ts
@@ -4,7 +4,7 @@ import { SalesforceConnector } from "@lib/integration/salesforceConnector";
 
 export async function UpdateSalesforceRecords() {
   const sfConnector = new SalesforceConnector();
-  sfConnector.login("vivian.nobrega@cds-snc.ca.qa", "QJC_rjd5ueg-wzb7hqp");
+  sfConnector.login("vivian.nobrega@cds-snc.ca.qa", "");
   //let meow = await sfConnector.GetAccountByName("test department");
   //console.log(meow);
   //sfConnector.AddPublishRecord("test department", "test reason", "test form id");
