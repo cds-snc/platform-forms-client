2f9e14ceb41144ab55cbf481e92ee3ca63c81e44
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-03-21 11:58:09 -0400

    Translated hint text (#3379)
    * Fixes #3371 - load french hint text from server action
    
    * Mock action and remove useHandleAdd from barrel file
    
    * Refactor to single file'

--- __utils__/mocks/server-actions/index.ts
+++ __utils__/mocks/server-actions/index.ts
@@ -2,3 +2,8 @@ jest.mock("app/(gcforms)/[locale]/(form filler)/id/[...props]/actions", () => ({
   __esModule: true,
   submitForm: jest.fn(),
 }));
+
+jest.mock("@formBuilder/actions", () => ({
+  __esModule: true,
+  getTranslatedElementProperties: jest.fn(),
+}));
