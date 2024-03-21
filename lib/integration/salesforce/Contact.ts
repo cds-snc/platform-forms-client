f574a907a5c7c5d66aecc46b415351561ba22a41
Vivian Nobrega <vivan.nobrega@cds-snc.ca>
2024-03-21 14:41:23 -0400

    * Salesforce API wants a JSON object not a string.

--- lib/integration/salesforce/Contact.ts
+++ lib/integration/salesforce/Contact.ts
@@ -10,37 +10,33 @@
 export class Contact {
   firstName: string;
   lastName: string;
-  title: string;
   contactId: string;
   email: string;
 
   constructor({
     firstName,
     lastName,
-    title,
     contactId,
     email,
   }: {
     firstName: string;
     lastName: string;
-    title: string;
     contactId: string;
     email: string;
   }) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.title = title;
     this.contactId = contactId;
     this.email = email;
   }
 
-  public toJSON(): string {
-    return JSON.stringify({
+  public toJSON(): object {
+    return {
       FirstName: this.firstName,
       LastName: this.lastName,
-      Title: this.title,
+      Title: "Created by GC Forms via API",
       CDS_Contact_ID__c: this.contactId,
       Email: this.email,
-    });
+    };
   }
 }
