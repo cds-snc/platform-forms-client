e9639e1bc1491b5a5f3faaed709952dda16ef56a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-03-25 14:55:52 -0400

    Disable preview submit if unsaved

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/page.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/page.tsx
@@ -26,6 +26,7 @@ export default async function Page({
   params: { locale: string; id: string };
 }) {
   const session = await auth();
+  const formDisabled = id === "0000" || !session?.user;
   const { t } = await serverTranslation("form-builder", { lang: locale });
 
   const formID = id;
@@ -63,5 +64,5 @@ export default async function Page({
     );
   }
 
-  return <Preview />;
+  return <Preview disableSubmit={formDisabled} />;
 }
