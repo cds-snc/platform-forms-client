9255cd3d5525ea2a2704f62f6bd14372e72157ed
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-03-26 11:34:27 -0400

    work'n every night and day

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,3 +1,4 @@
+"use server";
 import { createInstance } from "i18next";
 import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next/initReactI18next";
