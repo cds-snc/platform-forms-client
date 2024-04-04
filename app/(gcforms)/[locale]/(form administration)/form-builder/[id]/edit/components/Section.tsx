557989f69cdf0d3df4b24b2c5fea0ee2c1339a96
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-04-04 16:30:59 -0400

    Allow editing Group Name

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/Section.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/Section.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from "@i18n/client";
 import { FormElementTypes } from "@lib/types";
 import { useTemplateStore } from "@lib/store/useTemplateStore";
 import { useTreeRef } from "@formBuilder/components/shared/right-panel/treeview/provider/TreeRefProvider";
+import { useState } from "react";
+import { useGroupStore } from "@formBuilder/components/shared/right-panel/treeview/store";
 
 export const Section = ({ groupId }: { groupId: string }) => {
   const { t } = useTranslation("form-builder");
@@ -14,12 +16,30 @@ export const Section = ({ groupId }: { groupId: string }) => {
 
   const { wrapper } = useTreeRef();
   const { handleAddElement } = useHandleAdd(wrapper?.current);
+  const updateGroupName = useGroupStore((state) => state.updateGroupName);
+  const [editing, setEditing] = useState(false);
 
   const groupName = groups?.[groupId]?.name;
 
   if (groupId === "start" || groupId === "end") {
     return null;
   }
+
+  const handleOnBlur = (e: React.FocusEvent) => {
+    saveGroupName(e.currentTarget.textContent || "");
+  };
+
+  const handleOnKeyUp = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      saveGroupName(e.currentTarget.textContent || "");
+    }
+  };
+
+  const saveGroupName = (groupName: string) => {
+    updateGroupName({ id: groupId, name: groupName });
+    setEditing(false);
+  };
 
   return (
     <>
@@ -32,7 +52,14 @@ export const Section = ({ groupId }: { groupId: string }) => {
         />
       </div>
       <div className="flex max-w-[800px] justify-center rounded-t-lg border-1 border-slate-700 p-2">
-        <h4>{groupName}</h4>
+        <h4
+          onDoubleClick={() => setEditing(true)}
+          contentEditable={editing}
+          onBlur={handleOnBlur}
+          onKeyUp={handleOnKeyUp}
+        >
+          {groupName}
+        </h4>
       </div>
     </>
   );
