57d55ba6693a028593417330d2590e4e51e172bf
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-04-09 09:58:33 -0400

    Fix content widths on auth pages

--- app/(gcforms)/[locale]/(user authentication)/layout.tsx
+++ app/(gcforms)/[locale]/(user authentication)/layout.tsx
@@ -30,6 +30,28 @@ export default async function Layout({
   const headersList = headers();
   const currentPath = headersList.get("x-path")?.replace(`/${locale}`, "");
 
+  const containerWidth = () => {
+    const smallPanelPaths = [
+      "/auth/reset-password",
+      "/auth/setup-security-questions",
+      "/auth/logout",
+      "/auth/login",
+      "/auth/register",
+      "/auth/account-created",
+    ];
+    const onPath = smallPanelPaths.find((path) => {
+      if (path === currentPath || currentPath?.startsWith(path)) {
+        return true;
+      }
+    });
+
+    if (onPath) {
+      return "tablet:w-[658px] ";
+    }
+
+    return "tablet:w-[768px] laptop:w-[850px]";
+  };
+
   return (
     <div className="flex min-h-full flex-col bg-gray-soft">
       <SkipLink />
@@ -65,7 +87,7 @@ export default async function Layout({
         {currentPath?.startsWith("/auth/setup-security-questions") && <Info locale={locale} />}
         <div className="account-wrapper mt-10 flex items-center justify-center">
           <div
-            className={`max-w-[900px] tablet:min-w-[658px] laptop:w-[850px] rounded-2xl border-1 border-[#D1D5DB] bg-white p-10`}
+            className={`${containerWidth()} rounded-2xl border-1 border-[#D1D5DB] bg-white p-10`}
           >
             <main id="content">
               <Link
