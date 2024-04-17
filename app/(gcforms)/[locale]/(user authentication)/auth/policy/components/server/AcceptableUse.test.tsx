eb13c92842136d44e82062b7f4e62e23c19b3a43
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-04-17 11:24:16 -0400

    Restore acceptable use test file but commented out

---
+++ app/(gcforms)/[locale]/(user authentication)/auth/policy/components/server/AcceptableUse.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+// import AcceptableUseTerms from "./AcceptableUse";
+const AcceptableUseTerms = () => <>AcceptableUseTerms</>;
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
+
+const mockedUseSession = jest.mocked(useSession);
+
+describe.skip("Acceptable use terms", () => {
+  const props = {
+    locale: "en",
+  };
+
+  mockedUseSession.mockReturnValue({
+    data: { user: { id: "1" } } as Session,
+    status: "authenticated",
+    update: jest.fn(),
+  });
+  it("Renders the acceptable use page.", () => {
+    render(<AcceptableUseTerms {...props} />);
+    expect(screen.getByRole("button", { name: "acceptableUsePage.agree" })).toBeInTheDocument();
+    expect(screen.getByTestId("richText")).toBeVisible();
+  });
+
+  it("Agree on the terms of use", async () => {
+    const update = jest.fn();
+    mockedUseSession.mockReturnValue({
+      data: { user: { id: "1" } } as Session,
+      status: "authenticated",
+      update,
+    });
+    const user = userEvent.setup();
+
+    render(<AcceptableUseTerms {...props} />);
+    expect(screen.getByRole("button", { name: "acceptableUsePage.agree" })).toBeInTheDocument();
+    await user.click(screen.getByRole("button", { name: "acceptableUsePage.agree" }));
+    expect(update.mock.calls.length).toBe(1);
+  });
+});
