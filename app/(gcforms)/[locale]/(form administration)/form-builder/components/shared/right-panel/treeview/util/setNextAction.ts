2f7cba1e07287809c1b8403a8200337ceaf15857
Tim Arney <tim@line37.com>
2024-04-18 16:33:59 -0400

    add set next actions

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/setNextAction.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/setNextAction.ts
@@ -1,4 +1,4 @@
-import { GroupsType } from "@lib/formContext";
+import { GroupsType, Group } from "@lib/formContext";
 
 /*
   This function "auto" sets the nextAction for each group
@@ -17,5 +17,18 @@ export const autoSetNextAction = (formGroups: GroupsType) => {
     }
   }
 
+  return formGroups;
+};
+
+/*
+  This function sets the nextAction for a specific group
+  The nextAction is the key of the next group in the formGroups object
+*/
+export const setGroupNextAction = (
+  formGroups: GroupsType,
+  groupId: string,
+  nextAction: Group["nextAction"]
+) => {
+  formGroups[groupId].nextAction = nextAction;
   return formGroups;
 };
