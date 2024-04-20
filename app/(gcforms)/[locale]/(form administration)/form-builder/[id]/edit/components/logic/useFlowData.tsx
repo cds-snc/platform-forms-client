351bc7fe7e38918e92c7206fe746cea8c9993873
Tim Arney <tim@line37.com>
2024-04-20 10:43:40 -0400

    add edge helper

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/logic/useFlowData.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/logic/useFlowData.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { Edge } from "reactflow";
-import { TreeItem } from "react-complex-tree";
+import { TreeItem, TreeItemIndex } from "react-complex-tree";
 
 import { useGroupStore } from "@formBuilder/components/shared/right-panel/treeview/store/useGroupStore";
 import { useTemplateStore } from "@lib/store/useTemplateStore";
@@ -31,27 +31,72 @@ const endNode = {
   type: "groupNode",
 };
 
+const defaultEdges = {
+  start: "start",
+  end: "end",
+} as const;
+
+const getEdges = (nodeId: string, prevNodeId: string, group: Group | undefined): Edge[] => {
+  // Connect to end node as we don't have a next action
+  if (prevNodeId && group && typeof group.nextAction === "undefined") {
+    return [
+      {
+        id: `e-${nodeId}-end`,
+        source: nodeId,
+        target: defaultEdges.end,
+      },
+    ];
+  }
+
+  // Add edge from this node to next action
+  if (prevNodeId && group && typeof group.nextAction === "string") {
+    const nextAction = group.nextAction;
+    return [
+      {
+        id: `e-${nodeId}-${nextAction}`,
+        source: nodeId,
+        target: nextAction,
+      },
+    ];
+  }
+
+  // Add edges for multiple next actions
+  if (prevNodeId && group && Array.isArray(group.nextAction)) {
+    const nextActions = group.nextAction;
+    const edges = nextActions.map((action: NextActionRule) => {
+      return {
+        id: `e-${nodeId}-${action.choiceId}-${action.groupId}`,
+        source: nodeId,
+        target: action.groupId,
+      };
+    });
+
+    return edges;
+  }
+
+  return [];
+};
+
 export const useFlowData = () => {
   const getGroups = useGroupStore((s) => s.getGroups);
   const treeItems = getGroups();
   const formGroups = useTemplateStore((s) => s.form.groups);
 
   const { edges, nodes } = useMemo(() => {
-    let prevNodeId: string = "start";
+    const edges: Edge[] = [];
+    const treeIndexes = treeItems.root.children;
+
     let x_pos = 50;
     let y_pos = 50;
-    const edges: Edge[] = [];
-    const children = treeItems.root.children;
+    let prevNodeId: string = "start";
 
-    if (!children) {
+    if (!treeIndexes) {
       return { edges, nodes: [] };
     }
 
-    const nodes = children.map((key, i) => {
-      const treeItem = treeItems[key];
+    const nodes = treeIndexes.map((key: TreeItemIndex) => {
+      const treeItem: TreeItem = treeItems[key];
       const group: Group | undefined = formGroups && formGroups[key] ? formGroups[key] : undefined;
-      const id = treeItem.index as string;
-
       let elements: TreeItem[] = [];
 
       if (key === "start") {
@@ -64,49 +109,23 @@ export const useFlowData = () => {
         });
       }
 
+      const newEdges = getEdges(key as string, prevNodeId, group);
+
+      if (newEdges.length >= 2) {
+        y_pos += 300;
+      } else {
+        x_pos += 300;
+      }
+
       const flowNode = {
-        id,
-        position: key === "start" ? { x: x_pos, y: y_pos } : { x: x_pos, y: y_pos + 100 },
+        id: key,
+        position: { x: x_pos, y: y_pos },
         data: { label: treeItem.data, children: elements },
         type: "groupNode",
       };
 
-      x_pos += 350;
-
-      if (prevNodeId && group && typeof group.nextAction === "string") {
-        // Add edge from this node to next action
-        edges.push({
-          id: `e-${i}`,
-          source: id,
-          target: group.nextAction,
-        });
-      }
-
-      if (prevNodeId && group && Array.isArray(group.nextAction)) {
-        // Add edges for next actions
-        const nextActions = group.nextAction;
-        nextActions.forEach((action: NextActionRule, j) => {
-          edges.push({
-            id: `e-arr-${i}-${j}`,
-            source: id,
-            target: action.groupId,
-          });
-        });
-
-        x_pos -= 350;
-        y_pos += 200;
-      }
-
-      if (prevNodeId && group && typeof group.nextAction === "undefined") {
-        // Add edge from this node to end node
-        edges.push({
-          id: `e-end-${i}`,
-          source: id,
-          target: "end",
-        });
-      }
-
-      prevNodeId = id;
+      edges.push(...(newEdges as Edge[]));
+      prevNodeId = key as string;
       return flowNode;
     });
 
