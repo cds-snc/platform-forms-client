49a1025834de08f6d50e079062917e846a345f56
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-04-24 16:26:15 -0400

    Convert drag/drop sort to operate on groups instead

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/TreeView.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/TreeView.tsx
@@ -18,6 +18,7 @@ import { useTreeRef } from "./provider/TreeRefProvider";
 import { v4 as uuid } from "uuid";
 import { findParentGroup } from "./util/findParentGroup";
 import "react-complex-tree/lib/style-modern.css";
+import { Group, GroupsType } from "@lib/formContext";
 // import { Item } from "./Item";
 
 export interface TreeDataProviderProps {
@@ -29,22 +30,75 @@ export interface TreeDataProviderProps {
   // openSection?: (id: string) => void;
 }
 
+const findItemIndex = (items: string[], itemIndex: string | number) =>
+  items.indexOf(String(itemIndex));
+
+const isOldItemPriorToNewItem = (
+  items: string[],
+  itemIndex: string | number,
+  targetIndex: number
+) => (items.findIndex((child) => child === itemIndex) ?? Infinity) < targetIndex;
+
+const removeItemAtIndex = (items: string[], index: number) => {
+  const updatedItems = [...items];
+  updatedItems.splice(index, 1);
+  return updatedItems;
+};
+
+const insertItemAtIndex = (items: string[], item: string, index: number) => {
+  const updatedItems = [...items];
+  updatedItems.splice(index, 0, item);
+  return updatedItems;
+};
+
+const handleDrop = (items: TreeItem[], targetIndex: number, elements: string[]) => {
+  let itemsPriorToInsertion = 0;
+
+  items.forEach((item, index) => {
+    // Original location
+    const originIndex = findItemIndex(elements, item.index);
+
+    // Adjust index if dragging down
+    itemsPriorToInsertion += isOldItemPriorToNewItem(elements, item.index, targetIndex) ? 1 : 0;
+
+    // Remove from old position
+    elements = removeItemAtIndex(elements, originIndex);
+
+    // Insert at new position
+    elements = insertItemAtIndex(
+      elements,
+      String(item.index),
+      targetIndex - itemsPriorToInsertion + index
+    );
+  });
+
+  return elements;
+};
+
 const ControlledTree: ForwardRefRenderFunction<unknown, TreeDataProviderProps> = (
   { children },
   ref
 ) => {
   // export const TreeView = () => {
-  const { getTreeData, addGroup, setId, updateGroupName, updateGroup, updateElementTitle } =
-    useGroupStore((s) => {
-      return {
-        getTreeData: s.getTreeData,
-        addGroup: s.addGroup,
-        setId: s.setId,
-        updateGroupName: s.updateGroupName,
-        updateElementTitle: s.updateElementTitle,
-        updateGroup: s.updateGroup,
-      };
-    });
+  const {
+    getTreeData,
+    getGroups,
+    addGroup,
+    setId,
+    updateGroupName,
+    replaceGroups,
+    updateElementTitle,
+  } = useGroupStore((s) => {
+    return {
+      getTreeData: s.getTreeData,
+      getGroups: s.getGroups,
+      replaceGroups: s.replaceGroups,
+      addGroup: s.addGroup,
+      setId: s.setId,
+      updateGroupName: s.updateGroupName,
+      updateElementTitle: s.updateElementTitle,
+    };
+  });
 
   const { tree, environment } = useTreeRef();
   const [focusedItem, setFocusedItem] = useState<TreeItemIndex | undefined>();
@@ -132,80 +186,51 @@ const ControlledTree: ForwardRefRenderFunction<unknown, TreeDataProviderProps> =
         setSelectedItems([item.index]);
       }}
       onDrop={async (items: TreeItem[], target: DraggingPosition) => {
-        let itemsPriorToInsertion = 0;
+        // Current state of the tree in Groups format
+        const currentGroups = getGroups();
 
-        // current state of the tree
-        const currentItems = getTreeData();
-
-        // Ids of the items being dragged
-        const itemsIndices = items.map((i) => i.index);
-
-        // Target parent and index
-        const { parentItem: targetParentIndex, childIndex: targetIndex } =
-          target as DraggingPositionBetweenItems;
-
-        // Loop over the items being dragged
-        items.forEach((item) => {
-          // Find the parent of the item being dragged
-          const originParent = findParentGroup(currentItems, String(item.index));
-
-          if (!originParent) {
-            throw Error(`Could not find parent of item "${item.index}"`);
-          }
-
-          if (!originParent.children) {
-            throw Error(
-              `Parent "${originParent.index}" of item "${item.index}" did not have any children`
-            );
-          }
-
-          if (target.targetType === "between-items" && target.parentItem === item.index) {
-            // Trying to drop inside itself
-            return;
-          }
-
-          // Origin index of the item being dragged
-          const originIndex = originParent.children.indexOf(String(item.index));
-
-          if (originIndex === -1) {
-            throw Error(`Item "${item.index}" not found in parent "${originParent.index}"`);
-          }
-
-          // Get the target parent
-          const targetParent = currentItems[targetParentIndex];
-
-          // Adjust the index if the item is being moved to a position after itself
-          const isOldItemPriorToNewItem =
-            ((targetParent.children ?? []).findIndex((child) => child === item.index) ?? Infinity) <
-            targetIndex;
-          itemsPriorToInsertion += isOldItemPriorToNewItem ? 1 : 0;
-
-          // Remove the item from the origin parent
-          originParent.children.splice(originIndex, 1);
-
-          // Update groups
-          updateGroup(originParent.index, originParent.children);
-        });
-
-        // Get the new state of the tree
-        const newItems = getTreeData();
-
-        // Get the target parent in the new state
-        const targetParent = newItems[targetParentIndex];
-
-        // Initialize children if there are none
-        if (!targetParent.children) {
-          targetParent.children = [];
+        if (!currentGroups) {
+          return;
         }
 
-        // Insert the items into the target parent
-        targetParent.children.splice(targetIndex - itemsPriorToInsertion, 0, ...itemsIndices);
+        // Target parent and index
+        const { parentItem: targetParent, childIndex: targetIndex } =
+          target as DraggingPositionBetweenItems;
 
-        // Update groups
-        updateGroup(targetParentIndex, targetParent.children);
+        let newGroups: GroupsType;
+        let selectedItems: string[] = [];
 
-        // Set selected to trigger a re-render
-        setSelectedItems([targetParentIndex]);
+        // Handle root level elements
+        if (targetParent === "root") {
+          let elements = Object.keys(currentGroups);
+          elements = handleDrop(items, targetIndex, elements);
+
+          // Create a new Groups object
+          newGroups = elements.reduce((acc: GroupsType, key) => {
+            const data = currentGroups[key] as Group;
+            acc[key] = data;
+            return acc;
+          }, {});
+
+          selectedItems = [targetParent];
+        } else {
+          // Handle sub level elements
+          const parentGroup = currentGroups[targetParent];
+          let elements = [...parentGroup.elements];
+          elements = handleDrop(items, targetIndex, elements);
+
+          // Create a new Groups object
+          newGroups = { ...currentGroups };
+          newGroups[targetParent] = {
+            name: parentGroup.name,
+            elements,
+          };
+
+          selectedItems = [targetParent as string];
+        }
+
+        replaceGroups(newGroups);
+        setSelectedItems(selectedItems);
       }}
       onFocusItem={(item) => {
         setFocusedItem(item.index);
