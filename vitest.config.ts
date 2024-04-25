00a2ed0d599ef4cc39aee638bc14294423b9e4f1
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-04-25 08:29:50 -0400

    bring back the global

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    globals: true, // migration from Jest - By default, vitest does not provide global APIs for explicitness
     environment: "node",
     // TODO: remove include after complete Vitest migration
     include: ["__vitests__/**/*.test.ts", "lib/vitests/**/*.test.ts"],
