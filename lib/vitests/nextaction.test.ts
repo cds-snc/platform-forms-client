6acb06998202d6cde38c3ed5350df17b595970ee
Tim Arney <tim@line37.com>
2024-05-06 09:53:34 -0400

    add catch-all

--- lib/vitests/nextaction.test.ts
+++ lib/vitests/nextaction.test.ts
@@ -31,4 +31,23 @@ describe("getNextAction function", () => {
     expect(getNextAction(groups, "group1", ["1.1"])).toBe("group3");
     expect(getNextAction(groups, "end", [])).toBe("");
   });
+
+  it("Gets next action catch-all value", () => {
+
+    const groups = {
+      "start": { name: "start", nextAction: "group1", elements: [] },
+      "group1": {
+        name: "group1", nextAction: [
+          { "choiceId": "1.0", "groupId": "group2" },
+          { "choiceId": "catch-all", "groupId": "group3" }
+        ], elements: []
+      },
+      "group2": { name: "group2", nextAction: "end", elements: [] },
+      "group3": { name: "group3", nextAction: "end", elements: [] },
+      "end": { name: "end", elements: [] }
+    };
+
+    expect(getNextAction(groups, "group1", ["1.0"])).toBe("group2");
+    expect(getNextAction(groups, "group1", ["1.1"])).toBe("group3");
+  });
 });
