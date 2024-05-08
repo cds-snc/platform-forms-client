e8cef63624a0af3d9bbaa19b68baa682cabccc50
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-05-08 12:23:17 -0400

    Add session expiry to middleware

--- lib/auth/nextAuth.ts
+++ lib/auth/nextAuth.ts
@@ -106,6 +106,7 @@ export const {
     strategy: "jwt",
     // Seconds - How long until an idle session expires and is no longer valid.
     maxAge: 2 * 60 * 60, // 2 hours
+    updateAge: 30 * 60, // 30 minutes
   },
   // Only trust the host if we don't explicitly have a AUTH_URL set
   trustHost: process.env.AUTH_URL ? false : true,
