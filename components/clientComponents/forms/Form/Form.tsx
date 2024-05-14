6b4d9e29a1916928211b948803c4a132e681d04c
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-05-14 10:31:34 -0400

    Add error handling to form submit.

--- components/clientComponents/forms/Form/Form.tsx
+++ components/clientComponents/forms/Form/Form.tsx
@@ -309,7 +309,11 @@ export const Form = withFormik<FormProps, Responses>({
     formikBag.setStatus("submitting");
     try {
       const result = await submitForm(values, formikBag.props.language, formikBag.props.formRecord);
-      result && formikBag.props.onSuccess(result);
+      if (result.error) {
+        formikBag.setStatus("Error");
+      } else {
+        formikBag.props.onSuccess(result.id);
+      }
     } catch (err) {
       logMessage.error(err as Error);
       formikBag.setStatus("Error");
