e519be8b34deb45587f2da2ce3e25584cf3ea5d7
Tim Arney <tim@line37.com>
2024-05-16 10:51:31 -0400

    add check for test mode

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
@@ -1,5 +1,9 @@
 // import { checkFlag } from "@formBuilder/actions";
 
 export const allowGrouping = async () => {
+  if (process.env.APP_ENV === "test") {
+    return false;
+  }
+
   return true;
 };
