d5425d49b187f2f3631ef44ae2b4468d17a43486
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-05-22 13:36:58 -0400

    Add error codes

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/RetrievalError.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/RetrievalError.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Button } from "@clientComponents/globals";
 import * as Alert from "@clientComponents/globals/Alert/Alert";
 import { useTranslation } from "@i18n/client";
+import { FormServerErrorCodes } from "@lib/types/form-builder-types";
 
 export const RetrievalError = () => {
   const { t } = useTranslation("form-builder-responses");
@@ -10,15 +12,20 @@ export const RetrievalError = () => {
       <Alert.Title>{t("errors.retrieval.title")}</Alert.Title>
       <Alert.Body>
         <>
-          {t("errors.retrieval.message")}
-          <button
+          <p>{t("errors.retrieval.message")}</p>
+          <p>
+            {t("errors.errorCode")} {FormServerErrorCodes.RESPONSES_RETRIEVAL}
+          </p>
+          <Button
+            className="mt-4"
+            theme="secondary"
             onClick={
               // Attempt to recover by trying to re-render the page
               () => document.location.reload()
             }
           >
             {t("errors.retrieval.tryAgain")}
-          </button>
+          </Button>
         </>
       </Alert.Body>
     </Alert.Danger>
--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/error.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/error.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import * as Alert from "@clientComponents/globals/Alert/Alert";
+import { Button } from "@clientComponents/globals/Buttons/Button";
 import { useTranslation } from "@i18n/client";
+import { FormServerErrorCodes } from "@lib/types/form-builder-types";
 
 export default function Error({ reset }: { reset: () => void }) {
   const { t } = useTranslation("form-builder-responses");
@@ -12,8 +14,13 @@ export default function Error({ reset }: { reset: () => void }) {
         <Alert.Title>{t("errors.generic.title")}</Alert.Title>
         <Alert.Body>
           <>
-            {t("errors.generic.message")}{" "}
-            <button onClick={() => reset()}>{t("errors.generic.tryAgain")}</button>
+            <p>{t("errors.generic.message")}</p>
+            <p>
+              {t("errors.errorCode")} {FormServerErrorCodes.RESPONSES}
+            </p>
+            <Button className="mt-4" theme="secondary" onClick={() => reset()}>
+              {t("errors.retrieval.tryAgain")}
+            </Button>
           </>
         </Alert.Body>
       </Alert.Danger>
