9f6fe6ccc42d9f1f843986c8b36c5b7ca96abb87
Tim Arney <tim@line37.com>
2024-05-24 13:31:07 -0400

    move text

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ConfirmationTitle.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ConfirmationTitle.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useTranslation } from "@i18n/client";
 
 export const ConfirmationTitle = () => {
-  const { t } = useTranslation(["form-builder", "confirmation"]);
+  const { t } = useTranslation("form-builder");
   return (
     <div className="mb-8 text-[1rem]">
       <div
@@ -18,9 +18,7 @@ export const ConfirmationTitle = () => {
       <div className="mb-2 inline-block rounded-md border-1 border-slate-500 bg-slate-50 px-2 py-1 text-slate-500">
         {t("confirmation.sectionTitle")}
       </div>
-      <h2 className="mt-0 text-2xl text-slate-500 laptop:mt-0">
-        {t("title", { ns: "confirmation" })}
-      </h2>
+      <h2 className="mt-0 text-2xl text-slate-500 laptop:mt-0">{t("confirmation.title")}</h2>
     </div>
   );
 };
