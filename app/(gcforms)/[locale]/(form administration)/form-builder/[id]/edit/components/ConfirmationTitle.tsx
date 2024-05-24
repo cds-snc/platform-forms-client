114a26db15274c57ae7258e131e8735b18486d17
Tim Arney <tim@line37.com>
2024-05-24 12:13:21 -0400

    re-add-confirm-to-edit

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ConfirmationTitle.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ConfirmationTitle.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "@i18n/client";
 
 export const ConfirmationTitle = () => {
   const { t } = useTranslation("confirmation");
