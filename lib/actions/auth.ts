f4461420ff0be4755aebaf391143fbaf6d555f46
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-05-24 15:58:25 -0400

    cache authCheck.

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -2,9 +2,14 @@
 
 import { auth } from "@lib/auth";
 import { createAbility } from "@lib/privileges";
+import { cache } from "react";
 
-export const authCheck = async () => {
+const authInteralCached = cache(async () => {
   const session = await auth();
   if (!session) throw new Error("No session found");
   return { ability: createAbility(session), session };
+});
+
+export const authCheck = async () => {
+  return authInteralCached();
 };
