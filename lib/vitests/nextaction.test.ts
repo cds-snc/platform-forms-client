399a5376285d7d964179e5dbb62a87a4b7332c3e
Pete <107579368+thiessenp-cds@users.noreply.github.com>
2024-05-28 15:36:35 -0400

    Feature/group titles localized (#3705)
    * WIP
    
    * update group store
    
    * Fixes section title
    
    * Updates droping groups
    
    * Fixes some linting errors
    
    * Fixes more linting errors
    
    * Fixes even more linting errors
    
    * update templates schema
    
    * fix translation
    
    * Bugfix
    
    ---------
    
    Co-authored-by: Tim Arney <tim@line37.com>
    Co-authored-by: Dave Samojlenko <dave.samojlenko@cds-snc.ca>
    Co-authored-by: Tim Arney <timarney@users.noreply.github.com>

--- lib/vitests/nextaction.test.ts
+++ lib/vitests/nextaction.test.ts
@@ -4,10 +4,10 @@ describe("getNextAction function", () => {
 
   it("Gets next action when using a string value", () => {
     const groups = {
-      "start": { name: "start", nextAction: "group1", elements: [] },
-      "group1": { name: "group1", nextAction: "group2", elements: [] },
-      "group2": { name: "group2", nextAction: "group3", elements: [] },
-      "end": { name: "end", elements: [] }
+      "start": { name: "start", titleEn: "", titleFr: "", nextAction: "group1", elements: [] },
+      "group1": { name: "group1", titleEn: "", titleFr: "", nextAction: "group2", elements: [] },
+      "group2": { name: "group2", titleEn: "", titleFr: "", nextAction: "group3", elements: [] },
+      "end": { name: "end", titleEn: "", titleFr: "", elements: [] }
     };
 
     expect(getNextAction(groups, "group1", [])).toBe("group2");
@@ -15,16 +15,16 @@ describe("getNextAction function", () => {
 
   it("Gets next action when using a array value", () => {
     const groups = {
-      "start": { name: "start", nextAction: "group1", elements: [] },
+      "start": { name: "start", titleEn: "", titleFr: "", nextAction: "group1", elements: [] },
       "group1": {
-        name: "group1", nextAction: [
+        name: "group1", titleEn: "", titleFr: "", nextAction: [
           { "choiceId": "1.0", "groupId": "group2" },
           { "choiceId": "1.1", "groupId": "group3" }
         ], elements: []
       },
-      "group2": { name: "group2", nextAction: "end", elements: [] },
-      "group3": { name: "group3", nextAction: "end", elements: [] },
-      "end": { name: "end", elements: [] }
+      "group2": { name: "group2", titleEn: "", titleFr: "", nextAction: "end", elements: [] },
+      "group3": { name: "group3", titleEn: "", titleFr: "", nextAction: "end", elements: [] },
+      "end": { name: "end", titleEn: "", titleFr: "", elements: [] }
     };
 
     expect(getNextAction(groups, "group1", ["1.0"])).toBe("group2");
@@ -35,16 +35,16 @@ describe("getNextAction function", () => {
   it("Gets next action catch-all value", () => {
 
     const groups = {
-      "start": { name: "start", nextAction: "group1", elements: [] },
+      "start": { name: "start", titleEn: "", titleFr: "", nextAction: "group1", elements: [] },
       "group1": {
-        name: "group1", nextAction: [
+        name: "group1", titleEn: "", titleFr: "", nextAction: [
           { "choiceId": "1.0", "groupId": "group2" },
           { "choiceId": "1.catch-all", "groupId": "group3" }
         ], elements: []
       },
-      "group2": { name: "group2", nextAction: "end", elements: [] },
-      "group3": { name: "group3", nextAction: "end", elements: [] },
-      "end": { name: "end", elements: [] }
+      "group2": { name: "group2", titleEn: "", titleFr: "", nextAction: "end", elements: [] },
+      "group3": { name: "group3", titleEn: "", titleFr: "", nextAction: "end", elements: [] },
+      "end": { name: "end", titleEn: "", titleFr: "", elements: [] }
     };
 
     expect(getNextAction(groups, "group1", ["1.0"])).toBe("group2");
