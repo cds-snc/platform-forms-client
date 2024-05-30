3cde469ec6335d069bf864b1e4c65b21b176b082
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-05-30 16:25:32 -0400

    Fixes/cleanup for drop target

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/handlers/handleCanDropAt.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/handlers/handleCanDropAt.ts
@@ -22,8 +22,7 @@ export const handleCanDropAt = (
   // If any of the selected items is a group
   if (groupItemsCount >= 1) {
     // Groups can't be dropped on another group
-    const { parentItem } = target as DraggingPositionBetweenItems;
-    if (items[0].isFolder && parentItem !== "root") {
+    if ((<DraggingPositionBetweenItems>target).parentItem !== "root") {
       return false;
     }
 
@@ -31,12 +30,12 @@ export const handleCanDropAt = (
     const currentGroups = getGroups() as GroupsType;
     const reviewIndex = getReviewIndex(currentGroups);
 
-    if (target.depth === 0 && (<DraggingPositionBetweenItems>target).childIndex > reviewIndex) {
+    if ((<DraggingPositionBetweenItems>target).childIndex > reviewIndex) {
       return false;
     }
 
     // Groups can't be dropped before Start
-    if (target.linearIndex === 0) {
+    if ((<DraggingPositionBetweenItems>target).childIndex === 0) {
       return false;
     }
   }
