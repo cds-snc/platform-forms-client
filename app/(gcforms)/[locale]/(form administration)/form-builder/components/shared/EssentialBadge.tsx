7f13d4fbc7e0a147863c97385fdc7716d907428f
Tim Arney <tim@line37.com>
2024-06-04 11:34:49 -0400

    update badge

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/EssentialBadge.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/EssentialBadge.tsx
@@ -6,7 +6,7 @@ import { EssentialIcon } from "@serverComponents/icons/EssentialIcon";
 export const EssentialBadge = () => {
   const { t } = useTranslation("form-builder");
   return (
-    <div className="inline-block !bg-[#E9ECEF] leading-[24px]">
+    <div className="mb-4 inline-block !bg-[#E9ECEF] leading-[24px]">
       <label data-testid="locked-item">
         <EssentialIcon className="mr-2 inline-block" />
         <span className="inline-block pr-2 text-slate-800">{t("lockedBlock")}</span>
