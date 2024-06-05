afbc56726eb8127540afefd2f0378d4774348451
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-06-05 15:25:15 -0400

    Updates tabs contrast color

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/RightPanel.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/RightPanel.tsx
@@ -33,7 +33,7 @@ const TabButton = ({
           className={cn(
             selected
               ? "border-indigo-500 text-indigo-600"
-              : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+              : "border-transparent text-slate-500 hover:border-gray-300 hover:text-slate-700",
             "whitespace-nowrap border-b-2 px-2 py-2 flex justify-center w-full",
             className
           )}
