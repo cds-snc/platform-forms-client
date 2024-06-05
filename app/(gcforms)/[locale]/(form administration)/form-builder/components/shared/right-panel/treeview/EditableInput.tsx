0f32a3ac9dcf03ec0c756a21837a4d05c2461028
Tim Arney <timarney@users.noreply.github.com>
2024-06-05 13:16:11 -0400

    Tree view updates (#3744)
    * update tree view
    
    * update styles

---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/EditableInput.tsx
@@ -0,0 +1,48 @@
+import React, { useState, ReactNode } from "react";
+import { useTreeRef } from "./provider/TreeRefProvider";
+import { useTranslation } from "@i18n/client";
+import { TreeItemIndex, TreeItemRenderContext } from "react-complex-tree";
+
+export const EditableInput = ({
+  title,
+  context,
+}: {
+  title: ReactNode;
+  context: TreeItemRenderContext;
+}) => {
+  const { tree } = useTreeRef();
+  const { t } = useTranslation("form-builder");
+
+  const itemName = React.isValidElement(title) && title.props.title;
+  const [name, setName] = useState(itemName);
+  return (
+    <input
+      {...context.interactiveElementProps}
+      type="text"
+      placeholder={t("groups.addSectionPlaceholder")}
+      autoFocus
+      className="ml-12 w-5/6 rounded-md border-2 border-slate-950 p-2"
+      value={name}
+      onFocus={(e) => {
+        e.target.select();
+      }}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          const props = context.interactiveElementProps as unknown as Record<string, unknown>;
+          const id = props["data-rct-item-id"] as unknown as TreeItemIndex;
+          tree?.current?.renameItem(id, name);
+          context.stopRenamingItem();
+        }
+        if (e.key === "Escape" || e.key === "Tab") {
+          context.stopRenamingItem();
+        }
+      }}
+      onBlur={() => {
+        context.stopRenamingItem();
+      }}
+      onChange={(e) => {
+        setName(e.target.value);
+      }}
+    />
+  );
+};
