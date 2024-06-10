102058a65cdf4946c72ace4240d89e8b59db5781
Tim Arney <tim@line37.com>
2024-06-10 09:04:47 -0400

    fix logic view outlines

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/GroupNode.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/GroupNode.tsx
@@ -82,7 +82,9 @@ export const GroupNode = (node: NodeProps) => {
         {node.id !== "end" && node.id !== "review" && (
           <button
             {...handleClick}
-            className="absolute right-[-20px] top-[-20px] cursor-pointer outline-2 outline-violet-800 hover:scale-125"
+            className={cn(
+              "absolute right-[-20px] top-[-20px] cursor-pointer outline-offset-8 outline-slate-800 hover:scale-125 rounded-full"
+            )}
           >
             <QuestionRuleSvg title={t("linkGroup")} />
           </button>
@@ -140,12 +142,16 @@ export const GroupNode = (node: NodeProps) => {
                 setId(node.id);
                 setSelectedElementId(Number(child.index));
               }}
-              className={cn(nodeClassName, selected, "focus:border-violet-800")}
+              className={cn(
+                nodeClassName,
+                selected,
+                "focus:border-violet-800 outline-offset-8 outline-slate-800 hover:scale-125 rounded-full"
+              )}
             >
               <div className="line-clamp-2 truncate text-wrap">
                 {getTitle(child.data as ElementProperties).substring(0, 300)}
               </div>
-              <div className="absolute right-[10px] top-[10px] cursor-pointer hover:scale-125">
+              <div className="absolute right-[10px] top-[10px] cursor-pointer hover:scale-125 ">
                 <OptionRuleSvg title={t("linkGroup")} />
               </div>
             </button>
