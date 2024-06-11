90d4fb6ab57bca6ad4b36cc9c3a47bd1837d4532
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2024-06-11 14:42:47 -0400

    chore: Update error handling for 2fa (#3801)
    Update error handling for 2fa

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/processFormData.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/processFormData.ts
@@ -26,8 +26,6 @@ export const processFormData = async (
       throw new MissingFormDataError("No form submitted with request");
     }
 
-    logMessage.info(`Form ID: ${reqFields ? reqFields.formID : "No form attached"}`);
-
     const form = await getPublicTemplateByID(reqFields.formID as string);
 
     if (!form) {
@@ -90,6 +88,7 @@ export const processFormData = async (
         contentLanguage ? contentLanguage : "en",
         reqFields.securityAttribute ? (reqFields.securityAttribute as string) : "Protected A"
       );
+      logMessage.info(`Submitting Response for Form ID: ${form.id}`);
       return true;
     } catch (err) {
       logMessage.error(err as Error);
