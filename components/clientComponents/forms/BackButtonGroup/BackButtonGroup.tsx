31d6951370c5338baa31c4b9be9f9e2de83ce60e
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-06-12 09:48:38 -0400

    Initial start on back button for groups

---
+++ components/clientComponents/forms/BackButtonGroup/BackButtonGroup.tsx
@@ -0,0 +1,43 @@
+import { useTranslation } from "@i18n/client";
+import { useGCFormsContext } from "@lib/hooks/useGCFormContext";
+import { Button } from "@clientComponents/globals";
+import { LockedSections } from "@formBuilder/components/shared/right-panel/treeview/types";
+
+export const BackButtonGroup = () => {
+  const { currentGroup, getGroupHistory, previousGroup, setGroup } = useGCFormsContext();
+  const { t } = useTranslation("form-builder");
+
+  if (
+    !currentGroup ||
+    currentGroup === LockedSections.START ||
+    currentGroup === LockedSections.REVIEW ||
+    currentGroup === LockedSections.END
+
+    // TODO create a hasPreviousAction function
+    // || !hasNextAction(currentGroup)
+  ) {
+    return <></>;
+  }
+
+  // TODO handle case of using review page > edit (to go back) > then trying to go back more
+
+  return (
+    <>
+      {/* For debugging */}
+      <div className="hidden">
+        DEBUGGING: {`currentGroup=${currentGroup}`},
+        {`groupHistory=${JSON.stringify(getGroupHistory())}`}
+      </div>
+      <Button
+        onClick={async (e) => {
+          e.preventDefault();
+          setGroup(previousGroup);
+        }}
+        type="button"
+        className="mr-4"
+      >
+        {t("back")}
+      </Button>
+    </>
+  );
+};
