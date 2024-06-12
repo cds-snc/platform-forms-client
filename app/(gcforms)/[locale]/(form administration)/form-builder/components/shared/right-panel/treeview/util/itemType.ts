6e46a5d836ca4646c0e732273c45a0431d95ccdb
Tim Arney <timarney@users.noreply.github.com>
2024-06-12 11:06:05 -0400

    chore: Add section title element (#3804)
    * add section title element

---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/itemType.ts
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import { TreeItem } from "react-complex-tree";
+
+export const getItemFromElement = (el: ReactNode) => {
+  if (el && typeof el === "object" && "props" in el) {
+    return el.props.item;
+  }
+};
+
+export const isTitleElementType = (item: TreeItem) => {
+  const key = item?.index as string;
+  if (!key) return false;
+  return key.includes("section-title-");
+};
+
+export const isSectionElementType = (item: TreeItem) => {
+  return item?.isFolder ? true : false;
+};
+
+export const isFormElementType = (item: TreeItem) => {
+  return item?.isFolder && !isTitleElementType(item) ? false : true;
+};
+
+export const isGhostElementType = (item: TreeItem) => {
+  return ["intro", "policy", "end"].includes(String(item?.index));
+};
