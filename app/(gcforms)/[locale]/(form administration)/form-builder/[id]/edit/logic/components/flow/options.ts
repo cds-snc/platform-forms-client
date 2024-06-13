e7a6479f8238673c6cdd05f1ba0bf0d43475a3a6
Tim Arney <tim@line37.com>
2024-06-13 14:31:46 -0400

    update logic stypes

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/options.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/options.ts
@@ -4,7 +4,7 @@ import { MarkerType } from "reactflow";
 export const edgeOptions = {
   type: "smoothstep",
   markerEnd: { type: MarkerType.ArrowClosed },
-  pathOptions: { offset: 5 },
+  pathOptions: { offset: 20 },
 };
 
 export const layoutOptions: LayoutOptions = {
