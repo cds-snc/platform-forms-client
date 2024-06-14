560f2d3e293b7dcff96dc32d4e61260bd883b1d4
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-06-14 14:21:25 -0400

    chore:reduce logging noise

--- lib/auth/nextAuth.ts
+++ lib/auth/nextAuth.ts
@@ -118,9 +118,8 @@ export const {
   debug: process.env.NODE_ENV !== "production",
   logger: {
     error(error) {
-      if (error instanceof CredentialsSignin) {
-        logMessage.warn(`NextAuth - CredentialsSignin exception: ${JSON.stringify(error)}.`);
-      } else {
+      if (!(error instanceof CredentialsSignin)) {
+        // Not a CredentialsSignin error which is for invalid credentials (including 2FA)
         logMessage.error(`NextAuth error: ${JSON.stringify(error)}.`);
       }
     },
