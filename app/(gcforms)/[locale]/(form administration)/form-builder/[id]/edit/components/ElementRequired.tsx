21df18a6fe403d02ef79ab595f5eea712eb4821b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-06-17 16:22:23 -0400

    Use local state for required checkbox

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ElementRequired.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/ElementRequired.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "@i18n/client";
 
 import { Checkbox } from "@formBuilder/components/shared";
@@ -14,18 +14,20 @@ export const ElementRequired = ({
 }) => {
   const { t } = useTranslation("form-builder");
   const allRequired = item.properties.validation?.all;
+  const [checked, setChecked] = useState(item.properties.validation?.required);
+
   return (
     <div className="mt-5 [&>div>label]:!pt-[5px]">
       <Checkbox
         disabled={item.properties.validation?.all}
         id={`required-${item.id}-id`}
         value={`required-${item.id}-value`}
-        checked={item.properties.validation?.required}
+        checked={checked}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           if (!e.target) {
             return;
           }
-
+          setChecked(e.target.checked);
           onRequiredChange(item.id, e.target.checked);
         }}
         label={allRequired ? t("allRequired") : t("required")}
