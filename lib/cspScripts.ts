1f016922d99eafc14a65473e9130089024fa615d
Tim Arney <tim@line37.com>
2024-06-17 09:52:10 -0400

    test

--- lib/cspScripts.ts
+++ lib/cspScripts.ts
@@ -30,7 +30,7 @@ export const generateCSP = (): { csp: string; nonce: string } => {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}' unsafe-inline;
+    style-src 'self' 'nonce-${nonce}' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
