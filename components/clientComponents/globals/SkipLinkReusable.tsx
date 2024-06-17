e1ed5d6247529c2fdd2c01d20e89edb88b98dccc
Tim Arney <tim@line37.com>
2024-06-17 08:13:31 -0400

    move class

--- components/clientComponents/globals/SkipLinkReusable.tsx
+++ components/clientComponents/globals/SkipLinkReusable.tsx
@@ -14,13 +14,12 @@ export const SkipLinkReusable = ({
   children: string | React.ReactNode;
   anchor: string;
 }) => {
+  const skipLinkClass =
+    "absolute w-[1px] h-[1px] truncate whitespace-nowrap focus:block focus:w-auto focus:h-auto focus:overflow-auto focus:bg-white focus:p-2";
   return (
     <div className="relative">
       <div className="absolute z-10">
-        <a
-          href={anchor}
-          className="absolute size-[1px] truncate whitespace-nowrap focus:block focus:size-auto focus:overflow-auto focus:bg-white focus:p-2"
-        >
+        <a href={anchor} className={skipLinkClass}>
           {children}
         </a>
       </div>
