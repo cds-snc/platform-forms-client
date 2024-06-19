2993561ba6b739bd70f761347e3375265175e633
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-06-19 09:30:18 -0400

    Fixes a type

--- lib/utils/form-builder/groupsHistory.ts
+++ lib/utils/form-builder/groupsHistory.ts
@@ -26,19 +26,28 @@ export const clearHistoryAfterId = (groupId: string, history: string[]) => {
 };
 
 // TODO needs LOTS of unit tests (and related integration tests?)
+
+/**
+ * Removes user answers that are no longer relevant based on the group history.
+ * A user could navigate a form down one conditional logic path, get to the review page, and then
+ * navigate back to the a previous group and change their answers. Then the old answers from the
+ * previous navigation would still be there and the output would include both the old and the new
+ * answers. To keep only the relavant answers, the function removes the old answers.
+ */
 export const getGroupValues = (
   formValues: Responses,
   groups: GroupsType | undefined,
   groupHistory: string[]
 ) => {
   if (!Array.isArray(groupHistory) || !groups) return [];
-  const history = {};
-  // Iterate over each groupHistory Id and using the matched group Id, match the elements and their
-  // values
+  const history: Responses = {};
+  // Iterates over each groupHistory Id and using the matched group Id, match the elements and their
+  // values. This removes any "old" answers no longer relevant.
   groupHistory.forEach((key) => {
     groups[key].elements.forEach((element: string) => {
+      const elementKey = element as keyof typeof Response;
       const elementName = element as keyof typeof formValues;
-      history[element] = formValues[elementName];
+      history[elementKey] = formValues[elementName];
     });
   });
   return history;
