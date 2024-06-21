1e93c7b9401b6ff3cb9ca8feeb93fc60077448ef
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-06-21 10:35:41 -0400

    Updates live component

--- lib/hooks/useLiveMessage.tsx
+++ lib/hooks/useLiveMessage.tsx
@@ -1,15 +1,28 @@
 import { createContext, useContext, useState } from "react";
 
-// Remember to add the Provider near the root of the app, currently in ClientContexts may want to move lower down
+// TODO:
+// - think about queueing announcement to avoid overflowing an ATs buffer.
 
-// TODO Move LikeMessage to a near root CLIENT component - currently testing in Start.tsx
+enum Priority {
+  LOW = "polite",
+  HIGH = "assertive",
+}
+
+interface Message {
+  content: string;
+  priority: Priority;
+}
+
+const messageDefault = { content: "", priority: Priority.LOW };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const LiveMessageContext = createContext({ message: "", setMessage: (message: string) => {} });
+const LiveMessageContext = createContext({
+  message: messageDefault,
+  setMessage: (message: Message) => {},
+});
 
 export const LiveMessagePovider = ({ children }: { children: React.ReactNode }) => {
-  const [message, setMessage] = useState<string>("");
-
+  const [message, setMessage] = useState<Message>(messageDefault);
   return (
     <LiveMessageContext.Provider value={{ message, setMessage }}>
       {children}
@@ -17,20 +30,37 @@ export const LiveMessagePovider = ({ children }: { children: React.ReactNode }) 
   );
 };
 
-// TODO add message level
-// TODO add class sr-only
-// TODO add a slight delay?
-// TODO think about queueing messages..
+/**
+ * A component that will announce a message to screen readers (and other assistive technologies).
+ *
+ * Important:
+ * This component <LiveMessage /> should be placed near the root of your nearest Client component.
+ *
+ * For more info on aria-live defaults and e.g. polite vs assertive, see:
+ * https://www.w3.org/TR/wai-aria/#aria-live
+ */
 export const LiveMessage = () => {
   const { message } = useContext(LiveMessageContext);
   return (
-    <div aria-live="polite">
-      <h1>TestLiveMessage={message}</h1>
+    <div aria-live={message.priority} className="sr-only">
+      {message.content}
     </div>
   );
 };
 
+/**
+ * A Util to access the live region to announce a message.
+ *
+ * Example usage:
+ *   const [announce] = useLiveMessage();
+ *   ...
+ *   <button onClick={() => announce("Hello World", Priority.LOW)}>Click me</button>
+ */
 export const useLiveMessage = () => {
-  const { message, setMessage: sendMessage } = useContext(LiveMessageContext);
-  return [message, sendMessage];
+  const { message, setMessage } = useContext(LiveMessageContext);
+  function announce(content: string = "", priority: Priority = Priority.LOW) {
+    if (message.content === content) return;
+    setMessage({ content, priority } as Message);
+  }
+  return [announce];
 };
