ce4f853875222a672ea6ef96b0f940ce5b0a3c98
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-06-25 14:33:10 -0400

    Adds unit test for a complex form case

--- lib/vitests/conditionalInputHistory.test.ts
+++ lib/vitests/conditionalInputHistory.test.ts
@@ -1,14 +1,26 @@
 import { getGroupValues, removeNonFormValues, valuesOnlyInHistory } from "@lib/utils/form-builder/groupsHistory";
+import { GroupsType } from "@lib/formContext";
+import { Responses } from "@lib/types";
+
+// Fixtures captured by adding a break point in the form builder and copying the values from the debugger
 import {values, groupHistory, groups} from "../../__fixtures__/conditionalInputHistoryEmptySimple.json";
 import {values as valuesEntered, groupHistory as groupHistoryEntered, groups as groupsEntered} from "../../__fixtures__/conditionalInputHistoryEnteredSimple.json";
+import {values as valuesComplex, groupHistory as groupHistoryComplex, groups as groupsComplex} from "../../__fixtures__/conditionalInputHistoryComplex.json";
 
-// TODO test with dental form data
-
-// Tests the composition of util methods to get the final conditional history result
-// Like an integration tests for unit tests :)
+const getInputHistoryValues = (values: Responses, groupHistory: string[], groups: GroupsType | undefined) => {
+  const formValues = valuesOnlyInHistory(
+    values,
+    getGroupValues(
+      values,
+      groups,
+      groupHistory as string[]
+    )
+  );
+  return removeNonFormValues(formValues);
+};
 
 describe("Conditional History", () => {
-  it("Simple case: no questions answered", () => {
+  it("Handles a simple case with no questions answered", () => {
     const expectedOutput = {
       "1": "A",
       "2": [],
@@ -25,20 +37,12 @@ describe("Conditional History", () => {
       "13": [],
       "14": "",
       "15": ""
-  };
-    const formValues = valuesOnlyInHistory(
-        values,
-        getGroupValues(
-          values,
-          groups,
-          groupHistory as string[]
-        )
-      );
-    const formOnlyValues = removeNonFormValues(formValues);
+    };
+    const formOnlyValues = getInputHistoryValues(values, groupHistory, groups);
     expect(formOnlyValues).toEqual(expectedOutput);
   });
 
-  it("Simple case: all questions answered", () => {
+  it("Handles a simple case with all questions answered", () => {
     const expectedOutput = {
         "1": "A",
         "2": [ "1", "2", "3"],
@@ -56,15 +60,62 @@ describe("Conditional History", () => {
         "14": "1",
         "15": "Ottawa",
     };
-    const formValues = valuesOnlyInHistory(
-        valuesEntered,
-        getGroupValues(
-          valuesEntered,
-          groupsEntered,
-          groupHistoryEntered as string[]
-        )
-      );
-    const formOnlyValues = removeNonFormValues(formValues);
+    const formOnlyValues = getInputHistoryValues(valuesEntered, groupHistoryEntered, groupsEntered);
+    expect(formOnlyValues).toEqual(expectedOutput);
+  });
+
+  it("Handles a complex case", () => {
+    const expectedOutput = {
+      "3": "I am applying for myself",
+      "4": "Yes",
+      "5": "11/11/1977",
+      "8": "1",
+      "10": "1",
+      "11": "1111111",
+      "12": "Single",
+      "14": "1",
+      "15": "1",
+      "17": "1",
+      "18": "1",
+      "19": "1",
+      "20": "1",
+      "22": "",
+      "23": "",
+      "24": "",
+      "25": "",
+      "26": "Canada",
+      "27": "Yes",
+      "29": "English",
+      "30": "Email",
+      "31": "1@1.com",
+      "32": "1@1.com",
+      "35": "",
+      "36": "",
+      "38": "Yes",
+      "40": "Yes, I have federal benefits",
+      "41": "Yes, I have provincial or territorial benefits",
+      "43": "Non-Insured Health Benefits Program by Indigenous Services Canada",
+      "44": "Alberta",
+      "45": "Alberta Child Health Benefits",
+      "46": "",
+      "47": "",
+      "48": "",
+      "49": "",
+      "50": "",
+      "51": "",
+      "52": "",
+      "53": "",
+      "54": "",
+      "55": "",
+      "56": "",
+      "57": "",
+      "62": "",
+      "64": "",
+      "65": "",
+      "66": "",
+      "67": ""
+    };
+    const formOnlyValues = getInputHistoryValues(valuesComplex, groupHistoryComplex, groupsComplex);
     expect(formOnlyValues).toEqual(expectedOutput);
   });
 });
