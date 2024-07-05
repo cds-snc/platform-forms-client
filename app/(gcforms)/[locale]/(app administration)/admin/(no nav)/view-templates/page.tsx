58194e5c18ad6d527ca104f648422796d8337f08
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2024-07-05 10:10:01 -0400

    chore: fixes template view in adminstration panel (#3958)

--- app/(gcforms)/[locale]/(app administration)/admin/(no nav)/view-templates/page.tsx
+++ app/(gcforms)/[locale]/(app administration)/admin/(no nav)/view-templates/page.tsx
@@ -3,7 +3,7 @@ import { authCheckAndRedirect } from "@lib/actions";
 import { checkPrivilegesAsBoolean } from "@lib/privileges";
 import { Metadata } from "next";
 import { DataView } from "./clientSide";
-import { getAllTemplates } from "@lib/templates";
+import { getTemplates } from "./actions";
 
 export async function generateMetadata({
   params: { locale },
@@ -25,19 +25,7 @@ export default async function Page() {
     { redirect: true }
   );
 
-  const templates = (await getAllTemplates(ability)).map((template) => {
-    const {
-      id,
-      form: { titleEn, titleFr },
-      isPublished,
-    } = template;
-    return {
-      id,
-      titleEn,
-      titleFr,
-      isPublished,
-    };
-  });
+  const templates = await getTemplates();
 
   return <DataView templates={templates} />;
 }
