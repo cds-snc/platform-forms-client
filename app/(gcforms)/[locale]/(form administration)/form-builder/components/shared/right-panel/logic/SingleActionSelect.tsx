693e1e5317834e96e67ef9f9976afec5e776538d
Tim Arney <tim@line37.com>
2024-07-05 15:47:34 -0400

    handle remove exit

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/logic/SingleActionSelect.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/logic/SingleActionSelect.tsx
@@ -10,6 +10,7 @@ import { useTranslation } from "@i18n/client";
 import { SaveNote } from "./SaveNote";
 import { toast } from "@formBuilder/components/shared/Toast";
 import { Checkbox } from "@formBuilder/components/shared";
+import { usePathname } from "next/navigation";
 
 const ExitIcon = () => {
   return (
@@ -34,7 +35,7 @@ export const SingleActionSelect = ({
   const findParentGroup = useGroupStore((state) => state.findParentGroup);
   const setGroupNextAction = useGroupStore((state) => state.setGroupNextAction);
   const { t } = useTranslation("form-builder");
-
+  const pathname = usePathname();
   const currentGroup = id;
   const [nextActionId, setNextActionId] = useState(nextAction);
 
@@ -71,7 +72,10 @@ export const SingleActionSelect = ({
           <Button
             onClick={() => {
               currentGroup && setGroupNextAction(currentGroup, "review");
-              flow.current?.redraw();
+              // @todo look for a better way to handle this
+              if (window !== undefined) {
+                window.location.href = `${pathname}`;
+              }
               toast.success(t("logic.actionsSaved"));
             }}
             theme="secondary"
