846b0c2297bff7aeda2c37ef451a408c71413e31
Tim Arney <tim@line37.com>
2024-07-05 14:51:16 -0400

    add next panel

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/logic/SingleActionSelect.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/logic/SingleActionSelect.tsx
@@ -11,6 +11,17 @@ import { SaveNote } from "./SaveNote";
 import { toast } from "@formBuilder/components/shared/Toast";
 import { Checkbox } from "@formBuilder/components/shared";
 
+const ExitIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
+      <path
+        d="M2 18.3244C1.45 18.3244 0.979167 18.1286 0.5875 17.7369C0.195833 17.3453 0 16.8744 0 16.3244V12.3244H2V16.3244H16V2.32443H2V6.32443H0V2.32443C0 1.77443 0.195833 1.3036 0.5875 0.911932C0.979167 0.520266 1.45 0.324432 2 0.324432H16C16.55 0.324432 17.0208 0.520266 17.4125 0.911932C17.8042 1.3036 18 1.77443 18 2.32443V16.3244C18 16.8744 17.8042 17.3453 17.4125 17.7369C17.0208 18.1286 16.55 18.3244 16 18.3244H2ZM7.5 14.3244L6.1 12.8744L8.65 10.3244H0V8.32443H8.65L6.1 5.77443L7.5 4.32443L12.5 9.32443L7.5 14.3244Z"
+        fill="#1E293B"
+      />
+    </svg>
+  );
+};
+
 export const SingleActionSelect = ({
   item,
   nextAction = "review",
@@ -49,12 +60,24 @@ export const SingleActionSelect = ({
     return (
       <div>
         <div className="p-4">
-          <h4 className="mb-4 block text-sm font-bold">
-            {t("logic.exit.exitPanel.title1")}:
-            <span className="ml-2 font-normal">{t("logic.exit.exitPanel.title2")}</span>
+          <h4 className="relative mb-4 block text-sm font-bold">
+            <span className="absolute top-[4px] inline-block">
+              <ExitIcon />
+            </span>
+            <span className="ml-6 mr-3 inline-block">{t("logic.exit.exitPanel.title1")}:</span>
+            <span className="inline-block font-normal">{t("logic.exit.exitPanel.title2")}</span>
           </h4>
           <p className="mb-4 text-sm italic">{t("logic.exit.exitPanel.description")}</p>
-          <Button theme="secondary">{t("logic.exit.exitPanel.buttonLabel")}</Button>
+          <Button
+            onClick={() => {
+              currentGroup && setGroupNextAction(currentGroup, "review");
+              flow.current?.redraw();
+              toast.success(t("logic.actionsSaved"));
+            }}
+            theme="secondary"
+          >
+            {t("logic.exit.exitPanel.buttonLabel")}
+          </Button>
         </div>
       </div>
     );
