e7660df2aa7d0a697bebf5dbf35ada8416c321f5
Tim Arney <timarney@users.noreply.github.com>
2024-07-15 16:14:36 -0400

    chore: add translations for dialogs (#4000)
    * add translation
    
    * add strings
    
    ---------
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/confirm/ConfirmDeleteSectionDialog.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/confirm/ConfirmDeleteSectionDialog.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@clientComponents/globals";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
+import { useTranslation } from "@i18n/client";
+import Markdown from "markdown-to-jsx";
 
 export type handleCloseType = (value: boolean) => void;
 
@@ -10,35 +12,30 @@ export const ConfirmDeleteSectionDialog = ({
   open: boolean;
   handleClose: handleCloseType;
 }) => {
+  const { t } = useTranslation("form-builder");
+
   return (
     <AlertDialog.Root open={open}>
       <AlertDialog.Portal>
         <AlertDialog.Overlay className="fixed inset-0 z-[200] h-screen w-screen bg-gray-500/70" />
         <AlertDialog.Content className="absolute left-1/2 top-1/2 z-[201] w-full max-w-xl -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-white p-4 shadow-2xl">
           <AlertDialog.Title className="text-2xl font-extrabold leading-tight">
-            Delete this group?
+            {t("groups.confirmDeleteGroup.title")}
           </AlertDialog.Title>
           <AlertDialog.Description className="pb-6">
-            <p>
-              <strong>Questions will also be deleted</strong>
-              <br />
-              Elements within the group will also be deleted.
-            </p>
-            <p>
-              <strong>Move questions </strong>
-              <br />
-              Move all elements that you would like to keep to another group.
-            </p>
+            <Markdown options={{ forceBlock: true }}>
+              {t("groups.confirmDeleteGroup.description")}
+            </Markdown>
           </AlertDialog.Description>
           <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
             <AlertDialog.Cancel asChild>
               <Button onClick={() => handleClose(false)} theme="secondary">
-                Cancel
+                {t("groups.confirmDeleteGroup.cancel")}
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action asChild>
               <Button onClick={() => handleClose(true)} theme="destructive">
-                Delete group and elements
+                {t("groups.confirmDeleteGroup.delete")}
               </Button>
             </AlertDialog.Action>
           </div>
