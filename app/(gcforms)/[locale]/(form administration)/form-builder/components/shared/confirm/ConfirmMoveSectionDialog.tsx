23724b227412968e3fa79b4f335b98d8feeae66b
Tim Arney <tim@line37.com>
2024-07-15 12:28:43 -0400

    add strings

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/confirm/ConfirmMoveSectionDialog.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/confirm/ConfirmMoveSectionDialog.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@clientComponents/globals";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
-
 import { useTranslation } from "@i18n/client";
+import Markdown from "markdown-to-jsx";
 
 export type handleCloseType = (value: boolean) => void;
 
@@ -19,20 +19,22 @@ export const ConfirmMoveSectionDialog = ({
         <AlertDialog.Overlay className="fixed inset-0 z-[200] h-screen w-screen bg-gray-500/70" />
         <AlertDialog.Content className="absolute left-1/2 top-1/2 z-[201] w-full max-w-xl -translate-x-1/2 -translate-y-1/2 rounded-2xl bg-white p-4 shadow-2xl">
           <AlertDialog.Title className="text-2xl font-extrabold leading-tight">
-            {t("groups.confirmDeleteGroup.title")}
+            {t("groups.confirmMoveGroup.title")}
           </AlertDialog.Title>
           <AlertDialog.Description className="pb-6">
-            {t("groups.confirmDeleteGroup.description")}
+            <Markdown options={{ forceBlock: true }}>
+              {t("groups.confirmMoveGroup.description")}
+            </Markdown>
           </AlertDialog.Description>
           <div style={{ display: "flex", gap: 25, justifyContent: "flex-end" }}>
             <AlertDialog.Cancel asChild>
               <Button onClick={() => handleClose(false)} theme="secondary">
-                {t("groups.confirmDeleteGroup.cancel")}
+                {t("groups.confirmMoveGroup.cancel")}
               </Button>
             </AlertDialog.Cancel>
             <AlertDialog.Action asChild>
               <Button onClick={() => handleClose(true)} theme="primary">
-                {t("groups.confirmDeleteGroup.confirm")}
+                {t("groups.confirmMoveGroup.confirm")}
               </Button>
             </AlertDialog.Action>
           </div>
