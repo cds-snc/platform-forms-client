0dfce2ff682125276d30059f3d0cfd038069a689
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-07-17 12:17:59 -0400

    Add aws config for maxAttempts

--- lib/integration/awsServicesConnector.ts
+++ lib/integration/awsServicesConnector.ts
@@ -27,6 +27,10 @@ export const dynamoDBDocumentClient = DynamoDBDocumentClient.from(
   new DynamoDBClient({
     ...globalConfig,
     ...localstackConfig,
+    // For non Batch operations, we will retry 10 times
+    // Batch operations are retried manually
+    // SDK retries use exponential backoff with jitter by default
+    maxAttempts: 10,
   })
 );
 
