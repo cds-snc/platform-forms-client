d284ff73878bcd8e49cdff198ecaa6bd1db3f22b
Anik Brazeau <anik.brazeau@cds-snc.ca>
2024-07-25 09:36:28 -0400

    all files

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/translate/components/TranslateWithGroups.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/translate/components/TranslateWithGroups.tsx
@@ -198,7 +198,7 @@ export const TranslateWithGroups = () => {
       <h1 className="sr-only">{t("edit")}</h1>
       <div className="mr-10">
         <div className="flex w-[800px]">
-          <h2 id="translateTitle" tabIndex={-1}>
+          <h2 id="editTranslations" tabIndex={-1}>
             {t("editTranslations")}
           </h2>
           <div className="ml-5 mt-2">
