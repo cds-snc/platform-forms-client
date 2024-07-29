f5e4f7d6d437268ce91fd0be9a5f3e6cf5bc72ae
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-07-29 15:26:00 -0400

    Tries hardcoding allowGrouping

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
@@ -1,5 +1,8 @@
-import { checkFlag } from "@formBuilder/actions";
+// import { checkFlag } from "@formBuilder/actions";
 
 export const allowGrouping = async () => {
-  return checkFlag("conditionalLogic");
+  // return checkFlag("conditionalLogic");
+
+  // TEMP: for testing, do not merge to develop
+  return true;
 };
