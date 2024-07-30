d8a0729502fb7645aa4f4c742508cbb7c4f24c43
Anik Brazeau <anik.brazeau@cds-snc.ca>
2024-07-30 14:01:19 -0400

    change order of legend to put more important option first and add punctuation for consistency

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/Legend.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/logic/components/flow/Legend.tsx
@@ -7,17 +7,17 @@ export const Legend = async ({ lang }: { lang: Language }) => {
   const { t } = await serverTranslation("form-builder", { lang });
   return (
     <div className="my-6">
-      <div className="mb-4 flex text-base italic">
-        <span className="inline-block pr-4">
-          <LogicSectionArrowIcon />
-        </span>
-        <span className="inline-block">{t("logic.legend.page")}</span>
-      </div>
       <div className="text-base italic">
         <span className="inline-block pr-4">
           <LogicOptionArrowIcon className="inline-block" />
         </span>
         <span className="inline-block">{t("logic.legend.option")}</span>
+      </div>
+      <div className="mb-4 flex text-base italic">
+        <span className="inline-block pr-4">
+          <LogicSectionArrowIcon />
+        </span>
+        <span className="inline-block">{t("logic.legend.page")}</span>
       </div>
     </div>
   );
