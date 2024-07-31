753bafcfdff30fe7955c5bd563b533eef6c4fc2d
Vivian Nobrega <43480681+ShadeWyrm@users.noreply.github.com>
2024-07-31 10:41:40 -0400

    * Remove Containerd SkipLink check for Banner, it's not relevant.

--- components/clientComponents/globals/SkipLink.tsx
+++ components/clientComponents/globals/SkipLink.tsx
@@ -1,27 +1,13 @@
 "use client";
 import { useTranslation } from "@i18n/client";
 import Link from "next/link";
-import { checkFlag } from "@formBuilder/actions";
-import React, { useState, useEffect } from "react";
 
 export const SkipLink = () => {
   const { t } = useTranslation("common");
 
-  const [isBannerEnabled, setBannerData] = useState(false);
-
-  useEffect(() => {
-    async function fetchBannerData() {
-      const isEnabled = await checkFlag("campaign");
-      setBannerData(isEnabled);
-    }
-    fetchBannerData();
-  }, []);
-
-  const bannerCSS = isBannerEnabled ? "bg-white max-w-[260px]" : "";
-
   return (
     <div id="skip-link-container">
-      <Link href="#content" id="skip-link" prefetch={false} className={bannerCSS}>
+      <Link href="#content" id="skip-link" prefetch={false} className="bg-white max-w-[260px]">
         {t("skip-link")}
       </Link>
     </div>
