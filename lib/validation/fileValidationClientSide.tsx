ba5d89e36664279524675142b622b798c580848d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-08-09 15:51:48 -0400

    fix: FileInput max upload size for all files (#4173)
    * Fix Review page when there is a fileInput present
    
    * Increase bodySizeLimit to accommodate file uploads
    
    * Revert this before merging
    
    * Fix type issue
    
    * Some comments/cleanup
    
    * Make files validation function more self-contained
    
    * bit of cleanup
    
    * Add translation strings
    
    * Replace existing isFileSizeValid validation routine
    
    * Revert tmp changes
    
    * add a few bytes for accuracy
    
    * Update i18n/translations/en/common.json
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
    
    * Update i18n/translations/fr/common.json
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
    
    * Restore old function and rename for clarity
    
    * Fix tests
    
    * Add file size
    
    * Allow cancel file input
    
    * Style and translate
    
    ---------
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>

--- lib/validation/fileValidationClientSide.tsx
+++ lib/validation/fileValidationClientSide.tsx
@@ -1,3 +1,5 @@
+import { FileInputResponse, Responses } from "@lib/types";
+
 export const ALLOWED_FILE_TYPES = [
   { mime: "application/pdf", extensions: ["pdf"] },
   { mime: "text/plain", extensions: ["txt"] },
@@ -19,7 +21,7 @@ export const ALLOWED_FILE_TYPES = [
   { mime: "application/xml", extensions: ["xml"] },
 ];
 
-const MAXIMUM_FILE_SIZE_IN_BYTES = 8000000; // 8 MB
+const MAXIMUM_FILE_SIZE_IN_BYTES = 8 * 1024 * 1024 + 512; // 8.5MB
 
 // See https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept
 export const htmlInputAccept = ALLOWED_FILE_TYPES.map((t) =>
@@ -38,6 +40,51 @@ export function isFileExtensionValid(fileName: string): boolean {
     .includes(extension);
 }
 
-export function isFileSizeValid(sizeInBytes: number): boolean {
+export function isIndividualFileSizeValid(sizeInBytes: number): boolean {
   return sizeInBytes <= MAXIMUM_FILE_SIZE_IN_BYTES;
+}
+
+/**
+ * There is a 8.5MB bodySizeLimit that applies to the entire POST payload, including
+ * all uploaded files, configured in next.config.mjs
+ *
+ * Since uploaded files are serialized to base64, there is a ~35% overhead in file size,
+ * making the limit for all files approximately 6MB.
+ *
+ * @param values
+ * @param item
+ * @param files
+ * @param errors
+ */
+export function isAllFilesSizeValid(values: Responses): boolean {
+  const files: FileInputResponse[] = [];
+  for (const item in values) {
+    const element = values[item];
+    // We're in a repeating set
+    if (Array.isArray(element)) {
+      element.forEach((el) => {
+        // @ts-expect-error - we know that el[0] is a FileInputResponse
+        const nestedElement = el[0] as FileInputResponse;
+        if (nestedElement.size) {
+          files.push(nestedElement);
+        }
+      });
+    } else {
+      // Not repeating set
+      if (typeof element === "object" && element.size) {
+        files.push(element as FileInputResponse);
+      }
+    }
+    if (files) {
+      // file.size is the size reported by the browser which is the original file size.
+      // Since we are base64 encoding the file prior to transfer, there is a ~35% overhead
+      // in file size so we multiply by 1.35.
+      const totalSize = files.reduce((sum, file) => Number(sum) + Number(file.size) * 1.35, 0);
+      if (totalSize > MAXIMUM_FILE_SIZE_IN_BYTES) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
