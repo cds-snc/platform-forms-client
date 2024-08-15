f0efc9bd48d68b16f0181124336e1f9edf12c296
Vivian Nobrega <43480681+ShadeWyrm@users.noreply.github.com>
2024-08-15 14:31:53 -0400

    * Update AddressComplete to wait for server to get API key, and add Type for AutoCompleteChoice.

--- components/clientComponents/globals/AddressComplete.tsx
+++ components/clientComponents/globals/AddressComplete.tsx
@@ -5,10 +5,21 @@ const autoCompleteUrl =
   "https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/AutoComplete/v1.00/json3.ws";
 const retriveAddressUrl =
   "https://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/RetrieveById/v1.00/json3.ws";
-const addressCompleteKey = getAddressCompleteApiKey();
+
+// AutoComplete API returns an object like:
+export interface AddressCompleteChoice {
+  Id: string;
+  Text: string;
+  Highlight: string;
+  Description: string;
+  IsRetrievable: boolean;
+}
 
 // Function returns address complete list of choices.
 export const getAddressCompleteChoices = async (query: string) => {
+  // The server returns a promise, so we need to await it, Lint thinks it isn't one for some reason.
+  const addressCompleteKey = await getAddressCompleteApiKey(); // eslint-disable-line
+
   let params = "?";
   params += "Key=" + encodeURIComponent(addressCompleteKey);
   params += "&SearchTerm=" + encodeURIComponent(query);
@@ -20,11 +31,15 @@ export const getAddressCompleteChoices = async (query: string) => {
   });
 
   const responseData = await response.json(); //Todo - Error Handling
-  return responseData.Items;
+
+  return responseData.Items as AddressCompleteChoice[];
 };
 
 // Functions returns the selected address.
 export const getSelectedAddress = async (value: string) => {
+  // The server returns a promise, so we need to await it, Lint thinks it isn't one for some reason.
+  const addressCompleteKey = await getAddressCompleteApiKey(); // eslint-disable-line
+
   const selectedResult = value;
   let params = "?";
   params += "Key=" + encodeURIComponent(addressCompleteKey);
