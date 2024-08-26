2bc701abfbe0b499d6dd2f77632761cbd116c57d
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-08-26 08:21:20 -0400

    add flag check

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api/actions.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api/actions.ts
@@ -1,8 +1,13 @@
 "use server";
 import { createKey, deleteKey, refreshKey } from "@lib/serviceAccount";
 import { revalidatePath } from "next/cache";
+import { checkOne } from "@lib/cache/flags";
+// TODO: Implement error handling once we have designs and messaging for this interface
 
 export const createServiceAccountKey = async (templateId: string) => {
+  const flag = await checkOne("zitadelAuth");
+  if (!flag) throw new Error("Zitadel Auth flag is not active");
+
   revalidatePath(
     "/app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api",
     "page"
@@ -11,10 +16,14 @@ export const createServiceAccountKey = async (templateId: string) => {
 };
 
 export const refreshServiceAccountKey = async (templateId: string) => {
+  const flag = await checkOne("zitadelAuth");
+  if (!flag) throw new Error("Zitadel Auth flag is not active");
   return refreshKey(templateId);
 };
 
 export const deleteServiceAccountKey = async (templateId: string) => {
+  const flag = await checkOne("zitadelAuth");
+  if (!flag) throw new Error("Zitadel Auth flag is not active");
   revalidatePath(
     "/app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api",
     "page"
