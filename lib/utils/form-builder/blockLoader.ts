183bca41d00a28646c77fec9c489de1b85b8fbbd
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2024-09-03 11:00:19 -0400

    Chore: replace fetch with axios (#4242)

--- lib/utils/form-builder/blockLoader.ts
+++ lib/utils/form-builder/blockLoader.ts
@@ -19,7 +19,7 @@ export const blockLoader = async (
     data: {
       elementType: type,
     },
-    timeout: process.env.NODE_ENV === "production" ? 60000 : 0,
+    timeout: 5000,
   });
 
   result.data.forEach((data: FormElement, index: number) => {
