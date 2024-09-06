53ad617cc8003ceeeb37f951429c79fcc0a0f7a3
Tim Arney <tim@line37.com>
2024-09-06 10:08:40 -0400

    update bkd

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/sub-elements/PanelHightlight.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/sub-elements/PanelHightlight.tsx
@@ -12,11 +12,16 @@ export const PanelHightLight = ({
   conditionalChildren: React.ReactNode;
 }) => {
   const { focusWithinProps, isWithin } = useIsWithin();
-  const hightLight = isWithin ? "bg-violet-100" : "border-b-1 border-slate-500";
+  const highlight = isWithin ? "bg-violet-100" : "border-b-1 border-slate-500";
+  const transitionClasses = "transition-all duration-800"; // Add transition classes
+
   return (
-    <div {...focusWithinProps} className={cn(hightLight, "px-5", isWithin && "pt-5")}>
+    <div
+      {...focusWithinProps}
+      className={cn(highlight, "px-5", transitionClasses, isWithin && "pt-5")}
+    >
       {children}
-      <div className={`block laptop:hidden ${isWithin ? "laptop:!block" : "laptop:!hidden"}`}>
+      <div className={cn("block laptop:hidden", isWithin ? "laptop:!block" : "laptop:!hidden")}>
         {conditionalChildren}
       </div>
     </div>
