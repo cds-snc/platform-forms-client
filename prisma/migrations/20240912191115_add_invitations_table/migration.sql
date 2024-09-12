407bd2b5724c9abb402c6153827e86eb15fa1a7f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-12 15:19:25 -0400

    Drop token column

---
+++ prisma/migrations/20240912191115_add_invitations_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Invitation" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "role" TEXT NOT NULL,
+    "templateId" TEXT NOT NULL,
+
+    CONSTRAINT "Invitation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Invitation" ADD CONSTRAINT "Invitation_templateId_fkey" FOREIGN KEY ("templateId") REFERENCES "Template"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
