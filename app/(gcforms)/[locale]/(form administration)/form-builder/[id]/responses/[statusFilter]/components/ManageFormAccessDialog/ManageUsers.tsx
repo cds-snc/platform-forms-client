4b8fa5da02bb5c2014cd17ff918cc21567d82f64
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-12 09:58:42 -0400

    move things back

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/ManageUsers.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/ManageUsers.tsx
@@ -6,17 +6,70 @@ type ManageUsersProps = {
   usersWithAccess: TemplateUser[];
   loggedInUserEmail: string;
   handleAddEmail: (email: string) => void;
+  handleRemoveEmail: (email: string) => void;
   emailList: string[];
 };
 
 export const ManageUsers = ({
+  selectedEmail,
   setSelectedEmail,
   usersWithAccess,
   loggedInUserEmail,
   handleAddEmail,
+  handleRemoveEmail,
   emailList,
 }: ManageUsersProps) => {
   return (
-    //
+    <>
+      <section>
+        <label htmlFor="email" className="font-bold">
+          Add people to share access
+        </label>
+        <p>
+          You can only enter email addresses with your same domain. If they do not have an account,
+          they will be invited to create one.
+        </p>
+
+        <div className="border-2 border-black flex-wrap flex gap-2 p-2">
+          {emailList.map((email) => {
+            return (
+              <div key={email} className="bg-violet-50 px-2 py-1 rounded-md inline-block">
+                {email}{" "}
+                <button className="" onClick={() => handleRemoveEmail(email)}>
+                  x
+                </button>
+              </div>
+            );
+          })}
+
+          <input
+            id="email"
+            type="text"
+            className="inline-block grow border-none outline-none px-2 py-1"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setSelectedEmail(e.target.value);
+            }}
+            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === "Enter") {
+                handleAddEmail(e.currentTarget.value);
+              }
+            }}
+            value={selectedEmail}
+          />
+        </div>
+      </section>
+
+      <section className="mt-4">
+        <h3>People with access</h3>
+        <div className="border-1 border-black p-4">
+          {usersWithAccess.map((user) => (
+            <div className="flex flex-row py-2" key={user.email}>
+              <div className="grow">{user.email}</div>
+              <div>{loggedInUserEmail === user.email ? <span></span> : <button>X</button>}</div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/index.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/index.tsx
@@ -10,6 +10,7 @@ import { useCallback, useEffect, useState } from "react";
 import { TemplateUser } from "./types";
 import { InviteUser } from "./InviteUser";
 import { sendInvitation } from "./actions";
+import { ManageUsers } from "./ManageUsers";
 
 type ManageFormAccessDialogProps = {
   templateUsers: TemplateUser[] | undefined;
@@ -32,11 +33,9 @@ export const ManageFormAccessDialog = ({ templateUsers, formId }: ManageFormAcce
 
   const handleAddEmail = (emails: string) => {
     const emailArray = emails.split(",").map((email) => email.trim());
-    console.log({ emailArray });
     const validEmails = emailArray.filter(
       (email: string) => isValidEmail(email) && !emailList.includes(email)
     );
-    console.log({ validEmails });
     setEmailList([...emailList, ...validEmails]);
     setSelectedEmail("");
   };
@@ -139,67 +138,15 @@ export const ManageFormAccessDialog = ({ templateUsers, formId }: ManageFormAcce
         >
           <div className="p-4">
             {isManagementScreen && (
-              <>
-                <section>
-                  <label htmlFor="email" className="font-bold">
-                    Add people to share access
-                  </label>
-                  <p>
-                    You can only enter email addresses with your same domain. If they do not have an
-                    account, they will be invited to create one.
-                  </p>
-
-                  <div className="border-2 border-black flex-wrap flex gap-2 p-2">
-                    {emailList.map((email) => {
-                      return (
-                        <div key={email} className="bg-violet-50 px-2 py-1 rounded-md inline-block">
-                          {email}{" "}
-                          <button className="" onClick={() => handleRemoveEmail(email)}>
-                            x
-                          </button>
-                        </div>
-                      );
-                    })}
-
-                    <input
-                      id="email"
-                      type="text"
-                      className="inline-block grow border-none outline-none px-2 py-1"
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        setSelectedEmail(e.target.value);
-                      }}
-                      onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-                        if (e.key === "Enter") {
-                          handleAddEmail(e.currentTarget.value);
-                        }
-                      }}
-                      value={selectedEmail}
-                    />
-                  </div>
-                </section>
-
-                <section className="mt-4">
-                  <h3>People with access</h3>
-                  <div className="border-1 border-black p-4">
-                    {usersWithAccess.map((user) => (
-                      <div className="flex flex-row py-2" key={user.email}>
-                        <div className="grow">{user.email}</div>
-                        <div>
-                          {loggedInUserEmail === user.email ? <span></span> : <button>X</button>}
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </section>
-              </>
-              // <ManageUsers
-              //   selectedEmail={selectedEmail}
-              //   setSelectedEmail={setSelectedEmail}
-              //   usersWithAccess={usersWithAccess}
-              //   loggedInUserEmail={session?.user.email || ""}
-              //   handleAddEmail={handleAddEmail}
-              //   emailList={emailList}
-              // />
+              <ManageUsers
+                selectedEmail={selectedEmail}
+                setSelectedEmail={setSelectedEmail}
+                usersWithAccess={usersWithAccess}
+                loggedInUserEmail={session?.user.email || ""}
+                handleAddEmail={handleAddEmail}
+                emailList={emailList}
+                handleRemoveEmail={handleRemoveEmail}
+              />
             )}
             {isInvitationScreen && (
               <InviteUser selectedEmail={selectedEmail} setMessage={setMessage} />
