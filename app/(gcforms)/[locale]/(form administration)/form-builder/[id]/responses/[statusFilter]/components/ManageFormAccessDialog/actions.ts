1db3ec8da393660d9794013a698c1a12f0276e6b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-12 16:44:02 -0400

    comments

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/actions.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/actions.ts
@@ -3,7 +3,7 @@
 import { prisma, prismaErrors } from "@lib/integration/prismaConnector";
 import { logMessage } from "@lib/logger";
 
-// @TODO: security?
+// @TODO: check privileges?
 export const checkEmailExists = async (email: string) => {
   const user = await prisma.user
     .findUnique({ where: { email } })
@@ -12,17 +12,18 @@ export const checkEmailExists = async (email: string) => {
 };
 
 export const sendInvitation = async (email: string, templateId: string, message: string) => {
-  // create invitation and send email using lib/invitations.ts
+  // @TODO: create invitation and send email using lib/invitations.ts
   logMessage.info(
     `Sending invitation email to ${email} for form ${templateId} with message ${message}`
   );
 };
 
 export const removeUserFromForm = async (userId: string, formId: string) => {
-  // remove user from form using lib/templates.ts
+  // @TODO: remove user from form using lib/templates.ts
   logMessage.info(`Removing user ${userId} from form ${formId}`);
 };
 
+// @TODO: check privileges?
 export const getTemplateUsers = async (formId: string) => {
   const template = await prisma.template
     .findUnique({
