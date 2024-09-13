a6d252f97b0af9b30bfa89d4ed7fad65932fa24f
Tim Arney <tim@line37.com>
2024-09-13 09:38:29 -0400

    add util to re-order

---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/updateElementOrder.ts
@@ -0,0 +1,12 @@
+import { FormElement } from "@lib/types";
+
+export function updateElementOrder(
+  elements: FormElement[],
+  position: number,
+  nextPosition: number
+): FormElement[] {
+  const subElements = elements;
+  const [removed] = subElements.splice(position, 1);
+  subElements.splice(nextPosition, 0, removed);
+  return subElements;
+}
