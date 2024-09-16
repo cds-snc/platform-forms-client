7c0674fd1f9a3dd37046b93a9c23d51bfafbdc3f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-16 12:06:12 -0400

    Remove unused

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/actions.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/ManageFormAccessDialog/actions.ts
@@ -3,14 +3,6 @@
 import { prisma, prismaErrors } from "@lib/integration/prismaConnector";
 import { logMessage } from "@lib/logger";
 
-// @TODO: check privileges?
-export const checkEmailExists = async (email: string) => {
-  const user = await prisma.user
-    .findUnique({ where: { email } })
-    .catch((e) => prismaErrors(e, null));
-  return !!user;
-};
-
 export const sendInvitation = async (email: string, templateId: string, message: string) => {
   // @TODO: create invitation and send email using lib/invitations.ts
   logMessage.info(
