a5da63707aaad764516b4bb5073f5791c0a29c71
Vivian Nobrega <43480681+ShadeWyrm@users.noreply.github.com>
2024-09-16 10:29:58 -0400

    * Allow route when logged out.

--- app/api/components/route.ts
+++ app/api/components/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import { middleware, sessionExists } from "@lib/middleware";
 import { checkFlag } from "@formBuilder/actions";
 
 //Seperated from utils to avoid unavailable dependencies in testing and response downloads.
@@ -8,14 +7,8 @@ export const allowAddressComplete = async () => {
 };
 
 // This route is used to allow Cypress Testing of components, and provide the API key for AddressComplete.
-export const POST = middleware([sessionExists()], async (req, props) => {
-  const { id }: { id?: string } = props.body;
-  if (!id) {
-    const allowed = await allowAddressComplete();
-    const key = allowed ? process.env.NEXT_PUBLIC_ADDRESSCOMPLETE_API_KEY : "";
-    return NextResponse.json({ allowed: allowed, key: key });
-  }
-
-  //todo.... send cache.
-  return NextResponse.json({ error: "Malformed request" }, { status: 400 });
-});
+export const POST = async () => {
+  const allowed = await allowAddressComplete();
+  const key = allowed ? process.env.NEXT_PUBLIC_ADDRESSCOMPLETE_API_KEY : "";
+  return NextResponse.json({ allowed: allowed, key: key });
+};
