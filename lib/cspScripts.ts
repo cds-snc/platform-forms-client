c9a9fdd6acf1ae1eb4a705ea5b4cf437380e4dcb
Tim Arney <tim@line37.com>
2024-09-18 07:49:01 -0400

    revert csp

--- lib/cspScripts.ts
+++ lib/cspScripts.ts
@@ -28,7 +28,9 @@ export const generateCSP = (): { csp: string; nonce: string } => {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
   const cspHeader = `
+    default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
