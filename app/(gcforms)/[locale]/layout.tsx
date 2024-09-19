9c5f70e27911a49295be68e4f28ae5a1b9b27807
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-19 10:01:29 -0400

    Notify Interceptor

--- app/(gcforms)/[locale]/layout.tsx
+++ app/(gcforms)/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { authCheckAndThrow } from "@lib/actions";
 import { ClientContexts } from "@clientComponents/globals/ClientContexts";
 import { ReactHydrationCheck } from "@clientComponents/globals";
+import { NotifyCatcher } from "@lib/notifyCatcher/NotifyCatcher";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const { session } = await authCheckAndThrow().catch(() => ({ session: null }));
@@ -8,6 +9,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <>
       <ReactHydrationCheck />
       <ClientContexts session={session}>{children}</ClientContexts>
+      {process.env.APP_ENV === "local" && <NotifyCatcher />}
     </>
   );
 }
