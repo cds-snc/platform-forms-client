a582dfa647bf996017d445839c5d778d5c85960b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-23 16:53:36 -0400

    Revert "Notify Interceptor"
    This reverts commit b51241d808504e3a745bb838a5dd26fd7b6b94da.

--- app/(gcforms)/[locale]/layout.tsx
+++ app/(gcforms)/[locale]/layout.tsx
@@ -1,7 +1,6 @@
 import { authCheckAndThrow } from "@lib/actions";
 import { ClientContexts } from "@clientComponents/globals/ClientContexts";
 import { ReactHydrationCheck } from "@clientComponents/globals";
-import { NotifyCatcher } from "@lib/notifyCatcher/NotifyCatcher";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const { session } = await authCheckAndThrow().catch(() => ({ session: null }));
@@ -9,7 +8,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <>
       <ReactHydrationCheck />
       <ClientContexts session={session}>{children}</ClientContexts>
-      <NotifyCatcher />
     </>
   );
 }
