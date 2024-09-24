2aebd37cc7244dfb42d766d0c9c7d51dcf8b5bb3
Tim Arney <tim@line37.com>
2024-09-24 14:41:36 -0400

    add note

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/treeview/util/allowGrouping.ts
@@ -1,5 +1,5 @@
 export const allowGrouping = () => {
-  // @todo remove the feature flag in a future PR / release
   // The feature should be enabled by default now
+  // @todo - re-factor to remove this function
   return true;
 };
