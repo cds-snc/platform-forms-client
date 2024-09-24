c142739fb52b2afef01aa111d5409ddc24746c22
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-09-24 15:23:50 -0400

    Removes feature flag check

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/layout.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/layout.tsx
@@ -2,9 +2,6 @@ import { serverTranslation } from "@i18n";
 import { LoggedOutTab, LoggedOutTabName } from "@serverComponents/form-builder/LoggedOutTab";
 import { authCheckAndThrow } from "@lib/actions";
 import { SettingsNavigation } from "./components/SettingsNavigation";
-import { FeatureFlagsProvider } from "@lib/hooks/useFeatureFlags";
-import { getSomeFlags } from "@lib/cache/flags";
-import { FeatureFlags } from "@lib/cache/flags";
 
 export default async function Layout({
   children,
@@ -21,21 +18,14 @@ export default async function Layout({
     return <LoggedOutTab tabName={LoggedOutTabName.SETTINGS} />;
   }
 
-  const featureFlags = await getSomeFlags([
-    FeatureFlags.experimentalBlocks,
-    FeatureFlags.zitadelAuth,
-    FeatureFlags.conditionalLogic,
-    FeatureFlags.addressComplete,
-  ]);
-
   return (
-    <FeatureFlagsProvider featureFlags={featureFlags}>
+    <>
       <h1>{t("gcFormsSettings")}</h1>
       <SettingsNavigation id={id} />
       <p className="mb-10 inline-block bg-purple-200 p-3 text-sm font-bold">
         {t("settingsResponseDelivery.beforePublishMessage")}
       </p>
       {children}
-    </FeatureFlagsProvider>
+    </>
   );
 }
