c3774df9fb22777799452a60ac77e76859c7b73b
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-09-25 09:37:58 -0400

    Moves feature flag provider up to share client context

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/layout.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/layout.tsx
@@ -15,9 +15,6 @@ import { TemplateStoreProvider } from "@lib/store/useTemplateStore";
 import { Language } from "@lib/types/form-builder-types";
 import { FormRecord } from "@lib/types";
 import { logMessage } from "@lib/logger";
-import { FeatureFlagsProvider } from "@lib/hooks/useFeatureFlags";
-import { getSomeFlags } from "@lib/cache/flags";
-import { FeatureFlags } from "@lib/cache/flags";
 
 export default async function Layout({
   children,
@@ -51,49 +48,40 @@ export default async function Layout({
     }
   }
 
-  const featureFlags = await getSomeFlags([
-    FeatureFlags.experimentalBlocks,
-    FeatureFlags.zitadelAuth,
-    FeatureFlags.conditionalLogic,
-    FeatureFlags.addressComplete,
-  ]);
-
   return (
-    <FeatureFlagsProvider featureFlags={featureFlags}>
-      <TemplateStoreProvider {...{ ...initialForm, locale, allowGroupsFlag }}>
-        <SaveTemplateProvider>
-          <RefStoreProvider>
-            <div>
-              {/* @TODO: Backlink?? */}
-              <div className="flex flex-col">
-                <SkipLink />
-                <Header context="formBuilder" className="mb-0" />
-                <div className="shrink-0 grow basis-auto bg-gray-soft">
-                  <ToastContainer containerId="default" />
-                  <ToastContainer limit={1} containerId="wide" autoClose={false} width="600px" />
-                  <div className="flex h-full flex-row gap-7">
-                    <div id="left-nav" className="z-10 border-r border-slate-200 bg-white">
-                      <div className="sticky top-0">
-                        <LeftNavigation id={id} />
-                      </div>
+    <TemplateStoreProvider {...{ ...initialForm, locale, allowGroupsFlag }}>
+      <SaveTemplateProvider>
+        <RefStoreProvider>
+          <div>
+            {/* @TODO: Backlink?? */}
+            <div className="flex flex-col">
+              <SkipLink />
+              <Header context="formBuilder" className="mb-0" />
+              <div className="shrink-0 grow basis-auto bg-gray-soft">
+                <ToastContainer containerId="default" />
+                <ToastContainer limit={1} containerId="wide" autoClose={false} width="600px" />
+                <div className="flex h-full flex-row gap-7">
+                  <div id="left-nav" className="z-10 border-r border-slate-200 bg-white">
+                    <div className="sticky top-0">
+                      <LeftNavigation id={id} />
                     </div>
-                    <GroupStoreProvider>
-                      <main
-                        id="content"
-                        className="form-builder my-7 w-full min-h-[calc(100vh-300px)]"
-                      >
-                        {children}
-                      </main>
-                      {allowGroupsFlag && <RightPanel id={id} lang={locale as Language} />}
-                    </GroupStoreProvider>
                   </div>
+                  <GroupStoreProvider>
+                    <main
+                      id="content"
+                      className="form-builder my-7 w-full min-h-[calc(100vh-300px)]"
+                    >
+                      {children}
+                    </main>
+                    {allowGroupsFlag && <RightPanel id={id} lang={locale as Language} />}
+                  </GroupStoreProvider>
                 </div>
               </div>
-              <Footer displayFormBuilderFooter className="mt-0 lg:mt-0" />
             </div>
-          </RefStoreProvider>
-        </SaveTemplateProvider>
-      </TemplateStoreProvider>
-    </FeatureFlagsProvider>
+            <Footer displayFormBuilderFooter className="mt-0 lg:mt-0" />
+          </div>
+        </RefStoreProvider>
+      </SaveTemplateProvider>
+    </TemplateStoreProvider>
   );
 }
