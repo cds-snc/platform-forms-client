ddc0a52bc1b5aa3fee0d558d0314492493360a6e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-09-27 12:49:22 -0400

    Start on Jest testing

---
+++ lib/invitations/tests/invitations.test.ts
@@ -0,0 +1,164 @@
+import { inviteUserByEmail, acceptInvitation, cancelInvitation, declineInvitation } from "../index";
+import { prisma } from "@lib/integration/prismaConnector";
+import { UserAbility } from "@lib/types";
+import { prismaMock } from "@jestUtils";
+import { getUser } from "@lib/users";
+import { getTemplateWithAssociatedUsers } from "@lib/templates";
+import {
+  InvitationNotFoundError,
+  InvitationIsExpiredError,
+  UserAlreadyHasAccessError,
+  TemplateNotFoundError,
+} from "../exceptions";
+import { sendEmail } from "@lib/integration/notifyConnector";
+
+jest.mock("@lib/integration/prismaConnector");
+jest.mock("@lib/privileges");
+jest.mock("@lib/integration/notifyConnector");
+jest.mock("@lib/logger");
+jest.mock("@lib/auditLogs");
+jest.mock("@lib/users");
+jest.mock("@lib/templates");
+jest.mock("@lib/origin");
+
+describe("Invitations", () => {
+  const mockAbility: UserAbility = { userID: "1" };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("inviteUserByEmail", () => {
+    it("should invite a user by email", async () => {
+      (getUser as jest.MockedFunction<any>).mockResolvedValue({
+        id: "1",
+        email: "test@cds-snc.ca",
+      });
+
+      (getTemplateWithAssociatedUsers as jest.MockedFunction<any>).mockResolvedValue({
+        formRecord: {
+          id: "form-id",
+          name: "form-name",
+        },
+        users: [],
+      });
+
+      (prismaMock.invitation.create as jest.MockedFunction<any>).mockResolvedValue({
+        id: "invitation-id",
+      });
+
+      await inviteUserByEmail(mockAbility, "test@example.com", "form-id", "message");
+
+      expect(prisma.invitation.create).toHaveBeenCalled();
+      expect(sendEmail).toHaveBeenCalled();
+    });
+
+    // it("should throw UserAlreadyHasAccessError if user already has access", async () => {
+    //   prisma.user.findFirst.mockResolvedValue({ id: "user-id", email: "test@example.com" });
+    //   prisma.template.findFirst.mockResolvedValue({ users: [{ email: "test@example.com" }] });
+
+    //   await expect(
+    //     inviteUserByEmail(mockAbility, "test@example.com", "form-id", "message")
+    //   ).rejects.toThrow(UserAlreadyHasAccessError);
+    // });
+
+    // it("should throw TemplateNotFoundError if template is not found", async () => {
+    //   prisma.template.findFirst.mockResolvedValue(null);
+
+    //   await expect(
+    //     inviteUserByEmail(mockAbility, "test@example.com", "form-id", "message")
+    //   ).rejects.toThrow(TemplateNotFoundError);
+    // });
+  });
+
+  //   describe("acceptInvitation", () => {
+  //     it("should accept an invitation", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue({
+  //         id: "invitation-id",
+  //         email: "test@example.com",
+  //         expires: new Date(Date.now() + 10000),
+  //       });
+  //       prisma.user.findFirst.mockResolvedValue({ id: "user-id" });
+
+  //       await acceptInvitation(mockAbility, "invitation-id");
+
+  //       expect(prisma.invitation.delete).toHaveBeenCalled();
+  //     });
+
+  //     it("should throw InvitationNotFoundError if invitation is not found", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue(null);
+
+  //       await expect(acceptInvitation(mockAbility, "invitation-id")).rejects.toThrow(
+  //         InvitationNotFoundError
+  //       );
+  //     });
+
+  //     it("should throw InvitationIsExpiredError if invitation is expired", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue({
+  //         id: "invitation-id",
+  //         email: "test@example.com",
+  //         expires: new Date(Date.now() - 10000),
+  //       });
+
+  //       await expect(acceptInvitation(mockAbility, "invitation-id")).rejects.toThrow(
+  //         InvitationIsExpiredError
+  //       );
+  //     });
+  //   });
+
+  //   describe("cancelInvitation", () => {
+  //     it("should cancel an invitation", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue({
+  //         id: "invitation-id",
+  //         email: "test@example.com",
+  //         templateId: "template-id",
+  //       });
+
+  //       await cancelInvitation(mockAbility, "invitation-id");
+
+  //       expect(prisma.invitation.delete).toHaveBeenCalled();
+  //     });
+
+  //     it("should throw InvitationNotFoundError if invitation is not found", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue(null);
+
+  //       await expect(cancelInvitation(mockAbility, "invitation-id")).rejects.toThrow(
+  //         InvitationNotFoundError
+  //       );
+  //     });
+  //   });
+
+  //   describe("declineInvitation", () => {
+  //     it("should decline an invitation", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue({
+  //         id: "invitation-id",
+  //         email: "test@example.com",
+  //       });
+  //       prisma.user.findFirst.mockResolvedValue({ id: "user-id", email: "test@example.com" });
+
+  //       await declineInvitation(mockAbility, "invitation-id");
+
+  //       expect(prisma.invitation.delete).toHaveBeenCalled();
+  //     });
+
+  //     it("should throw InvitationNotFoundError if invitation is not found", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue(null);
+
+  //       await expect(declineInvitation(mockAbility, "invitation-id")).rejects.toThrow(
+  //         InvitationNotFoundError
+  //       );
+  //     });
+
+  //     it("should throw UserNotFoundError if user is not found", async () => {
+  //       prisma.invitation.findUnique.mockResolvedValue({
+  //         id: "invitation-id",
+  //         email: "test@example.com",
+  //       });
+  //       prisma.user.findFirst.mockResolvedValue(null);
+
+  //       await expect(declineInvitation(mockAbility, "invitation-id")).rejects.toThrow(
+  //         UserNotFoundError
+  //       );
+  //     });
+  //   });
+});
