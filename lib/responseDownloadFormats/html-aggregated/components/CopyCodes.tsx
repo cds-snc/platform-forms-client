941b0ea117c313ab28c773cfbd56ebab1a69b857
Tim Arney <tim@line37.com>
2024-10-04 10:05:31 -0400

    fix button styles

--- lib/responseDownloadFormats/html-aggregated/components/CopyCodes.tsx
+++ lib/responseDownloadFormats/html-aggregated/components/CopyCodes.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { customTranslate } from "../../../i18nHelpers";
 import { getOrigin } from "@lib/origin";
-import { themes } from "@clientComponents/globals/Buttons/themes";
 
 export const CopyCodes = ({
   confirmationCodes,
@@ -19,7 +18,7 @@ export const CopyCodes = ({
   return (
     <div className="flex flex-row gap-4">
       <button
-        className={`${themes.base} ${themes.primary}`}
+        className="inline-flex items-center rounded-md border-2 border-solid border-gcds-blue-800 bg-gcds-blue-900 p-3 font-medium leading-[24px] text-white-default transition-all duration-150 ease-in-out hover:border-gcds-blue-800 hover:bg-gcds-blue-800 hover:text-white-default focus:border-gcds-blue-850 focus:bg-gcds-blue-850 focus:text-white-default focus:outline focus:outline-[3px] focus:outline-offset-2 focus:outline-gcds-blue-850 active:top-0.5 active:border-black active:bg-black active:text-white-default active:outline-[3px] active:outline-offset-2 active:outline-gcds-blue-850 disabled:cursor-not-allowed disabled:!border-none disabled:bg-gray-light disabled:text-gray-dark"
         id={`copyCodeButton${capitalizedLang}`}
         aria-labelledby={`confirmReceiptInfo${capitalizedLang}`}
         data-clipboard-text={confirmationCodes}
