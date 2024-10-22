c772f743d63788c17389e5a128d630c8418a335b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-10-22 12:41:54 -0400

    Revert "Add unique constraint on invitations to ensure no duplicate invitations get created"
    This reverts commit 4317aaae1db3326119a164a7181f25f6925f9688.

--- prisma/migrations/20241016200038_add_email_templateid_unique_constraint_on_invitations/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email,templateId]` on the table `Invitation` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Invitation_email_templateId_key" ON "Invitation"("email", "templateId");
