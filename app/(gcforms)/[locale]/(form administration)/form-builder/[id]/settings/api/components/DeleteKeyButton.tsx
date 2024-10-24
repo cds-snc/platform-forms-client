4e2961d20d88ba8c80ade51302f60fc36da5e2db
Tim Arney <timarney@users.noreply.github.com>
2024-10-24 13:42:18 -0400

    chore: Move key generation to inside modal / download  (#4465)
    * update dialog
    ---------
    
    Co-authored-by: Clément JANIN <clement.janin@cds-snc.ca>

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api/components/DeleteKeyButton.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/api/components/DeleteKeyButton.tsx
@@ -1,20 +1,31 @@
+import React, { useState } from "react";
 import { useTranslation } from "@i18n/client";
-
-import { Button } from "@clientComponents/globals";
 import { ApiTooltip } from "./ApiTooltip";
 import { deleteServiceAccountKey } from "../actions";
 
+import { SubmitButton as DeleteApiKeyButton } from "@clientComponents/globals/Buttons/SubmitButton";
+
 export const DeleteKeyButton = ({ id, keyId }: { id: string; keyId: string }) => {
   const { t } = useTranslation("form-builder");
+  const [deleting, setDeleting] = useState(false);
   return (
     <>
       <div className="mb-4">
         <div className="font-bold">{t("settings.api.keyId")}</div>
         {keyId} <ApiTooltip />
       </div>
-      <Button theme="destructive" className="mr-4" onClick={() => deleteServiceAccountKey(id)}>
+      <DeleteApiKeyButton
+        loading={deleting}
+        theme="destructive"
+        className="mr-4"
+        onClick={async () => {
+          setDeleting(true);
+          await deleteServiceAccountKey(id);
+          setDeleting(false);
+        }}
+      >
         {t("settings.api.deleteKey")}
-      </Button>
+      </DeleteApiKeyButton>
     </>
   );
 };
