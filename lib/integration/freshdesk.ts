6fcdc0b04a9bcc770653643a590b733a370e40f8
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-10-31 11:47:14 -0400

    Add a signal timeout on FreskDesk axios call

--- lib/integration/freshdesk.ts
+++ lib/integration/freshdesk.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { logMessage } from "@lib/logger";
 import { getOrigin } from "@lib/origin";
 
@@ -84,7 +84,7 @@ export const createTicket = async ({
 }: createTicketProps) => {
   if (process.env.APP_ENV === "test") {
     logMessage.info("Not sending to Fresh Desk, application in Test mode");
-    return { status: 200 };
+    return { status: 200 } as AxiosResponse;
   }
 
   const username = process.env.FRESHDESK_API_KEY;
@@ -93,7 +93,7 @@ export const createTicket = async ({
 
   if (!username) throw new Error("Freshdesk API key not found");
 
-  const response = await axios({
+  return axios({
     url: "https://cds-snc.freshdesk.com/api/v2/tickets",
     method: "POST",
     headers: {
@@ -102,6 +102,9 @@ export const createTicket = async ({
     },
 
     data: JSON.stringify(data),
+    // If no connection can be made with Freskdesk within 5 seconds, abort the call.
+    signal: AbortSignal.timeout(5000),
+    // If Freskdesk accepts the connection but does not respond within 5 seconds, abort the call.
     timeout: 5000,
   }).catch((error) => {
     if (error.response) {
@@ -111,12 +114,14 @@ export const createTicket = async ({
           error.response.data
         )} - ${email} - ${JSON.stringify(data)}`
       );
+    } else if (axios.isCancel(error)) {
+      // No response was received from FreshDesk
+      logMessage.error(`Call to FreshDesk timed out.  Could not submit: ${JSON.stringify(data)}`);
+    } else {
+      // Unknown Error
+      logMessage.error(`Could not connect to FreshDesk: ${error.message}`);
     }
-    // No response was received from FreshDesk
-    logMessage.error(`Call to FreshDesk timed out.  Could not submit: ${JSON.stringify(data)}`);
     // Error back to client.
     throw new Error("Could not connect to Freshdesk");
   });
-
-  return response;
 };
