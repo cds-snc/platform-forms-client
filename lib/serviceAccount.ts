a33986fc129a4d94cc40023c8a6cccc76c464a37
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-11-04 14:59:19 -0500

    Return false on unsaved form

--- lib/serviceAccount.ts
+++ lib/serviceAccount.ts
@@ -116,6 +116,10 @@ export const checkMachineUserExists = async (templateId: string) => {
 };
 
 export const checkKeyExists = async (templateId: string) => {
+  if (templateId === "0000") {
+    return false;
+  }
+
   const { ability } = await authCheckAndThrow();
   await checkUserHasTemplateOwnership(ability, templateId);
 
