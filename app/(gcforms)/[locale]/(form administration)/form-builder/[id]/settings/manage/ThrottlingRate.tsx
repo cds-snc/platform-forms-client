3e7daec968fdcae8b9f0c8e02955899da3ecc9c6
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-11-13 11:26:36 -0500

    Updates to use controlled inputs

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ThrottlingRate.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ThrottlingRate.tsx
@@ -1,77 +1,93 @@
 "use client";
-import { Button } from "@clientComponents/globals";
+import { useState } from "react";
 import { useTranslation } from "@i18n/client";
-import { logMessage } from "@lib/logger";
+import { Button } from "@clientComponents/globals";
 import {
   permanentThrottling,
   resetThrottling,
   scheduledThrottling,
 } from "@lib/cache/throttlingCache";
+import { Checkbox } from "@formBuilder/components/shared/MultipleChoice";
+import { Input } from "@formBuilder/components/shared/Input";
+import { logMessage } from "@lib/logger";
+
+// TODO only show this if the user has access
 
 export const ThrottlingRate = ({ formId }: { formId: string }) => {
   const { t } = useTranslation();
+  const [weeks, setWeeks] = useState("");
+  const [permanent, setPermanent] = useState("");
+  const [success, setSuccess] = useState("");
 
-  // TODO: Probably drop GCDS for custom control
+  const formAction = async (/*formData*/) => {
+    logMessage.info(`Client updateThrottling ${formId}, ${weeks}, ${permanent}`);
+
+    if (permanent) {
+      await permanentThrottling(formId);
+      setSuccess("permanent");
+      return;
+    }
+
+    if (weeks) {
+      await scheduledThrottling(formId, Number(weeks));
+      // TODO convert weeks to YYYY-MM-DD
+      setSuccess("weeks");
+      return;
+    }
+
+    // Reset - either blank form or reset clicked
+    await resetThrottling(formId);
+    setSuccess("reset");
+  };
+
   return (
     <div className="mb-20">
-      <form
-        action={async (formData) => {
-          const weeks = Number(formData.get("throttling-weeks"));
-          const permanent = Boolean(formData.get("throttling-permanent"));
-          logMessage.info(`Client updateThrottling ${formId}, ${weeks}, ${permanent}`);
-
-          if (permanent) {
-            await permanentThrottling(formId);
-            return;
-          }
-
-          if (weeks) {
-            await scheduledThrottling(formId, weeks);
-            return;
-          }
-
-          // How get here?
-          await resetThrottling(formId);
-        }}
-      >
+      <form action={formAction}>
         <h2>{t("TODO-Throttling Rate")}</h2>
         <p>
           <strong>{t("TODO-Increase throttling rate for up to 12 weeks")}</strong>
         </p>
-        <div className="focus-group gcds-input-wrapper flex mb-0">
-          {/* wrapped - hard to overwrite gcds width.. */}
-          <div className="w-16">
-            <input
-              className="mb-2 gc-input-text mr-2"
-              name="throttling-weeks"
-              id="throttling-weeks"
-              maxLength={2}
-              pattern="\d{1,2}"
-            />
-          </div>
-          <label className="gc-label required mt-2 ml-2" htmlFor="throttling-weeks">
+        <div>
+          <Input
+            className="w-16"
+            id="throttling-weeks"
+            name="throttling-weeks"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setWeeks(e.target.value)}
+          />
+          <label className="mt-2 ml-2" htmlFor="throttling-weeks">
             {t("TODO-Weeks")}
           </label>
         </div>
-        {/*
-          TODO: success message
-          Throttling rate increased.
-          Rate will stay in effect until YYYY-MM-DD) 
-        */}
-        <div className="focus-group gc-input-checkbox mb-8">
-          <input
-            className="gc-input-checkbox__input"
-            type="checkbox"
-            name="throttling-permanent"
+        <div role="alert">
+          {success && (success === "weeks" || success === "permanent") && (
+            <div>
+              Throttling rate increased.
+              <br />
+              Rate will stay in effect until: {success}
+            </div>
+          )}
+          {success && success === "reset" && <div>Throttling rate reset.</div>}
+        </div>
+        <div className="focus-group mb-4 flex align-middle">
+          <Checkbox
+            data-testid="required"
             id="throttling-permanent"
-          />
-          <label className="checkbox-label-text mt-2 ml-12" htmlFor="throttling-permanent">
+            name="throttling-permanent"
+            value={permanent}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPermanent(e.target.checked)}
+          ></Checkbox>
+          <label className="" htmlFor="throttling-permanent">
             {t("TODO-Permanently increase throttling rate")}
           </label>
         </div>
-        <Button dataTestId="increase-throttle" theme="secondary" type="submit">
-          {t("TODO-Update rate")}
-        </Button>
+        <div className="flex">
+          <Button dataTestId="increase-throttle" theme="secondary" type="submit">
+            {t("TODO-Update rate")}
+          </Button>
+          <Button className="ml-4" dataTestId="reset-throttle" theme="link" type="submit">
+            {t("TODO-Reset throttle")}
+          </Button>
+        </div>
       </form>
     </div>
   );
