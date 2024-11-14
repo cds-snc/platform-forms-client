67014dfcdbb67e7074e288bf71397d0280b9e94f
Tim Arney <tim@line37.com>
2024-11-14 15:13:54 -0500

    add box

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/SystemStatus.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/SystemStatus.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { HealthCheckBox } from "@clientComponents/globals/HealthCheckBox/HealthCheckBox";
 
 export const SystemStatus = () => {
   const { t } = useTranslation("form-builder");
   return (
     <div>
       <h3 className="mb-8">{t("systemHealthCheck.title")}</h3>
+
+      <HealthCheckBox.Success titleKey="systemHealth.contactSupport.title">
+        <HealthCheckBox.Text i18nKey="systemHealth.contactSupport.description" />
+      </HealthCheckBox.Success>
     </div>
   );
 };
