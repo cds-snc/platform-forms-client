5113c07ea98a9befb9cf91ad1da87cf6325ea63a
Tim Arney <tim@line37.com>
2024-11-14 08:39:40 -0500

    add view for submissions available

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/NoResponses.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/NoResponses.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, HeadingLevel } from "@clientComponents/globals/card/Card";
 import Image from "next/image";
 import { VaultStatus } from "@lib/types";
 import { useTranslation } from "react-i18next";
 import { useFormBuilderConfig } from "@lib/hooks/useFormBuilderConfig";
+import { newResponsesExist } from "../actions";
 
-export const NoResponses = ({ statusFilter }: { statusFilter: string }) => {
+export const NoResponses = ({ statusFilter, formId }: { statusFilter: string; formId: string }) => {
   const { t } = useTranslation("form-builder-responses");
   const { hasApiKeyId } = useFormBuilderConfig();
+
+  const [checkingApiSubmissions, setCheckingApiSubmissions] = useState(true);
+  const [hasApiSubmissions, setHasApiSubmissions] = useState(false);
+
+  useEffect(() => {
+    const getApiSubmissions = async () => {
+      const result = await newResponsesExist(formId);
+      if (result === true) {
+        setHasApiSubmissions(true);
+        setCheckingApiSubmissions(false);
+      }
+    };
+    if (hasApiKeyId) {
+      getApiSubmissions();
+    }
+  }, [hasApiKeyId, formId]);
+
   return (
     <>
       {statusFilter === VaultStatus.NEW && (
@@ -37,15 +55,7 @@ export const NoResponses = ({ statusFilter }: { statusFilter: string }) => {
           headingStyle="gc-h2 text-[#748094]"
         />
       )}
-      {hasApiKeyId && statusFilter === VaultStatus.CONFIRMED && (
-        <Card
-          icon={<Image src="/img/mailbox.svg" alt="" width="200" height="200" />}
-          title={t("downloadResponsesTable.card.noNewResponsesApi")}
-          content={t("downloadResponsesTable.card.noNewResponsesApiMessage")}
-          headingTag={HeadingLevel.H1}
-          headingStyle="gc-h2 text-[#748094]"
-        />
-      )}
+
       {statusFilter === VaultStatus.PROBLEM && (
         <>
           <h1 className="visually-hidden">{t("tabs.problemResponses.title")}</h1>
@@ -55,6 +65,37 @@ export const NoResponses = ({ statusFilter }: { statusFilter: string }) => {
             content={t("downloadResponsesTable.card.noProblemResponsesMessage")}
           />
         </>
+      )}
+
+      {/* 
+          API user has no responses available
+        */}
+      {hasApiKeyId &&
+        !checkingApiSubmissions &&
+        !hasApiSubmissions &&
+        statusFilter === VaultStatus.CONFIRMED && (
+          <Card
+            icon={<Image src="/img/mailbox.svg" alt="" width="200" height="200" />}
+            title={t("downloadResponsesTable.card.noNewResponsesApi")}
+            content={t("downloadResponsesTable.card.noNewResponsesApiMessage")}
+            headingTag={HeadingLevel.H1}
+            headingStyle="gc-h2 text-[#748094]"
+          />
+        )}
+
+      {/* 
+          No responses are available for this view 
+          but there are submissions available for API retreival 
+        */}
+
+      {!checkingApiSubmissions && hasApiKeyId && hasApiSubmissions && (
+        <Card
+          icon={<Image src="/img/mailbox.svg" alt="" width="200" height="200" />}
+          title={t("downloadResponsesTable.card.apiResponsesAvailable")}
+          content={t("downloadResponsesTable.card.apiResponsesAvailableMessage")}
+          headingTag={HeadingLevel.H1}
+          headingStyle="gc-h2 text-[#748094]"
+        />
       )}
     </>
   );
