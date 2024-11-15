7cd092cfae15cd1fb80cf5e15bc81b491e6bc092
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-11-15 09:40:42 -0500

    Moves redis strings to consts

--- lib/cache/throttlingCache.ts
+++ lib/cache/throttlingCache.ts
@@ -3,6 +3,13 @@ import { logMessage } from "@lib/logger";
 import { getRedisInstance } from "../integration/redisConnector";
 import { getWeeksInSeconds } from "@lib/utils/date/dateConversions";
 
+const REDIS_RATE_LIMIT_KEY_PREFIX: string = "rate-limit";
+
+// future settings will probably be low and medium
+const THROTTLE_SETTING = {
+  high: "high",
+} as const;
+
 export const getThrottling = async (
   formId: string
 ): Promise<{ rate: string | null; expires: number }> => {
@@ -21,7 +28,7 @@ export const getThrottling = async (
 // Increases the throttling rate for a limited duration by leveraging the Redis built-in expiration
 // feature by setting an expiryDelay
 export const scheduledThrottling = async (formId: string, weeks: number) => {
-  const modifyParameter = `rate-limit:${formId}`;
+  const modifyParameter = `${REDIS_RATE_LIMIT_KEY_PREFIX}:${formId}`;
   try {
     const redis = await getRedisInstance();
     const weeksInSeconds = getWeeksInSeconds(weeks);
@@ -31,7 +38,7 @@ export const scheduledThrottling = async (formId: string, weeks: number) => {
         expiresDateInSeconds
       )}`
     );
-    await redis.setex(modifyParameter, expiresDateInSeconds, "high");
+    await redis.setex(modifyParameter, expiresDateInSeconds, THROTTLE_SETTING.high);
   } catch (e) {
     logMessage.error(e as Error);
     throw new Error("Could not connect to cache");
@@ -40,11 +47,11 @@ export const scheduledThrottling = async (formId: string, weeks: number) => {
 
 // Permanently increases the throttling rate (uses high capacity token bucket)
 export const permanentThrottling = async (formId: string) => {
-  const modifyParameter = `rate-limit:${formId}`;
+  const modifyParameter = `${REDIS_RATE_LIMIT_KEY_PREFIX}:${formId}`;
   try {
     const redis = await getRedisInstance();
     logMessage.info(`Permanent throttling for ${formId}`);
-    await redis.set(modifyParameter, "high");
+    await redis.set(modifyParameter, THROTTLE_SETTING.high);
   } catch (e) {
     logMessage.error(e as Error);
     throw new Error("Could not connect to cache");
@@ -53,7 +60,7 @@ export const permanentThrottling = async (formId: string) => {
 
 // Goes back to the initial throttling rate (uses low capacity token bucket)
 export const resetThrottling = async (formId: string) => {
-  const modifyParameter = `rate-limit:${formId}`;
+  const modifyParameter = `${REDIS_RATE_LIMIT_KEY_PREFIX}:${formId}`;
   try {
     const redis = await getRedisInstance();
     logMessage.info(`Removing throttling for ${formId}`);
