fa8b4b491144e1b955f8119d5b96ab34371345a0
Tim Arney <tim@line37.com>
2024-11-21 08:38:21 -0500

    response okay

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/SystemStatus/ResponsesOkay.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[statusFilter]/components/SystemStatus/ResponsesOkay.tsx
@@ -1,3 +1,11 @@
+import { useTranslation } from "react-i18next";
+import { HealthCheckBox } from "@clientComponents/globals/HealthCheckBox/HealthCheckBox";
+
 export const ResponsesOkay = () => {
-  return <div>Responses Okay</div>;
+  const { t } = useTranslation("form-builder-responses");
+  return (
+    <HealthCheckBox.Success titleKey="systemHealth.okay.title">
+      {t("systemHealth.okay.description")}
+    </HealthCheckBox.Success>
+  );
 };
