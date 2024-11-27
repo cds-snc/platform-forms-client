133f942b96c5028f1fa0ab7de10cb8bb44311ec8
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-11-27 09:51:38 -0500

    Update to make the form group focussable

--- components/clientComponents/forms/FormGroup/FormGroup.tsx
+++ components/clientComponents/forms/FormGroup/FormGroup.tsx
@@ -22,6 +22,9 @@ export const FormGroup = (props: FormGroupProps): React.ReactElement => {
       data-testid="formGroup"
       className={classes}
       aria-describedby={ariaDescribedBy}
+      // Used to programmatically focus a form group by e.g. a form validation skip ahead link
+      // -1 is used over 0, so the group is not in the natural tab order which is confusing for AT
+      tabIndex={-1}
     >
       {children}
     </fieldset>
