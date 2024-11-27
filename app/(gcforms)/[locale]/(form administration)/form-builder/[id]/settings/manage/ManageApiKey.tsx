d323f4fa17103d561f1639a92f367935646bea40
Tim Arney <tim@line37.com>
2024-11-27 09:35:37 -0500

    add icon

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ManageApiKey.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ManageApiKey.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { useTranslation } from "@i18n/client";
 import { ApiKeyButton } from "../components/ApiKeyButton";
+import { SettingsApplicationsIcon } from "@serverComponents/icons";
 export const ManageApiKey = () => {
   const { t } = useTranslation("form-builder");
   return (
     <div className="mb-10">
-      <h2>{t("switchToApiMode.title")}</h2>
+      <h2>
+        <SettingsApplicationsIcon className="-mt-10px mr-1 inline-block" />{" "}
+        {t("switchToApiMode.title")}{" "}
+      </h2>
       <p className="mb-4">{t("switchToApiMode.description")}</p>
       <ApiKeyButton
         theme="secondary"
