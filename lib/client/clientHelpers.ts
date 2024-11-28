30921c0d281eadca254243e72efea69512954a8a
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-11-28 10:57:34 -0500

    Updates comments

--- lib/client/clientHelpers.ts
+++ lib/client/clientHelpers.ts
@@ -21,21 +21,24 @@ export const scrollToBottom = (containerEl: HTMLElement) => {
 };
 
 /**
- * Tries to focus the first input or if that fails a fallback selector. This is part of focus
- * management to set page load entry point using a focus for an AT user. A form control
- * is preferred if one exists to avoid confusion. e.g. #4690
+ * Tries to focus the first form control or if that fails a fallback selector. This is part of focus
+ * management to set a page load entry point using a DOM focus for an AT user. A form control
+ * is preferred over a fallback selector like a heading, if one exists to avoid confusion e.g. #4690
+ *
  * Note: the setTimeout is a hack to help sequence the query selector after the DOM loads. Use a
  * react ref over this method if possible.
  */
 export const tryFocusOnPageLoad = (fallbackSelector = "H1") => {
-  const NEXT_TICK = 0;
+  const NEXT_TICK = 4;
   setTimeout(() => {
     let focusEl = null;
 
     // Note: checkVisibility() is still fairly new so should check if available, if not, use the
-    // fallback instead.
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/checkVisibility
+    // fallback instead. Because controls may or may not be nested in a parent element that is used
+    // for controlling visibility, determining an elements visibility using the computed style etc.
+    // is tricky and error prone. The WebAPI is probably the best reliable way.
     // https://chromestatus.com/feature/5163102852087808
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/checkVisibility#browser_compatibility
     if (window.document.documentElement.checkVisibility !== undefined) {
       // Get the list of form controls in order, if any and take the first visible one
       // Note: the CSS selector could be more efficient but e.g. not all form controls are wrapped
