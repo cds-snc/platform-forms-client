b5cc226c8b2d45576da42b80f569c510c390f26f
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-11-28 11:00:54 -0500

    Updats comments

--- lib/client/clientHelpers.ts
+++ lib/client/clientHelpers.ts
@@ -22,8 +22,9 @@ export const scrollToBottom = (containerEl: HTMLElement) => {
 
 /**
  * Tries to focus the first form control or if that fails a fallback selector. This is part of focus
- * management to set a page load entry point using a DOM focus for an AT user. A form control
- * is preferred over a fallback selector like a heading, if one exists to avoid confusion e.g. #4690
+ * management to set a page load entry point using a DOM focus for AT users. If a form control
+ * exists it is preferred over a fallback selector like a heading, this is to avoid confusion
+ * e.g. #4690
  *
  * Note: the setTimeout is a hack to help sequence the query selector after the DOM loads. Use a
  * react ref over this method if possible.
@@ -33,7 +34,7 @@ export const tryFocusOnPageLoad = (fallbackSelector = "H1") => {
   setTimeout(() => {
     let focusEl = null;
 
-    // Note: checkVisibility() is still fairly new so should check if available, if not, use the
+    // Note: checkVisibility() is still fairly new so we should check if available, if not, use the
     // fallback instead. Because controls may or may not be nested in a parent element that is used
     // for controlling visibility, determining an elements visibility using the computed style etc.
     // is tricky and error prone. The WebAPI is probably the best reliable way.
@@ -41,7 +42,7 @@ export const tryFocusOnPageLoad = (fallbackSelector = "H1") => {
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/checkVisibility#browser_compatibility
     if (window.document.documentElement.checkVisibility !== undefined) {
       // Get the list of form controls in order, if any and take the first visible one
-      // Note: the CSS selector could be more efficient but e.g. not all form controls are wrapped
+      // Note: the CSS selector could be more efficient but e.g. not all form controls are nested
       // in a form element.
       const formControlEls = document.querySelectorAll(
         "input, textarea, select"
