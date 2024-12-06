97e87bcac89e3d42612a2993e47a67be5a28841d
Tim Arney <tim@line37.com>
2024-12-06 12:03:06 -0500

    update types

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/transformFormResponses.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/transformFormResponses.ts
@@ -4,7 +4,30 @@ import { safeJSONParse } from "@lib/utils";
 
 const IGNORED_KEYS = ["formID", "securityAttribute"];
 
-type TransformedResponse = { [key: string]: any }; // @TODO: fix this type
+type ResponseValue = string | string[] | DynamicRow[];
+
+interface DynamicRow {
+  [key: string]: ResponseValue;
+}
+
+interface TransformedResponse {
+  [key: string | number]: ResponseValue;
+}
+
+/*
+Examples of transformed responses:
+Text field
+{ '1': 'my text here!!' }
+
+Text with radio
+{ '1': 'my text', '2': 'b' }
+
+My text with radio + checkbox
+{ '1': 'my text', '2': 'a', '3': [ 'a', 'b' ] }
+
+My text with radio + checkbox + dynamic row
+{"1":"my text","2":"a","3":["a"],"4":[{"0":"repeated 1","1":["a"]},{"0":"repeated 2","1":["b","c"]}]}
+*/
 
 /**
  * Cleans up and formats the Response object for storage
@@ -29,7 +52,7 @@ export function transformFormResponses(payload: Submission): TransformedResponse
       if (_isCheckbox(value as string)) {
         transformed[key] = _transformCheckboxResponse(value);
       } else {
-        transformed[key] = value;
+        transformed[key] = value as string;
       }
     }
   }
