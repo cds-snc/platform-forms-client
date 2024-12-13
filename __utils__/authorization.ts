58d717d720dc70fc82b64f6fead87aa8f75199a6
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-12-13 09:09:06 -0500

    add mock for getAbility

--- __utils__/authorization.ts
+++ __utils__/authorization.ts
@@ -1,20 +1,27 @@
-import { AccessControlError, authorization } from "@lib/privileges";
-jest.mock("@lib/privileges");
+import { AccessControlError, authorization, getAbility } from "@lib/privileges";
+import { UserAbility } from "@lib/types";
 
 type MockedAuthFunction = {
   [key: string]: jest.Mock;
 };
 
-export const authorizationPass = () => {
+export const authorizationPass = (userID: string) => {
   const mockedAuth: MockedAuthFunction = jest.mocked(authorization);
   for (const property in authorization) {
-    mockedAuth[property] = jest.fn().mockReturnValue(Promise.resolve());
+    mockedAuth[property] = jest.fn().mockReturnValue(Promise.resolve({ user: { id: userID } }));
   }
 };
 
-export const authorizationFail = () => {
+export const authorizationFail = (userID: string) => {
   const mockedAuth: MockedAuthFunction = jest.mocked(authorization);
   for (const property in authorization) {
-    mockedAuth[property] = jest.fn().mockRejectedValue(new AccessControlError("userId"));
+    mockedAuth[property] = jest.fn().mockRejectedValue(new AccessControlError(userID));
   }
+};
+
+export const getAbilityMock = (userID: string) => {
+  const mockedAbility = jest.mocked(getAbility);
+  mockedAbility.mockResolvedValue({
+    userID,
+  } as UserAbility);
 };
