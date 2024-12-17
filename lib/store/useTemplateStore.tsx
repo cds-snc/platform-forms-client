4df7caccdd5834ff278b2dd2be497242d86dfc89
Tim Arney <tim@line37.com>
2024-12-17 06:53:27 -0500

    add init helpers

--- lib/store/useTemplateStore.tsx
+++ lib/store/useTemplateStore.tsx
@@ -21,7 +21,6 @@ import { TemplateStoreProps, TemplateStoreState, InitialTemplateStoreProps } fro
 import { getPathString } from "../utils/form-builder/getPath";
 import { TreeRefProvider } from "@formBuilder/components/shared/right-panel/treeview/provider/TreeRefProvider";
 import { FlowRefProvider } from "@formBuilder/[id]/edit/logic/components/flow/provider/FlowRefProvider";
-import { initializeGroups } from "@formBuilder/components/shared/right-panel/treeview/util/initializeGroups";
 import {
   removeElementById,
   removeById,
@@ -31,10 +30,8 @@ import {
 } from "../utils/form-builder";
 import { decrementChoiceIds, decrementNextActionChoiceIds } from "@lib/formContext";
 import { Language } from "../types/form-builder-types";
-import { defaultForm } from "./defaults";
 import { storageOptions } from "./storage";
 import { clearTemplateStorage } from "./utils";
-import { orderGroups } from "@lib/utils/form-builder/orderUsingGroupsLayout";
 import { initStore } from "./initStore";
 
 import {
@@ -46,6 +43,7 @@ import {
   duplicateElement,
 } from "./helpers/add";
 import { moveUp, moveDown, subMoveUp, subMoveDown } from "./helpers/move";
+import { initialize, importTemplate } from "./helpers/init";
 
 const createTemplateStore = (initProps?: Partial<InitialTemplateStoreProps>) => {
   const props = initStore(initProps);
@@ -264,58 +262,8 @@ const createTemplateStore = (initProps?: Partial<InitialTemplateStoreProps>) => 
                 state.closingDate = value;
               });
             },
-            initialize: async (language = "en") => {
-              set((state) => {
-                const allowGroups = state.allowGroupsFlag;
-                state.id = "";
-                state.lang = language as Language;
-                state.translationLanguagePriority = language as Language;
-                state.form = initializeGroups({ ...defaultForm }, allowGroups);
-
-                // Ensure order by groups layout
-                if (!state.form.groupsLayout) {
-                  /* No need to order as the groups layout does not exist */
-                  state.form.groupsLayout = [];
-                } else {
-                  state.form.groups = orderGroups(state.form.groups, state.form.groupsLayout);
-                }
-
-                state.isPublished = false;
-                state.name = "";
-                state.deliveryOption = undefined;
-                state.formPurpose = "";
-                state.publishReason = "";
-                state.publishFormType = "";
-                state.publishDesc = "";
-                state.closingDate = null;
-              });
-            },
-            importTemplate: async (jsonConfig) => {
-              set((state) => {
-                const allowGroups = state.allowGroupsFlag;
-                state.id = "";
-                state.lang = "en";
-                state.form = initializeGroups({ ...defaultForm, ...jsonConfig }, allowGroups);
-
-                // Ensure order by groups layout
-                if (!state.form.groupsLayout) {
-                  /* No need to order as the groups layout does not exist */
-                  state.form.groupsLayout = [];
-                } else {
-                  state.form.groups = orderGroups(state.form.groups, state.form.groupsLayout);
-                }
-
-                state.isPublished = false;
-                state.name = "";
-                state.securityAttribute = "Protected A";
-                state.deliveryOption = undefined;
-                state.formPurpose = "";
-                state.publishReason = "";
-                state.publishFormType = "";
-                state.publishDesc = "";
-                state.closingDate = null;
-              });
-            },
+            initialize: initialize(set),
+            importTemplate: importTemplate(set),
             getGroupsEnabled: () => get().allowGroupsFlag,
             setGroupsLayout: (layout) => {
               set((state) => {
