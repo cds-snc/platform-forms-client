7645849ed37a1116aed0c8a4068db8eb11492a73
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-12-19 14:55:15 -0500

    Bit of refactoring

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/transformFormResponses.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/transformFormResponses.ts
@@ -4,14 +4,95 @@ import { safeJSONParse } from "@lib/utils";
 
 const IGNORED_KEYS = ["formID", "securityAttribute"];
 
-type ResponseValue = string | string[] | DynamicRow[];
-
-interface DynamicRow {
-  [key: string]: ResponseValue;
-}
+type DynamicRowResponse = TransformedResponse[];
 
 interface TransformedResponse {
-  [key: string | number]: ResponseValue;
+  [key: string]: Response | DynamicRowResponse;
+}
+
+/**
+ * A dynamicRow response key is in the format "parentKey-subKey-subSubKey"
+ *
+ * @param key string
+ * @returns boolean
+ */
+const _isDynamicRow = (key: string) => key.includes("-");
+
+/**
+ * A checkbox response is in the format '{"value":[]}'
+ *
+ * @param value string
+ * @returns boolean
+ */
+const _isCheckbox = (value: string) => value.startsWith('{"value"');
+
+/**
+ * Cleans up a checkbox response
+ * @param response i.e. {"value":["b"]}
+ * @returns
+ */
+const _transformCheckboxResponse = (response: string): string[] => {
+  return safeJSONParse<{ value: string[] }>(response)?.value || [];
+};
+
+/**
+ * Cleans up and formats the Response object for storage
+ *
+ * @param payload Submission
+ * @returns TransformedResponse
+ */
+export function transformFormResponses(payload: Submission): TransformedResponse {
+  const { responses } = payload;
+  let transformed: TransformedResponse = {};
+
+  for (const [key, value] of Object.entries(responses)) {
+    if (IGNORED_KEYS.includes(key)) {
+      continue;
+    }
+
+    // DynamicRow
+    if (_isDynamicRow(key)) {
+      transformed = _handleDynamicRow(key, value, transformed);
+    } else {
+      // Checkboxes need a bit of massaging
+      if (_isCheckbox(value as string)) {
+        transformed[key] = _transformCheckboxResponse(value as string);
+      } else {
+        transformed[key] = value as string;
+      }
+    }
+  }
+
+  return transformed;
+}
+
+/**
+ * Add structure to Dynamic Row responses
+ *
+ * @param key string - i.e. 5-0-0 or 5-0-1
+ * @param value Response
+ * @param response TransformedResponse
+ * @returns TransformedResponse
+ */
+function _handleDynamicRow(key: string, value: Response, response: TransformedResponse) {
+  const [parentKey, subKey, subSubKey] = key.split("-");
+
+  // subKey is the index of the dynamic row array so must be an integer
+  const subKeyInt = parseInt(subKey, 10);
+
+  // a dynamic row is an array of TransformedResponses
+  response[parentKey] = (response[parentKey] as DynamicRowResponse) || [];
+
+  response[parentKey][subKeyInt] = response[parentKey][subKeyInt] || {};
+
+  if (_isCheckbox(value as string)) {
+    response[parentKey][subKeyInt][subSubKey] = _transformCheckboxResponse(value as string);
+    return response;
+  }
+
+  response[parentKey][subKeyInt][subSubKey] = value as string | string[];
+
+  return response;
 }
 
 /*
@@ -26,70 +107,19 @@ My text with radio + checkbox
 { '1': 'my text', '2': 'a', '3': [ 'a', 'b' ] }
 
 My text with radio + checkbox + dynamic row
-{"1":"my text","2":"a","3":["a"],"4":[{"0":"repeated 1","1":["a"]},{"0":"repeated 2","1":["b","c"]}]}
+{
+  "1":"my text",
+  "2":"a",
+  "3":["a"],
+  "4": [
+    {
+      "0":"repeated 1",
+      "1":["a"]
+    },
+    {
+      "0":"repeated 2",
+      "1":["b","c"]
+    }
+  ]
+}
 */
-
-/**
- * Cleans up and formats the Response object for storage
- *
- * @param payload
- * @returns
- */
-export function transformFormResponses(payload: Submission): TransformedResponse {
-  const { responses } = payload;
-  const transformed: TransformedResponse = {};
-
-  for (const [key, value] of Object.entries(responses)) {
-    if (IGNORED_KEYS.includes(key)) {
-      continue;
-    }
-
-    // DynamicRow
-    if (_isDynamicRow(key)) {
-      _transformDynamicRow(key, value, transformed);
-    } else {
-      // Checkboxes need a bit of massaging
-      if (_isCheckbox(value as string)) {
-        transformed[key] = _transformCheckboxResponse(value as string);
-      } else {
-        transformed[key] = value as string;
-      }
-    }
-  }
-
-  return transformed;
-}
-
-const _isDynamicRow = (key: string) => key.includes("-");
-
-const _isCheckbox = (key: string) => key.startsWith('{"value"');
-
-/**
- * Cleans up a checkbox response
- * @param response i.e. {"value":["b"]}
- * @returns
- */
-const _transformCheckboxResponse = (response: string): string[] => {
-  return safeJSONParse<{ value: string[] }>(response)?.value || [];
-};
-
-/**
- * Add structure to Dynamic Row responses
- *
- * @param key i.e. 5-0-0 or 5-0-1
- * @param value
- * @param transformed
- */
-function _transformDynamicRow(key: string, value: Response, transformed: TransformedResponse) {
-  const [parentKey, subKey, subSubKey] = key.split("-");
-  transformed[parentKey] = transformed[parentKey] || [];
-
-  // @ts-expect-error - intentionally skipping type check
-  transformed[parentKey][subKey] = transformed[parentKey][subKey] || {};
-
-  // transformed[5][0] = { 0: 'repeated 1', 1: ['a'] }
-  // @ts-expect-error - intentionally skipping type check
-  transformed[parentKey][subKey][subSubKey] = _isCheckbox(value as string)
-    ? _transformCheckboxResponse(value as string)
-    : (value as string | string[]);
-}
