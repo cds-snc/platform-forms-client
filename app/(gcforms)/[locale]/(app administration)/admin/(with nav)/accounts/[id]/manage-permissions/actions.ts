c8e2e0fdb078ee10b6246eb0b28ae43e67d5dd6d
Clément Janin <clement.janin@cds-snc.ca>
2024-12-19 12:38:14 -0500

    remove privilege checks duplcation

--- app/(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-permissions/actions.ts
+++ app/(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-permissions/actions.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { updatePrivilegesForUser } from "@lib/privileges";
-import { checkPrivilegesAsBoolean } from "@lib/privileges";
 import { revalidatePath } from "next/cache";
 import { authCheckAndThrow } from "@lib/actions";
 
@@ -13,16 +12,15 @@ export const updatePrivileges = async (
   action: "add" | "remove"
 ) => {
   const { ability } = await authCheckAndThrow();
-  if (checkPrivilegesAsBoolean(ability, [{ action: "update", subject: "User" }])) {
-    try {
-      const result = await updatePrivilegesForUser(ability, userID, [{ id: privilegeID, action }]);
-      revalidatePath(
-        "(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-permissions",
-        "page"
-      );
-      return { data: result };
-    } catch (e) {
-      return { error: "Failed to update permissions." };
-    }
+
+  try {
+    const result = await updatePrivilegesForUser(ability, userID, [{ id: privilegeID, action }]);
+    revalidatePath(
+      "(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-permissions",
+      "page"
+    );
+    return { data: result };
+  } catch (e) {
+    return { error: "Failed to update permissions." };
   }
 };
