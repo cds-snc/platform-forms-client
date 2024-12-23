2f167870d27073457049ee041056d08bbe8afaf2
Peter Thiessen <peter.thiessen@cds-snc.ca>
2024-12-23 14:02:38 -0500

    Updates DynamicRows to use custom title

--- components/clientComponents/forms/Review/FormElements/DyanmicRow/DynamicRow.tsx
+++ components/clientComponents/forms/Review/FormElements/DyanmicRow/DynamicRow.tsx
@@ -5,7 +5,6 @@ import { FormItemFactory } from "../../FormItemFactory";
 import { Language } from "@lib/types/form-builder-types";
 import { randomId } from "@lib/client/clientHelpers";
 import { getReviewSectionFromDynamicRows } from "./helpers";
-import { useTranslation } from "@i18n/client";
 
 const getDynamicRowElements = (formItem: FormItem, language: Language) => {
   if (!Array.isArray(formItem.values)) {
@@ -18,7 +17,6 @@ const getDynamicRowElements = (formItem: FormItem, language: Language) => {
 
 // Note: DynamicRow = Sub Elements = Repeating Sets = {more names ?}
 export const DynamicRow = ({ formItem, language }: { formItem: FormItem; language: Language }) => {
-  const { t } = useTranslation(["review"]);
   const { getValues } = useGCFormsContext();
 
   const formValues: FormValues | void = getValues();
@@ -34,7 +32,7 @@ export const DynamicRow = ({ formItem, language }: { formItem: FormItem; languag
         const dynamicRowElements = getDynamicRowElements(formItem as FormItem, language);
         return (
           <dl className="mt-12 mb-10" key={`${(formItem as FormItem).element?.id}-${index}`}>
-            <h5 className="mb-8">{t("instance", { index: index + 1 })}</h5>
+            <h5 className="mb-8">{(formItem as FormItem).label}</h5>
             {dynamicRowElements}
           </dl>
         );
--- components/clientComponents/forms/Review/FormElements/DyanmicRow/helpers.ts
+++ components/clientComponents/forms/Review/FormElements/DyanmicRow/helpers.ts
@@ -16,12 +16,14 @@ export const getReviewSectionFromDynamicRows = (
 
   const parentId = element.id;
   const parentTitle = element.properties?.[getLocalizedProperty("title", language)];
+  const childTitle =
+    element.properties?.dynamicRow?.[
+      getLocalizedProperty("rowTitle", language) as keyof typeof getLocalizedProperty
+    ];
   const subElements = element.properties?.subElements;
 
   // Get the children elements of the Dynamic Row
-  const subFormItems = (formValues[parentId] as string[]).map((valueRows, valueRowsIndex) => {
-    const parentDynamicRowLabel = `${parentTitle} - ${valueRowsIndex + 1}`;
-
+  const subFormItems = (formValues[parentId] as string[]).map((valueRows, parentRowIndex) => {
     // Use FormValues as the source of truth and for each FormValue value, map the related
     // subElement title to the FormValue value
     const valueRowsAsArray = Object.keys(valueRows).map(
@@ -29,8 +31,8 @@ export const getReviewSectionFromDynamicRows = (
     );
 
     // Create the children Form Items (Dynamic Rows)
-    const childFormItems = valueRowsAsArray.map((formValue, valueRowIndex) => {
-      const subElement = subElements?.[valueRowIndex];
+    const childFormItems = valueRowsAsArray.map((formValue, childRowIndex) => {
+      const subElement = subElements?.[childRowIndex];
       // Match the FormValue index to the subElement index to assign the label
       const label = subElement?.properties?.[getLocalizedProperty("title", language)];
       return {
@@ -44,7 +46,7 @@ export const getReviewSectionFromDynamicRows = (
     // Create the Parent Form Item (Dynamic Row)
     return {
       type: element.type,
-      label: parentDynamicRowLabel,
+      label: `${childTitle} - ${parentRowIndex + 1}`,
       values: childFormItems,
       element,
     } as FormItem;
