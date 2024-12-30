a6762e9fed257d1c278cea0c3b3c4d061abd8639
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2024-12-30 16:51:01 -0500

    Add TaggedResponse type

--- lib/types/form-response-types.ts
+++ lib/types/form-response-types.ts
@@ -9,10 +9,16 @@ export type Response =
   | Record<string, unknown>[]
   | FileInputResponse
   | FileInputResponse[]
-  | Record<string, unknown>;
+  | Record<string, unknown>
+  | TaggedResponse;
 
 export type FileInputResponse = {
   name: string | null;
   size: number | null;
   based64EncodedFile: string | null;
+};
+
+export type TaggedResponse = {
+  tag: string;
+  answer: Response;
 };
