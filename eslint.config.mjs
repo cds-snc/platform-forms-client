509813bec20c1775fb78a73573fbfcff55ed8226
Tim Arney <tim@line37.com>
2025-01-03 09:06:25 -0500

    config

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+});
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next', 'prettier'],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+    ignorePatterns: ["/utils", "/public/static/scripts/", "/__tests__/api/"],
+  }),
+];
+export default eslintConfig;
