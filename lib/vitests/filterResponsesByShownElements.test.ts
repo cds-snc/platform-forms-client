7014f892414c0e805bba307be869857b144fcae5
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-01-09 12:05:33 -0500

    Updates related  unit test

--- lib/vitests/filterResponsesByShownElements.test.ts
+++ lib/vitests/filterResponsesByShownElements.test.ts
@@ -1,49 +1,49 @@
 import { expect } from 'vitest'
-import { filterValuesByShownElements } from "@lib/formContext";
+import { filterResponsesByShownElements } from "@lib/formContext";
 import { FormElement } from "@lib/types";
 
 // Fixtures captured by adding a break point in Forms.tsx and copying the values from the debugger
-import {inputHistoryValues, shownElements} from "../../__fixtures__/filterValuesByShownElements.json";
+import {inputHistoryValues, shownElements} from "../../__fixtures__/filterResponsesByShownElements.json";
 
-describe("formContext filterValuesByShownElements()", () => {
+describe("formContext filterResponsesByShownElements()", () => {
   it("Handles filtering out correct element", () => {
     const expectedOutput = {
       "1": "A",
       "2": "A",
       "3": "P1-Q1-A",
-      "4": "",
+      // "4": "",
       "5": "",
       "6": "",
-      "7": "",
+      // "7": "",
       "8": "",
       "9": "",
-      "10": "",
-      "currentGroup": "",
-      "groupHistory": "",
-      "matchedIds": ""
+      // "10": "",
+      // "currentGroup": "",
+      // "groupHistory": "",
+      // "matchedIds": ""
     };
-    const result = filterValuesByShownElements(inputHistoryValues, shownElements as FormElement[]);
+    const result = filterResponsesByShownElements(inputHistoryValues, shownElements as FormElement[]);
     expect(result).toEqual(expectedOutput);
   });
 
   it("Handles bad input", () => {
     const expectedOutput = undefined;
     // @ts-expect-error - testing invalid input
-    const result = filterValuesByShownElements();
+    const result = filterResponsesByShownElements();
     expect(result).toEqual(expectedOutput);
   });
 
   it("Handles partial input 1", () => {
     const expectedOutput = inputHistoryValues;
     // @ts-expect-error - testing invalid input
-    const result = filterValuesByShownElements(inputHistoryValues, undefined);
+    const result = filterResponsesByShownElements(inputHistoryValues, undefined);
     expect(result).toEqual(expectedOutput);
   });
 
   it("Handles partial input 2", () => {
     const expectedOutput = undefined;
     // @ts-expect-error - testing invalid input
-    const result = filterValuesByShownElements(undefined, shownElements as FormElement[]);
+    const result = filterResponsesByShownElements(undefined, shownElements as FormElement[]);
     expect(result).toEqual(expectedOutput);
   });
 });
