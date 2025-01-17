fa7316dc09b2883846ba302dffe20ffc9b513b39
Tim Arney <timarney@users.noreply.github.com>
2025-01-17 09:44:36 -0500

    chore: remove log message  (#4996)
    remove validate

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/actions.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/actions.ts
@@ -7,7 +7,7 @@ import { processFormData } from "./lib/processFormData";
 import { MissingFormDataError } from "./lib/exceptions";
 import { logMessage } from "@lib/logger";
 import { getPublicTemplateByID } from "@lib/templates";
-import { validateResponses } from "@lib/validation/validation";
+// import { validateResponses } from "@lib/validation/validation";
 
 // Public facing functions - they can be used by anyone who finds the associated server action identifer
 
@@ -25,18 +25,21 @@ export async function submitForm(
       throw new Error(`Could not find any form associated to identifier ${formId}`);
     }
 
-    const validateResponsesResult = await validateResponses(values, template);
+    // const validateResponsesResult = await validateResponses(values, template);
 
-    if (Object.keys(validateResponsesResult).length !== 0) {
+    // if (Object.keys(validateResponsesResult).length !== 0) {
+
+    /*
       logMessage.warn(
         `[server-action][submitForm] Detected invalid response(s) in submission on form ${formId}. Errors: ${JSON.stringify(
           validateResponsesResult
         )}`
       );
+      */
 
-      // Turn this on after we've monitored the logs for a while
-      // throw new MissingFormDataError("Form data validation failed");
-    }
+    // Turn this on after we've monitored the logs for a while
+    // throw new MissingFormDataError("Form data validation failed");
+    //}
 
     const formDataObject = buildFormDataObject(template, values);
 
