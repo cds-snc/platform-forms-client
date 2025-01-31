a9bfc573da5bd741ce65ab6969fd0e96e3df75f8
Tim Arney <tim@line37.com>
2025-01-31 09:19:55 -0500

    add optional date modified

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/page.tsx
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/page.tsx
@@ -83,7 +83,7 @@ export default async function Page(props0: {
 
   if (step === "resume") {
     return (
-      <FormDisplayLayout formRecord={formRecord}>
+      <FormDisplayLayout formRecord={formRecord} dateModified={false}>
         <ResumeForm formId={formID} />
       </FormDisplayLayout>
     );
