7219bf4a9c81627755876c10c2e95850bd88bf04
Tim Arney <tim@line37.com>
2025-02-06 09:51:44 -0500

    update

--- components/clientComponents/forms/SaveAndResume/actions.ts
+++ components/clientComponents/forms/SaveAndResume/actions.ts
@@ -2,10 +2,10 @@
 
 import { DownloadProgressHtml, type HTMLProps } from "@lib/saveAndResume/DownloadProgressHtml";
 
-const renderToStaticMarkup = (await import("react-dom/server")).renderToStaticMarkup;
-
 export const generateDownloadProgressHtml = async (props: HTMLProps) => {
   try {
+    const renderToStaticMarkup = (await import("react-dom/server")).renderToStaticMarkup;
+
     return { data: renderToStaticMarkup(DownloadProgressHtml(props)) };
   } catch (error) {
     return { data: "", error: (error as Error).message };
