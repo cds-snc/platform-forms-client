f1e3360aca16ae4d1b74cdb4979d6f614fa221d7
Tim Arney <tim@line37.com>
2025-02-06 09:05:27 -0500

    update html

--- components/clientComponents/forms/SaveAndResume/html/NextSteps.tsx
+++ components/clientComponents/forms/SaveAndResume/html/NextSteps.tsx
@@ -9,9 +9,34 @@ export const NextSteps = ({
   language: Language;
 }) => {
   return (
-    <div>
-      {securityAttribute}
-      {language}
-    </div>
+    <>
+      <div className="hidden">
+        {securityAttribute}
+        {language}
+      </div>
+      <div className="rounded-lg bg-gcds-green-100 p-4">
+        <h2 className="!mb-4 !text-2xl font-bold underline">What to do next - </h2>
+        <div className="grid grid-cols-2 gap-28">
+          <div>
+            <div className="mb-4 rounded-lg border-2 border-slate-400 p-4">
+              <h3 className="!mb-2 !text-2xl font-bold">Resume completing your form</h3>
+              <p>Upload this file to resume completing your form</p>
+            </div>
+
+            <div className="rounded-lg border-2 border-slate-400 p-4">
+              <h3 className="!mb-2 !text-2xl font-bold">Save a copy for the future</h3>
+              <p>Keep a receipt of your answers to review in the future</p>
+            </div>
+          </div>
+
+          <div>
+            <h3 className="!my-6 !text-2xl font-bold">Keep your information safe</h3>
+            <p className="italic max-w-56">
+              Make sure to keep this file in a secure spot on your device
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
