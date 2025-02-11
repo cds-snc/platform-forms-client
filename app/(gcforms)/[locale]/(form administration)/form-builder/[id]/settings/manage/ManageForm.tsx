21af9487178c1c966ccca783eedb135324fc4f82
Tim Arney <tim@line37.com>
2025-02-11 10:22:11 -0500

    update manage

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ManageForm.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/ManageForm.tsx
@@ -9,6 +9,7 @@ import { updateTemplateUsers } from "@formBuilder/actions";
 import { ManageApiKey } from "./ManageApiKey";
 import { ThrottlingRate } from "./ThrottlingRate";
 import { useFormBuilderConfig } from "@lib/hooks/useFormBuilderConfig";
+import { SaveAndResumeToggle } from "./saveAndResume/SaveAndResumeToggle";
 
 interface User {
   id: string;
@@ -57,6 +58,7 @@ export const ManageForm = (props: ManageFormProps) => {
   return (
     <>
       {canSetClosingDate && <SetClosingDate formId={id} closedDetails={closedDetails} />}
+      <SaveAndResumeToggle />
       <FormOwnership
         nonce={nonce}
         formRecord={formRecord}
---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/saveAndResume/SaveAndResumeToggle.tsx
@@ -0,0 +1,3 @@
+export const SaveAndResumeToggle = () => {
+  return <div>SaveAndResume</div>;
+};
