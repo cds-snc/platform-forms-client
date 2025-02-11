7a1b5652a47bfddb0851e1a1a2f0f6a3cfe02277
Tim Arney <tim@line37.com>
2025-02-11 07:55:18 -0500

    add more error handling

--- components/clientComponents/forms/SaveAndResume/actions.ts
+++ components/clientComponents/forms/SaveAndResume/actions.ts
@@ -5,16 +5,27 @@ import { logMessage } from "@lib/logger";
 
 import { getOrigin } from "@lib/origin";
 
+import { getPublicTemplateByID } from "@lib/templates";
+
 export const generateDownloadProgressHtml = async (props: HTMLProps) => {
   try {
+    const formId = props.formId;
+
+    if (!formId) {
+      throw new Error("formId is required to generate download progress html");
+    }
+
+    // Ensure the form exists
+    if (!(await getPublicTemplateByID(formId))) {
+      throw new Error(`Could not find any form associated to identifier ${formId}`);
+    }
+
     const renderToStaticMarkup = (await import("react-dom/server")).renderToStaticMarkup;
     const host = await getOrigin();
     const html = DownloadProgressHtml({ ...props, host });
     return { data: renderToStaticMarkup(html) };
   } catch (error) {
     if (error instanceof Error) {
-      logMessage.warn(error);
-
       logMessage.warn(
         `Error generating HTML due to error: ${error.message} -  formID: {${props.formId}}`
       );
