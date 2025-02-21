dfee606d7d7b55fbf477a97ad7b251a5bd1b1cc9
Pete <107579368+thiessenp-cds@users.noreply.github.com>
2025-02-21 11:11:16 -0500

    fix: a11y updates for front facing form(s) (#5148)
    * Updates Review to use parent DLs
    
    * Updates focus management for Group buttons
    
    * Adds focus on close to dialog
    
    ---------
    
    Co-authored-by: Tim Arney <timarney@users.noreply.github.com>

--- components/clientComponents/forms/Review/FormElements/BaseElement.tsx
+++ components/clientComponents/forms/Review/FormElements/BaseElement.tsx
@@ -1,14 +1,18 @@
 import { FormItem } from "../helpers";
 
-export const BaseElement = ({ formItem }: { formItem: FormItem | undefined }): React.ReactElement => {
+export const BaseElement = ({
+  formItem,
+}: {
+  formItem: FormItem | undefined;
+}): React.ReactElement => {
   if (!formItem) {
     return <></>;
   }
-  
+
   return (
-    <div className="mb-8">
+    <dl className="mb-8">
       <dt className="mb-2 font-bold">{formItem.label}</dt>
       <dd>{formItem.values ? String(formItem.values) : "-"}</dd>
-    </div>
+    </dl>
   );
 };
