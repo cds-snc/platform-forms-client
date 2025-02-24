b06aba2a43bc64f729d8b85a1d7b5666d0c9903e
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-02-24 15:32:01 -0500

    Updates back button to handle case of start page with no question

--- components/clientComponents/forms/BackButtonGroup/BackButtonGroup.tsx
+++ components/clientComponents/forms/BackButtonGroup/BackButtonGroup.tsx
@@ -4,6 +4,7 @@ import { Button } from "@clientComponents/globals";
 import { LockedSections } from "@formBuilder/components/shared/right-panel/treeview/types";
 import { Language } from "@lib/types/form-builder-types";
 import { BackArrowIcon24x24 } from "@serverComponents/icons";
+import { tryFocusOnPageLoad } from "@lib/client/clientHelpers";
 
 export const BackButtonGroup = ({
   language,
@@ -38,6 +39,11 @@ export const BackButtonGroup = ({
           e.preventDefault();
           handlePreviousAction();
           onClick && onClick();
+          // Focus the H1 on the start page as the beginning of the page, and then for any other
+          // sub page, focus the H2.
+          const groupHistory = getGroupHistory();
+          const focusHeadding = groupHistory && groupHistory.length === 1 ? "h1" : "h2";
+          tryFocusOnPageLoad(focusHeadding);
         }}
         type="button"
         className="group mr-4"
