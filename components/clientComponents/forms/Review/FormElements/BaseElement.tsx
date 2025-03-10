c75d5713a6ddbcb0dd08833ea19899ffa2979ba1
Tim Arney <tim@line37.com>
2025-03-10 12:38:42 -0400

    ensure-newline

--- components/clientComponents/forms/Review/FormElements/BaseElement.tsx
+++ components/clientComponents/forms/Review/FormElements/BaseElement.tsx
@@ -12,7 +12,18 @@ export const BaseElement = ({
   return (
     <dl className="mb-8">
       <dt className="mb-2 font-bold">{formItem.label}</dt>
-      <dd>{formItem.values ? String(formItem.values) : "-"}</dd>
+      <dd>
+        {formItem.values
+          ? String(formItem.values)
+              .split("\n")
+              .map((line, i) => (
+                <span key={i}>
+                  {line}
+                  {i < String(formItem.values).split("\n").length - 1 && <br />}
+                </span>
+              ))
+          : "-"}
+      </dd>
     </dl>
   );
 };
