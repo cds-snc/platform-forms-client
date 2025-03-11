9af67a96ff8d5bc92185a484063769486d1c7d59
Vivian Nobrega <vivian.nobrega@cds-snc.ca>
2025-03-11 13:15:28 -0400

    * swap classes

--- app/(gcforms)/[locale]/(form administration)/forms/components/server/NewFormButton.tsx
+++ app/(gcforms)/[locale]/(form administration)/forms/components/server/NewFormButton.tsx
@@ -8,7 +8,7 @@ export const NewFormButton = async () => {
   } = await serverTranslation("my-forms");
 
   return (
-    <div className="inline mr-6">
+    <div className="mr-6 inline">
       <LinkButton.Primary href={`/${language}/form-builder`}>
         <>
           <span aria-hidden="true" className="mr-2 inline-block">
