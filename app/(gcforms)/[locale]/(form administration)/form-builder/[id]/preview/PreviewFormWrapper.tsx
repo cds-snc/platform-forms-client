1d47247c765b104fc73e71d33d3facd2ec20cb83
Pete <107579368+thiessenp-cds@users.noreply.github.com>
2025-03-18 13:16:13 -0400

    chore: hCAPTCHA updates (#5169)
    * Adds a README
    
    * Separates a null check out and fixes a log typo
    
    * Fixes some typos in the README
    
    * Fixes some types in the README
    
    * Refactors form into the Captcha component
    
    * Fixes a log typo
    
    * Fixes some more typos
    
    * Updates readme
    
    * Removes README
    
    * Refactors form related code to be clearer
    
    * Fixes a typo
    
    * Fixes a typo
    
    * Fixes a typo
    
    * Renames CaptchaForm to FormCaptcha for scannability
    
    * Updates a comment
    
    * Adds comment to try to rebuild
    
    * Removes comment to try to rebuild
    
    * Moves Preview Form into a wrapper

---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PreviewFormWrapper.tsx
@@ -0,0 +1,101 @@
+"use client";
+import { useGCFormsContext } from "@lib/hooks/useGCFormContext";
+import { Form } from "@clientComponents/forms/Form/Form";
+import { NextButton } from "@clientComponents/forms";
+import { tryFocusOnPageLoad } from "@lib/client/clientHelpers";
+import { BackButton } from "./BackButton";
+import { Button } from "@clientComponents/globals";
+import Markdown from "markdown-to-jsx";
+import { showReviewPage } from "@lib/utils/form-builder/showReviewPage";
+import { useTranslation } from "@i18n/client";
+import { useTemplateStore } from "@lib/store/useTemplateStore";
+import { useSession } from "next-auth/react";
+import { FormRecord } from "@gcforms/types";
+import { TypeOmit } from "@lib/types";
+
+// Component created to allow access to the parent GCFormsProvider context
+export const PreviewFormWrapper = ({
+  children,
+  formRecord,
+  disableSubmit,
+  allowGrouping,
+  setSent,
+}: {
+  children: React.JSX.Element[];
+  formRecord: TypeOmit<FormRecord, "name" | "deliveryOption">;
+  allowGrouping: boolean;
+  disableSubmit: boolean;
+  setSent: React.Dispatch<React.SetStateAction<string | null | undefined>>;
+}) => {
+  const { status } = useSession();
+  const { saveSessionProgress, currentGroup } = useGCFormsContext();
+
+  const { translationLanguagePriority, getLocalizationAttribute } = useTemplateStore((s) => ({
+    translationLanguagePriority: s.translationLanguagePriority,
+    getLocalizationAttribute: s.getLocalizationAttribute,
+  }));
+
+  const translationNamespaces = ["common", "form-builder", "form-closed"];
+  const { t } = useTranslation(translationNamespaces);
+  const translatedT = useTranslation(translationNamespaces, { lng: translationLanguagePriority }).t;
+
+  const isShowReviewPage = showReviewPage(formRecord.form);
+
+  return (
+    <Form
+      formRecord={formRecord}
+      saveSessionProgress={saveSessionProgress}
+      isPreview={true}
+      language={translationLanguagePriority}
+      t={translatedT}
+      onSuccess={setSent}
+      renderSubmit={({ validateForm }) => {
+        return (
+          <div id="PreviewSubmitButton">
+            <span {...getLocalizationAttribute()}>
+              <NextButton
+                formRecord={formRecord}
+                language={translationLanguagePriority}
+                validateForm={validateForm}
+                fallBack={() => {
+                  return (
+                    <>
+                      {allowGrouping && isShowReviewPage && (
+                        <BackButton
+                          language={translationLanguagePriority}
+                          onClick={() => tryFocusOnPageLoad("h2")}
+                        />
+                      )}
+                      <Button
+                        type="submit"
+                        id="SubmitButton"
+                        onClick={(e) => {
+                          if (disableSubmit) {
+                            e.preventDefault();
+                          }
+                        }}
+                      >
+                        {t("submitButton", { ns: "common", lng: translationLanguagePriority })}
+                      </Button>
+                    </>
+                  );
+                }}
+              />
+            </span>
+            {status !== "authenticated" && (
+              <div className="inline-block bg-purple-200 px-4 py-1" {...getLocalizationAttribute()}>
+                <Markdown options={{ forceBlock: true }}>
+                  {t("signInToTest", { ns: "form-builder", lng: translationLanguagePriority })}
+                </Markdown>
+              </div>
+            )}
+          </div>
+        );
+      }}
+      allowGrouping={allowGrouping}
+      currentGroup={currentGroup}
+    >
+      {children}
+    </Form>
+  );
+};
