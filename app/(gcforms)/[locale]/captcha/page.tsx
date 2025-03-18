95ff7dd8d9bf4da8eedee5358ed8cf115e8b519f
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-03-18 14:34:23 -0400

    Removes a comment

--- app/(gcforms)/[locale]/captcha/page.tsx
+++ app/(gcforms)/[locale]/captcha/page.tsx
@@ -18,21 +18,3 @@ export async function generateMetadata(props: {
 export default async function Page() {
   return <CaptchaFail />;
 }
-/*
-# Your form could not be submitted
-
-You might try to:
-- Turn off a virtual private network (VPN), firewall, or proxy server, if you use one.
-- Use a different browser, device, or network, if an alternate is available.
-- Submit your form again later or contact the program or service directly.
-*/
-
-/*
-# Votre formulaire n'a pas pu être soumis
-
-Vous pouvez essayer de :
-- Désactiver un réseau privé virtuel (VPN), un pare-feu ou un serveur proxy, si vous en utilisez un.
-- Utiliser un autre navigateur, un autre appareil ou un autre réseau, si une alternative est disponible.
-- Soumettre à nouveau votre formulaire plus tard ou contacter directement le programme ou le service.
-
-*/
