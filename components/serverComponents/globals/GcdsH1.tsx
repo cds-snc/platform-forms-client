2e16f797b34dfff251b3f41fe06a2741cb19aead
Tim Arney <tim@line37.com>
2025-03-19 13:18:51 -0400

    use -1

--- components/serverComponents/globals/GcdsH1.tsx
+++ components/serverComponents/globals/GcdsH1.tsx
@@ -11,7 +11,7 @@ export const GcdsH1 = ({
 }) => {
   return (
     <h1
-      tabIndex={tabIndex || 0}
+      tabIndex={tabIndex || -1}
       className={cn(
         "relative !mb-12 inline-block border-none after:absolute after:-bottom-1 after:left-0 after:h-[var(--gcds-heading-h1-border-height)] after:w-[var(--gcds-heading-h1-border-width)] after:bg-[var(--gcds-heading-h1-border-background)] after:content-['']",
         className
