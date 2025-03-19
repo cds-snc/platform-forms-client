625c406952a887534fa65a4e91a36987f35c3124
Tim Arney <tim@line37.com>
2025-03-19 12:18:18 -0400

    update heading

--- components/clientComponents/globals/FormCaptcha/CaptchaFail.tsx
+++ components/clientComponents/globals/FormCaptcha/CaptchaFail.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useTranslation } from "@i18n/client";
+import { GcdsH1 } from "@serverComponents/globals/GcdsH1";
 
 // Future TODO: add pause-resume logic and support flow.
 
@@ -8,7 +9,7 @@ export const CaptchaFail = () => {
   const { t } = useTranslation("captcha");
   return (
     <>
-      <h1>{t("title")}</h1>
+      <GcdsH1 tabIndex={-1}>{t("title")}</GcdsH1>
       <p>{t("helpOptions.title")}</p>
       <ul>
         <li>{t("helpOptions.item1")}</li>
