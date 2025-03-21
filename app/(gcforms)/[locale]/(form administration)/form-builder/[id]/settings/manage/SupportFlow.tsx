ee6c34ce7bb040b47f475eff2447cfcc3d8fd5e5
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-03-21 13:18:12 -0400

    Fixes some css border issues

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@clientComponents/globals";
-import { useTranslation } from "@i18n/client";
-import { logMessage } from "@lib/logger";
-import { isValidGovEmail } from "@lib/validation/validation";
 import { useState } from "react";
+import { useTranslation } from "@i18n/client";
+import { Button } from "@clientComponents/globals";
+import { isValidGovEmail } from "@lib/validation/validation";
+import { cn } from "@lib/utils";
+import { logMessage } from "@lib/logger";
 
 export const SupportFlow = () => {
   const { t } = useTranslation("form-builder");
@@ -25,36 +26,44 @@ export const SupportFlow = () => {
 
   const InvalidEmail = () => {
     return (
-      <div className="mb-4 w-3/5 bg-red-100 p-2 text-sm">
+      <div className="mb-4 w-3/5 bg-red-100 p-2 text-sm" id="support-flow-invalid-email">
         <p>{t("supportFLowAddEmail.error.validEmail.title")}</p>
         <p> {t("supportFLowAddEmail.error.validEmail.description")}</p>
       </div>
     );
   };
 
+  const cssBorderDefault = "border-1 border-slate-500";
+  const cssBorderError =
+    "border-2 border-red outline-red focus:outline-red-focus focus:outline-red focus:border-red-focus";
+
   return (
-    <form className="mb-10" onSubmit={handleSubmit}>
+    <form className="mb-10" onSubmit={handleSubmit} noValidate>
       <h2>{t("supportFLowAddEmail.title")}</h2>
       <p className="mb-4" id="support-flow-description">
         {t("supportFLowAddEmail.description")}
       </p>
       <div role="alert">{emailError && <InvalidEmail />}</div>
       <div className="mb-4">
-        <div className="inline-block rounded-md border-1 border-solid  border-slate-500">
-          <label
-            htmlFor="support-flow-email"
-            className="inline-block h-full rounded-md border-l-1 bg-slate-100 px-4 py-2"
-          >
-            {t("supportFLowAddEmail.email")}
-          </label>
-          <input
-            id="support-flow-email"
-            name="support-flow-email"
-            className="w-96 rounded-r-md border-x-1 border-l-slate-500  p-2"
-            type="email"
-            aria-describedby="support-flow-description"
-          />
-        </div>
+        <label
+          htmlFor="support-flow-email"
+          className="inline-block rounded-l-md border-1 border-r-0 border-solid  border-slate-500 px-4 py-2"
+        >
+          {t("supportFLowAddEmail.email")}
+        </label>
+        <input
+          id="support-flow-email"
+          name="support-flow-email"
+          className={cn(
+            "w-96 rounded-r-md border-solid p-2",
+            emailError ? cssBorderError : cssBorderDefault
+          )}
+          type="email"
+          aria-describedby="support-flow-description"
+          required
+          // aria-invalid={emailError}
+          // aria-errormessage="support-flow-invalid-email"
+        />
       </div>
       <Button theme="secondary" type="submit">
         {t("supportFLowAddEmail.submit")}
