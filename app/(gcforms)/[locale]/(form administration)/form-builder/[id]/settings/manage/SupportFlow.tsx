0a36b43a47eae478803d5c3b021cafd593b7e00a
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-03-21 11:36:03 -0400

    Adds email validation

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
@@ -1,16 +1,35 @@
 import { Button } from "@clientComponents/globals";
 import { useTranslation } from "@i18n/client";
 import { logMessage } from "@lib/logger";
+import { isValidGovEmail } from "@lib/validation/validation";
+import { useState } from "react";
 
 export const SupportFlow = () => {
   const { t } = useTranslation("form-builder");
+  const [emailError, setEmailError] = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    setEmailError(false);
+
     const formData = new FormData(e.target as HTMLFormElement);
     const email = formData.get("support-flow-email") as string;
 
+    if (!isValidGovEmail(email)) {
+      setEmailError(true);
+      return;
+    }
+
     logMessage.info(`SupportFlow email set to - TODO send to DB: ${email}`);
+  };
+
+  const InvalidEmail = () => {
+    return (
+      <div className="mb-4 w-3/5 bg-red-100 p-2 text-sm">
+        <p>{t("supportFLowAddEmail.error.validEmail.title")}</p>
+        <p> {t("supportFLowAddEmail.error.validEmail.description")}</p>
+      </div>
+    );
   };
 
   return (
@@ -19,6 +38,7 @@ export const SupportFlow = () => {
       <p className="mb-4" id="support-flow-description">
         {t("supportFLowAddEmail.description")}
       </p>
+      <div role="alert">{emailError && <InvalidEmail />}</div>
       <div className="mb-4">
         <div className="inline-block rounded-sm border-1 border-solid  border-slate-500">
           <label
