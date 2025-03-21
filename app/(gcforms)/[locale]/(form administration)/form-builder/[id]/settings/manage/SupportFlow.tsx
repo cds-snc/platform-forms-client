e376fdbe22f85df1b13e252ee76ddad9955e7f7d
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-03-21 11:14:28 -0400

    Adds language strings

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/SupportFlow.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@clientComponents/globals";
+import { useTranslation } from "@i18n/client";
 import { logMessage } from "@lib/logger";
 
 export const SupportFlow = () => {
+  const { t } = useTranslation("form-builder");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
@@ -12,24 +15,30 @@ export const SupportFlow = () => {
 
   return (
     <form className="mb-10" onSubmit={handleSubmit}>
-      <h2>Support channels</h2>
-      <p className="mb-4">Add contact information to your form in case there is an issue.</p>
-      <div className="mb-4 inline-block rounded-sm border-1 border-solid  border-slate-500">
-        <label htmlFor="support-flow-email" className="inline-block h-full bg-slate-100 px-4 py-2">
-          email
-        </label>
-        <input
-          id="support-flow-email"
-          name="support-flow-email"
-          className="w-96 border-l-1 border-solid  border-slate-500 p-2"
-          type="email"
-        />
+      <h2>{t("supportFLowAddEmail.title")}</h2>
+      <p className="mb-4" id="support-flow-description">
+        {t("supportFLowAddEmail.description")}
+      </p>
+      <div className="mb-4">
+        <div className="inline-block rounded-sm border-1 border-solid  border-slate-500">
+          <label
+            htmlFor="support-flow-email"
+            className="inline-block h-full bg-slate-100 px-4 py-2"
+          >
+            {t("supportFLowAddEmail.email")}
+          </label>
+          <input
+            id="support-flow-email"
+            name="support-flow-email"
+            className="w-96 border-l-1 border-solid  border-slate-500 p-2"
+            type="email"
+            aria-describedby="support-flow-description"
+          />
+        </div>
       </div>
-      <div>
-        <Button theme="secondary" type="submit">
-          Save changes
-        </Button>
-      </div>
+      <Button theme="secondary" type="submit">
+        {t("supportFLowAddEmail.submit")}
+      </Button>
     </form>
   );
 };
