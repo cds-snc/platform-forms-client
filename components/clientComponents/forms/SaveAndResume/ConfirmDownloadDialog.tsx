f95ccfcdae715929f7e1d0f2ef10da581b415fd6
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-03-25 10:10:55 -0400

    Don't need nested Content

--- components/clientComponents/forms/SaveAndResume/ConfirmDownloadDialog.tsx
+++ components/clientComponents/forms/SaveAndResume/ConfirmDownloadDialog.tsx
@@ -85,21 +85,21 @@ export const ConfirmDownloadDialog = ({
           <AlertDialog.Title className="text-2xl font-extrabold leading-tight">
             {t(`${tParent}.prompt.title`)}
           </AlertDialog.Title>
-          <AlertDialog.Content className="pb-6">
-            <div>
-              <div className="mb-2 flex">
-                <span className="mr-4 inline-block text-2xl font-bold"> 1 </span>
-                {t(`${tParent}.prompt.text1`)}
-              </div>
 
-              <div className="mb-6 flex">
-                <span className="mr-4 inline-block text-2xl font-bold"> 2 </span>{" "}
-                {t(`${tParent}.prompt.text2`)}
-              </div>
-
-              <div className="mb-2 ml-6 font-bold italic">{t(`${tParent}.prompt.text3`)}</div>
+          <div>
+            <div className="mb-2 flex">
+              <span className="mr-4 inline-block text-2xl font-bold"> 1 </span>
+              {t(`${tParent}.prompt.text1`)}
             </div>
-          </AlertDialog.Content>
+
+            <div className="mb-6 flex">
+              <span className="mr-4 inline-block text-2xl font-bold"> 2 </span>{" "}
+              {t(`${tParent}.prompt.text2`)}
+            </div>
+
+            <div className="mb-2 ml-6 font-bold italic">{t(`${tParent}.prompt.text3`)}</div>
+          </div>
+
           <div style={{ display: "flex", gap: 15, justifyContent: "flex-end" }}>
             <AlertDialog.Cancel asChild>
               <Button onClick={() => handleClose(false)} theme="secondary">
