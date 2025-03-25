6994b37130ff324baae70928ebdd93d46f9ca63a
Tim Arney <tim@line37.com>
2025-03-25 09:16:26 -0400

    focus styles

--- components/clientComponents/forms/SaveAndResume/MobileDrawer.tsx
+++ components/clientComponents/forms/SaveAndResume/MobileDrawer.tsx
@@ -37,20 +37,20 @@ export const MobileDrawer = ({
       <div className="flex flex-col gap-4">
         <Button
           theme="secondary"
-          className="flex w-full items-center justify-center rounded-full border border-slate-900 bg-slate-100 py-4"
+          className="group flex w-full items-center justify-center rounded-full border border-slate-900 bg-slate-100 py-4"
           onClick={handleSave}
         >
           <>
-            <SaveProgressIcon className="mr-4 size-8" />
+            <SaveProgressIcon className="mr-4 size-8 group-focus:fill-white" />
             {t("saveAndResume.saveToDevice")}
           </>
         </Button>
         <Link
           href={`/${language}/id/${formId}/resume`}
-          className="flex w-full items-center justify-center rounded-full border border-slate-900 bg-slate-100 py-4 no-underline"
+          className="group flex w-full items-center justify-center rounded-full border border-slate-900 bg-slate-100 py-4 no-underline hover:cursor-pointer hover:border-indigo-700 hover:bg-violet-50 focus:cursor-pointer focus:border-gcds-blue-850 focus:bg-gcds-blue-850 focus:text-white-default focus:outline focus:outline-[3px] focus:outline-offset-2 focus:outline-gcds-blue-850"
         >
           <>
-            <UploadIcon className="mr-4 size-8" />
+            <UploadIcon className="mr-4 size-8 group-focus:fill-white" />
             {t("saveAndResume.resumeForm")}
           </>
         </Link>
