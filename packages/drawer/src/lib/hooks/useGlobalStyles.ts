049bcecc65107b8e85df3461b0484c9e05155660
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-03-25 17:23:11 -0400

    Try removing it

--- packages/drawer/src/lib/hooks/useGlobalStyles.ts
+++ packages/drawer/src/lib/hooks/useGlobalStyles.ts
@@ -4,24 +4,28 @@ import globalStylesheet, { getClassNames } from "../styles";
 function useGlobalStyles(duration: number, hideScrollbars: boolean, nonce?: string) {
   const identifier = React.useMemo(() => Math.random().toString(36).substr(2), []);
   const classNames = React.useMemo(() => getClassNames(identifier), [identifier]);
-  
+  if (nonce) {
+    // nothing
+  }
   React.useEffect(() => {
     if (typeof document === "undefined") {
       return;
     }
-    const styles = globalStylesheet(identifier, {duration, hideScrollbars});
+    const styles = globalStylesheet(identifier, { duration, hideScrollbars });
 
     const tag = document.createElement("style");
     tag.setAttribute("data-react-bottom-drawer", identifier);
-    nonce && tag.setAttribute("nonce", nonce);
+    // nonce && tag.setAttribute("nonce", nonce);
     tag.innerHTML = styles;
 
     document.head.appendChild(tag);
 
-    return function() {
+    return function () {
       const stylesheet = document.querySelector(`style[data-react-bottom-drawer='${identifier}']`);
-      if (stylesheet) { stylesheet.remove(); }
-    }
+      if (stylesheet) {
+        stylesheet.remove();
+      }
+    };
   }, [duration, hideScrollbars]);
 
   return classNames;
