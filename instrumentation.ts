82f4528a2fd2d1ed6333a1f50cd253a8292a37bc
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-03-27 07:54:10 -0400

    update comments in instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,18 +1,16 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    // Flush the privilege cache for users when the app server is started
-    // This ensures that if privileges were changed they take immediate global effect.
-
-    // We cannot reuse the same function from the main application because Pino is not compatible
-    // with this runtime. We need to use the native console.log function instead.
     if (!process.env.REDIS_URL) {
       // eslint-disable-next-line no-console
       console.log("No REDIS_URL environment variable found, skipping privilege cache flush");
       return;
     }
+    // Initialize the feature flags when the app server is started
     await import("@lib/flags/initialization").then(async (m) => {
       m.initiateFlags();
     });
+    // Flush the privilege cache for users when the app server is started
+    // This ensures that if privileges were changed they take immediate global effect.
     await import("@lib/cache/privilegeCache").then(async (m) => {
       m.flushValues();
     });
