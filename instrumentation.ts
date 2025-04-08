5d0e919c16d50ba752b5ee792f04fba713a4071e
Clément Janin <clement.janin@cds-snc.ca>
2025-04-08 13:50:25 -0400

    fix: remove Redis disconnect function call (tentative fix)

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,13 +2,17 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     if (!process.env.REDIS_URL) {
       // eslint-disable-next-line no-console
-      console.log("No REDIS_URL environment variable found, skipping flags initialization and privilege cache flush");
+      console.log(
+        "No REDIS_URL environment variable found, skipping flags initialization and privilege cache flush"
+      );
       return;
     }
+
     // Initialize the feature flags when the app server is started
     await import("@lib/flags/initialization").then(async (m) => {
       m.initiateFlags();
     });
+
     // Flush the privilege cache for users when the app server is started
     // This ensures that if privileges were changed they take immediate global effect.
     await import("@lib/cache/privilegeCache").then(async (m) => {
