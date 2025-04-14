309dbc8615b5517121cf8aafea38fbf8f7f384e9
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-04-14 11:08:07 -0400

    Fix up and de-tailwind ToolTip styles

--- packages/editor/src/ui/ToolTip.tsx
+++ packages/editor/src/ui/ToolTip.tsx
@@ -1,24 +1,22 @@
 "use client";
 import React, { useId, Children } from "react";
+import "./ToolTip.css";
 
 export const ToolTip = ({ children, text }: { children: React.ReactElement; text: string }) => {
   const id = `tooltip-${useId()}`;
 
   return (
-    <span className="relative">
+    <span className="gc-tooltip-container">
       {Children.map(children, (child) => {
         return React.cloneElement(child, {
           // @ts-expect-error  -- TODO: fix this
           ...child.props,
           // @ts-expect-error  -- TODO: fix this
-          className: child.props.className + " peer",
+          className: child.props.className + " gc-tooltip-target",
           "aria-labelledby": id,
         });
       })}
-      <span
-        id={id}
-        className={`invisible absolute -left-14 -top-10 z-[1000] w-36 whitespace-nowrap rounded bg-gray-800 p-1 text-center text-sm text-white after:absolute after:left-1/2 after:top-full after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700 after:content-[''] peer-hover:visible peer-focus:visible`}
-      >
+      <span id={id} className="gc-tooltip">
         {text}
       </span>
     </span>
