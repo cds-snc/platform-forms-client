e4cb08aa5be07f04911c7318c665bd29c60ecd76
Tim Arney <tim@line37.com>
2025-04-14 11:25:53 -0400

    update styles

--- components/serverComponents/globals/Version.tsx
+++ components/serverComponents/globals/Version.tsx
@@ -1,12 +1,18 @@
 import packageJson from "../../../package.json";
+import { cn } from "@lib/utils";
 
 const deploymentId = process.env.NEXT_DEPLOYMENT_ID || "local";
 
-export const Version = ({ label }: { label: string }) => {
+export const Version = ({ label, isFormBuilder }: { label: string; isFormBuilder: boolean }) => {
   const { version } = packageJson;
 
   return (
-    <div className="mt-2 text-sm text-slate-800">
+    <div
+      className={cn(
+        isFormBuilder && "mt-2 text-sm text-slate-800",
+        !isFormBuilder && " text-sm text-slate-800 inline-block mr-10"
+      )}
+    >
       {label}: {version} <span className="hidden"> - {deploymentId}</span>
     </div>
   );
