43895e508bf977e9cdcd4a833f0188614c01cadc
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-04-28 16:03:42 -0400

    Update notifications to send to all form owners

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/notifications.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/notifications.ts
@@ -7,11 +7,11 @@ import { getOrigin } from "@lib/origin";
 // Self-contained function for sending email notifications when a user has new form submissions
 export const sendNotification = async (formId: string) => {
   try {
-    logMessage.info(`sendNotification: ${formId}`);
+    logMessage.info(`sendNotification: ${formId}`); // @TODO temp
 
     // Gather template settings
     const template = await getTemplateWithAssociatedUsers(formId);
-    logMessage.info(`-----------sendNotification: template ${JSON.stringify(template)}`);
+    logMessage.info(`-----------sendNotification: template ${JSON.stringify(template)}`); // @TODO temp
     if (!template) {
       throw new Error(`template not found with id ${formId}`);
     }
@@ -21,27 +21,30 @@ export const sendNotification = async (formId: string) => {
         form: { titleEn, titleFr },
       },
     } = template;
-    // null = off, 1440=1 day, 10080=1 week
-    const notifcationsInterval = template.formRecord.notifcationsInterval;
-    if (!notifcationsInterval) {
-      return;
-    }
-    if (!Array.isArray(users) || users.length === 0) {
-      throw "template missing users";
-    }
-    const email = users[0].email; // TODO: send to all users or just the first one?
 
     // Check if an email has been sent and if so do nothing
     const marker = await getMarker(formId);
     if (marker) {
-      logMessage.info(`sendNotification: marker exists`);
+      logMessage.info(`sendNotification: marker exists, no need to send email`); // @TODO temp
       return;
     }
+    logMessage.info(`sendNotification: marker does not exist`); // @TODO temp
 
-    // No email has been sent, update redis and send an email
-    logMessage.info(`sendNotification: marker does not exist`);
+    // No email has been sent, update redis
+    // @TODO document somwhere: null = off, 1440=1 day, 10080=1 week
+    const notifcationsInterval = template.formRecord.notifcationsInterval;
+    if (!notifcationsInterval) {
+      return;
+    }
     setMarker(formId, notifcationsInterval);
-    sendEmailNotification(email, formId, String(titleEn), String(titleFr));
+
+    // and send an email to each user
+    if (!Array.isArray(users) || users.length === 0) {
+      throw "template missing users";
+    }
+    users.forEach(({ email }) =>
+      sendEmailNotification(email, formId, String(titleEn), String(titleFr))
+    );
   } catch (err) {
     // TODO: the error message could be more specific from the function that throws it
     logMessage.error(`sendNotification failed with error: ${err}`);
@@ -56,20 +59,19 @@ const getMarker = async (formId: string) => {
 };
 
 const setMarker = async (formId: string, emailNotification: number) => {
-  const ttl = emailNotification * 60; // convert to seconds
+  const ttl = emailNotification * 60; // convert from minutes to seconds
   const redis = await getRedisInstance();
   await redis.set(`notification:formId${formId}`, 1, "EX", ttl);
   logMessage.info(`setMarker: notification:formId${formId} with ttl ${ttl}`);
 };
 
-// TODO: French version
 const defaultEmailTemplate = (
   HOST: string,
   formId: string,
   formTitleEn: string,
   formTitleFr: string
 ) => {
-  // For notify formatting see: https://documentation.notification.canada.ca/en/send.html#sending-an-email
+  // For Notify formatting see: https://documentation.notification.canada.ca/en/send.html#sending-an-email
   return `
 ${formTitleEn} has new responses.
 
@@ -81,7 +83,7 @@ ${formTitleFr} nouvelles réponses
 
 [Ovrir ${formTitleFr}](${HOST}/fr/form-builder/${formId}/responses)
     `;
-};
+}; // @TODO language strings
 
 const sendEmailNotification = async (
   email: string,
@@ -92,7 +94,7 @@ const sendEmailNotification = async (
   logMessage.info(`sendEmail: ${email} ${formId} ${formTitleEn} | ${formTitleFr}`);
   const HOST = await getOrigin();
   await sendEmail(email, {
-    subject: "New responses | nouvelles réponses",
+    subject: "New responses | nouvelles réponses", // @TODO language strings
     formResponse: defaultEmailTemplate(HOST, formId, formTitleEn, formTitleFr),
   });
 };
