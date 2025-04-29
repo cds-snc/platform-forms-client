522047adc1aa09f7543fcddde4637c178d357e28
Tim Arney <tim@line37.com>
2025-04-29 06:57:11 -0400

    cleanup

--- components/serverComponents/globals/Header/Fip.tsx
+++ components/serverComponents/globals/Header/Fip.tsx
@@ -3,20 +3,16 @@ import { type Language } from "@lib/types/form-builder-types";
 import { SignatureEn } from "./assets/SignatureEn";
 import { SignatureFr } from "./assets/SignatureFr";
 
-export const Fip = ({ language }: { language: Language }) => {
+export const Fip = ({ language, link }: { language: Language; link: string }) => {
   return (
     <div className="gcds-signature brand__signature">
       {language === "en" ? (
-        <a href="">
-          <div>
-            <SignatureEn />
-          </div>
+        <a href={link}>
+          <SignatureEn />
         </a>
       ) : (
-        <a href="">
-          <div>
-            <SignatureFr />
-          </div>
+        <a href={link}>
+          <SignatureFr />
         </a>
       )}
     </div>
--- components/serverComponents/globals/Header/GCHeader.tsx
+++ components/serverComponents/globals/Header/GCHeader.tsx
@@ -1,4 +1,5 @@
 import { LanguageToggle } from "./LanguageToggle";
+import { BrandContainer } from "./BrandContainer";
 import { Fip } from "./Fip";
 import { type Language } from "@lib/types/form-builder-types";
 
@@ -6,12 +7,10 @@ export const GCHeader = ({ language }: { language: Language }) => {
   return (
     <div className="gcds-header__container">
       <header className="gcds-header">
-        <div className="gcds-header__brand">
-          <div className="brand__container">
-            <Fip language={language} />
-            <LanguageToggle language={language} />
-          </div>
-        </div>
+        <BrandContainer>
+          <Fip language={language} link={""} />
+          <LanguageToggle language={language} />
+        </BrandContainer>
       </header>
     </div>
   );
