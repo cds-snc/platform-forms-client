823cab1c1f0d22efbb30c46da0405411f7e3cff5
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-04-29 15:47:18 -0400

    Adds translations

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/Notifications.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/manage/Notifications.tsx
@@ -3,6 +3,7 @@ import { Button } from "@clientComponents/globals";
 import { useCallback, useState } from "react";
 import { updateNotificationsInterval } from "./actions";
 import { NotificationsInterval } from "packages/types/src/form-types";
+import { useTranslation } from "@i18n/client";
 
 export const Notifications = ({
   formId,
@@ -11,6 +12,7 @@ export const Notifications = ({
   formId: string;
   notifcationsInterval: NotificationsInterval | undefined;
 }) => {
+  const { t } = useTranslation("form-builder");
   const [notificationValue, setNotificationValue] = useState<string>(
     notifcationsInterval ? String(notifcationsInterval) : "off"
   );
@@ -35,9 +37,9 @@ export const Notifications = ({
 
   return (
     <div className="mb-10" data-testid="form-notifications">
-      <h2>Notifications</h2>
-      <p className="mb-2 font-bold">Email notifications for repsonses</p>
-      <p className="mb-8">Receive emails when there are new responses available.</p>
+      <h2>{t("settings.notifications.title")}</h2>
+      <p className="mb-2 font-bold">{t("settings.notifications.description1")}</p>
+      <p className="mb-8">{t("settings.notifications.description2")}</p>
       <div className="">
         <div className="mb-6">
           <Radio
@@ -45,7 +47,7 @@ export const Notifications = ({
             checked={notificationValue === "1440"}
             name="notifications"
             value={"1440"}
-            label={"Daily"}
+            label={t("settings.notifications.options.daily")}
             onChange={updateNotificationsValue}
             className="mb-0"
           />
@@ -56,7 +58,7 @@ export const Notifications = ({
             checked={notificationValue === "10080"}
             name="notifications"
             value={"10080"}
-            label={"Weekly"}
+            label={t("settings.notifications.options.weekly")}
             onChange={updateNotificationsValue}
             className="mb-0"
           />
@@ -67,7 +69,7 @@ export const Notifications = ({
             checked={notificationValue === "off"}
             name="notifications"
             value={"off"}
-            label={"None"}
+            label={t("settings.notifications.options.never")}
             onChange={updateNotificationsValue}
             className="mb-0"
           />
@@ -79,7 +81,7 @@ export const Notifications = ({
         type="submit"
         onClick={() => saveNotificationsValue()}
       >
-        Save
+        {t("settings.notifications.save")}
       </Button>
     </div>
   );
