36b260df68d10b0c93975799a89331a2bb7d2d7c
Tim Arney <tim@line37.com>
2025-04-30 12:14:37 -0400

    update

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PreviewNavigation.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PreviewNavigation.tsx
@@ -2,11 +2,8 @@
 import React from "react";
 import { LangSwitcher } from "@formBuilder/components/shared/LangSwitcher";
 import { SaveButton } from "@formBuilder/components/shared/SaveButton";
-import { useSession } from "next-auth/react";
 
 export const PreviewNavigation = () => {
-  const { status } = useSession();
-
   return (
     <div className="relative">
       <div className="absolute left-0 top-0 mt-2">
@@ -14,7 +11,7 @@ export const PreviewNavigation = () => {
             SaveButton should mostly be invisible, but is here to handle 
             autosave when opening a form file to prevent errors on Settings 
         */}
-        {status === "authenticated" && <SaveButton />}
+        <SaveButton />
       </div>
       <div className="absolute right-0 top-0">
         <LangSwitcher descriptionLangKey="previewingIn" />
