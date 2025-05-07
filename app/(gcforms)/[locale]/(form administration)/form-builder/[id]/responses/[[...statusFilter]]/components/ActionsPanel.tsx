2960c298b45178b0197072e6fb9bfa870dcb04d4
Tim Arney <tim@line37.com>
2025-05-07 07:56:12 -0400

    fix-z-index

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[[...statusFilter]]/components/ActionsPanel.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses/[[...statusFilter]]/components/ActionsPanel.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export const ActionsPanel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="fixed bottom-0 left-0 z-100 h-32 w-full border-t-2 border-black bg-white py-8">
-      <div className="mx-4 laptop:mx-24">
+    <section className="fixed bottom-0 left-0 z-[101] h-32 w-full border-t-2 border-black bg-white py-8">
+      <div className="mx-4 tablet:mx-24">
         <div className="">{children}</div>
       </div>
     </section>
