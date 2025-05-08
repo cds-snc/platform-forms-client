2a4f2960d2c1998046c6d40dc739acfddfaeaf2e
Tim Arney <tim@line37.com>
2025-05-08 08:28:08 -0400

    fix default

--- app/(gcforms)/[locale]/(support)/components/client/FocusHeader.tsx
+++ app/(gcforms)/[locale]/(support)/components/client/FocusHeader.tsx
@@ -4,7 +4,7 @@ import { ReactElement, useRef } from "react";
 
 export const FocusHeader = ({
   children,
-  headingTag: HeadingTag = "h2",
+  headingTag: HeadingTag = "h1",
 }: {
   children: ReactElement | string;
   headingTag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
