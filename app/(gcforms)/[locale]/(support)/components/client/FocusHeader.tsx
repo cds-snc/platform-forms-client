a198d41981ccfc540c191730d196d8816e37dc13
Tim Arney <tim@line37.com>
2025-05-08 08:11:39 -0400

    update heading tag

--- app/(gcforms)/[locale]/(support)/components/client/FocusHeader.tsx
+++ app/(gcforms)/[locale]/(support)/components/client/FocusHeader.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { useFocusIt } from "@lib/hooks/useFocusIt";
-import { ReactElement, useRef, createElement } from "react";
+import { ReactElement, useRef } from "react";
 
 export const FocusHeader = ({
   children,
-  tag = "h1",
+  headingTag: HeadingTag = "h2",
 }: {
   children: ReactElement | string;
-  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  headingTag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }) => {
   const headingSuccessRef = useRef(null);
   useFocusIt({ elRef: headingSuccessRef });
 
-  return createElement(tag, { ref: headingSuccessRef }, children);
+  if (typeof children !== "string") {
+    return <>{children}</>;
+  }
+
+  return <HeadingTag ref={headingSuccessRef}>{children}</HeadingTag>;
 };
