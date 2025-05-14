8275c6ff844e1e3c81acc6b9b4aa6d9b2838147c
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2025-05-14 11:37:25 -0400

    chore: Updating banner content to announce email delivery retirement (#5537)
    * missing translation
    
    * make banner generic feature flag rather than specific to caretaker convention
    
    * update banner content
    
    * Update i18n/translations/fr/admin-flags.json
    
    * Update i18n/translations/fr/admin-flags.json
    
    * enable
    
    * Clean up
    
    * missing change
    
    * fix error

--- app/(gcforms)/[locale]/layout.tsx
+++ app/(gcforms)/[locale]/layout.tsx
@@ -10,7 +10,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     FeatureFlags.addressComplete,
     FeatureFlags.formTimer,
     FeatureFlags.hCaptcha,
-    FeatureFlags.caretakerPeriod,
+    FeatureFlags.topBanner,
     FeatureFlags.fileUpload,
   ]);
 
