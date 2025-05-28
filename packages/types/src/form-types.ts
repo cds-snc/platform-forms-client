e2e68aac65cdb98aace93f5df03a04c9ee396f28
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-05-28 13:57:30 -0400

    Updates notifications to be off for newly created forms

--- packages/types/src/form-types.ts
+++ packages/types/src/form-types.ts
@@ -255,7 +255,7 @@ export const NotificationsInterval = {
   WEEK: 10080,
 } as const;
 
-export const NotificationsIntervalDefault = NotificationsInterval.DAY; // Default in prisma also
+export const NotificationsIntervalDefault = NotificationsInterval.OFF; // Prisma default in prisma is 1440
 
 export type NotificationsInterval =
   (typeof NotificationsInterval)[keyof typeof NotificationsInterval];
