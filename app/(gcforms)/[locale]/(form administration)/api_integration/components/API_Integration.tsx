22cb1e37820b52b4c4ed992c1e7e837487d406e1
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-05-29 07:28:31 -0400

    wip dummy page created

---
+++ app/(gcforms)/[locale]/(form administration)/api_integration/components/API_Integration.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const APIIntegration = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">API Integration</h1>
+      <p className="mt-4 text-lg">
+        This page is under construction. Please check back later for updates.
+      </p>
+    </div>
+  );
+};
