04b212cdcd71f1c85bb30b3a0322a23405fe9461
Tim Arney <tim@line37.com>
2025-05-29 07:38:19 -0400

    add file types

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/components/dialogs/MoreDialog/FileTypeOptions.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/components/dialogs/MoreDialog/FileTypeOptions.tsx
@@ -1,7 +1,6 @@
 import { FormElement } from "@lib/types";
 import { useTranslation } from "@i18n/client";
-
-// import { ALLOWED_FILE_TYPES } from "@lib/validation/fileValidationClientSide";
+import { ALLOWED_FILE_TYPES } from "@lib/validation/fileValidationClientSide";
 
 export const FileTypeOptions = ({
   item,
@@ -16,33 +15,54 @@ export const FileTypeOptions = ({
     return null;
   }
 
+  // Ensure fileType is an array for multiple selections
+  const selectedTypes: string[] = Array.isArray(item.properties.fileType)
+    ? item.properties.fileType
+    : item.properties.fileType
+    ? [item.properties.fileType]
+    : [];
+
+  const handleCheckboxChange = (type: string, checked: boolean) => {
+    let newTypes: string[];
+    if (checked) {
+      newTypes = [...selectedTypes, type];
+    } else {
+      newTypes = selectedTypes.filter((t) => t !== type);
+    }
+    setItem({
+      ...item,
+      properties: {
+        ...item.properties,
+        fileType: newTypes,
+      },
+    });
+  };
+
   return (
     <section className="mb-4">
       <div className="mb-2">
         <h3>{t("fileTypeOptions")}</h3>
       </div>
-      <div className="gcds-select-wrapper">
-        <select
-          data-testid="file-type"
-          className="gc-dropdown mb-4 inline-block"
-          id={`file-type--modal--${item.id}`}
-          value={item.properties.fileType || ""}
-          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-            setItem({
-              ...item,
-              properties: {
-                ...item.properties,
-                fileType: e.target.value,
-              },
-            });
-          }}
-        >
-          <option value="">{t("selectFileType")}</option>
-          <option value="image">{t("image")}</option>
-          <option value="document">{t("document")}</option>
-          <option value="audio">{t("audio")}</option>
-          <option value="video">{t("video")}</option>
-        </select>
+      <div className="">
+        {ALLOWED_FILE_TYPES.map((type) => (
+          <div className="gc-input-checkbox" key={type.extensions[0]}>
+            <input
+              id={`file-type-checkbox-${type.extensions[0]}`}
+              type="checkbox"
+              checked={selectedTypes.includes(type.extensions[0])}
+              onChange={(e) => handleCheckboxChange(type.extensions[0], e.target.checked)}
+              className="gc-input-checkbox__input"
+              data-testid={`file-type-checkbox-${type}`}
+            />
+            <label
+              key={type.extensions[0]}
+              className="gc-checkbox-label"
+              htmlFor={`file-type-checkbox-${type.extensions[0]}`}
+            >
+              <span className="checkbox-label-text"> {type.extensions[0]}</span>
+            </label>
+          </div>
+        ))}
       </div>
     </section>
   );
