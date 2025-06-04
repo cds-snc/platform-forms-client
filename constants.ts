5631018e43f996fb71f96e70ec91e5d530e39ad9
Tim Arney <tim@line37.com>
2025-06-04 13:38:24 -0400

    update note

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-// Note 5mb is also defined for Server actions in next.config.mjs
+// Note also defined for Server actions in next.config.mjs
 export const bodySizeLimit = 5 * 1024 * 1024; // 5MB
 export const bodySizeLimitWithFiles = 10 * 1024 * 1024; // 10MB
 export const maxResponsesSize = 380 * 1024; // 380KB
