763c555f27b391cc022533bf181e385b0ee2998b
Tim Arney <timarney@users.noreply.github.com>
2025-06-10 14:41:28 -0400

    chore: add speak for file input (#5677)
    add speak

--- components/clientComponents/forms/FileInput/FileInput.tsx
+++ components/clientComponents/forms/FileInput/FileInput.tsx
@@ -10,6 +10,8 @@ import { htmlInputAccept, ALLOWED_FILE_TYPES } from "@lib/validation/fileValidat
 import { themes } from "@clientComponents/globals/Buttons/themes";
 import { BODY_SIZE_LIMIT_WITH_FILES } from "@root/constants";
 import { bytesToMb, bytesToKbOrMbString } from "@lib/utils/fileSize";
+import { EventKeys, useCustomEvent } from "@lib/hooks/useCustomEvent";
+import { Priority } from "@clientComponents/globals/LiveRegion";
 
 interface FileInputProps extends InputFieldProps {
   error?: boolean;
@@ -33,6 +35,8 @@ export const FileInput = (props: FileInputProps): React.ReactElement => {
 
   const { name, disabled, allowMulti, required, ariaDescribedBy, lang } = props;
 
+  const { Event } = useCustomEvent();
+
   const { t } = useTranslation("common", { lng: lang });
 
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -45,6 +49,10 @@ export const FileInput = (props: FileInputProps): React.ReactElement => {
   }>(bytesToKbOrMbString(value?.size));
 
   const resetInput = () => {
+    Event.fire(EventKeys.liveMessage, {
+      message: t("fileInput.removedMessage", { fileName }),
+      priority: Priority.LOW,
+    });
     setFileName("");
     setFileSize({ size: 0, unit: "bytes" });
     setValue({});
@@ -82,6 +90,12 @@ export const FileInput = (props: FileInputProps): React.ReactElement => {
 
           if (newFile.name !== fileName) {
             setFileName(newFile.name);
+
+            Event.fire(EventKeys.liveMessage, {
+              message: t("fileInput.addedMessage", { fileName: newFile.name }),
+              priority: Priority.LOW,
+            });
+
             setFileSize(bytesToKbOrMbString(newFile.size));
             setValue({
               name: newFile.name,
