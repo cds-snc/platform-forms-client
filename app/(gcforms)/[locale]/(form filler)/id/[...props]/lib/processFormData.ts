d32fa2961de94d2c52d1a79640ce1b52eff04d86
Tim Arney <tim@line37.com>
2025-06-10 08:46:58 -0400

    add note

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/processFormData.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/processFormData.ts
@@ -57,7 +57,11 @@ export const processFormData = async (
                 fields[splitKey[0]] = [];
               }
 
+              // Note:
+              // currentValue is a reference to the fields object,
+              // updating currentValue will mutate the fields object.
               const currentValue = fields[splitKey[0]] as Record<string, unknown>[];
+
               if (!currentValue[Number(splitKey[1])]) {
                 currentValue[Number(splitKey[1])] = {};
               }
