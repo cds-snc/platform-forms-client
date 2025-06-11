ab94eb92e3981e03f6e0098e6ac8a12c3780e432
Vivian Nobrega <vivian.nobrega@cds-snc.ca>
2025-06-11 10:52:55 -0400

    * Initial working copy.

---
+++ lib/userFeatureFlags.ts
@@ -0,0 +1,26 @@
+import { prisma } from "@lib/integration/prismaConnector";
+import { featureFlagsCheck, featureFlagsPut } from "@lib/cache/userFeatureFlagsCache";
+
+/**
+ * Get all feature flags enabled for a user.
+ * @param userId id of the user
+ * @returns Array of feature flag keys (strings)
+ */
+export const getUserFeatureFlags = async (userId: string): Promise<string[]> => {
+  // Try cache first
+  const cachedFlags = await featureFlagsCheck(userId);
+  if (cachedFlags?.length) return cachedFlags;
+
+  // Query DB for enabled features
+  const userFeatures = await prisma.userFeature.findMany({
+    where: { userId },
+    select: { feature: true },
+  });
+
+  const featureKeys = userFeatures.map((uf) => uf.feature);
+
+  // Cache result
+  await featureFlagsPut(userId, featureKeys);
+
+  return featureKeys;
+};
