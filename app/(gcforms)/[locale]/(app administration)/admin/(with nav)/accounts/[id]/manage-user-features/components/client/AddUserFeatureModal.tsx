fc0ba196b86b4b5aa4ef4f6da70a459cd1bf9251
Tim Arney <tim@line37.com>
2025-06-19 12:50:31 -0400

    filter flags

--- app/(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-user-features/components/client/AddUserFeatureModal.tsx
+++ app/(gcforms)/[locale]/(app administration)/admin/(with nav)/accounts/[id]/manage-user-features/components/client/AddUserFeatureModal.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 import { AppUser } from "@lib/types/user-types";
 import { setUserFlags } from "../../actions";
 
+import { UserFeatureFlags, UserFeatureFlagKeys } from "@lib/cache/types";
+
 export const AddUserFeatureModal = ({
   formUser,
   flags,
@@ -15,6 +17,11 @@ export const AddUserFeatureModal = ({
   const { t } = useTranslation("admin-flags");
   const [showModal, setShowModal] = useState(false);
   const [selectedFlags, setSelectedFlags] = useState<string[]>([]);
+
+  // Filter the list of flags to only include user level feature flags
+  const userFlags = flags.filter((flag) =>
+    Object.values(UserFeatureFlags).includes(flag as UserFeatureFlagKeys)
+  );
 
   const handleCheckboxChange = (flag: string) => {
     setSelectedFlags((prev) =>
@@ -44,7 +51,7 @@ export const AddUserFeatureModal = ({
             <h2 className="mb-4 text-lg font-semibold">{t("Select Features")}</h2>
             <form>
               <div className="mb-6 flex flex-col gap-2">
-                {flags.map((flag) => (
+                {userFlags.map((flag) => (
                   <label key={flag} className="flex items-center gap-2">
                     <input
                       type="checkbox"
