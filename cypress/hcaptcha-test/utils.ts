a8ef9b314fcbbddd22890c2892d7e2425909399d
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-06-24 11:46:43 -0400

    remove smoke tests that will be moved outside the repo

--- cypress/hcaptcha-test/utils.ts
+++
@@ -1,7 +0,0 @@
-export const ignoreExceptions = () =>
-  Cypress.on("uncaught:exception", (err) => {
-    // See https://docs.cypress.io/api/cypress-api/catalog-of-events#Uncaught-Exceptions
-    if (err.message.includes("Minified React error #418")) {
-      return false;
-    }
-  });
