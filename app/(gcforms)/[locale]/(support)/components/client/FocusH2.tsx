91dda939b3fdf7cccc99d52ad28dc0336eb60eaa
Tim Arney <tim@line37.com>
2025-06-30 10:29:19 -0400

    update focus logic

---
+++ app/(gcforms)/[locale]/(support)/components/client/FocusH2.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { ReactElement, useRef, useEffect } from "react";
+
+export const FocusH2 = ({
+  children,
+  group,
+}: {
+  children: ReactElement | string;
+  group?: string; // Optional group prop to identify the section
+}) => {
+  const headingSuccessRef = useRef(null);
+
+  useEffect(() => {
+    const el = headingSuccessRef.current as unknown as HTMLElement;
+
+    if (el) {
+      // Add a tabindex if one does not exist on the element. This needed to focus an element,
+      // unless it is a form control like an input or button
+
+      // Give the DOM a little time to update
+      setTimeout(() => {
+        el?.focus();
+      }, 40);
+    }
+  }, [group]);
+
+  return (
+    <h2 key={group} tabIndex={-1} data-group={group} data-testid="focus-h2" ref={headingSuccessRef}>
+      {children}
+    </h2>
+  );
+};
