cee6c22a36a7a521b37260e4724d0af3b55fafb7
Tim Arney <tim@line37.com>
2025-07-02 07:28:18 -0400

    fix typo

--- lib/hooks/useFeatureDetect.tsx
+++ lib/hooks/useFeatureDetect.tsx
@@ -7,7 +7,7 @@ export const useFeatureDetect = () => {
   useEffect(() => {
     const features = {
       hasCrypto: "crypto" in window,
-      hasFilePicker: "crypto" in window,
+      hasFilePicker: "showOpenFilePicker" in window,
     };
 
     // Send the feature detection results to Google Analytics
