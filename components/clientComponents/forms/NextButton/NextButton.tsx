db9cbe0388f53b701ad49713282c414a500a90fd
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-07-07 11:10:50 -0400

    Handle missing groups

--- components/clientComponents/forms/NextButton/NextButton.tsx
+++ components/clientComponents/forms/NextButton/NextButton.tsx
@@ -17,6 +17,7 @@ import { useFormDelay } from "@lib/hooks/useFormDelayContext";
 import { ForwardArrowIcon24x24 } from "@serverComponents/icons";
 import { isFormClosed } from "app/(gcforms)/[locale]/(form filler)/id/[...props]/actions";
 import { useRouter } from "next/navigation";
+import { formHasGroups } from "@root/lib/utils/form-builder/formHasGroups";
 
 export const NextButton = ({
   validateForm,
@@ -51,7 +52,7 @@ export const NextButton = ({
     return false;
   };
 
-  if (currentGroup && !hasNextAction(currentGroup)) {
+  if (formHasGroups(formRecord.form) && currentGroup && !hasNextAction(currentGroup)) {
     return <div data-id="dead-end"></div>;
   }
 
