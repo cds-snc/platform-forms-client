5528c6cd5e0a051a3fc560b1b5b9af5d21324ae8
Tim Arney <tim@line37.com>
2025-07-08 09:05:53 -0400

    update styles

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PublishedLinks.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/preview/PublishedLinks.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "@i18n/client";
 import { getHost } from "@lib/utils/form-builder";
 
 export const PublishedLinks = ({ id, locale }: { id: string; locale: string }) => {
-  const { t } = useTranslation(["common", "form-builder"], { lng: locale });
+  const { t } = useTranslation(["form-builder"], { lng: locale });
 
   const linkEn = `${getHost()}/en/id/${id}`;
   const linkFr = `${getHost()}/fr/id/${id}`;
@@ -10,7 +10,7 @@ export const PublishedLinks = ({ id, locale }: { id: string; locale: string }) =
   return (
     <div>
       <p className="mb-2 mt-5">{t("publishedViewLinks", { ns: "form-builder" })}</p>
-      <div className="grid grid-cols-[max-content_1fr] gap-x-4">
+      <div className="grid grid-cols-[max-content_1fr] gap-x-2">
         <strong>{t("english")}</strong>
         <a href={linkEn}>{linkEn}</a>
         <strong>{t("french")}</strong>
