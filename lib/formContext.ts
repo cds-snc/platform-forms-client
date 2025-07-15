9457fb9ee58ca638894ba2e246431d2d40882efb
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-07-15 13:30:43 -0400

    no need to return values with default values

--- lib/formContext.ts
+++ lib/formContext.ts
@@ -728,10 +728,9 @@ export const filterValuesByShownElements = (values: Responses, elementsShown: Fo
     if (elementsShown.find((el) => el.id === Number(key))) {
       // Note: want to keep original value type (e.g. Checkbox=Array) or Formik may get confused
       filteredValues[key] = values[key];
-    } else {
-      filteredValues[key] = Array.isArray(values[key]) ? [] : "";
     }
   });
+
   return filteredValues;
 };
 
