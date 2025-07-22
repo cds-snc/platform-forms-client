dea28e120d37ccdd15870a432bed135eb512a375
Tim Arney <tim@line37.com>
2025-07-22 07:25:52 -0400

    cleanup

--- components/clientComponents/forms/SubmitProgress/SubmitProgress.tsx
+++ components/clientComponents/forms/SubmitProgress/SubmitProgress.tsx
@@ -8,21 +8,46 @@ import { GcFormsIcon } from "./GCFormsIcon";
 import { ProgressBar } from "./ProgressBar";
 import { Spinner } from "./Spinner";
 
+export function getPercentage(value: number): number {
+  const percentage = Math.round(value * 100);
+  return Math.max(0, Math.min(100, percentage));
+}
+
+/**
+ * Displays a loading spinner or a progress bar based on the submission status of a form.
+ *
+ * It listens for custom events to update the progress and message displayed.
+ * Example event:
+ *   document.dispatchEvent(
+ *     new CustomEvent(EventKeys.submitProgress, {
+ *       detail: {
+ *         progress: totalProgress,
+ *         message: `Processing ${totalProgress}%`,
+ *       },
+ *     })
+ *   );
+ */
+
 export const SubmitProgress = ({ spinner = true }: { spinner?: boolean }) => {
   const { t } = useTranslation("common");
+
+  const submitText = t("submitProgress.text");
+  const [message, setMessage] = useState(submitText);
+  const [displaySpinner, setDisplaySpinner] = useState(spinner);
 
   const { Event } = useCustomEvent();
 
   const [progress, setProgress] = useState(0);
 
-  const submitText = t("submitProgress.text");
-  const [message, setMessage] = useState(submitText);
-
   const handleProgressUpdate = (detail: { progress: number; message?: string }) => {
-    if (detail && detail.progress >= 0 && detail.progress <= 100) {
-      setProgress(detail.progress);
+    if (detail && detail.progress) {
+      setProgress(getPercentage(detail.progress));
       setMessage(detail.message || submitText);
+      setDisplaySpinner(false); // Show progress bar
+      return;
     }
+
+    setDisplaySpinner(true); // Show spinner
   };
 
   useEffect(() => {
@@ -45,7 +70,7 @@ export const SubmitProgress = ({ spinner = true }: { spinner?: boolean }) => {
       <GcFormsIcon />
       <div>
         <div className="mx-4 mb-3 font-bold">{message}</div>
-        {spinner ? <Spinner /> : <ProgressBar progress={progress} />}
+        {displaySpinner ? <Spinner /> : <ProgressBar progress={progress} />}
       </div>
     </div>
   );
