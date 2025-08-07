2f9abe611bc6b9d1bea2ec354360d0bd0ee54bf4
Tim Arney <tim@line37.com>
2025-08-07 13:04:40 -0400

    fix merge

--- constants.ts
+++ constants.ts
@@ -9,6 +9,4 @@ export const BODY_SIZE_LIMIT_WITH_FILES = Number(
 export const MAX_FILE_SIZE = 10485760; // 10 MB matches file upload lambda see: generateSignedUrl
 export const MAX_RESPONSE_SIZE = kbToBytes(380);
 
-export const MAX_FILE_SIZE = mbToBytes(10);
-
 export const MAX_DYNAMIC_ROW_AMOUNT = 25;
