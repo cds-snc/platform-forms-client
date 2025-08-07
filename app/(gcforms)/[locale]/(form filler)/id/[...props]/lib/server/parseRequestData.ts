85b18f73d3f3c461fd857f0e91832d23b6197848
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-08-07 14:03:56 -0400

    Refactor a bit for simplicity

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/server/parseRequestData.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/server/parseRequestData.ts
@@ -27,7 +27,7 @@ const isFileInputObj = (response: unknown): response is FileInputObj => {
  * This function takes a dynamic row element and an array of response values,
  * and fills in any missing sub-element values with empty strings.
  */
-const dynamicRowFiller = (element: FormElement, values: Responses[]) => {
+const dynamicRowFiller = (values: Responses[], element: FormElement) => {
   const subElements = element.properties.subElements || [];
 
   // If the values are not an array, we need to create an empty array for it
@@ -41,21 +41,13 @@ const dynamicRowFiller = (element: FormElement, values: Responses[]) => {
         return;
       }
 
-      if (subElement.type === FormElementTypes.fileInput) {
-        // If the sub-element is a file input, we need to ensure it has a valid structure
-        value[index] = fileInputFiller(value[index]);
-      }
-
-      if (subElement.type === FormElementTypes.checkbox) {
-        // Ensure checkboxes always return an array
-        value[index] = checkboxFiller(value[index]);
-      }
-
       // Check to see if the sub-element key exists in the response object
       if (!(index in value)) {
         // Add the missing sub-element key to the response value as an empty string
         value[index] = "";
       }
+
+      value[index] = fillData(value[index], subElement);
     });
   });
 
@@ -82,6 +74,22 @@ export const checkboxFiller = (value: Response) => {
   return value;
 };
 
+const fillData = (value: Response | Responses[], element: FormElement) => {
+  const fillers = {
+    dynamicRow: dynamicRowFiller,
+    checkbox: checkboxFiller,
+    fileInput: fileInputFiller,
+  };
+
+  const filler = fillers[element?.type as keyof typeof fillers];
+
+  if (!filler) {
+    return value;
+  }
+
+  return filler(value, element);
+};
+
 export const buildCompleteFormDataObject = (formRecord: PublicFormRecord, values: Responses) => {
   const originalValues = JSON.parse(JSON.stringify(values)) as Responses;
   const formData = { ...originalValues };
@@ -90,40 +98,24 @@ export const buildCompleteFormDataObject = (formRecord: PublicFormRecord, values
     // find the elements key in the formRecord.form.elements
     const element = formRecord.form.elements.find((el) => String(el.id) === key);
 
+    // If the element is not found, preserve user submitted value just in case
+    // * Should only be possible in draft forms.
     if (!element) {
-      formData[key] = ""; // should we discard?
+      formData[key] = originalValues[key];
+      return;
     }
 
-    if (Array.isArray(formData[key])) {
-      if (element?.type === FormElementTypes.dynamicRow) {
-        formData[key] = dynamicRowFiller(element, originalValues[key] as Responses[]);
-      }
-    }
-
-    if (element?.type === FormElementTypes.fileInput) {
-      formData[key] = fileInputFiller(originalValues[key]);
-    }
-
-    if (element?.type === FormElementTypes.checkbox) {
-      // Ensure checkboxes always return an array
-      formData[key] = checkboxFiller(originalValues[key]);
-    }
+    formData[key] = fillData(originalValues[key], element);
   });
 
   formRecord.form.elements.forEach((element) => {
+    if (element.type === FormElementTypes.richText) {
+      return;
+    }
+
     if (!formData[element.id]) {
-      if (
-        element.type === FormElementTypes.dynamicRow ||
-        element.type === FormElementTypes.checkbox
-      ) {
-        formData[element.id] = [];
-        return;
-      }
-      if (element.type === FormElementTypes.fileInput) {
-        formData[element.id] = fileInputFiller({});
-        return;
-      }
       formData[element.id] = "";
+      formData[element.id] = fillData(formData[element.id], element);
     }
   });
 
