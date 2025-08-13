00ef4c6a983e770aa1197379c28ed5e62f88a28b
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-08-13 14:36:47 -0400

    swap progress back to div element and use aria semantics instead

--- components/clientComponents/forms/SubmitProgress/ProgressBar.tsx
+++ components/clientComponents/forms/SubmitProgress/ProgressBar.tsx
@@ -1,10 +1,12 @@
 export const ProgressBar = ({ progress }: { progress: number }) => (
   <div className="h-2 w-full overflow-hidden rounded-full bg-slate-300">
-    <progress
+    <div
       className="h-full bg-indigo-500 transition-all duration-300"
       style={{ width: `${progress}%` }}
-      max="100"
-      value={progress}
+      role="progressbar"
+      aria-valuemax={100}
+      aria-valuemin={0}
+      aria-valuenow={progress}
     />
     <div className="sr-only" aria-live="polite">{`${progress}%`}</div>
   </div>
