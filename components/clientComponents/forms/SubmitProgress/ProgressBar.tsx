b059da3a7abba01bf36d90082e4ad331d61559c9
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-08-13 14:17:46 -0400

    add progress bar semantics

--- components/clientComponents/forms/SubmitProgress/ProgressBar.tsx
+++ components/clientComponents/forms/SubmitProgress/ProgressBar.tsx
@@ -1,8 +1,11 @@
 export const ProgressBar = ({ progress }: { progress: number }) => (
   <div className="h-2 w-full overflow-hidden rounded-full bg-slate-300">
-    <div
+    <progress
       className="h-full bg-indigo-500 transition-all duration-300"
       style={{ width: `${progress}%` }}
+      max="100"
+      value={progress}
     />
+    <div className="sr-only" aria-live="polite">{`${progress}%`}</div>
   </div>
 );
