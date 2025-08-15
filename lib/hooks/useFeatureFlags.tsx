219baaf51ff1a0b8f3539044bb066d26e5c8bf04
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-08-15 14:31:17 -0400

    add update function to flags hook

--- lib/hooks/useFeatureFlags.tsx
+++ lib/hooks/useFeatureFlags.tsx
@@ -4,9 +4,12 @@ import { Flags } from "@lib/cache/types";
 import { createContext, useContext, useState } from "react";
 import { useSession } from "next-auth/react";
 
+// Uses the checkAll as a server action
+import { checkAll } from "../cache/flags";
+
 const FeatureFlagsContext = createContext({
   flags: {} as Flags,
-  getFlag: () => {},
+  update: async () => {},
 });
 
 export const FeatureFlagsProvider = ({
@@ -17,7 +20,7 @@ export const FeatureFlagsProvider = ({
   featureFlags: Flags;
 }) => {
   const { data: session } = useSession();
-  const [flags] = useState(featureFlags);
+  const [flags, setFlags] = useState(featureFlags);
 
   const userFlags: string[] = session?.user?.featureFlags ?? [];
   // Loop through flags and set them to true if they are in the user's feature flags
@@ -27,18 +30,23 @@ export const FeatureFlagsProvider = ({
     }
   });
 
+  const update = async () => {
+    await checkAll().then((flags) => setFlags(flags));
+  };
+
   return (
-    <FeatureFlagsContext.Provider value={{ flags, getFlag: () => {} }}>
+    <FeatureFlagsContext.Provider value={{ flags, update }}>
       {children}
     </FeatureFlagsContext.Provider>
   );
 };
 
 export const useFeatureFlags = () => {
-  const { flags } = useContext(FeatureFlagsContext);
+  const { flags, update } = useContext(FeatureFlagsContext);
   return {
     getFlag: (key: string) => {
       return flags[key as keyof typeof flags];
     },
+    update,
   };
 };
