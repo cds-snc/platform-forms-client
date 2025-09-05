84dcbe3b70a0d8b15c32bd918963557909c3866b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-09-05 11:15:17 -0400

    Unnecessary barrel file

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/right-panel/headless-treeview/index.ts
+++
@@ -1,5 +0,0 @@
-export { TreeView } from "./TreeView";
-export { TreeItem } from "./TreeItem";
-export { createHeadlessDropHandler } from "./handlers/handleOnDrop";
-export { createHeadlessCanDropHandler } from "./handlers/handleCanDropAt";
-export type { TreeItemData, TreeItemInstance } from "./types";
