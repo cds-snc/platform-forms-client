df16b9a0b98643a7c1c53cd162d32f750b410434
Tim Arney <tim@line37.com>
2025-09-19 14:25:16 -0400

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       ? 4
       : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["list"]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
