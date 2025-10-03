5ce1dae322ed4b50a16eed19a1cacc8a20c7c313
Tim Arney <tim@line37.com>
2025-10-03 14:42:15 -0400

    sync

--- lib/store/useTemplateStore.tsx
+++ lib/store/useTemplateStore.tsx
@@ -9,9 +9,9 @@ import { useStoreWithEqualityFn } from "zustand/traditional";
 import { immer } from "zustand/middleware/immer";
 import { shallow } from "zustand/shallow";
 import { persist, subscribeWithSelector } from "zustand/middleware";
+import { syncStoreMiddleware, WebSocketTransportProvider } from "@ryanntannn/zustand-sync";
 import update from "lodash.set";
 import unset from "lodash.unset";
-
 /**
  * Internal dependencies
  */
@@ -73,96 +73,103 @@ const createTemplateStore = (
     immer(
       subscribeWithSelector(
         persist(
-          (set, get) => ({
-            ...props,
-            toggleLang: () =>
-              set((state) => {
-                state.lang = state.lang === "en" ? "fr" : "en";
-              }),
-            toggleTranslationLanguagePriority: () =>
-              set((state) => {
-                state.translationLanguagePriority =
-                  state.translationLanguagePriority === "en" ? "fr" : "en";
-              }),
-            getFocusInput: () => get().focusInput,
-            // Use on a child element to declare the language when the parent element lang attribute is different
-            getLocalizationAttribute: () =>
-              get().lang !== get().translationLanguagePriority
-                ? { lang: get().translationLanguagePriority }
-                : undefined,
-            updateField: (path, value) =>
-              set((state) => {
-                update(state, path, cleanInput(value));
-              }),
-            unsetField: (path) =>
-              set((state) => {
-                unset(state, path);
-              }),
-            localizeField: localizeField(set, get),
-            getPathString: getPathString(set, get),
-            propertyPath: propertyPath(set, get),
-            moveUp: moveUp(set),
-            moveDown: moveDown(set),
-            subMoveUp: subMoveUp(set),
-            subMoveDown: subMoveDown(set),
-            add: add(set, get),
-            addSubItem: addSubItem(set, get),
-            addChoice: addChoice(set),
-            addLabeledChoice: addLabeledChoice(set),
-            addSubChoice: addSubChoice(set),
-            removeChoiceFromRules: removeChoiceFromRules(set),
-            removeChoiceFromNextActions: removeChoiceFromNextActions(set),
-            remove: remove(set),
-            removeSubItem: removeSubItem(set),
-            removeChoice: removeChoice(set),
-            removeSubChoice: removeSubChoice(set),
-            getChoice: getChoice(set, get),
-            duplicateElement: duplicateElement(set, get),
-            initialize: initialize(set),
-            importTemplate: importTemplate(set),
-            getHighestElementId: getHighestElementId(set, get),
-            generateElementId: generateElementId(set, get),
-            transform: transform(set, get),
-            getSchema: () => {
-              // hasHydrated should work here but we get an error. leaving this timeout for now.
-              setTimeout(() => {
-                if (!get().hasTransformed) {
-                  get().transform();
-                }
-                set({ hasTransformed: true });
-              }, 500);
+          syncStoreMiddleware(
+            (set, get) => ({
+              ...props,
+              toggleLang: () =>
+                set((state) => {
+                  state.lang = state.lang === "en" ? "fr" : "en";
+                }),
+              toggleTranslationLanguagePriority: () =>
+                set((state) => {
+                  state.translationLanguagePriority =
+                    state.translationLanguagePriority === "en" ? "fr" : "en";
+                }),
+              getFocusInput: () => get().focusInput,
+              // Use on a child element to declare the language when the parent element lang attribute is different
+              getLocalizationAttribute: () =>
+                get().lang !== get().translationLanguagePriority
+                  ? { lang: get().translationLanguagePriority }
+                  : undefined,
+              updateField: (path, value) =>
+                set((state) => {
+                  update(state, path, cleanInput(value));
+                }),
+              unsetField: (path) =>
+                set((state) => {
+                  unset(state, path);
+                }),
+              localizeField: localizeField(set, get),
+              getPathString: getPathString(set, get),
+              propertyPath: propertyPath(set, get),
+              moveUp: moveUp(set),
+              moveDown: moveDown(set),
+              subMoveUp: subMoveUp(set),
+              subMoveDown: subMoveDown(set),
+              add: add(set, get),
+              addSubItem: addSubItem(set, get),
+              addChoice: addChoice(set),
+              addLabeledChoice: addLabeledChoice(set),
+              addSubChoice: addSubChoice(set),
+              removeChoiceFromRules: removeChoiceFromRules(set),
+              removeChoiceFromNextActions: removeChoiceFromNextActions(set),
+              remove: remove(set),
+              removeSubItem: removeSubItem(set),
+              removeChoice: removeChoice(set),
+              removeSubChoice: removeSubChoice(set),
+              getChoice: getChoice(set, get),
+              duplicateElement: duplicateElement(set, get),
+              initialize: initialize(set),
+              importTemplate: importTemplate(set),
+              getHighestElementId: getHighestElementId(set, get),
+              generateElementId: generateElementId(set, get),
+              transform: transform(set, get),
+              getSchema: () => {
+                // hasHydrated should work here but we get an error. leaving this timeout for now.
+                setTimeout(() => {
+                  if (!get().hasTransformed) {
+                    get().transform();
+                  }
+                  set({ hasTransformed: true });
+                }, 500);
 
-              return JSON.stringify(getSchemaFromState(get(), get().allowGroupsFlag), null, 2);
-            },
-            getId: () => get().id,
-            getIsPublished: () => get().isPublished,
-            getFormElementById: getFormElementById(set, get),
-            getFormElementWithIndexById: getFormElementWithIndexById(set, get),
-            getFormElementIndexes: getFormElementIndexes(set, get),
-            getName: () => get().name,
-            getDeliveryOption: () => get().deliveryOption,
-            getSecurityAttribute: () => get().securityAttribute,
-            getGroupsEnabled: () => get().allowGroupsFlag,
-            setChangeKey: (key: string) => set({ changeKey: key }),
-            setHasHydrated: () => set({ hasHydrated: true }),
-            setId: (id) => set({ id }),
-            setLang: (lang) => set({ lang }),
-            setTranslationLanguagePriority: (lang: Language) =>
-              set({ translationLanguagePriority: lang }),
-            setFocusInput: (isSet) => set({ focusInput: isSet }),
-            setIsPublished: (isPublished) => set({ isPublished }),
-            setClosingDate: (value) => set({ closingDate: value }),
-            setSaveAndResume: (value) => set({ saveAndResume: value }),
-            setNotificationsInterval: (value) => set({ notificationsInterval: value }),
-            setGroupsLayout: (layout) => {
-              set((state) => {
-                state.form.groupsLayout = layout;
-              });
-            },
-            updateSecurityAttribute: (value) => set({ securityAttribute: value }),
-            resetDeliveryOption: () => set({ deliveryOption: undefined }),
-            setHasTransformed: () => set({ hasTransformed: true }),
-          }),
+                return JSON.stringify(getSchemaFromState(get(), get().allowGroupsFlag), null, 2);
+              },
+              getId: () => get().id,
+              getIsPublished: () => get().isPublished,
+              getFormElementById: getFormElementById(set, get),
+              getFormElementWithIndexById: getFormElementWithIndexById(set, get),
+              getFormElementIndexes: getFormElementIndexes(set, get),
+              getName: () => get().name,
+              getDeliveryOption: () => get().deliveryOption,
+              getSecurityAttribute: () => get().securityAttribute,
+              getGroupsEnabled: () => get().allowGroupsFlag,
+              setChangeKey: (key: string) => set({ changeKey: key }),
+              setHasHydrated: () => set({ hasHydrated: true }),
+              setId: (id) => set({ id }),
+              setLang: (lang) => set({ lang }),
+              setTranslationLanguagePriority: (lang: Language) =>
+                set({ translationLanguagePriority: lang }),
+              setFocusInput: (isSet) => set({ focusInput: isSet }),
+              setIsPublished: (isPublished) => set({ isPublished }),
+              setClosingDate: (value) => set({ closingDate: value }),
+              setSaveAndResume: (value) => set({ saveAndResume: value }),
+              setNotificationsInterval: (value) => set({ notificationsInterval: value }),
+              setGroupsLayout: (layout) => {
+                set((state) => {
+                  state.form.groupsLayout = layout;
+                });
+              },
+              updateSecurityAttribute: (value) => set({ securityAttribute: value }),
+              resetDeliveryOption: () => set({ deliveryOption: undefined }),
+              setHasTransformed: () => set({ hasTransformed: true }),
+            }),
+            {
+              transport: WebSocketTransportProvider<TemplateStoreState>({
+                projectId: props.id,
+              }),
+            }
+          ),
           storageOptions
         )
       )
