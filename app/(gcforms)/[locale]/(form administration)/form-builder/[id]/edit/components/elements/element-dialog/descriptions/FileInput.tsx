bbf2f3fe0284ddb1e4153e4a0081e6d028dd0af0
Tim Arney <tim@line37.com>
2025-10-15 08:17:52 -0400

    remove flag

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/element-dialog/descriptions/FileInput.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/element-dialog/descriptions/FileInput.tsx
@@ -7,7 +7,6 @@ import { ExampleWrapper } from "./ExampleWrapper";
 import { Label, FileInput as FileInputComponent } from "@clientComponents/forms";
 import { useFormBuilderConfig } from "@lib/hooks/useFormBuilderConfig";
 import { useTemplateStore } from "@lib/store/useTemplateStore";
-import { BetaBadge } from "@clientComponents/globals/BetaBadge";
 
 export const FileInput = ({ title }: { title: string }) => {
   const { hasApiKeyId } = useFormBuilderConfig();
@@ -32,30 +31,6 @@ const Title = ({ title }: { title: string }) => {
   return (
     <div className="mb-4 flex items-center space-x-3">
       <h3 className="mb-0">{title}</h3>
-      <BetaBadge />
-    </div>
-  );
-};
-
-export const FileInputTrialDescription = () => {
-  const { translationLanguagePriority, id } = useTemplateStore((s) => ({
-    id: s.id,
-    translationLanguagePriority: s.translationLanguagePriority,
-  }));
-
-  const link = `/${translationLanguagePriority}/form-builder/${id}/settings`;
-
-  const { t } = useTranslation("form-builder");
-  return (
-    <div>
-      <p className="mb-4">{t("addElementDialog.fileInputWithApi.trialFeature.text1")}</p>
-      <ul className="mb-8">
-        <li>
-          <Link href={link}>{t("addElementDialog.fileInputWithApi.trialFeature.bullet1")}</Link>
-        </li>
-        <li>{t("addElementDialog.fileInputWithApi.trialFeature.bullet2")}</li>
-        <li>{t("addElementDialog.fileInputWithApi.trialFeature.bullet3")}</li>
-      </ul>
     </div>
   );
 };
@@ -72,11 +47,6 @@ const WithApiDescription = ({ title }: { title: string }) => {
         </Label>
         <FileInputComponent label="title" id="name" name={"name"} className="mb-0" />
       </ExampleWrapper>
-
-      <h4 className="mb-2 font-medium">
-        {t("addElementDialog.fileInputWithApi.trialFeature.title")}
-      </h4>
-      <FileInputTrialDescription />
     </div>
   );
 };
