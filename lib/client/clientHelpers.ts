003117977da9f0bad8d14e9595852b46f0a26b87
Clément Janin <clement.janin@cds-snc.ca>
2025-10-16 12:00:16 -0400

    fix: window object detection in Google Analytics function was not working as intended

--- lib/client/clientHelpers.ts
+++ lib/client/clientHelpers.ts
@@ -172,12 +172,11 @@ export const truncateString = (str: string, maxLength: number = 50): string => {
 
 // Google Analytics util to simplify firing an event
 export const ga = (eventName: string, data?: object) => {
-  if (!window || !eventName) {
-    return;
+  if (typeof window !== "undefined") {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: eventName,
+      ...data,
+    });
   }
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    event: eventName,
-    ...data,
-  });
 };
