34a113c4987762cd175d2e22cd37c1865dcee813
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-10-22 10:30:25 -0400

    remove manual invoke of db seed

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from "cypress";
-import { logMessage } from "@lib/logger";
 
 export default defineConfig({
   video: false,
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on) {
-      on("before:run", async () => {
-        const dbTearDown = await import("./__utils__/dbTearDown");
-        logMessage.info("Tearing down database");
-        await dbTearDown.default();
-        logMessage.info("Seeding database");
-        const dbSeed = await import("./packages/database/src/seed");
-        await dbSeed.default("test");
-      });
-    },
   },
   component: {
     devServer: {
--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -1,16 +1,7 @@
 import { FullConfig } from "@playwright/test";
-import { logMessage } from "@lib/logger";
-import dbTearDown from "../__utils__/dbTearDown";
-import dbSeed from "@gcforms/database/src/seed";
 
 async function globalSetup(_config: FullConfig) {
-  logMessage.info("Playwright Global Setup: Tearing down database");
-  await dbTearDown();
-
-  logMessage.info("Playwright Global Setup: Seeding database");
-  await dbSeed("test");
-
-  logMessage.info("Playwright Global Setup: Complete");
+  // Any additional setup for future requirements
 }
 
 export default globalSetup;
