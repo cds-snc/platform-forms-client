138095e9460044bc7877ecbd9c3559cc18126ae3
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-10-30 13:32:11 -0400

    Remove old files/directory

--- app/(gcforms)/[locale]/(form administration)/api_integration/components/ResponsesAvailable.tsx
+++
@@ -1,8 +0,0 @@
-import { Success } from "@clientComponents/globals/Alert/Alert";
-
-export const ResponsesAvailable = ({ message }: { message: string | null }) => {
-  if (message) {
-    return <Success className="mb-4" title="New Responses Available" body={message} />;
-  }
-  return null;
-};
