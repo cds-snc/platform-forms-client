4a52263d92e425b6a70eb4ea3af1f7607981558d
Tim Arney <tim@line37.com>
2025-10-30 08:21:25 -0400

    init setup

---
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-beta/page.tsx
@@ -0,0 +1,51 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import { serverTranslation } from "@i18n";
+
+import { authCheckAndThrow } from "@lib/actions";
+import { featureFlagAllowedForUser } from "@lib/userFeatureFlags";
+import { FeatureFlags } from "@root/lib/cache/types";
+import { logMessage } from "@lib/logger";
+
+export async function generateMetadata(props: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const params = await props.params;
+
+  const { locale } = params;
+
+  const { t } = await serverTranslation("form-builder", { lang: locale });
+  return {
+    title: `${t("gcFormsPublished")} — ${t("gcForms")}`,
+  };
+}
+
+export default async function Page(props: { params: Promise<{ locale: string; id: string }> }) {
+  const params = await props.params;
+
+  const { locale, id } = params;
+
+  const { session } = await authCheckAndThrow().catch(() => ({
+    session: null,
+  }));
+
+  if (!session) {
+    return null;
+  }
+
+  if (!featureFlagAllowedForUser(session.user.id, FeatureFlags.responsesBeta)) {
+    logMessage.info(
+      `User ${session.user.id} attempted to access Responses Beta page without the feature flag enabled.`
+    );
+
+    redirect(`/${locale}/form-builder/${id}/responses`);
+  }
+
+  return (
+    <div>
+      <p>
+        Responses Beta Page for form {id} in locale {locale}
+      </p>
+    </div>
+  );
+}
