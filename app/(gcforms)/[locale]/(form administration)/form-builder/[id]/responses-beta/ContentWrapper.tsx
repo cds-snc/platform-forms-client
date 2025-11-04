32c918489ddc35c024553d6255117073eda4b209
Tim Arney <tim@line37.com>
2025-11-04 09:39:09 -0500

    remove unused toast

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-beta/ContentWrapper.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-beta/ContentWrapper.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { ToastContainer } from "@formBuilder/components/shared/Toast";
-
 export const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="account-wrapper">
       <div className="w-[750px] rounded-2xl border-1 border-[#D1D5DB] bg-white p-10">
         <main id="content w-full">
           <div>{children}</div>
-          <ToastContainer autoClose={false} containerId="response-api" />
         </main>
       </div>
     </div>
