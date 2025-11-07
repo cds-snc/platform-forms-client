64ac0b68da1bfea60026607385cecbf4d8e884a0
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-11-07 13:59:23 -0500

    clean up for rebase

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/actions.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/actions.ts
@@ -10,10 +10,10 @@ import { verifyHCaptchaToken } from "@lib/validation/hCaptcha";
 import { checkOne } from "@lib/cache/flags";
 import { FeatureFlags } from "@lib/cache/types";
 import { dateHasPast } from "@lib/utils";
-import { validateResponses } from "@lib/validation/validation";
 import { validateOnSubmit } from "@gcforms/core";
 import { serverTranslation } from "@root/i18n";
 import { sendNotifications } from "@lib/notifications";
+import { otel } from "@lib/otel";
 
 // Public facing functions - they can be used by anyone who finds the associated server action identifer
 
@@ -31,99 +31,93 @@ export async function submitForm(
   values: Responses,
   language: string,
   formRecordOrId: PublicFormRecord | string,
-  captchaToken?: string | undefined
+  captchaToken?: string | undefined,
+  fileChecksums?: Record<string, string>
 ): Promise<{
   id: string;
   submissionId?: string;
   error?: Error;
   fileURLMap?: SignedURLMap;
 }> {
-  const formId = typeof formRecordOrId === "string" ? formRecordOrId : formRecordOrId.id;
+  return otel("submitForm", async () => {
+    const formId = typeof formRecordOrId === "string" ? formRecordOrId : formRecordOrId.id;
 
-  try {
-    const template = await getPublicTemplateByID(formId);
+    try {
+      const template = await getPublicTemplateByID(formId);
 
-    if (!template) {
-      throw new Error(`Could not find any form associated to identifier ${formId}`);
-    }
+      if (!template) {
+        throw new Error(`Could not find any form associated to identifier ${formId}`);
+      }
 
-    if (template.closingDate && dateHasPast(Date.parse(template.closingDate))) {
-      return {
-        id: formId,
-        error: { name: FormStatus.FORM_CLOSED_ERROR, message: "Form is closed" },
-      };
-    }
-
-    const hCaptchaBlockingMode = await checkOne(FeatureFlags.hCaptcha);
-    // Skip hCaptcha verification for form-builder Preview (drafts)
-    if (template?.isPublished && process.env.APP_ENV !== "test") {
-      // hCaptcha runs regardless but only block submissions if the feature flag is enabled
-      const captchaVerified = await verifyHCaptchaToken(captchaToken || "");
-      if (hCaptchaBlockingMode && !captchaVerified) {
+      if (template.closingDate && dateHasPast(Date.parse(template.closingDate))) {
         return {
           id: formId,
           error: {
-            name: FormStatus.CAPTCHA_VERIFICATION_ERROR,
-            message: "Captcha verification failure",
+            name: FormStatus.FORM_CLOSED_ERROR,
+            message: "Form is closed",
           },
         };
       }
-    }
 
-    /**
-     * This validation checks the response values against the template element types.
-     */
-    const validateResponsesResult = await validateResponses(values, template);
+      const hCaptchaBlockingMode = await checkOne(FeatureFlags.hCaptcha);
+      // Skip hCaptcha verification for form-builder Preview (drafts)
+      if (template?.isPublished && process.env.APP_ENV !== "test") {
+        // hCaptcha runs regardless but only block submissions if the feature flag is enabled
+        const captchaVerified = await verifyHCaptchaToken(captchaToken || "", formId);
+        if (hCaptchaBlockingMode && !captchaVerified) {
+          return {
+            id: formId,
+            error: {
+              name: FormStatus.CAPTCHA_VERIFICATION_ERROR,
+              message: "Captcha verification failure",
+            },
+          };
+        }
+      }
 
-    if (Object.keys(validateResponsesResult).length !== 0) {
-      // See: https://gcdigital.slack.com/archives/C05G766KW49/p1737063028759759
-      logMessage.info(
-        `[server-action][submitForm] Detected invalid response(s) in submission on form ${formId}. Errors: ${JSON.stringify(
-          validateResponsesResult
-        )}`
+      const { t } = await serverTranslation();
+
+      /**
+       * This validation runs the client-side validation on the server.
+       */
+      const validateOnSubmitResult = validateOnSubmit(values, {
+        formRecord: template,
+        t: t,
+      });
+
+      if (Object.keys(validateOnSubmitResult).length !== 0) {
+        logMessage.info(
+          `[server-action][submitForm] Detected validation errors on form ${formId}. Errors: ${JSON.stringify(
+            validateOnSubmitResult
+          )}`
+        );
+        // Keeping in "passive mode" for now.
+        // Uncomment following line to throw validation error from server.
+        // throw new MissingFormDataError("Form data validation failed");
+      }
+
+      const formData = normalizeFormResponses(template, values);
+
+      const { submissionId, fileURLMap } = await processFormData({
+        responses: formData,
+        securityAttribute: template.securityAttribute,
+        formId,
+        language,
+        fileChecksums,
+      });
+
+      sendNotifications(formId, template.form.titleEn, template.form.titleFr);
+
+      return { id: formId, submissionId, fileURLMap };
+    } catch (e) {
+      logMessage.error(
+        `Could not submit response for form ${formId}. Received error: ${(e as Error).message}`
       );
-      // Turn this on after we've monitored the logs for a while
-      // throw new MissingFormDataError("Form data validation failed");
+
+      return {
+        id: formId,
+        error: { name: (e as Error).name, message: (e as Error).message },
+      };
     }
-
-    const { t } = await serverTranslation();
-
-    /**
-     * This validation runs the client-side validation on the server.
-     */
-    const validateOnSubmitResult = validateOnSubmit(values, {
-      formRecord: template,
-      t: t,
-    });
-
-    if (Object.keys(validateOnSubmitResult).length !== 0) {
-      logMessage.info(
-        `[server-action][submitForm] Detected validation errors on form ${formId}. Errors: ${JSON.stringify(
-          validateOnSubmitResult
-        )}`
-      );
-      // Keeping in "passive mode" for now.
-      // Uncomment following line to throw validation error from server.
-      // throw new MissingFormDataError("Form data validation failed");
-    }
-
-    const formData = normalizeFormResponses(template, values);
-
-    const { submissionId, fileURLMap } = await processFormData({
-      responses: formData,
-      securityAttribute: template.securityAttribute,
-      formId,
-      language,
-    });
-
-    sendNotifications(formId, template.form.titleEn, template.form.titleFr);
-
-    return { id: formId, submissionId, fileURLMap };
-  } catch (e) {
-    logMessage.error(
-      `Could not submit response for form ${formId}. Received error: ${(e as Error).message}`
-    );
-
-    return { id: formId, error: { name: (e as Error).name, message: (e as Error).message } };
-  }
+  });
 }
