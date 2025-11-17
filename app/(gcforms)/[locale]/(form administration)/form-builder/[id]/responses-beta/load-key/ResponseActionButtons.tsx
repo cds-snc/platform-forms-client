601deefc5761cb23e1e442bc49d64d28a4315796
Tim Arney <timarney@users.noreply.github.com>
2025-11-17 10:16:41 -0500

    chore: check responses button (#6362)
    * breakout check responses button
    
    * add checking
    
    * cleanup text

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-beta/load-key/ResponseActionButtons.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-beta/load-key/ResponseActionButtons.tsx
@@ -3,12 +3,12 @@ import { useRouter } from "next/navigation";
 
 import { useTranslation } from "@i18n/client";
 import { useResponsesContext } from "../context/ResponsesContext";
+import { CheckForResponsesButton } from "../components/CheckForResponsesButton";
 
 export const ResponseActionButtons = () => {
   const { t } = useTranslation(["response-api", "common"]);
   const router = useRouter();
-  const { apiClient, retrieveResponses, newFormSubmissions, resetState, locale, formId } =
-    useResponsesContext();
+  const { apiClient, newFormSubmissions, resetState, locale, formId } = useResponsesContext();
 
   const handleBack = () => {
     resetState();
@@ -20,10 +20,6 @@ export const ResponseActionButtons = () => {
     router.push(`/${locale}/form-builder/${formId}/responses-beta/location`);
   };
 
-  const handleCheck = async () => {
-    void retrieveResponses();
-  };
-
   return (
     <div className="mt-8 flex flex-row gap-4">
       <Button theme="secondary" onClick={handleBack}>
@@ -31,9 +27,7 @@ export const ResponseActionButtons = () => {
       </Button>
 
       {apiClient && newFormSubmissions && newFormSubmissions.length === 0 ? (
-        <Button theme="primary" onClick={handleCheck}>
-          {t("loadKeyPage.checkForNewResponsesButton")}
-        </Button>
+        <CheckForResponsesButton />
       ) : (
         <Button
           theme="primary"
