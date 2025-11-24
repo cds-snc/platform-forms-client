2ecc1693a9365f746452f5b30697948b97ae2f20
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-11-24 07:46:07 -0500

    use default url for prisma while building

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+const databaseURL = process.env.DATABASE_URL ?? "postgres://postgres:chummy@localhost:5432/forms";
 
 export default defineConfig({
   datasource: {
-    url: env("DATABASE_URL"),
+    url: databaseURL,
   },
   schema: "prisma/schema.prisma",
   migrations: {
