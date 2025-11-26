63d024882f4d48aaabb9a533e2c0fa68220a2cf1
Tim Arney <timarney@users.noreply.github.com>
2025-11-26 12:00:11 -0500

    chore: flatten api key download - just use the json file (#6444)
    remove api zip

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/actions.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/settings/actions.ts
@@ -2,8 +2,6 @@
 
 import { createKey, deleteKey, refreshKey } from "@lib/serviceAccount";
 import { revalidatePath } from "next/cache";
-import { promises as fs } from "fs";
-import path from "path";
 import { AuthenticatedAction } from "@lib/actions";
 
 import { submissionTypeExists } from "@lib/vault";
@@ -11,16 +9,6 @@ import { VaultStatus } from "@lib/types";
 import { ServerActionError } from "@root/lib/types/form-builder-types";
 
 // Public facing functions - they can be used by anyone who finds the associated server action identifer
-
-export const getReadmeContent = AuthenticatedAction(async () => {
-  try {
-    const readmePath = path.join(process.cwd(), "./public/static/api/Readme.md");
-    const content = await fs.readFile(readmePath, "utf-8");
-    return { content };
-  } catch (e) {
-    return { error: true };
-  }
-});
 
 export const createServiceAccountKey = AuthenticatedAction(async (_, templateId: string) => {
   revalidatePath(
