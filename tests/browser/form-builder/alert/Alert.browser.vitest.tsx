575bd9df2f643e16a2a1f050541510fa6232ada6
Tim Arney <tim@line37.com>
2025-12-05 14:26:40 -0500

    clean

--- tests/browser/form-builder/alert/Alert.browser.vitest.tsx
+++ tests/browser/form-builder/alert/Alert.browser.vitest.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeAll } from "vitest";
-import { page, userEvent } from "@vitest/browser/context";
+import { page } from "@vitest/browser/context";
 import { Alert } from "@clientComponents/globals";
 import { CircleCheckIcon } from "@serverComponents/icons/CircleCheckIcon";
 import { Button } from "@clientComponents/globals";
