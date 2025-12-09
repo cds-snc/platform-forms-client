dbe76fb5ba3d768c4d0d556af7e62baa52313f37
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-12-09 14:10:24 -0500

    Initial commit

--- packages/connectors/CHANGELOG.md
+++ packages/connectors/CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.2.14] - 2025-12-09
+
+- Add unitility function to get AWS SQS Queue URL
+- Add utility functions for notification
+
 ## [2.2.13] - 2025-10-27
 
 - Update AWS SDK package from version `3.917.0`
---
+++ packages/connectors/src/notification-connector.ts
@@ -0,0 +1,75 @@
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { getAwsSQSQueueURL } from "./getAwsSQSQueueURL";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const globalConfig = {
+  region: process.env.AWS_REGION ?? "ca-central-1",
+};
+
+const sqsClient = new SQSClient({
+  ...globalConfig,
+});
+
+export const dynamoDBDocumentClient = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    ...globalConfig,
+    // SDK retries use exponential backoff with jitter by default
+    maxAttempts: 15,
+  })
+);
+
+const DYNAMODB_NOTIFICATION_TABLE_NAME = "Notification";
+
+export const createNotificationRecord = async ({
+  notificationId,
+  emails,
+  subject,
+  body,
+}: {
+  notificationId: string;
+  emails: string[];
+  subject: string;
+  body: string;
+}): Promise<void> => {
+  try {
+    const ttl = Math.floor(Date.now() / 1000) + 86400; // 24 hours from now
+    const command = new PutCommand({
+      TableName: DYNAMODB_NOTIFICATION_TABLE_NAME,
+      Item: {
+        NotificationID: notificationId,
+        Emails: emails,
+        Subject: subject,
+        Body: body,
+        TTL: ttl,
+      },
+    });
+    await dynamoDBDocumentClient.send(command);
+  } catch (error) {
+    throw new Error(
+      `Could not create deferred notification id ${notificationId} + ${JSON.stringify(error)}`
+    );
+  }
+};
+
+export const enqueueDeferredNotification = async (notificationId: string) => {
+  try {
+    const queueUrl = await getAwsSQSQueueURL("NOTIFICATION_QUEUE_URL", "notification_queue");
+    if (!queueUrl) {
+      throw new Error("Notification Queue not connected");
+    }
+
+    const command = new SendMessageCommand({
+      MessageBody: JSON.stringify({ notificationId }),
+      QueueUrl: queueUrl,
+    });
+    const sendMessageCommandOutput = await sqsClient.send(command);
+    if (!sendMessageCommandOutput.MessageId) {
+      throw new Error("Received null SQS message identifier");
+    }
+  } catch (error) {
+    throw new Error(
+      `Could not enqueue deferred notification id ${notificationId} + ${JSON.stringify(error)}`
+    );
+  }
+};
