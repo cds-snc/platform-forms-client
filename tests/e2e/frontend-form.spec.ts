0e640ebe4a0edbeb58fc81635c9e9de38b2d39b1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2025-12-11 14:59:50 -0500

    Improved frontend form e2e test

---
+++ tests/e2e/frontend-form.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect } from "@playwright/test";
+import { DatabaseHelper } from "../helpers/database-helper";
+
+test.describe("Testing a basic frontend form", () => {
+  let publishedFormPath: string;
+  let formId: string;
+  let dbHelper: DatabaseHelper;
+
+  test.beforeAll(async () => {
+    // Create a published template directly in the database
+    dbHelper = new DatabaseHelper();
+    formId = await dbHelper.createPublishedTemplate("kitchenSinkTestForm");
+    publishedFormPath = `en/id/${formId}`;
+  });
+
+  test.afterAll(async () => {
+    // Clean up: delete the template and disconnect
+    if (formId) {
+      await dbHelper.deleteTemplate(formId);
+    }
+    await dbHelper.disconnect();
+  });
+
+  test("Renders properly", async ({ page }) => {
+    await page.goto(publishedFormPath);
+
+    // Check that the attestation text is displayed
+    await expect(page.locator("body")).toContainText("A Kitchen Sink Form");
+  });
+
+  test("Has correct autocomplete attributes on fields", async ({ page }) => {
+    await page.goto(publishedFormPath);
+
+    const autocompleteOptions = [
+      ["additional-name", "Middle name"],
+      ["address-level1", "Province"],
+      ["address-level2", "City"],
+      ["address-line1", "Address line 1"],
+      ["address-line2", "Address line 2"],
+      ["address-line3", "Address line 3"],
+      ["bday", "Date of birth"],
+      ["bday-day", "Birth day"],
+      ["bday-month", "Birth month"],
+      ["bday-year", "Birth year"],
+      ["country", "Country code"],
+      ["country-name", "Country"],
+      ["email", "Email address"],
+      ["family-name", "Last name"],
+      ["given-name", "First name"],
+      ["honorific-prefix", "Name prefix"],
+      ["honorific-suffix", "Name suffix"],
+      ["language", "Language"],
+      ["name", "Full name"],
+      ["organization-title", "Job title"],
+      ["tel", "Phone number"],
+      ["postal-code", "Postal code"],
+      ["url", "Website address"],
+    ];
+
+    for (const [autocompleteValue, description] of autocompleteOptions) {
+      const locator = page.locator(`input[autocomplete='${autocompleteValue}']`);
+      // eslint-disable-next-line no-await-in-loop
+      await expect(
+        locator,
+        `Field with autocomplete='${autocompleteValue}' (${description})`
+      ).toHaveCount(1);
+    }
+  });
+});
