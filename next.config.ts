e446c079fb7f4c5198d30f2b58a6819ee8a99456
Tim Arney <tim@line37.com>
2026-01-02 09:43:31 -0500

    deployment id for local

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  deploymentId: process.env.NEXT_DEPLOYMENT_ID,
+  deploymentId: process.env.NEXT_DEPLOYMENT_ID ?? "local",
   reactCompiler: {
     compilationMode: "annotation",
   },
