3a95e0a9dea7f31a2449e6185014fa8746088001
Vivian Nobrega <43480681+ShadeWyrm@users.noreply.github.com>
2026-01-05 13:49:03 -0500

    feat: Decimal Functionality for Number Fields (#6589)
    * * Basic decimal functionality for numbers, fix for Numbers options showing when they shouldn't.
    
    * * Ensure data goes into the system with english structure.
    
    * * Translate output for non-JSON output.
    
    * * Handle language swap for numeric values.
    
    * * Update to decimal operator for CSV out
    
    * i18n update
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
    
    * * add a bit of spacing.
    
    ---------
    
    Co-authored-by: Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>

--- packages/types/src/form-types.ts
+++ packages/types/src/form-types.ts
@@ -131,6 +131,7 @@ export interface ElementProperties {
   autoComplete?: string;
   dateFormat?: string;
   allowNegativeNumbers?: boolean;
+  stepCount?: number;
   conditionalRules?: ConditionalRule[];
   full?: boolean;
   addressComponents?: AddressComponents | undefined;
