ac7fb9a2e3feef4e51119432ebc5c16799912922
Tim Arney <tim@line37.com>
2026-01-07 10:46:39 -0500

    update indicator

--- app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/conditionals/ConditionalIndicator.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/components/shared/conditionals/ConditionalIndicator.tsx
@@ -1,49 +1,11 @@
 "use client";
-import React from "react";
-import { useTranslation } from "@i18n/client";
-import { FormElementWithIndex } from "@lib/types/form-builder-types";
-import { useTemplateStore } from "@lib/store/useTemplateStore";
-import { ConditionalIcon } from "@serverComponents/icons/ConditionalIcon";
-import { getQuestionNumber, sortByLayout } from "@lib/utils/form-builder";
-import { getElementIndexes } from "@lib/utils/form-builder/getPath";
 
-const RuleIndicator = ({ choiceId }: { choiceId: string }) => {
-  const { t } = useTranslation("form-builder");
-  const getChoice = useTemplateStore((state) => state.getChoice);
-  const layout = useTemplateStore((state) => state.form.layout);
-
-  const translationLanguagePriority = useTemplateStore(
-    (state) => state.translationLanguagePriority
-  );
-
-  const elements = useTemplateStore((state) => state.form.elements);
-  // Ensure elements are sorted by layout
-  const sortedElements = sortByLayout({ layout, elements: [...elements] });
-  const parentId = Number(choiceId.split(".")[0]);
-  const childId = Number(choiceId.split(".")[1]);
-  const choice = getChoice(parentId, childId);
-  if (!choice) return null;
-  const choiceValue = choice[translationLanguagePriority];
-  const indexes = getElementIndexes(parentId, elements);
-  if (!indexes || !indexes[0]) return null;
-  const itemId = indexes[0];
-  const questionNumber = getQuestionNumber(elements[itemId], sortedElements);
-  // For debugging
-  const elementId = elements[itemId]?.id;
-  return (
-    <div data-element-id={elementId}>
-      <ConditionalIcon className="mr-2 mt-[-5px] inline-block" />
-      <div className="inline-block p-2">
-        {t("question")} {questionNumber}
-      </div>
-      <span className="hidden">{`${choiceValue}`}</span>
-    </div>
-  );
-};
+import { type FormElementWithIndex } from "@lib/types/form-builder-types";
+import { RuleIndicator } from "./RuleIndicator";
 
 export const ConditionalIndicator = ({ item }: { item: FormElementWithIndex }) => {
+  "use memo";
   const hasConditionalRules = item.properties.conditionalRules;
-
   if (!hasConditionalRules || !item.properties.conditionalRules) return null;
 
   const rules = item.properties.conditionalRules?.map((rule) => {
