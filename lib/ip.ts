eb74e5f67535947215d6bdf6abc6684dfa29ceca
Clément Janin <clement.janin@cds-snc.ca>
2026-01-09 15:44:17 -0500

    chore: get client IP from request headers once modified by AWS load balancer

--- lib/ip.ts
+++ lib/ip.ts
@@ -1,12 +1,19 @@
 import { headers } from "next/headers";
 
-// See https://nextjs.org/docs/app/api-reference/functions/headers#ip-address
-export async function getClientIP() {
-  const FALLBACK_IP_ADDRESS = "0.0.0.0";
-  const head = await headers();
-  const forwardedFor = head.get("x-forwarded-for");
-  if (forwardedFor) {
-    return forwardedFor.split(",")[0] ?? FALLBACK_IP_ADDRESS;
+const FALLBACK_IP_ADDRESS = "0.0.0.0";
+
+export async function getClientIP(): Promise<string> {
+  const requestHeaders = await headers();
+
+  const xForwardedForHeader = requestHeaders.get("x-forwarded-for");
+
+  if (xForwardedForHeader !== null) {
+    /**
+     * Only consider last IP as the source of truth as it has been added by AWS ECS Load balancer
+     * See https://docs.aws.amazon.com/elasticloadbalancing/latest/application/x-forwarded-headers.html#x-forwarded-for-append
+     */
+    return xForwardedForHeader.split(",").at(-1) ?? FALLBACK_IP_ADDRESS;
   }
-  return head.get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+
+  return FALLBACK_IP_ADDRESS;
 }
