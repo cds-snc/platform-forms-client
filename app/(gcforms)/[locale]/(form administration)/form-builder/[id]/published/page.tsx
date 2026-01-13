a56ae05b547f46d0aad4f136ecd8e655611ab95d
Tim Arney <tim@line37.com>
2026-01-13 12:46:28 -0500

    move published to server

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/published/page.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/published/page.tsx
@@ -1,8 +1,14 @@
+import { ViewTransition } from "react";
 import { serverTranslation } from "@i18n";
 import { authCheckAndThrow } from "@lib/actions";
 import { Metadata } from "next";
-import { ClientContainer } from "./ClientContainer";
-import { Published } from "./Published";
+import { redirect } from "next/navigation";
+import { getFullTemplateByID } from "@lib/templates";
+
+import { RocketIcon } from "@serverComponents/icons/RocketIcon";
+import { LinkButton } from "@serverComponents/globals/Buttons/LinkButton";
+import { getOrigin } from "@lib/origin";
+import Markdown from "markdown-to-jsx";
 
 export async function generateMetadata(props: {
   params: Promise<{ locale: string }>;
@@ -19,26 +25,72 @@ export async function generateMetadata(props: {
 
 export default async function Page(props: { params: Promise<{ locale: string; id: string }> }) {
   const params = await props.params;
-
   const { locale, id } = params;
 
-  const { session, ability } = await authCheckAndThrow().catch(() => ({
+  const {
+    t,
+    i18n: { language },
+  } = await serverTranslation("form-builder", { lang: locale });
+
+  const { session } = await authCheckAndThrow().catch(() => ({
     session: null,
     ability: null,
   }));
 
+  const template = await getFullTemplateByID(id);
+
   if (!session) {
-    return null;
+    redirect(`/${locale}/auth/login`);
   }
 
-  try {
-    const canView = ability?.can("view", "FormRecord");
-    return (
-      <ClientContainer>
-        <Published id={id} locale={locale} canView={canView} />
-      </ClientContainer>
-    );
-  } catch (e) {
-    return null;
+  if (!template || !template.isPublished) {
+    redirect(`/${locale}/form-builder/${id}/publish`);
   }
+
+  const baseUrl = await getOrigin();
+
+  const linkEn = `${baseUrl}/en/id/${id}`;
+  const linkFr = `${baseUrl}/fr/id/${id}`;
+
+  return (
+    <ViewTransition>
+      <div className="mr-10">
+        <h1 className="visually-hidden">{t("published")}</h1>
+        <div className="mb-10 flex bg-purple-200 p-7">
+          <div className="flex">
+            <div className="flex p-7">
+              <RocketIcon className="block self-center" />
+            </div>
+          </div>
+          <div>
+            <h2 className="mb-1 pb-0"> {t("publishedTitle")}</h2>
+            <p className="mb-5 mt-0">{t("publishedViewLinks")}</p>
+            <p>
+              <strong>{t("english")}</strong>{" "}
+              <a href={linkEn} data-testid="published-link-en">
+                {linkEn}
+              </a>
+            </p>
+            <p>
+              <strong>{t("french")}</strong>{" "}
+              <a href={linkFr} data-testid="published-link-fr">
+                {linkFr}
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="mb-5">
+          <h3 className="mb-1">{t("publishedErrors")}</h3>
+          <Markdown options={{ forceBlock: true }}>{t("ifYouAreExperiencingProblems")}</Markdown>
+        </div>
+        <div className="mb-10">
+          <h3 className="mb-1">{t("provideFeedback")}</h3>
+          <Markdown options={{ forceBlock: true }}>{t("didYouFindThisToolHelpful")}</Markdown>
+        </div>
+        <div>
+          <LinkButton.Primary href={`/${language}/forms`}>{t("publishedBack")}</LinkButton.Primary>
+        </div>
+      </div>
+    </ViewTransition>
+  );
 }
