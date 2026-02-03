2eef56083d87a7bfc27b1af300aab1b977eaad5d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-02-03 15:40:31 -0500

    Use element.id for unique ids on richText components

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/translate/components/RichText.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/translate/components/RichText.tsx
@@ -10,7 +10,6 @@ import { FieldsetLegend } from "./FieldsetLegend";
 
 export const RichText = ({
   element,
-  index,
   primaryLanguage,
 }: {
   element: FormElement;
@@ -38,10 +37,10 @@ export const RichText = ({
               content={element.properties[fieldEn] ?? ""}
               lang={primaryLanguage}
               ariaLabel={t("pageText") + " " + t(primaryLanguage)}
-              ariaDescribedBy={`elements-${index}-description-${primaryLanguage}-language`}
+              ariaDescribedBy={`elements-${element.id}-description-${primaryLanguage}-language`}
             />
             <LanguageLabel
-              id={`elements-${index}-description-${primaryLanguage}-language`}
+              id={`elements-${element.id}-description-${primaryLanguage}-language`}
               lang={primaryLanguage}
             >
               <>{primaryLanguage}</>
@@ -53,10 +52,10 @@ export const RichText = ({
               content={element.properties[fieldFr] ?? ""}
               lang={secondaryLanguage}
               ariaLabel={t("pageText") + " " + secondaryLanguage}
-              ariaDescribedBy={`elements-${index}-description-${secondaryLanguage}-language`}
+              ariaDescribedBy={`elements-${element.id}-description-${secondaryLanguage}-language`}
             />
             <LanguageLabel
-              id={`elements-${index}-description-${secondaryLanguage}-language`}
+              id={`elements-${element.id}-description-${secondaryLanguage}-language`}
               lang={secondaryLanguage}
             >
               <>{secondaryLanguage}</>
