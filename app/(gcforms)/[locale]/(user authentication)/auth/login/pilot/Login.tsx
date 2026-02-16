36f3f8d4d3febff22846a19e5bf8c1df0441b12a
Tim Arney <tim@line37.com>
2026-02-16 08:05:57 -0500

    update pilot login page

--- app/(gcforms)/[locale]/(user authentication)/auth/login/pilot/Login.tsx
+++ app/(gcforms)/[locale]/(user authentication)/auth/login/pilot/Login.tsx
@@ -1,29 +1,33 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
+import { useTranslation } from "@i18n/client";
+
+import { Button } from "@clientComponents/globals";
+import Loader from "@clientComponents/globals/Loader";
 
 export const Login = ({ language = "en" }) => {
   const { data, status } = useSession();
+  const { t } = useTranslation("common");
 
   if (status === "loading") {
-    return;
+    return <Loader />;
   }
 
   if (status === "authenticated") {
     return <div>{`Hello ${data.user?.name}`}</div>;
   }
   return (
-    <form
-      action={async () => {
-        signIn("gcForms", { redirectTo: `/${language}/auth/policy` });
-      }}
-    >
-      <button
-        className="rounded-xl border-3 border-gcds-grayscale-500 p-3 text-gcds-blue-muted hover:border-gcds-blue-vivid hover:bg-gcds-blue-vivid hover:text-white-default active:border-black"
-        type="submit"
+    <div>
+      <h1 className="mb-12 mt-6 border-b-0">{t("loginPilot.title")}</h1>
+      <p className="-mt-6 mb-10">{t("loginPilot.description")}</p>
+      <form
+        action={async () => {
+          signIn("gcForms", { redirectTo: `/${language}/auth/policy` });
+        }}
       >
-        Sign in with Zitadel
-      </button>
-    </form>
+        <Button type="submit">{t("loginPilot.button")}</Button>
+      </form>
+    </div>
   );
 };
