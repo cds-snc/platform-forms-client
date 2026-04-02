51ecc100b1bf7602d516034c96a5f87ee67ffe92
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-04-02 14:07:38 -0400

    chore: Refactor Text + Number inputs (#6956)
    * First cut
    
    * Fix numeric element test
    
    * Try using more specific selector
    
    * Bit of cleanup
    
    * Don't show autocomplete for NumberInput
    
    * use language for key

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/element-dialog/descriptions/Number.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/edit/components/elements/element-dialog/descriptions/Number.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { ExampleWrapper } from "./ExampleWrapper";
-import { Description, Label, TextInput } from "@clientComponents/forms";
+import { Description, Label, NumberInput } from "@clientComponents/forms";
 import { useTranslation } from "@i18n/client";
 
 export const Number = () => {
@@ -19,7 +19,7 @@ export const Number = () => {
           {t("addElementDialog.number.amount")}
         </Label>
         <Description>{t("addElementDialog.number.enterOnlyNumbers")}</Description>
-        <TextInput label="title" type="number" name="name" placeholder="123456789" />
+        <NumberInput label="title" name="name" placeholder="123456789" />
       </ExampleWrapper>
     </div>
   );
