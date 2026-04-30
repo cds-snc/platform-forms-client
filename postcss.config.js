3b3c81594fe4731930caced4657ee89ece42b880
Clément Janin <clement.janin@cds-snc.ca>
2026-04-30 10:26:52 -0400

    enforce ESM for both the web app and the database package

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
-  },
+export const plugins = {
+  "@tailwindcss/postcss": {},
+  ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
 };
