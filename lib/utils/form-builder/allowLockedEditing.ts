6cc1ecc9e310ecd6c51a5d62fd6d145349cfec45
GitHub Copilot <github-copilot@example.com>
2026-05-11 14:30:21 -0400

    update flag check

--- lib/utils/form-builder/allowLockedEditing.ts
+++ lib/utils/form-builder/allowLockedEditing.ts
@@ -1,4 +1,11 @@
 import { checkOne } from "@lib/cache/flags";
 import { FeatureFlags } from "@lib/cache/types";
+import { featureFlagAllowedForUser } from "@lib/userFeatureFlags";
 
-export const allowLockedEditing = async () => checkOne(FeatureFlags.lockedEditing);
+export const allowLockedEditing = async (userId?: string) => {
+  if (userId) {
+    return featureFlagAllowedForUser(userId, FeatureFlags.lockedEditing);
+  }
+
+  return checkOne(FeatureFlags.lockedEditing);
+};
