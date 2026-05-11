5fb7d3d4d7c11b14cd52d6c41c81d42309c59e3d
GitHub Copilot <github-copilot@example.com>
2026-05-11 14:38:29 -0400

    update

--- lib/utils/form-builder/allowLockedEditing.ts
+++ lib/utils/form-builder/allowLockedEditing.ts
@@ -3,9 +3,17 @@ import { FeatureFlags } from "@lib/cache/types";
 import { featureFlagAllowedForUser } from "@lib/userFeatureFlags";
 
 export const allowLockedEditing = async (userId?: string) => {
+  const globalFlag = await checkOne(FeatureFlags.lockedEditing);
+
+  if (globalFlag) {
+    // If the global flag is enabled, allow locked editing for all users
+    return true;
+  }
+
   if (userId) {
+    // If the user has the feature flag enabled, allow locked editing for that user
     return featureFlagAllowedForUser(userId, FeatureFlags.lockedEditing);
   }
 
-  return checkOne(FeatureFlags.lockedEditing);
+  return false;
 };
