23f30cdc15c2e7abd978ee36224f6e380be5c523
Tim Arney <timarney@users.noreply.github.com>
2026-05-20 10:10:14 -0400

    chore: add account menu to forms layout (#7198)
    add account menu

--- app/(gcforms)/[locale]/(form administration)/forms/layout.tsx
+++ app/(gcforms)/[locale]/(form administration)/forms/layout.tsx
@@ -6,17 +6,23 @@ import { TemplateStoreProvider } from "@lib/store/useTemplateStore";
 import { SkipLink } from "@serverComponents/globals/SkipLink";
 import { Footer } from "@serverComponents/globals/Footer";
 import { AuthenticatedLayout } from "@lib/pages/auth";
+import { authorization } from "@lib/privileges";
 
-export default AuthenticatedLayout(async ({ children, params }) => {
+export default AuthenticatedLayout(async ({ children, params, session }) => {
   const { locale } = await params;
+  const publishFormsEnabled = await authorization.hasPublishFormsPrivilege().catch(() => false);
 
   return (
     <TemplateStoreProvider {...{ locale }}>
       <SaveTemplateProvider>
         <div className="bkd-soft flex h-full flex-col">
           <SkipLink />
-          <Header context={"default"} />
-          <div className="mx-4 shrink-0 grow basis-auto laptop:mx-32 desktop:mx-64">
+          <Header
+            context={"default"}
+            showAccountMenu={!!session}
+            accountMenuPublishingEnabled={publishFormsEnabled}
+          />
+          <div className="laptop:mx-32 desktop:mx-64 mx-4 shrink-0 grow basis-auto">
             <main id="content" className="mb-10">
               {children}
             </main>
