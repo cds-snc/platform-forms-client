58241f7defedc8b13e365611ad3861174c193b50
GitHub Copilot <Tim Arney>
2026-05-22 11:59:30 -0400

    cleanup

--- components/clientComponents/forms/ResumeForm/FormRestoredWarning.tsx
+++ components/clientComponents/forms/ResumeForm/FormRestoredWarning.tsx
@@ -4,7 +4,7 @@ export const FormRestoredWarning = () => {
   const { t } = useTranslation("common");
 
   return (
-    <div className="w-full text-black">
+    <div className="w-full px-4 py-3 text-black">
       <h3 className="mt-0! mb-2! pb-0 text-xl font-semibold text-black">
         {t("saveAndResume.formRestoredWarning.title")}
       </h3>
