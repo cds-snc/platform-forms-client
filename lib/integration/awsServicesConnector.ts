8dea72d46737059b508ba3701d17d098ffbffd71
Clément Janin <clement.janin@cds-snc.ca>
2026-05-28 10:00:23 -0400

    chore: remove unused AWS S3 package

--- lib/integration/awsServicesConnector.ts
+++ lib/integration/awsServicesConnector.ts
@@ -3,7 +3,6 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import { LambdaClient } from "@aws-sdk/client-lambda";
 import { SQSClient } from "@aws-sdk/client-sqs";
-import { S3Client } from "@aws-sdk/client-s3";
 
 const globalConfig = {
   region: process.env.AWS_REGION ?? "ca-central-1",
@@ -35,9 +34,4 @@ export const lambdaClient = new LambdaClient({
 
 export const sqsClient = new SQSClient({
   ...globalConfig,
-});
-
-export const s3Client = new S3Client({
-  ...globalConfig,
-  ...(process.env.LOCAL_AWS_ENDPOINT && { forcePathStyle: true }),
 });
