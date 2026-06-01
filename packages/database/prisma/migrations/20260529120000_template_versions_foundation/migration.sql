ad5847fbba22bc081919788ddf11731d2e3e8830
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-06-01 14:25:39 -0400

    Add partial unique index to ensure only one DRAFT and one PUBLISHED version per form

--- packages/database/prisma/migrations/20260529120000_template_versions_foundation/migration.sql
+++ packages/database/prisma/migrations/20260529120000_template_versions_foundation/migration.sql
@@ -31,6 +31,10 @@ CREATE UNIQUE INDEX "Template_currentDraftVersionId_key" ON "Template"("currentD
 CREATE INDEX "TemplateVersion_templateId_status_idx" ON "TemplateVersion"("templateId", "status");
 CREATE UNIQUE INDEX "TemplateVersion_templateId_versionNumber_key" ON "TemplateVersion"("templateId", "versionNumber");
 
+-- Partial unique indexes to ensure only one DRAFT and one PUBLISHED per templateId
+CREATE UNIQUE INDEX "TemplateVersion_templateId_draft_unique" ON "TemplateVersion"("templateId") WHERE "status" = 'DRAFT';
+CREATE UNIQUE INDEX "TemplateVersion_templateId_published_unique" ON "TemplateVersion"("templateId") WHERE "status" = 'PUBLISHED';
+
 -- AddForeignKey
 ALTER TABLE "TemplateVersion" ADD CONSTRAINT "TemplateVersion_templateId_fkey" FOREIGN KEY ("templateId") REFERENCES "Template"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 ALTER TABLE "Template" ADD CONSTRAINT "Template_currentPublishedVersionId_fkey" FOREIGN KEY ("currentPublishedVersionId") REFERENCES "TemplateVersion"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
