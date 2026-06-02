841c049c8c5d0c627cbfcd997b2a5c24d0b80a25
GitHub Copilot <Tim Arney>
2026-06-02 06:42:21 -0400

    cleanup

--- packages/database/prisma/migrations/20260529120000_add_template_versions/migration.sql
+++ packages/database/prisma/migrations/20260529120000_add_template_versions/migration.sql
@@ -4,6 +4,7 @@ CREATE TYPE "TemplateVersionStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'SUPERSEDED')
 -- AlterTable
 ALTER TABLE "Template" ADD COLUMN "currentPublishedVersionId" TEXT;
 ALTER TABLE "Template" ADD COLUMN "currentDraftVersionId" TEXT;
+ALTER TABLE "Template" ADD COLUMN "lastEditedByUserId" TEXT;
 
 -- CreateTable
 CREATE TABLE "TemplateVersion" (
@@ -28,6 +29,7 @@ CREATE TABLE "TemplateVersion" (
 -- CreateIndex
 CREATE UNIQUE INDEX "Template_currentPublishedVersionId_key" ON "Template"("currentPublishedVersionId");
 CREATE UNIQUE INDEX "Template_currentDraftVersionId_key" ON "Template"("currentDraftVersionId");
+CREATE INDEX "Template_lastEditedByUserId_idx" ON "Template"("lastEditedByUserId");
 CREATE INDEX "TemplateVersion_templateId_status_idx" ON "TemplateVersion"("templateId", "status");
 CREATE UNIQUE INDEX "TemplateVersion_templateId_versionNumber_key" ON "TemplateVersion"("templateId", "versionNumber");
 
@@ -39,3 +41,4 @@ CREATE UNIQUE INDEX "TemplateVersion_templateId_published_unique" ON "TemplateVe
 ALTER TABLE "TemplateVersion" ADD CONSTRAINT "TemplateVersion_templateId_fkey" FOREIGN KEY ("templateId") REFERENCES "Template"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 ALTER TABLE "Template" ADD CONSTRAINT "Template_currentPublishedVersionId_fkey" FOREIGN KEY ("currentPublishedVersionId") REFERENCES "TemplateVersion"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
 ALTER TABLE "Template" ADD CONSTRAINT "Template_currentDraftVersionId_fkey" FOREIGN KEY ("currentDraftVersionId") REFERENCES "TemplateVersion"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+ALTER TABLE "Template" ADD CONSTRAINT "Template_lastEditedByUserId_fkey" FOREIGN KEY ("lastEditedByUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
--- packages/database/prisma/migrations/20260602120000_add_template_last_edited_by/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Template" ADD COLUMN "lastEditedByUserId" TEXT;
-
--- CreateIndex
-CREATE INDEX "Template_lastEditedByUserId_idx" ON "Template"("lastEditedByUserId");
-
--- AddForeignKey
-ALTER TABLE "Template" ADD CONSTRAINT "Template_lastEditedByUserId_fkey" FOREIGN KEY ("lastEditedByUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
