fbd4a5a47c3b584ba577df1a421d27cfba537899
Tim Arney <Tim Arney>
2026-06-09 11:49:55 -0400

    add note

--- app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/server/getTemplateVersion.ts
+++ app/(gcforms)/[locale]/(form filler)/id/[...props]/lib/server/getTemplateVersion.ts
@@ -1,3 +1,4 @@
 export const getTemplateVersion = async () => {
+  // TODO: implement actual logic to get the template version. For now, we return a hardcoded value to simulate the behavior.
   return "2";
 };
