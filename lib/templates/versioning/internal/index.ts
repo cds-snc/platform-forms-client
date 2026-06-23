50d3c0a86395fbabd236fad605b3200850c8ba63
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-06-23 14:15:24 -0400

    Fork remaining queries

--- lib/templates/versioning/internal/index.ts
+++ lib/templates/versioning/internal/index.ts
@@ -11,6 +11,7 @@ import {
   FormProperties,
   FormRecord,
   NotificationsInterval,
+  PublicFormRecord,
   SecurityAttribute,
 } from "@gcforms/types";
 
@@ -88,6 +89,28 @@ export const parseTemplate = (
     saveAndResume: template.saveAndResume,
     notificationsInterval: template.notificationsInterval as NotificationsInterval,
     ...(template.ttl && { ttl: template.ttl }),
+  };
+};
+
+/*
+ * Extract only the public properties from a form record.
+ * The public properties are the ones that are needed to display the form
+ * to unauthenticated users. (e.g. when filling out a form)
+ * Also sets some of the default values for properties that are not set.
+ * @param template A Form Record, containing all the properties
+ * @returns a Public Form Record, with only the public properties
+ */
+export const mapTemplateToPublicFormRecord = (template: FormRecord): PublicFormRecord => {
+  return {
+    id: template.id,
+    updatedAt: template.updatedAt,
+    versionNumber: template.versionNumber,
+    closingDate: template.closingDate,
+    closedDetails: template.closedDetails,
+    form: template.form,
+    isPublished: template.isPublished,
+    securityAttribute: template.securityAttribute,
+    saveAndResume: template.saveAndResume,
   };
 };
 
