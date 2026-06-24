9888e0ebd458eedd875738395a7dc71f0f6bd376
Anik Brazeau <38330843+anikbrazeau@users.noreply.github.com>
2026-06-24 11:44:09 -0400

    Fix formatting in Instructions.tsx

--- components/clientComponents/forms/ResumeForm/Instructions.tsx
+++ components/clientComponents/forms/ResumeForm/Instructions.tsx
@@ -14,7 +14,7 @@ export const Instructions = () => {
         <p className="text-sm">{t("saveAndResume.resumePage.readyToContinue.text1")}</p>
         <p className="text-sm">{t("saveAndResume.resumePage.readyToContinue.text2")}</p>
         <p className="mb-10 text-sm">{t("saveAndResume.resumePage.readyToContinue.text3")}</p>
-         <p className="mb-10 text-sm">{t("saveAndResume.resumePage.readyToContinue.text4")}</p>
+        <p className="mb-10 text-sm">{t("saveAndResume.resumePage.readyToContinue.text4")}</p>
       </div>
     </div>
   );
