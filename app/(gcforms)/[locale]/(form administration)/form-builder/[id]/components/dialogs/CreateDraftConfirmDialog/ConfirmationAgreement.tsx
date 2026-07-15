5504f5d0135bc937cc271215894641c7f7cbd73b
Tim Arney <timarney@users.noreply.github.com>
2026-07-15 13:56:45 -0400

    chore: move agree for re-publish step (#7502)
    * move agree
    
    * update text
    
    * make re-publish 2 steps

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/components/dialogs/CreateDraftConfirmDialog/ConfirmationAgreement.tsx
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/components/dialogs/CreateDraftConfirmDialog/ConfirmationAgreement.tsx
@@ -12,9 +12,27 @@ export const ConfirmationAgreement = ({
 
   return (
     <div className="mb-4">
-      <label className="mb-2 block font-bold" id="enterAgreeInput">
-        {t("confirm.createDraft.label")}{" "}
-        <span className="text-gcds-red-500">{t("confirm.createDraft.required")}</span>
+      <p className="mb-6 font-bold">{t("prePublishFormDialog.republish.confirm.title")}</p>
+
+      <p className="mb-4">{t("prePublishFormDialog.republish.confirm.text1")}</p>
+      <p className="mb-8">{t("prePublishFormDialog.republish.confirm.text2")}</p>
+
+      <p className="mb-2 font-bold">{t("prePublishFormDialog.republish.confirm.list")}</p>
+
+      <ul className="mb-8">
+        <li>{t("prePublishFormDialog.republish.confirm.listitem1")}</li>
+        <li>{t("prePublishFormDialog.republish.confirm.listitem2")}</li>
+        <li>{t("prePublishFormDialog.republish.confirm.listitem3")}</li>
+      </ul>
+
+      <label className="mb-2 block" id="enterAgreeInput">
+        {t("prePublishFormDialog.republish.confirm.agree.label")}{" "}
+        <strong className="mr-2 inline-block">
+          {t("prePublishFormDialog.republish.confirm.agree.placeholder")}
+        </strong>
+        <span className="text-gcds-red-500">
+          {t("prePublishFormDialog.republish.confirm.agree.required")}
+        </span>
       </label>
       <div>
         <input
@@ -22,7 +40,7 @@ export const ConfirmationAgreement = ({
           id="confirmation-agree"
           className="gc-input-text mb-1 w-4/5"
           value={agreeValue}
-          placeholder={t("confirm.createDraft.placeholder")}
+          placeholder={t("prePublishFormDialog.republish.confirm.agree.placeholder")}
           onChange={(e) => {
             setAgreeValue(e.target.value);
             handleAgreement((e.target.value || "").toUpperCase().trim());
