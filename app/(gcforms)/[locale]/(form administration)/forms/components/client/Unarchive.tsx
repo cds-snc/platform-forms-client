b466c404b7f0661681cc5cdb7999f96bb359bf37
Tim Arney <timarney@users.noreply.github.com>
2026-07-30 10:02:25 -0400

    chore: update archive card (#7605)
    update archive card

--- app/(gcforms)/[locale]/(form administration)/forms/components/client/Unarchive.tsx
+++ app/(gcforms)/[locale]/(form administration)/forms/components/client/Unarchive.tsx
@@ -2,47 +2,31 @@
 import { memo } from "react";
 import { useTranslation } from "@i18n/client";
 import { clearTemplateStorage } from "@lib/store/utils";
-import { cloneForm, restoreForm } from "../../actions";
+import { restoreForm } from "../../actions";
 import { toast, ToastContainer } from "@formBuilder/components/shared/Toast";
 
-export const Unarchive = memo(
-  ({ id, isPublished, language }: { id: string; isPublished: boolean; language: string }) => {
-    const { t } = useTranslation("my-forms");
+export const Unarchive = memo(({ id }: { id: string; isPublished: boolean; language: string }) => {
+  const { t } = useTranslation("my-forms");
 
-    return (
-      <>
-        <button
-          className="action my-4 block text-sm whitespace-nowrap underline"
-          onClick={async () => {
-            if (!isPublished) {
-              const { error } = (await restoreForm(id)) ?? {};
+  return (
+    <>
+      <button
+        className="action my-4 block text-sm whitespace-nowrap underline"
+        onClick={async () => {
+          const { error } = (await restoreForm(id)) ?? {};
 
-              if (error) {
-                toast.error(t("errors.formUnarchiveFailed"));
-              }
-              clearTemplateStorage(id);
-            } else {
-              try {
-                const res = await cloneForm(id, true, language);
-                if (res && res.formRecord && !res.error) {
-                  toast.success(t("card.menu.cloneSuccess"));
-                  window.location.href = `/${language}/form-builder/${res.formRecord.id}/edit`;
-                  return;
-                }
-                throw new Error(res?.error || "Clone failed");
-              } catch (e) {
-                toast.error(t("card.menu.cloneFailed"));
-              }
-            }
-          }}
-        >
-          {isPublished ? t("actions.duplicatePublishedForm") : t("actions.unarchiveForm")}
-        </button>
-        <div className="sticky top-0">
-          <ToastContainer autoClose={false} containerId="default" />
-        </div>
-      </>
-    );
-  }
-);
+          if (error) {
+            toast.error(t("errors.formUnarchiveFailed"));
+          }
+          clearTemplateStorage(id);
+        }}
+      >
+        {t("actions.unarchiveForm")}
+      </button>
+      <div className="sticky top-0">
+        <ToastContainer autoClose={false} containerId="default" />
+      </div>
+    </>
+  );
+});
 Unarchive.displayName = "Unarchive";
