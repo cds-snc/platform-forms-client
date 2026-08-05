aaecf2865aa7ef3614fc8d8d6deb866c1757038a
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-08-05 13:46:09 -0400

    Fix managed combobox invalid aria-laballedby

--- components/clientComponents/forms/AddressComplete/AddressComplete.tsx
+++ components/clientComponents/forms/AddressComplete/AddressComplete.tsx
@@ -323,6 +323,7 @@ export const AddressComplete = (props: AddressCompleteProps): React.ReactElement
           <div className="mt-4 mb-6">
             <Label
               htmlFor={`${name}-country`}
+              id={`label-${name}-country`}
               className={props.required ? "gcds-label required" : "gcds-label"}
               required={props.required}
               lang={lang}
@@ -349,6 +350,7 @@ export const AddressComplete = (props: AddressCompleteProps): React.ReactElement
         <div className="mb-6">
           <Label
             htmlFor={`${name}-streetAddress`}
+            id={`label-${name}-streetAddress`}
             className={props.required ? "gcds-label required" : "gcds-label"}
             required={props.required}
             lang={lang}
--- components/clientComponents/forms/AddressComplete/ManagedCombobox.tsx
+++ components/clientComponents/forms/AddressComplete/ManagedCombobox.tsx
@@ -90,33 +90,35 @@ export const ManagedCombobox = React.forwardRef(
         performChangeInputValue(value, keepOpen),
     }));
 
+    const inputProps = getInputProps({
+      id,
+      name,
+      required,
+      "aria-describedby": `desc-${ariaDescribedBy}`,
+      onFocus: () => setIsOpen(true),
+      onBlur: () => setIsOpen(false),
+      onChange: (e) => {
+        setInputValue((e.target as HTMLInputElement).value);
+      },
+      // Prevents a double input refresh from being fired, this is a workaround for a downshift bug
+      // https://github.com/downshift-js/downshift/issues/1108
+      // Downshift won't be updated to fix this issue, so we need to handle it ourselves
+    });
+    // Use the for-id label-input association over aria-labeledby but downshift only allows nulling it that
+    // that keeps an empty `aria-labelledby=""`, not ideal. So just delete the property manually..
+    delete inputProps["aria-labelledby"];
+
     return (
       <div className={classes} data-testid="combobox">
         {(overrideError || meta.error) && (
           <ErrorMessage id={"errorMessage" + id}>{overrideError ?? meta.error}</ErrorMessage>
         )}
 
-        <input
-          {...getInputProps({
-            id,
-            name,
-            required,
-            "aria-describedby": ariaDescribedBy,
-            onFocus: () => setIsOpen(true),
-            onBlur: () => setIsOpen(false),
-            onChange: (e) => {
-              setInputValue((e.target as HTMLInputElement).value);
-            },
-            // Prevents a double input refresh from being fired, this is a workaround for a downshift bug
-            // https://github.com/downshift-js/downshift/issues/1108
-            // Downshift won't be updated to fix this issue, so we need to handle it ourselves
-          })}
-          data-testid="combobox-input"
-          placeholder={placeholderText}
-        />
+        <input {...inputProps} data-testid="combobox-input" placeholder={placeholderText} />
 
         <ul
           className={cn({ hidden: !isOpen || items.length === 0 })}
+          // {...getMenuProps({ "aria-labelledby": `label-${id}` })}
           {...getMenuProps()}
           data-testid="combobox-listbox"
         >
