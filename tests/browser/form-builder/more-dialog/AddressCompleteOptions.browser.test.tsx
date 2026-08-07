829de4e6ccd1d7bade1c373854ff8518e1375cf4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-08-07 10:09:37 -0400

    Fix

--- tests/browser/form-builder/more-dialog/AddressCompleteOptions.browser.test.tsx
+++ tests/browser/form-builder/more-dialog/AddressCompleteOptions.browser.test.tsx
@@ -42,9 +42,17 @@ describe("<AddressCompleteOptions />", () => {
     await render(<AddressCompleteOptions item={item} setItem={setItemSpy} />);
 
     // Verify the component renders both radio groups
-    await expect.element(page.getByLabel("Allow international addresses")).toBeVisible();
-    await expect.element(page.getByLabel("Canadian addresses only")).toBeChecked();
-    await expect.element(page.getByLabel("Address split into parts")).toBeVisible();
-    await expect.element(page.getByLabel("Entire address combined")).toBeChecked();
+    await expect
+      .element(page.getByRole("radio", { name: "Allow international addresses" }))
+      .toBeVisible();
+    await expect
+      .element(page.getByRole("radio", { name: "Canadian addresses only" }))
+      .toBeChecked();
+    await expect
+      .element(page.getByRole("radio", { name: "Address split into parts" }))
+      .toBeVisible();
+    await expect
+      .element(page.getByRole("radio", { name: "Entire address combined" }))
+      .toBeChecked();
   });
 });
