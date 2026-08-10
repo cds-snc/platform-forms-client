a22c7cb65c052ec7c54ed4cd70382191cbb6afa5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-08-10 12:33:41 -0400

    chore: Revert change to api/version url (#7682)
    Back out of previous change

--- app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-pilot/lib/apiClient.ts
+++ app/(gcforms)/[locale]/(form administration)/form-builder/[id]/responses-pilot/lib/apiClient.ts
@@ -89,11 +89,7 @@ export class GCFormsApiClient {
       return Promise.resolve(this.cachedFormTemplate);
     }
 
-    let endpoint = `/forms/${this.formId}/template`;
-
-    if (selectedVersion > 1) {
-      endpoint += `?version=${selectedVersion}`;
-    }
+    const endpoint = `/forms/${this.formId}/template?version=${selectedVersion}`;
 
     return this.httpClient
       .get<FormProperties>(endpoint)
