8f68a143594eee51abc1565389047d029f51e710
Vivian Nobrega <vivian.nobrega@cds-snc.ca>
2026-08-11 13:26:55 -0400

    * remove import

--- tests/browser/form-builder/add-element-dialog/ElementDialog.browser.test.tsx
+++ tests/browser/form-builder/add-element-dialog/ElementDialog.browser.test.tsx
@@ -3,7 +3,6 @@ import { page, userEvent } from "vitest/browser";
 import { ElementDialog } from "@formBuilder/[id]/edit/components/elements/element-dialog/ElementDialog";
 import { render } from "../testUtils";
 import { setupFonts } from "../../helpers/setupFonts";
-import { setAddressCompleteCache, checkAddressCompleteCache } from "@lib/cache/addressCompleteCache";
 
 import "@root/styles/app.css";
 
