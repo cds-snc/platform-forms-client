04329ef04c3e9c7559b2d89c45659e0b8da3b700
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-08-25 11:37:52 -0400

    cleanup

--- components/clientComponents/forms/StarRating/utils.ts
+++ components/clientComponents/forms/StarRating/utils.ts
@@ -1,40 +1,6 @@
 import type { StarRatingObject } from "./types";
 import { Language } from "@lib/types/form-builder-types";
 
-// type StarRatingItem = {
-//   type?: FormElementTypes;
-//   answer: unknown;
-//   [key: string]: unknown;
-// };
-
-/**
- * Utility function to use when rendering a formatted star rating string
- *
- * @param item
- * @returns string
- */
-// export const getFormattedStarRatingFromObject = (item: StarRatingItem): string | undefined => {
-//   if (item.type !== undefined && item.type !== FormElementTypes.starRating) {
-//     return undefined;
-//   }
-
-//   const parsed = parseStarRatingAnswer(String(item.answer));
-//   if (!parsed) {
-//     return "-";
-//   }
-
-//   return formatStarRating(parsed.value, parsed.numberOfStars);
-// };
-
-// export const parseStarRatingAnswer = (answer: string): StarRatingObject | undefined => {
-//   try {
-//     const parsed: unknown = JSON.parse(answer);
-//     return isValidStarRatingObject(parsed) ? parsed : undefined;
-//   } catch {
-//     // Not a valid star rating JSON object
-//   }
-// };
-
 /**
  * Check that a star rating object is valid
  *
@@ -51,17 +17,6 @@ export const isValidStarRatingObject = (obj: unknown): obj is StarRatingObject =
     typeof obj.numberOfStars === "number"
   );
 };
-
-// export const formatStarRating = (
-//   value: string | number | undefined,
-//   numberOfStars: number
-// ): string => {
-//   if (!value || value === "-" || numberOfStars <= 0) {
-//     return String(value);
-//   }
-
-//   return `${value} out of ${numberOfStars}`;
-// };
 
 export const getFormattedStarRatingFromObject = (
   starRatingObject: StarRatingObject | string,
