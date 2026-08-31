8aebf605e8f98709104540a13693e66deab34515
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-08-31 14:45:23 -0400

    Remove form timer flag and related code

--- lib/cache/types.ts
+++ lib/cache/types.ts
@@ -6,7 +6,6 @@ export const UserFeatureFlags = {
 } as const;
 
 export const FeatureFlags = {
-  formTimer: "formTimer",
   hCaptcha: "hCaptcha",
   notification: "notification",
   topBanner: "topBanner",
