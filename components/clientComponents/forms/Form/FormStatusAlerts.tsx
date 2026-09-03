885576a5b5a3b9e887b76e2829357a3ecd1349cc
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-09-03 15:47:26 -0400

    test: expand form refactor coverage
    Bring the expanded characterization tests into the stacked refactor branch, keep the refactor-only validation-before-hCaptcha assertion, and move getFormStatusError out of the component file to keep React Doctor clean.

--- components/clientComponents/forms/Form/FormStatusAlerts.tsx
+++ components/clientComponents/forms/Form/FormStatusAlerts.tsx
@@ -8,35 +8,6 @@ import { StatusError } from "../StatusError/StatusError";
 
 import { type FormRenderProps } from "./types";
 
-export const getFormStatusError = (
-  props: FormRenderProps,
-  language: string,
-  serverError: string,
-  formClosedError: string
-) => {
-  if (typeof props.status === "object" && props.status !== null) {
-    return true;
-  }
-
-  if (props.status === FormStatus.CAPTCHA_VERIFICATION_ERROR) {
-    return null;
-  }
-
-  if (props.status === FormStatus.ERROR) {
-    return serverError;
-  }
-
-  if (props.status === FormStatus.FORM_CLOSED_ERROR) {
-    return (
-      (language === "en"
-        ? props.formRecord.closedDetails?.messageEn
-        : props.formRecord.closedDetails?.messageFr) || formClosedError
-    );
-  }
-
-  return null;
-};
-
 export const FormStatusAlerts = ({
   props,
   formStatusError,
