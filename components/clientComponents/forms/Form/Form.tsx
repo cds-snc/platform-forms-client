4581f32bd68bcb86611c4da991f73d3055531028
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-09-16 09:31:19 -0400

    setup temp hCaptcah prompt test

--- components/clientComponents/forms/Form/Form.tsx
+++ components/clientComponents/forms/Form/Form.tsx
@@ -119,8 +119,11 @@ export const Form: React.FC<FormProps> = (props) => {
   const { setCaptchaFail } = props;
   const { hCaptchaEnabledSetting } = useGCFormsContext();
 
+  // TEMP TESTING
+  const TEMP_HCAPTCHA_SITE_KEY = "72924bde-40f6-4f84-b86a-85ca705ce0c6";
+
   const captchaRequired = shouldCheckCaptcha(props.formRecord.isPublished, hCaptchaEnabledSetting);
-  const siteKey = process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY?.trim() ?? "";
+  const siteKey = TEMP_HCAPTCHA_SITE_KEY; //process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY?.trim() ?? "";
   // Avoid a hCaptcha browser error by checking for the required siteKey as well
   const captchaEnabled = captchaRequired && Boolean(siteKey);
   const { captcha, execute, reset } = useHCaptcha({
