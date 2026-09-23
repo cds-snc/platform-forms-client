8db16bbb5705be70a0e4fd52ed9d94ac707b4b17
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-09-23 11:05:54 -0400

    Remove unused ElementProperties

--- packages/types/src/form-types.ts
+++ packages/types/src/form-types.ts
@@ -129,8 +129,6 @@ export interface ElementProperties {
   managedChoices?: string | string[];
   subElements?: FormElement[];
   fileType?: string | string[] | undefined;
-  headingLevel?: string | undefined;
-  isSectional?: boolean;
   maxNumberOfRows?: number;
   autoComplete?: string;
   dateFormat?: string;
@@ -140,7 +138,6 @@ export interface ElementProperties {
   useThousandsSeparator?: boolean;
   numberOfStars?: number;
   conditionalRules?: ConditionalRule[];
-  full?: boolean;
   addressComponents?: AddressComponents | undefined;
   dynamicRow?: dynamicRowType;
   sortOrder?: SortValue;
